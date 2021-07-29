c757565613aec4d998183f4230bce7739a6ae995
weili10 <liwei386@huawei.com>
2021-07-29 10:53:20 +0800

    解决原生pytorch中dos编码文件打patch报错问题

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -9,6 +9,13 @@ function main()
     # patch
     cp $ROOT_DIR/patch/npu.patch $PT_DIR
     cd $PT_DIR
+    dos2unix docs/make.bat
+    dos2unix scripts/appveyor/install.bat
+    dos2unix scripts/appveyor/install_cuda.bat
+    dos2unix scripts/build_windows.bat
+    dos2unix scripts/proto.ps1
+    dos2unix torch/distributions/von_mises.py
+    dos2unix torch/nn/modules/transformer.pyi.in
     patch -p1 < npu.patch
     cp -r $ROOT_DIR/src/* $PT_DIR
 }
