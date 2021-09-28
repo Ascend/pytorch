同步1.5.0 2c2f79aaccfa455fb24553f77cca62d838b75bdf     !2559 【BugFix】修复输入shape不一致，算子侧未broadcast，导致不支持     Merge pull request !2559 from 姚斌/2.0.3.tr5 --     !2558 modify：del test_cases of 1 dim and NZ format in UT files     Merge pull request !2558 from 郭夏/2.0.3.tr5 --     !2556 修复IOU输出数据类型转换     Merge pull request !2556 from 李攀/2.0.3.tr5 同步1.8.1 7afd57f35ebc3b81dd4c3f642e3f8ba81c1f07f0

pytorch1.5.0/test/test_npu/test_network_ops/test_min.py
@@ -329,14 +329,14 @@ class TestMin(TestCase):
 
     # ---------------------------------------dim
     def test_min_dim_shape_format_fp16_1d(self, device):
-        format_list = [0, 3, 4, 29]
+        format_list = [0, 3, 4]
         keepdim_list = [True, False]
         shape_format = [[[np.float16, i, [18]], np.random.randint(0, 1), j] for i in format_list for j in keepdim_list
                         ]
         self.min_result_dim(shape_format)
 
     def test_min_dim_shape_format_fp32_1d(self, device):
-        format_list = [0, 3, 4, 29]
+        format_list = [0, 3, 4]
         keepdim_list = [True, False]
         shape_format = [[[np.float32, i, [18]], np.random.randint(0, 1), j] for i in format_list for j in
                         keepdim_list
@@ -394,14 +394,14 @@ class TestMin(TestCase):
         self.min_result_dim(shape_format)
 
     def test_min_dim_shape_format_fp16_1d_(self, device):
-        format_list = [0, 3, 4, 29]
+        format_list = [0, 3, 4]
         keepdim_list = [True, False]
         shape_format = [[[np.float16, i, [18]], np.random.randint(0, 1), j] for i in format_list for j in keepdim_list
                         ]
         self._min_result_dim(shape_format)
 
     def test_min_dim_shape_format_fp32_1d_(self, device):
-        format_list = [0, 3, 4, 29]
+        format_list = [0, 3, 4]
         keepdim_list = [True, False]
         shape_format = [[[np.float32, i, [18]], np.random.randint(0, 1), j] for i in format_list for j in
                         keepdim_list