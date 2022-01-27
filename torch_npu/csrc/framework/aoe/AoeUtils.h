9537fce933c2f3a372716b97090f79be0347b117
shibo19 <shibo19@huawei.com>
2022-01-27 16:42:05 +0800

    增加对autotune功能的支持,并且增加对应UT

---
+++ torch_npu/csrc/framework/aoe/AoeUtils.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2020 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __NATIVE_NPU_TOOLS_AOEUTILS__
+#define __NATIVE_NPU_TOOLS_AOEUTILS__
+
+#include <third_party/acl/inc/acl/acl_op_compiler.h>
+#include <c10/npu/NPUException.h>
+
+namespace at_npu {
+namespace native {
+namespace aoe {
+
+class AoeDumpGraphManager  {
+public:
+  void SetDumpGraphPath(const std::string& dump_path);
+  std::string GetDumpGraphPath() const;
+
+  aclGraphDumpOption* CreateGraphDumpOption();
+  void DestropyGraphDumpOption();
+
+  void EnableAoe();
+  bool IsAoeEnabled() const;
+
+  bool aoe_enable=false;
+  // to save graph for autotune, default path is ./
+  std::string autotune_graphdumppath="./";
+  aclGraphDumpOption* AclGraphDumpOption=NULL;
+   
+};
+
+AoeDumpGraphManager& aoe_manager();
+
+} // namespace aoe
+} // namespace native
+} // namespace at_npu
+
+#endif // __NATIVE_NPU_TOOLS_AOEUTILS__
