3a6d0a692642fa6a935ad19c5478f9252b36ca48
wangxiao <wangxiao99@huawei.com>
2022-01-28 09:11:18 +0800

    rm redundant code

--- torch_npu/csrc/aten/ops/AllKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/AllKernelNpu.cpp
@@ -13,8 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "c10/npu/OptionsManager.h"
-
 #include "torch_npu/csrc/framework/utils/CalcuOpUtil.h"
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
