f782fd01b689dc25c62328d48c3a500683f1b61c
孙幸 <346736790@qq.com>
2022-02-17 03:28:48 +0000

    Affine+linspace算子迁移

---
+++ test/test_network_ops/test_affine_grid_generator_backward.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2020, Huawei Technologies.All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch_npu
+import numpy as np
+from torch.nn import functional as F
+
+from torch_npu.testing.common_utils import TestCase, run_tests
+from torch_npu.testing.common_device_type import Dtypes, instantiate_device_type_tests
+from torch_npu.testing.util_test import create_common_tensor, test_2args_broadcast, create_dtype_tensor, UT_FAST_MODE
+
+class TestAffineGridGeneratorBackward(TestCase):
+    def test_affine_grid_generator_backward_common_shape(self, device):
+        shape_list = [[100, 2, 3], [10, 2, 3]]
+        shape_format = [
+            [np.float32, -1, j] for j in shape_list
+        ]
+        for item in shape_format:
+            cpu_input1, npu_input1 = create_common_tensor(item, 0, 1)
+            size = torch.Size((item[2][0], 2, 28, 2))
+            cpu_input1.requires_grad = True
+            cpu_output = self.cpu_op_exec(cpu_input1, size)
+            npu_input1.requires_grad = True
+            npu_output = self.npu_op_exec(npu_input1, size)
+            self.assertRtolEqual(cpu_output, npu_output)
+    
+    def test_affine_grid_generator_backward_fp16(self, device):
+        shape_list = [[100, 2, 3], [10, 2, 3]]
+        shape_format = [
+            [np.float16, -1, j] for j in shape_list
+        ]
+        for item in shape_format:
+            cpu_input1, npu_input1 = create_common_tensor(item, 0, 1)
+            cpu_input1 = cpu_input1.to(torch.float32)
+            npu_input1 = npu_input1.to(torch.float32)
+            size = torch.Size((item[2][0], 2, 28, 2))
+            cpu_input1.requires_grad = True
+            cpu_output = self.cpu_op_exec(cpu_input1, size)
+            npu_input1.requires_grad = True
+            npu_output = self.npu_op_exec(npu_input1, size)
+            self.assertRtolEqual(cpu_output.astype(np.float16), npu_output.astype(np.float16))
+    
+    def cpu_op_exec(self, input1, size):
+        out = F.affine_grid(input1, size, True)
+        input1.requires_grad = True
+        grad_output = torch.ones(out.size(), dtype=torch.float)
+        out.backward(gradient=grad_output)
+        output = input1.grad.numpy()
+        return output
+
+    def npu_op_exec(self, input1, size):
+        input1.requires_grad = True
+        out = F.affine_grid(input1, size, True)
+        grad_output = torch.ones(out.size(), dtype=torch.float).npu()
+        out.backward(gradient=grad_output)
+        output = input1.grad.to("cpu").numpy()
+        return output
+
+instantiate_device_type_tests(TestAffineGridGeneratorBackward, globals(), except_for="cpu")
+if __name__ == "__main__":
+    run_tests()
---
+++ torch_npu/csrc/aten/ops/LinspaceKernelNpu.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2020 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "torch_npu/csrc/framework/utils/OpAdapter.h"
+#include "torch_npu/csrc/framework/utils/CalcuOpUtil.h"
+#include "torch_npu/csrc/aten/NPUNativeFunctions.h"
+
+namespace at_npu {
+namespace native {
+at::Tensor linspace_assist(int64_t steps) {
+  c10::SmallVector<float, N> assist;
+  assist.resize(steps);
+
+  for (int64_t i = 0; i < steps; i++) {
+    assist[i] = (float)(i);
+  }
+  at::Tensor assistTensor =
+      at::from_blob(assist.data(), {steps}, dtype(at::ScalarType::Float));
+  return CalcuOpUtil::copy_tensor_host_to_device(assistTensor);
+}
+
+at::Tensor& NPUNativeFunctions::linspace_out(at::Scalar start, at::Scalar end, c10::optional<int64_t> step, at::Tensor& result) {
+  int64_t steps = step.has_value()? step.value():-65530;
+  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
+
+  if (result.numel() != steps) {
+    result.resize_({steps});
+  }
+  at::Tensor r = result.is_contiguous() ? result : result.contiguous();
+  r = r.to(at::kFloat);
+  if(steps == 0){
+    // skip
+  } else if (steps == 1) {
+    r.fill_(start);
+  } else {
+    c10::SmallVector<int64_t, N> sizeVec = {steps};
+    OpCommand cmd;
+    cmd.Name("LinSpace")
+        .Input(start, at::ScalarType::Float)
+        .Input(end, at::ScalarType::Float)
+        .Input(sizeVec, at::ScalarType::Int)
+        .Output(r)
+        .Run();
+  }
+
+  if(r.dtype() != result.dtype()) {
+    r = r.to(result.dtype());
+  }
+
+  return result.copy_(r);
+}
+
+at::Tensor NPUNativeFunctions::linspace(at::Scalar start, at::Scalar end, 
+    c10::optional<int64_t> step, 
+    c10::optional<at::ScalarType> dtype_opt,
+    c10::optional<at::Layout> layout_opt,
+    c10::optional<at::Device> device_opt,
+    c10::optional<bool> pin_memory_opt
+) {
+  int64_t steps = step.has_value()? step.value():-65530;
+  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
+
+  auto device =  device_or_default(device_opt);
+  at::TensorOptions option;
+  option = option.dtype(dtype_opt)
+                 .layout(layout_opt)
+                 .device(device)
+                 .pinned_memory(pin_memory_opt);
+
+  // construct the output tensor of the NPU
+  at::Tensor result = OpPreparation::ApplyTensorWithFormat({steps}, option, ACL_FORMAT_ND);
+  at::Tensor resultCast = result.to(at::kFloat);
+
+  // calculate the output result of the NPU
+  NPUNativeFunctions::linspace_out(start, end, steps, resultCast);
+
+  if(option.dtype() != resultCast.dtype()) {
+    resultCast = resultCast.to(option.dtype());
+  }
+
+  return resultCast;
+}
+} // namespace native
+} // namespace at_npu
