18194e7f0c12d37b35fb9ea7fafc051b5a2cf97e
li_chao10 <xwqs123@163.com>
2022-02-17 11:04:15 +0800

    add contiguous UTs

---
+++ test/test_trans_contiguous/test_combined_squeeze_x_copy_to_contiguous.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2020, Huawei Technologies.All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import torch
+import torch_npu
+import numpy as np
+
+from torch_npu.testing.common_utils import TestCase, run_tests
+from torch_npu.testing.common_device_type import instantiate_device_type_tests
+from torch_npu.testing.util_test import create_common_tensor, check_operators_in_prof
+
+os.environ["COMBINED_ENABLE"] = "1"  # Open combined-view cases optimization
+
+# Note: NPU only support trans-contiguous with base format, so format_list uses -1
+class CombinedSqueezeXCopyToContiguous(TestCase):
+    def test_squeeze_permute_copy_contiguous(self, device):
+        dtype_list1 = [np.float16, np.float32]
+        format_list1 = [-1]
+        shape_list1 = [
+                      [2, 1, 3, 4],
+                      ]
+        shape_format1 = [
+            [i, j, k] for i in dtype_list1 for j in format_list1 for k in shape_list1
+        ]
+
+        for item in shape_format1: 
+            cpu_input, npu_input = create_common_tensor(item, 0, 100)
+            # case 1: squeeze+permute ==> can be optimized as single permute(npuCombined should not be called)
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out1 = npu_input.squeeze(1).transpose(0,1).contiguous()
+            self.assertEqual(check_operators_in_prof(['npuTranspose'], prof, ['npuCombined']), \
+                True, "Error operators called!")
+            cpu_out1 = cpu_input.squeeze(1).transpose(0,1).contiguous()
+            self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
+
+            # case 2: permute+squeeze ==> can be optimized as single permute(npuCombined should not be called)
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out2 = npu_input.permute(1,0,3,2).squeeze(0).contiguous()
+            self.assertEqual(check_operators_in_prof(['npuTranspose'], prof, ['npuCombined']), \
+                True, "Error operators called!")
+            cpu_out2 = cpu_input.permute(1,0,3,2).squeeze(0).contiguous()
+            self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy()) 
+    
+    def test_squeeze_narrow_copy_contiguous(self, device):
+        dtype_list2 = [np.float16, np.float32]
+        format_list2 = [-1]
+        shape_list2 = [
+                      [20, 1, 30, 40, 16],
+                      [20, 1, 30, 40]
+                      ]
+        shape_format2 = [
+            [i, j, k] for i in dtype_list2 for j in format_list2 for k in shape_list2
+        ]
+
+        for item in shape_format2: 
+            cpu_input, npu_input = create_common_tensor(item, 0, 100)
+            # case 1: squeeze + narrow 
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out1 = npu_input.squeeze(1)[:,1:10,:].contiguous()
+            self.assertEqual(check_operators_in_prof(['npuMatch', 'narrow_npuSlice'], prof), \
+                True, "Error operators called!")
+            cpu_out1 = cpu_input.squeeze(1)[:,1:10,:].contiguous()
+            self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
+            # case 2: narrow + squeeze
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out2 = npu_input[:,:,:,10:19].squeeze(1).contiguous()
+            self.assertEqual(check_operators_in_prof(['npuMatch', 'narrow_npuSlice'], prof), \
+                True, "Error operators called!")
+            cpu_out2 = cpu_input[:,:,:,10:19].squeeze(1).contiguous()
+            self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy())
+
+    def test_squeeze_select_copy_contiguous(self, device):
+        dtype_list3 = [np.float16, np.float32]
+        format_list3 = [-1]
+        shape_list3 = [
+                      [20, 1, 40, 16],
+                      ]
+        shape_format3 = [
+            [i, j, k] for i in dtype_list3 for j in format_list3 for k in shape_list3
+        ]
+
+        for item in shape_format3: 
+            cpu_input, npu_input = create_common_tensor(item, 0, 100)
+            # case 1: squeeze+select
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out1 = npu_input.squeeze().select(2,1).contiguous()
+            self.assertEqual(check_operators_in_prof(['npuMatch', 'select_npuStridedSlice'], prof), \
+                True, "Error operators called!")
+            cpu_out1 = cpu_input.squeeze().select(2,1).contiguous()
+            self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
+            # case 2: select+squeeze
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out2 = npu_input.select(2,1).squeeze().contiguous()
+            self.assertEqual(check_operators_in_prof(['npuMatch', 'select_npuStridedSlice'], prof), \
+                True, "Error operators called!")
+            cpu_out2 = cpu_input.select(2,1).squeeze().contiguous()
+            self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy())
+
+    def test_squeeze_strideslice_copy_contiguous(self, device):
+        dtype_list4 = [np.float16, np.float32]
+        format_list4 = [-1]
+        shape_list4 = [
+                      [20, 1, 200, 40, 10],
+                      ]
+        shape_format4 = [
+            [i, j, k] for i in dtype_list4 for j in format_list4 for k in shape_list4
+        ]
+
+        for item in shape_format4: 
+            cpu_input, npu_input = create_common_tensor(item, 0, 100)
+            # case 1: squeeze + strideslice ==> cannot be optimized(npuCombined should not called)
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out1 = npu_input.squeeze(1)[:,20:150:3].contiguous()
+            self.assertEqual(check_operators_in_prof(['npuAsStrided'], prof, ['npuCombined']), \
+                True, "Error operators called!")
+            cpu_out1 = cpu_input.squeeze(1)[:,20:150:3].contiguous()
+            self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
+            # case 2: strideslice + squeeze ==> cannot be optimized(npuCombined should not called)
+            with torch.autograd.profiler.profile(use_npu=True) as prof:
+                npu_out2 = npu_input[:,:,10:19:3].squeeze(1).contiguous()
+            self.assertEqual(check_operators_in_prof(['npuAsStrided'], prof, ['npuCombined']), \
+                True, "Error operators called!")
+            cpu_out2 = cpu_input[:,:,10:19:3].squeeze(1).contiguous()
+            self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy()) 
+
+         
+instantiate_device_type_tests(CombinedSqueezeXCopyToContiguous, globals(), except_for='cpu')
+if __name__ == "__main__":
+    run_tests()
