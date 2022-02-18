7d9f8d157d68a5c1d9124dde826153f7fd18facc
尤安升 <youansheng@huawei.com>
2022-02-18 09:53:40 +0800

    Add NPUAllocator & SecondaryStreamGuard.

---
+++ torch_npu/csrc/core/npu/NPUAllocator.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2020 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION. 
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstring>
+#include <unordered_map>
+
+#include <c10/core/Allocator.h>
+#include <c10/util/Logging.h>
+#include <c10/util/numa.h>
+
+// TODO: rename to c10
+C10_DECLARE_bool(caffe2_report_npu_memory_usage);
+C10_DECLARE_bool(caffe2_npu_allocator_do_zero_fill);
+C10_DECLARE_bool(caffe2_npu_allocator_do_junk_fill);
+
+namespace torch_npu {
+
+// Use 64-byte alignment should be enough for computation up to AVX512.
+constexpr size_t gAlignment = 64;
+
+using MemoryDeleter = void (*)(void*);
+
+// A helper function that is basically doing nothing.
+// C10_API void NoDelete(void*);
+
+// Fill the data memory region of num bytes with a particular garbage pattern.
+// The garbage value is chosen to be NaN if interpreted as floating point value,
+// or a very large integer.
+void memset_junk_npu(void* data, size_t num);
+
+void* alloc_npu(size_t nbytes);
+void free_npu(void* data);
+
+// Get the CPU Allocator.
+at::Allocator* GetNPUAllocator();
+// Sets the CPU allocator to the given allocator: the caller gives away the
+// ownership of the pointer.
+void SetNPUAllocator(at::Allocator* alloc);
+
+// Get the Default CPU Allocator
+at::Allocator* GetDefaultNPUAllocator();
+
+} // namespace torch_npu
