9b67ebf7657fa773622cc23cacf2c5d12de838c7
yskhhh <yushikang1@hisilicon.com>
2022-02-19 11:21:59 +0800

    sync third party

---
+++ third_party/acl/inc/op_proto/array_ops.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2019 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file array_ops.h
+ * \brief
+ */
+#ifndef OPS_BUILT_IN_OP_PROTO_INC_ARRAY_OPS_H_
+#define OPS_BUILT_IN_OP_PROTO_INC_ARRAY_OPS_H_
+
+#include "graph/operator_reg.h"
+#include "graph/operator.h"
+namespace ge {
+
+/**
+*@brief Creates a constant tensor from a tensor-like object. This operator is used for inference.
+Operator Const has the same definition as operator Constant. \n
+
+*@par Attributes:
+*value: Required. The value and type of the resulting tensor, and no restrictions on type. \n
+
+*@par Outputs:
+*y: A constant tensor. \n
+
+*@par Third-party framework compatibility
+*Compatible with the TensorFlow operator Const.
+*/
+REG_OP(Const)
+    .OUTPUT(y, TensorType({DT_FLOAT, DT_FLOAT16, DT_INT8, DT_INT16, DT_UINT16, \
+        DT_UINT8, DT_INT32, DT_INT64, DT_UINT32, DT_UINT64, DT_BOOL, DT_DOUBLE}))
+    .ATTR(value, Tensor, Tensor())
+    .OP_END_FACTORY_REG(Const)
+
+/**
+*@brief Input data for other operators. \n
+
+*@par Inputs:
+*x: A tensor. \n
+
+*@par Attributes:
+*index: Index of the input tensor.The data type must be int32 or int64.
+Assume that net has three data nodes, one should be set 0, another should
+be set 1, and the left should be set 2. \n
+
+*@par Outputs:
+*y: A tensor. \n
+
+*@par Third-party framework compatibility
+*Compatible with the Caffe operator Data.
+*/
+REG_OP(Data)
+    .INPUT(x, TensorType::ALL())
+    .OUTPUT(y, TensorType::ALL())
+    .ATTR(index, Int, 0)
+    .OP_END_FACTORY_REG(Data)
+
+}  // namespace ge
+
+#endif  // OPS_BUILT_IN_OP_PROTO_INC_ARRAY_OPS_H_
