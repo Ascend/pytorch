c5803d4caf46c6d065d804b817c0b3539aaed099
AtlasAccount <yan199634@126.com>
2022-03-21 16:04:25 +0800

    update pytorch

--- patch/make_patch.sh
+++ patch/make_patch.sh
@@ -21,6 +21,5 @@ PATCH_PYTORCH=$2
 OUT_PATCH_FILE=$3
 
 diff -Nur --exclude=.git* --exclude=OWNERS \
-    --exclude=access_control_test.py \
-    --exclude=build.sh --exclude=third_party \
+    --exclude=access_control_test.py --exclude=third_party \
     --exclude=README* -Nur ${OFFICIAL_PYTORCH}  ${PATCH_PYTORCH} > ${OUT_PATCH_FILE}
