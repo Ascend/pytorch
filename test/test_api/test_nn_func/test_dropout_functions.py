f51f8b66ec32ae769a13a23538b4c7ddc16804c2
xiaxia3 <wangxiaxia3@huawei.com>
2022-04-13 18:07:07 +0800

    更新 dropout UT

--- test/test_api/test_nn_func/test_dropout_functions.py
+++ test/test_api/test_nn_func/test_dropout_functions.py
@@ -16,7 +16,6 @@ import copy
 import torch
 import torch.nn.functional as F
 import torch_npu
-
 from torch_npu.testing.testcase import TestCase, run_tests
 
 
@@ -27,7 +26,7 @@ class TestDropoutFunctions(TestCase):
         cpu_output = F.dropout(input1)
         npu_output = F.dropout(npu_input)
 
-        self.assertRtolEqual(cpu_output.numpy(), npu_output.cpu().numpy())
+        self.assertTrue(str(cpu_output.shape), str(npu_output.shape))
 
     def test_alpha_dropout(self):
         input1 = torch.randn(10, 30)
@@ -35,7 +34,7 @@ class TestDropoutFunctions(TestCase):
         cpu_output = F.alpha_dropout(input1)
         npu_output = F.alpha_dropout(npu_input)
 
-        self.assertRtolEqual(cpu_output.numpy(), npu_output.cpu().numpy())
+        self.assertTrue(str(cpu_output.shape), str(npu_output.shape))
 
     def test_dropout2d(self):
         input1 = torch.randn(10, 30)
@@ -43,7 +42,7 @@ class TestDropoutFunctions(TestCase):
         cpu_output = F.dropout2d(input1)
         npu_output = F.dropout2d(npu_input)
 
-        self.assertRtolEqual(cpu_output.numpy(), npu_output.cpu().numpy())
+        self.assertTrue(str(cpu_output.shape), str(npu_output.shape))
 
     def test_dropout3d(self):
         input1 = torch.randn(10, 30)
@@ -51,7 +50,7 @@ class TestDropoutFunctions(TestCase):
         cpu_output = F.dropout3d(input1)
         npu_output = F.dropout3d(npu_input)
 
-        self.assertRtolEqual(cpu_output.numpy(), npu_output.cpu().numpy())
+        self.assertTrue(str(cpu_output.shape), str(npu_output.shape))
 
 if __name__ == "__main__":
     run_tests()
