54c1b37228d6941ba10722989a83d1623c8c50ab
AtlasAccount <yan199634@126.com>
2022-05-10 23:01:53 +0800

    同步1.5.0 9ecfb14a51719a8d6a917337560e90f5358dba40     !3397 更新apply_adam底层算子     Merge pull request !3397 from 王晓/applyadam --     !3409 [bugfix] 暂时回退 get_npu_format修改     Merge pull request !3409 from 李超/master

--- pytorch1.5.0/src/aten/src/ATen/native/npu/utils/CalcuOpUtil.cpp
+++ pytorch1.5.0/src/aten/src/ATen/native/npu/utils/CalcuOpUtil.cpp
@@ -312,28 +312,9 @@ aclError CalcuOpUtil::LaunchAsyncCopyTaskWithModeSwitch(
   return ret;
 }
 
-bool check_npu_format_unchanged_in_format_contiguous(
-    const Tensor &tensor, const NPUStorageDesc &tensor_desc) {
-  if (!tensor.is_contiguous()) {
-    return TransContiguous::CanOptimize(tensor, {"slice"});
-  }
-
-  if (!StorageDescHelper::MetaDataAreMatch(&tensor) &&
-      (tensor_desc.npu_format_ == ACL_FORMAT_FRACTAL_NZ)) {
-    return CanUseMemcpyForOtherFormat(tensor);
-  }
-
-  bool isPadding = FormatHelper::IsPadded(&tensor);
-  if (isPadding && (!StorageDescHelper::OffsetAreMatch(&tensor))) {
-    return false;
-  }
-  return true;
-}
-
-int64_t CalcuOpUtil::get_tensor_npu_format(const Tensor &tensor) {
-  const NPUStorageDesc &tensor_desc =
-      tensor.storage().unsafeGetStorageImpl()->npu_desc_;
-  if (check_npu_format_unchanged_in_format_contiguous(tensor, tensor_desc)) {
+int64_t CalcuOpUtil::get_tensor_npu_format(const Tensor& tensor) {
+  if (NpuUtils::check_match(&tensor) || CanUseMemcpyForOtherFormat(tensor)) {
+    auto tensor_desc = tensor.storage().unsafeGetStorageImpl()->npu_desc_;
     return tensor_desc.npu_format_;
   } else {
     return InferFormat::GuessFormatWhenContiguous(tensor);
--- pytorch1.5.0/test/test_npu/test_network_ops/test_apply_adam.py
+++ pytorch1.5.0/test/test_npu/test_network_ops/test_apply_adam.py
@@ -21,8 +21,8 @@ class TestApplyAdam(TestCase):
         m = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
         v = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
         grad = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
-        bt1p = 1
-        bt2p = 1
+        bt1p = 0.9
+        bt2p = 0.9
         lr = 0.2
         bt1 = 0.2
         bt2 = 0.2
@@ -30,6 +30,22 @@ class TestApplyAdam(TestCase):
         ul = False
         un = False
         var_o, m_o, v_o = torch.npu_apply_adam(var, m, v, bt1p, bt2p, lr, bt1, bt2, ep, grad, ul, un)
+        expect_varo = torch.tensor([[[[-0.7631, -0.4471],
+                                      [ 0.8030, -0.2334]],
+                                     [[ 0.6573,  0.1233],
+                                      [ 0.6493, -0.9261]]],
+                                    [[[-0.7625,  0.2999],
+                                      [-1.3133, -1.0723]],
+                                     [[-0.3252, -0.7887],
+                                      [ 0.9137, -1.5635]]]])
+        expect_mo = torch.tensor([[[[ 1.4386, -0.0559],
+                                    [-1.3626, -1.9319]],
+                                   [[-1.1560,  0.4082],
+                                    [-1.0249,  0.4093]]],
+                                  [[[ 1.0244,  1.1617],
+                                    [ 0.5887,  0.6287]],
+                                   [[-0.2391, -1.1880],
+                                    [-1.6345,  0.8329]]]])
         expect_vo = torch.tensor([[[[1.7452, 0.1779],
                                     [1.6296, 3.0590]],
                                    [[1.7282, 0.0648],
@@ -38,6 +54,8 @@ class TestApplyAdam(TestCase):
                                     [0.3080, 0.1884]],
                                    [[0.3690, 1.9991],
                                     [3.0633, 0.4669]]]], dtype = torch.float32)
+        self.assertRtolEqual(expect_varo, var_o.cpu())
+        self.assertRtolEqual(expect_mo, m_o.cpu())
         self.assertRtolEqual(expect_vo, v_o.cpu())
 
     def test_apply_adam_out_fp32(self, device):
@@ -45,8 +63,8 @@ class TestApplyAdam(TestCase):
         m = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
         v = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
         grad = torch.randn(2, 2, 2, 2, dtype=torch.float32).to("npu")
-        bt1p = 1
-        bt2p = 1
+        bt1p = 0.9
+        bt2p = 0.9
         lr = 0.2
         bt1 = 0.2
         bt2 = 0.2
@@ -54,6 +72,22 @@ class TestApplyAdam(TestCase):
         ul = False
         un = False
         var_o, m_o, v_o = torch.npu_apply_adam(bt1p, bt2p, lr, bt1, bt2, ep, grad, ul, un, out = (var, m, v))
+        expect_varo = torch.tensor([[[[-0.7631, -0.4471],
+                                      [ 0.8030, -0.2334]],
+                                     [[ 0.6573,  0.1233],
+                                      [ 0.6493, -0.9261]]],
+                                    [[[-0.7625,  0.2999],
+                                      [-1.3133, -1.0723]],
+                                     [[-0.3252, -0.7887],
+                                      [ 0.9137, -1.5635]]]])
+        expect_mo = torch.tensor([[[[ 1.4386, -0.0559],
+                                    [-1.3626, -1.9319]],
+                                   [[-1.1560,  0.4082],
+                                    [-1.0249,  0.4093]]],
+                                  [[[ 1.0244,  1.1617],
+                                    [ 0.5887,  0.6287]],
+                                   [[-0.2391, -1.1880],
+                                    [-1.6345,  0.8329]]]])
         expect_vo = torch.tensor([[[[1.7452, 0.1779],
                                     [1.6296, 3.0590]],
                                    [[1.7282, 0.0648],
@@ -62,6 +96,8 @@ class TestApplyAdam(TestCase):
                                     [0.3080, 0.1884]],
                                    [[0.3690, 1.9991],
                                     [3.0633, 0.4669]]]], dtype = torch.float32)
+        self.assertRtolEqual(expect_varo, var_o.cpu())
+        self.assertRtolEqual(expect_mo, m_o.cpu())
         self.assertRtolEqual(expect_vo, v_o.cpu())
 
 instantiate_device_type_tests(TestApplyAdam, globals(), except_for="cpu")
