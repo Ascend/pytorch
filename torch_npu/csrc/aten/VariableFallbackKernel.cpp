667878f33cde3aa659a560a19b2af8d6c053d174
尤安升 <youansheng@huawei.com>
2022-05-11 14:32:14 +0800

    Add VariableFallbackKernel.

---
+++ torch_npu/csrc/aten/VariableFallbackKernel.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <ATen/core/dispatch/Dispatcher.h>
+#include <ATen/core/LegacyTypeDispatch.h>
+#include <torch/library.h>
+
+/*
+ * This file implements a variable fallback kernel for custom operators.
+ * Since tensors always have the Autograd set, but custom operators
+ * usually don't have a kernel registered for Autograd, the dispatcher
+ * will call into this fallback kernel instead.
+ * Note that this is not a correct autograd implementation. It will just
+ * fallthrough to the custom operator implementation.
+ * If you want a custom operator to work with autograd, you need to use
+ * autograd::Function so that the custom operator implementation knows how to
+ * do autograd.
+ * Note also that ops from native_functions.yaml register their own variable
+ * kernels, so this is never called for them.
+ */
+
+using c10::OperatorHandle;
+using c10::Stack;
+using c10::DispatchKey;
+using c10::DispatchKeySet;
+using c10::Dispatcher;
+using c10::KernelFunction;
+
+namespace {
+
+// Register fallthrough for Autograd backends dispatch keys
+// NB: But not the private use ones; maybe the extension wants
+// to override it themselves!
+
+TORCH_LIBRARY_IMPL(_, AutogradNPU, m) {
+  m.fallback(torch::CppFunction::makeFallthrough());
+}
+
+}
