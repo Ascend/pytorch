1b0d01acb6f6f6459c57a1500e39cb12b0b3827e
wang-ruizhe7 <wangruizhe7@hisilicon.com>
2022-05-26 10:27:31 +0800

    set default compile cache mode

--- torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
+++ torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
@@ -1,3 +1,18 @@
+// Copyright (c) 2022 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "npu_sys_ctrl.h"
 #include <Python.h>
 #include "torch_npu/csrc/core/npu/npu_log.h"
@@ -124,7 +139,7 @@ NpuSysCtrl::NpuSysCtrl() : init_flag_(false), device_id_(0) {}
   }
 
   // set default compile cache mode and dir for users to improve op compile time
-  // set option has been moved to plugin, wait to be decoupled
+  MakeCompileCacheDirAndSetOption();
 
   return INIT_SUCC;
 }
