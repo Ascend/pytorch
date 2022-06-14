52fc7825cfd51d43066ca9d58ec4525198a11a6b
尤安升 <youansheng@huawei.com>
2022-06-14 09:26:55 +0800

    Fix hccl stub code.

--- third_party/acl/libs/hccl.cpp
+++ third_party/acl/libs/hccl.cpp
@@ -15,10 +15,10 @@
 // limitations under the License.
 #include "hccl.h"
 
-hcclResult_t hcclCommInitUniqueId(hcclComm_t* comm, u32 nranks, hcclUniqueId commId, u32 myrank) {return HCCL_SUCCESS;}
-hcclResult_t hcclGetUniqueId(hcclUniqueId* id) {return HCCL_SUCCESS;}
-hcclResult_t hcclAllReduce(void *inputPtr, void *outputPtr, u64 count, hcclDataType_t dataType,
+hcclResult_t HcclCommInitUniqueId(hcclComm_t* comm, u32 nranks, hcclUniqueId commId, u32 myrank) {return HCCL_SUCCESS;}
+hcclResult_t HcclGetUniqueId(hcclUniqueId* id) {return HCCL_SUCCESS;}
+hcclResult_t HcclAllReduce(void *inputPtr, void *outputPtr, u64 count, hcclDataType_t dataType,
                                   hcclRedOp_t op, hcclComm_t comm, rtStream_t stream) {return HCCL_SUCCESS;}
-hcclResult_t hcclBroadcast(void *ptr, u64 count, hcclDataType_t dataType, u32 root, hcclComm_t comm,
+hcclResult_t HcclBroadcast(void *ptr, u64 count, hcclDataType_t dataType, u32 root, hcclComm_t comm,
                                   rtStream_t stream) {return HCCL_SUCCESS;}
-hcclResult_t hcclCommDestroy(hcclComm_t comm) {return HCCL_SUCCESS;}
+hcclResult_t HcclCommDestroy(hcclComm_t comm) {return HCCL_SUCCESS;}
