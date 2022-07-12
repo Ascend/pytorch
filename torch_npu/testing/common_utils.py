a3052144211db42e7d856a9214bb3c079e18c101
junqiang521 <gongjunqiang@huawei.com>
2022-07-12 19:07:27 +0800

    修改测试用例

--- torch_npu/testing/common_utils.py
+++ torch_npu/testing/common_utils.py
@@ -77,7 +77,6 @@ def is_iterable(obj):
 def set_npu_device():
     npu_device = get_npu_device()
     torch.npu.set_device(npu_device)
-    print(f"Your device is {npu_device}")
     return npu_device
 
 
