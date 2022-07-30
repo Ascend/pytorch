9e052749d5a83f45fce9a4f6b888e49a4163ceaa
shibo19 <shibo19@huawei.com>
2022-07-30 17:42:48 +0800

    e2e prof功能函数命名统一风格

--- torch_npu/csrc/profiler/e2e_profiler.h
+++ torch_npu/csrc/profiler/e2e_profiler.h
@@ -29,15 +29,15 @@
 namespace torch_npu {
 namespace profiler {
 
-void initMsPorf(const std::string dump_path, uint64_t npu_event,
+void InitMsPorf(const std::string dump_path, uint64_t npu_event,
     uint64_t aicore_metrics);
 
-void pushStartTime(at::RecordFunction& fn);
-void popEndTime(const at::RecordFunction& fn);
+void PushStartTime(at::RecordFunction& fn);
+void PopEndTime(const at::RecordFunction& fn);
 
-void init_e2e_profiler(const std::string dump_path,  uint64_t npu_event, uint64_t aicore_metrics);
+void InitE2eProfiler(const std::string dump_path,  uint64_t npu_event, uint64_t aicore_metrics);
 
-void finalize_e2e_profiler();
+void FinalizeE2eProfiler();
 
 } // namespace profiler
 } // namespace torch_npu
