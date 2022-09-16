85049f12135b79cd56e679adc6324f64e702ae36
weili10 <liwei386@huawei.com>
2022-09-16 02:38:51 +0000

    回退 'Pull Request !1234 : 【BCM整改】去除Nllloss的过适配'

--- torch_npu/csrc/aten/ops/loss/NLLLoss2dBackwardKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/loss/NLLLoss2dBackwardKernelNpu.cpp
@@ -75,13 +75,14 @@ at::Tensor NPUNativeFunctions::nll_loss2d_backward(
   TORCH_CHECK(scalar_type == at::kLong || scalar_type == at::kInt,
       "Expected object of scalar type ", at::kLong, " or ", at::kInt, " but got scalar type ", scalar_type,
       " for argument 'target'  in call to nll_loss2d_backward");
+  at::Tensor targetCast = target.to(at::kInt);
 
   auto self_input = self.contiguous();
   self_input = self_input.permute({0, 2, 3, 1});
   self_input = self_input.reshape({-1, self.size(1)});
 
-  auto target_input = target.contiguous();
-  target_input = target.reshape({-1});
+  auto target_input = targetCast.contiguous();
+  target_input = targetCast.reshape({-1});
 
   auto grad_output_reshape = grad_output.contiguous();
   if (reduction == at::Reduction::None) {
