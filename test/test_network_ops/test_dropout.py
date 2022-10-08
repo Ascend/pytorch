7cb2f3f615f4f431d779b2651ab1bcb69f6c4d37
shaoyf <shaoyifan1@huawei.com>
2022-10-08 08:56:25 +0000

    fixed f296e85 from https://gitee.com/shaoyf/pytorch/pulls/1352 Clean code for wrong import order.

--- test/test_network_ops/test_dropout.py
+++ test/test_network_ops/test_dropout.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import torch
 import torch_npu
-import numpy as np
 import torch.nn as nn
 
 from torch_npu.testing.testcase import TestCase, run_tests
