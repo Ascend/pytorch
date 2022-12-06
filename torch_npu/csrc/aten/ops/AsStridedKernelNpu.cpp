761e5be7fcf3e1cdf31f7566bf4d51fc5d4cbee9
刘嘉巍 <843972097@qq.com>
2022-12-06 09:38:12 +0800

    AsStrided + Transpose instead of AsStrided

--- torch_npu/csrc/aten/ops/AsStridedKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/AsStridedKernelNpu.cpp
@@ -49,16 +49,54 @@ at::Tensor& stride_copy_out_npu_nocheck(
         .Run();
     return result;
   }
-  // (Ascend) Fix multi-compiling of asstrided op by wrapping attr storage_offset as a NPU Tensor instead of GE Const node.
-  // If GE Data node can pass vaule of storage_offset to op, we can switch storage_offset to Data node finally.
-  cmd.Name("AsStrided")
+
+  // When the last dimension of the input tensor stride is greater than 32, we use 
+  // AsStrided + Transpose instead of AsStrided to get better performance.
+  if (stride.back() >= 32) {
+    std::set<std::pair<int, std::pair<int, int>>> stride_perm_shape_set;
+    int tensor_dim = stride.size();
+    for (int i = 0; i < tensor_dim; i++) {
+      stride_perm_shape_set.insert({stride[i], {i, shape[i]}});
+    }
+    std::set<std::pair<int, std::pair<int, int>>>::reverse_iterator iter;
+    std::vector<int64_t> output_stride;
+    std::vector<int64_t> output_shape;
+    std::vector<int64_t> output_perm_origin;
+    for (iter = stride_perm_shape_set.rbegin(); iter != stride_perm_shape_set.rend(); iter++) {
+      output_stride.emplace_back((*iter).first);
+      output_shape.emplace_back((*iter).second.second);
+      output_perm_origin.emplace_back((*iter).second.first);
+    }
+    at::IntArrayRef output_stride_array(output_stride);
+    at::IntArrayRef output_shape_array(output_shape);
+    at::Tensor result_out = OpPreparation::ApplyTensorWithFormat(
+        output_shape_array, self.options(), ACL_FORMAT_ND);
+    cmd.Name("AsStrided")
       .InputWithoutContiguous(self)
-      .Input(shape)
-      .Input(stride)
+      .Input(output_shape_array)
+      .Input(output_stride_array)
       .Input(at::Scalar(0), at::kLong)
-      .Output(result)
+      .Output(result_out)
       .Run();
-  return result;
+    std::vector<int64_t> output_perm(tensor_dim);
+    for (auto i = 0; i < tensor_dim; i++) {
+      output_perm[output_perm_origin[i]] = i;
+    }
+    at::IntArrayRef output_perm_array(output_perm);
+    result = NPUNativeFunctions::npu_transpose(result_out, output_perm_array, true);
+    return result;
+  } else {
+    // (Ascend) Fix multi-compiling of asstrided op by wrapping attr storage_offset as a NPU Tensor instead of GE Const node.
+    // If GE Data node can pass vaule of storage_offset to op, we can switch storage_offset to Data node finally.
+    cmd.Name("AsStrided")
+        .InputWithoutContiguous(self)
+        .Input(shape)
+        .Input(stride)
+        .Input(at::Scalar(0), at::kLong)
+        .Output(result)
+        .Run();
+    return result;
+  }
 }
 
 at::Tensor& NPUNativeFunctions::npu_stride_copy_out(
