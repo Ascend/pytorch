bb82c454bf3c3cf83e233beb23c26cd3a1cbe905
wangxiaoxin_sherie <wangxiaoxin7@huawei.com>
2022-12-08 20:40:12 +0800

    添加indexput aicore 适配

---
+++ third_party/acl/inc/op_proto/experiment_ops.h
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file split_combination_ops.h
+ * \brief
+ */
+#ifndef OPS_BUILT_IN_OP_PROTO_INC_SPLIT_COMBINATION_OPS_H_
+#define OPS_BUILT_IN_OP_PROTO_INC_SPLIT_COMBINATION_OPS_H_
+#include "graph/operator_reg.h"
+namespace ge {
+/**
+* @brief According to the index number of indexes, replace the value
+* corresponding to X with the value.
+
+* @par Inputs:
+* Five inputs, including:
+* @li x: A ND Tensor.
+* @li value: A Tensor of the same type as "x".
+* @li indexed_sizes: A 1D Tensor of int64 with shape (N). Sizes for each one of the indexed data.
+* @li indexed_strides: A 1D Tensor of int64 with shape (N). Strides for each one of the indexed data.
+* @li indices: Dynamic input. A Tensor of the indices.
+
+* @par Attributes:
+* @li accumulate: Does it support self accumulation. Defaults to false.
+
+* @par Outputs:
+* @li x: A Tensor.
+
+* @par Third-party framework compatibility
+* Compatible with the Pytorch operator index_put.
+
+* @par Restrictions:
+* Warning:THIS FUNCTION IS EXPERIMENTAL. Please do not use.
+*/
+REG_OP(IndexPutV2)
+    .INPUT(x, TensorType::BasicType())
+    .INPUT(value, TensorType::BasicType())
+    .INPUT(indexed_sizes, TensorType({DT_INT64}))
+    .INPUT(indexed_strides, TensorType({DT_INT64}))
+    .DYNAMIC_INPUT(indices, TensorType({DT_INT64}))
+    .OUTPUT(x, TensorType::BasicType())
+    .ATTR(accumulate, Bool, false)
+    .OP_END_FACTORY_REG(IndexPutV2)
+} // namespace ge
+#endif  // OPS_BUILT_IN_OP_PROTO_INC_SPLIT_COMBINATION_OPS_H_
