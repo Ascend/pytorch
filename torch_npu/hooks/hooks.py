984f69376577956eca9fde9b1ef3a4a295cc28f8
dujinhang <dujinhang@huawei.com>
2022-12-12 17:39:57 +0800

    Add call stack information for overflow detection.

--- torch_npu/hooks/hooks.py
+++ torch_npu/hooks/hooks.py
@@ -14,12 +14,20 @@
 # limitations under the License.
 
 import os
+import inspect
 import json
 import stat
+
+from datetime import datetime, timezone
 
 import torch
 
 import torch_npu
+
+
+def get_time_stamp():
+    time_stamp = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
+    return time_stamp
 
 
 def set_dump_path(fpath=None):
@@ -45,22 +53,22 @@ def get_dump_path():
     return os.environ.get("DUMP_PATH")
 
 
-def dump_tensor(x, prefix="", sample=True):
+def dump_tensor_for_acc_cmp(x, prefix="", sample=True):
     if "DUMP_PATH" not in os.environ:
         return
 
     if isinstance(x, (tuple, list)) and x:
         for i, item in enumerate(x):
-            dump_tensor(item, prefix="{}.{}".format(prefix, i), sample=sample)
+            dump_tensor_for_acc_cmp(item, prefix="{}.{}".format(prefix, i), sample=sample)
     elif isinstance(x, torch.Tensor):
         if len(x.shape) == 0 or not x.is_floating_point():
             return
 
-        if hasattr(dump_tensor, "call_number"):
-            dump_tensor.call_number = dump_tensor.call_number + 1
+        if hasattr(dump_tensor_for_acc_cmp, "call_number"):
+            dump_tensor_for_acc_cmp.call_number = dump_tensor_for_acc_cmp.call_number + 1
         else:
-            dump_tensor.call_number = 0
-        prefix = f"{dump_tensor.call_number}_{prefix}"
+            dump_tensor_for_acc_cmp.call_number = 0
+        prefix = f"{dump_tensor_for_acc_cmp.call_number}_{prefix}"
         with os.fdopen(os.open(get_dump_path(), os.O_RDWR|os.O_CREAT, stat.S_IWUSR|stat.S_IRUSR), "a") as f:
             if sample:
                 tensor_max = torch._C._VariableFunctionsClass.max(x).cpu().detach().float().numpy().tolist()
@@ -75,14 +83,28 @@ def dump_tensor(x, prefix="", sample=True):
             f.write('\n')
 
 
+def dump_tensor_for_overflow(x, dump_file_name, prefix=""):
+    if isinstance(x, (tuple, list)) and x:
+        for i, item in enumerate(x):
+            dump_tensor_for_overflow(item, dump_file_name, prefix="{}.{}".format(prefix, i))
+    else:
+        with os.fdopen(os.open(dump_file_name, os.O_RDWR|os.O_CREAT, stat.S_IWUSR|stat.S_IRUSR), "a") as f:
+            if isinstance(x, torch.Tensor):
+                save_tensor = x.contiguous().view(-1).cpu().detach().float().numpy().tolist()
+                json.dump([prefix, save_tensor, str(x.dtype), tuple(x.shape)], f)
+            else:
+                json.dump([prefix, x], f)
+            f.write('\n')
+
+
 def wrap_acc_cmp_hook(name, **kwargs):
 
     sample = kwargs.get('sample', True)
 
     def acc_cmp_hook(module, in_feat, out_feat):
         name_template = f"{name}" + "_{}"
-        dump_tensor(in_feat, name_template.format("input"), sample)
-        dump_tensor(out_feat, name_template.format("output"), sample)
+        dump_tensor_for_acc_cmp(in_feat, name_template.format("input"), sample)
+        dump_tensor_for_acc_cmp(out_feat, name_template.format("output"), sample)
 
     return acc_cmp_hook
 
@@ -94,8 +116,12 @@ def wrap_checkoverflow_hook(name, **kwargs):
         module.has_overflow = torch_npu._C._check_overflow_npu()
         if module.has_overflow:
             name_template = f"{name}" + "_{}"
-            dump_tensor(in_feat, name_template.format("input"), False)
-            dump_tensor(out_feat, name_template.format("output"), False)
-            raise ValueError("[check overflow]:module name :'{}' is overflow!".format(module_name))
+            dump_file_name = f"Overflow_info_{get_time_stamp()}.pkl"
+            stack_str = [str(_) for _ in inspect.stack()[3:]]
+            dump_tensor_for_overflow(stack_str, dump_file_name, name_template.format("stack_info"))
+            dump_tensor_for_overflow(in_feat, dump_file_name, name_template.format("input"))
+            dump_tensor_for_overflow(out_feat, dump_file_name, name_template.format("output"))
+            raise ValueError("[check overflow]: module name :'{}' is overflow and dump file is saved in '{}'.".format(
+                module_name, os.path.realpath(dump_file_name)))
 
     return checkoverflow_hook
