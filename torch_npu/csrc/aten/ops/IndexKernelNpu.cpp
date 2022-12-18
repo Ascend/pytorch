61daeb2ec0e0188d790cc3c2ffd3c13defc986ba
weili10 <liwei386@huawei.com>
2022-12-18 13:04:25 +0000

    回退 'Pull Request !1930 : Add Index/IndexPut Adaptation'

--- torch_npu/csrc/aten/ops/IndexKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/IndexKernelNpu.cpp
@@ -12,158 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <third_party/acl/inc/op_proto/index_ops.h>
-
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/framework/utils/AdvancedIndex.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
-#include "torch_npu/csrc/framework/graph/util/GraphModeGuard.h"
 
 namespace at_npu {
 namespace native {
-namespace {
-DynamicInputRegFunc index_func =
-  [](DyNumAndIndex num_and_index, std::string op_name) -> ge::OperatorPtr {
-    auto ge_op = std::make_shared<ge::op::Index>(op_name.c_str());
-    ge_op->create_dynamic_input_byindex_indices(
-        num_and_index.front().first, num_and_index.front().second);
-    return ge_op;
-  };
-}
-
-// Limitations of the aicore branch
-bool check_index_aicore(const at::Tensor& self, const at::TensorList& indices, const at::IntArrayRef masks) {
-  // support limit is 2D when idx is int64, support limit is 3D when idx is bool,
-  if ((self.dim() > 2 && indices[0].scalar_type() == at::kLong) ||
-      (self.dim() > 3 && indices[0].scalar_type() == at::kBool)) {
-    return false;
-  }
-
-  // The input of the aicore branch does not support float64, Indices should start from dimension 0 of x and continuous
-  if (self.scalar_type() == at::kDouble || masks[0] == 0 || masks.size() > indices.size()) {
-    return false;
-  }
-
-  if (indices.size() <= 2) {
-    // The dtype of indices can only be int64 or bool
-    for (int i = 0; i < indices.size(); i++) {
-      if (indices[i].scalar_type() != at::kLong && indices[i].scalar_type() != at::kBool) {
-        return false;
-      }
-    }
-
-    /**
-     * If there are two indices, their dtype must be the same.
-     * When the dtype of indices is int64, if there are two, then both must be the same shape,
-     * and the dim of input must be greater than or equal to the number of indices.
-     * When the dtype of indices is bool, the shape of the indices should be the same as input,
-     * or the shape in the 0 dimension of indices is the same as the input, and there is only one indices.
-     */
-    if (indices.size() == 2) {
-      if (indices[0].scalar_type() != at::kLong || indices[1].scalar_type() != at::kLong) {
-        return false;
-      }
-      if (self.dim() >= 2 && indices[0].dim() == 1 && indices[1].dim() == 1 && 
-          indices[0].sizes() == indices[1].sizes()) {
-        return true;
-      }
-    } else if (indices[0].scalar_type() == at::kBool) {
-      if (indices[0].sizes() == self.sizes() || (indices[0].size(0) == self.size(0) && indices[0].dim() == 1)) {
-        return true;
-      }
-    } else if (indices[0].dim() == 1) {
-      return true;
-    }
-  }
-  return false;
-}
 
 at::Tensor& index_out_nocheck_npu(
     const at::Tensor& self,
     const at::IntArrayRef masks,
     const at::TensorList& indices,
     at::Tensor& result) {
-  bool is_aicore = check_index_aicore(self, indices, masks);
   OpCommand cmd;
-  if (!is_aicore) { 
-    cmd.Name("Index")
-        .Input(self)
-        .Input(masks, at::kLong, CompileType::MEMORY_HOST_COMPILE_INDEPENDENT)
-        .Input(result.sizes(), at::kLong, CompileType::MEMORY_HOST_COMPILE_INDEPENDENT);
-    for (int i = 0; i < indices.size(); i++) {
-      std::string name = "indices" + std::to_string(i);
-      cmd.Input(indices[i], name);
-    }
-    cmd.Output(result)
-        .Attr("_exclude_engines", (string)"AiCore")
-        .Run();
-  } else {
-    at::Tensor make_sizes_tensor = at::tensor(masks, self.options().dtype(at::kLong));
-    at::Tensor make_strides_tensor = at::tensor(result.sizes(), self.options().dtype(at::kLong));
-    cmd.Name("Index")
-        .Input(self, (string)"x")
-        .Input(make_sizes_tensor, (string)"indexed_sizes")
-        .Input(make_strides_tensor, (string)"indexed_strides");
-    for (int i = 0; i < indices.size(); i++) {
-      std::string name = "indices" + std::to_string(i);
-      cmd.Input(indices[i], name);
-    }
-    cmd.DynamicInputReg(index_func, {{indices.size(), 3}})
-        .Output(result)
-        .Run();
+  cmd.Name("Index")
+      .Input(self)
+      .Input(masks, at::kLong, CompileType::MEMORY_HOST_COMPILE_INDEPENDENT)
+      .Input(result.sizes(), at::kLong, CompileType::MEMORY_HOST_COMPILE_INDEPENDENT);
+  for (int i = 0; i < indices.size(); i++) {
+    std::string name = "indices" + std::to_string(i);
+    cmd.Input(indices[i], name);
   }
-
+  cmd.Output(result)
+      .Attr("_exclude_engines", (string)"AiCore")
+      .Run();
   return result;
 }
 
-at::Tensor NPUNativeFunctions::index(const at::Tensor& self, const torch::List<c10::optional<at::Tensor>>& orig) {
-  /**
-   * In the cann framework, index operator belongs to the fourth type of
-   * operator, which means that the execution of the index operator must go
-   * through the dynamic shape execution framework. In this case, constructing
-   * a large dynamic shape graph is not beneficial to the overall execution
-   * performance, because more dynamic shape operators are introduced.
-   * Therefore, when the fourth type of operator is encountered in graph
-   * mode, the single op mode is switched to execute by default.
-   */
-  GraphModeGuard mode_guard(c10_npu::ModeKind::SINGLE_OP_MODE);
-
+at::Tensor NPUNativeFunctions::index(const at::Tensor& self, const torch::List<c10::optional<at::Tensor>>& orig) {  
   at::native::checkIndexTensorTypes(orig);
+  // first expand BoolTensor (masks) or ByteTensor (masks) into 1 or more LongTensors
+  auto indices = at::native::expandTensors(self, orig);
+  at::Tensor formatCastOfSelf = NPUNativeFunctions::npu_format_cast(self, ACL_FORMAT_ND);
+
+  // calculate the output size
+  auto outputSize = index_npu_output_size(formatCastOfSelf, indices);
+
+  // construct the output tensor of the NPU
+  at::Tensor result = OpPreparation::ApplyTensorWithFormat(formatCastOfSelf,  outputSize, ACL_FORMAT_ND);
+
   // masks corresponds to indices. 0 indicates undefined tensor.
   at::SmallVector<int64_t, N> masks;
   std::vector<at::Tensor> allDefinedIndices;
-  for (c10::optional<at::Tensor> index_opt : orig) {
-    if (index_opt.has_value()) {
-      at::Tensor index = std::move(*index_opt);
-      if (index.defined()) {
-        allDefinedIndices.emplace_back(index);
-        masks.emplace_back(1);
-      } else {
-        masks.emplace_back(0);
-      }
+  for (int64_t i = 0; i < indices.size(); i++) {
+    if (indices[i].defined()) {
+      masks.emplace_back(1);
+      allDefinedIndices.emplace_back(indices[i]);
     } else {
       masks.emplace_back(0);
     }
   }
-
-  /**
-   * When input.size(0) = 1, if the dtype of indices is int64,
-   * and indices only for 0 dimension, can broadcast to output.
-   */
-  if (self.size(0) == 1 && masks.size() == 1 && masks[0] == 1 &&
-      allDefinedIndices[0].scalar_type() == at::kLong && allDefinedIndices[0].dim() == 1) {
-    c10::SmallVector<int64_t, N> output_size = array_to_small_vector(self.sizes());
-    output_size[0] = allDefinedIndices[0].size(0);
-    at::Tensor result = NPUNativeFunctions::npu_broadcast(self, output_size);
-    return result;
-  }
-
-  at::Tensor formatCastOfSelf = NPUNativeFunctions::npu_format_cast(self, ACL_FORMAT_ND);
-
-  auto indices = at::native::expandTensors(self, orig);
-  auto outputSize = index_npu_output_size(formatCastOfSelf, indices);
-  at::Tensor result = OpPreparation::ApplyTensorWithFormat(formatCastOfSelf,  outputSize, ACL_FORMAT_ND);
-
   // calculate the output result of the NPU
   index_out_nocheck_npu(formatCastOfSelf, masks, allDefinedIndices, result);
 
