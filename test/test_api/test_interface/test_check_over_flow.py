899a56efb95318fa03a190fff4feee397242e07e
zhaojingwei <zhaojingwei2@huawei.com>
2022-12-19 19:40:07 +0800

    [ut]Check over flow interface

--- test/test_api/test_interface/test_check_over_flow.py
+++ test/test_api/test_interface/test_check_over_flow.py
@@ -18,17 +18,24 @@ import torch
 import torch_npu
 import torch_npu._C
 
+import numpy as np
+
 import torch_npu.npu.utils as utils
 
 from torch_npu.testing.testcase import TestCase, run_tests
 
 class TestCheckOverFlow(TestCase):
 
-    def test_check_Ascend910PremiumA_over_flow(self):
+    def test_check_over_flow(self):
         soc_version = utils.get_soc_version()
+        a = torch.Tensor([np.inf, np.inf]).npu()
+        a = a + a
         if (soc_version < 220):
-            rtn = utils.npu_check_over_flow(1.0)
+            rtn = utils.npu_check_over_flow(a)
             self.assertTrue(rtn == utils.get_npu_overflow_flag())
+        else:
+            rtn = utils.npu_check_over_flow(a)
+            self.assertTrue(rtn)
     
 
 if __name__ == "__main__":
