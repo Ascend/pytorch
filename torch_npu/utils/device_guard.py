b8109f542aa04d69dc93798d6842e842ff7a5c70
shibo19 <shibo19@huawei.com>
2023-01-30 20:54:24 +0800

    Add class "Device" for torch_npu

--- torch_npu/utils/device_guard.py
+++ torch_npu/utils/device_guard.py
@@ -13,24 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
 import torch_npu
 
-device = collections.namedtuple('device', ['type', 'index'])
 
 def torch_device_guard(func):
-    # Parse args/kwargs from namedtuple(torch.device) to matched torch.device objects
+    # Parse args/kwargs matched torch.device objects
     def wrapper(*args, **kwargs):
         if args:
             args_list = list(args)
             for index, arg in enumerate(args_list):
-                if isinstance(arg, tuple) and "type='npu'" in str(arg):
-                    args_list[index] = torch_npu.new_device(type=torch_npu.npu.native_device, index=arg.index)
+                if isinstance(arg, torch_npu._C.device):
+                    args_list[index] = str(arg).replace("npu", torch_npu.npu.native_device)
+                    break
+                elif isinstance(arg, str) and "npu" in arg:
+                    args_list[index] = arg.replace("npu", torch_npu.npu.native_device)
                     break
             args = tuple(args_list)
-        if kwargs and isinstance(kwargs.get("device"), tuple):
-            namedtuple_device = kwargs.get("device")
-            if "type='npu'" in str(namedtuple_device):
-                kwargs['device'] = torch_npu.new_device(type=torch_npu.npu.native_device, index=namedtuple_device.index)
+        if kwargs and kwargs.get("device"):
+            device_kwarg = kwargs.get("device")
+            if isinstance(device_kwarg, torch_npu._C.device):
+                kwargs['device'] = str(kwargs['device']).replace("npu", torch_npu.npu.native_device)
+            elif isinstance(device_kwarg, str) and "npu" in device_kwarg:
+                kwargs['device'] = device_kwarg.replace("npu", torch_npu.npu.native_device)
         return func(*args, **kwargs)
     return wrapper
