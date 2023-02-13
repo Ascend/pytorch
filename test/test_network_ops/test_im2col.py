98638b44ec0ed87b5a420bcb1fb4b552bd152068
pipihugh <jifang5@h-partners.com>
2023-02-13 12:12:43 +0800

    Optimize im2col to support 3D input.

--- test/test_network_ops/test_im2col.py
+++ test/test_network_ops/test_im2col.py
@@ -13,49 +13,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import torch
 import numpy as np
-import torch_npu
 
+import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.decorator import graph_mode
 from torch_npu.testing.common_utils import create_common_tensor
 
+
 class TestIm2col(TestCase):
-    def generate_single_data(self, min_d, max_d, shape, dtype):
-        input1 = np.random.uniform(min_d, max_d, shape).astype(dtype)
-        npu_input1 = torch.from_numpy(input1)
-        return npu_input1
 
     def cpu_op_exec(self, input1, ksizes, rates, padding, strides):
         output = torch._C._nn.im2col(input1, ksizes, rates, padding, strides)
-        output = output.numpy()
         return output
 
-    def npu_op_exec_tensor_need_to_npu(self, input1, ksizes, rates, padding, strides):
+    def npu_op_exec(self, input1, ksizes, rates, padding, strides):
         output = torch._C._nn.im2col(input1, ksizes, rates, padding, strides)
-        output = output.to("cpu")
-        output = output.numpy()
+        output = output.cpu()
         return output
 
     @graph_mode
     def test_im2col_float16_shape_format(self):
-        def cpu_op_exec_fp16(input1, ksizes, rates, padding, strides):
-            input1 = input1.to(torch.float32)
-            output = torch._C._nn.im2col(input1, ksizes, rates, padding, strides)
-            output = output.numpy()
-            output = output.astype(np.float16)
-            return output
         shape_format = [
             [np.float16, -1, (1, 3, 3, 3)],
             [np.float16, -1, (2, 16, 4, 6)],
+            [np.float16, -1, (2, 16, 4)],
+            [np.float16, -1, (256, 16, 3)],
         ]
         for item in shape_format:
             cpu_input1, npu_input1 = create_common_tensor(item, 1, 100)
-            cpu_output = cpu_op_exec_fp16(cpu_input1, (1,1), (1,1),(0,0),(1,1))
-            npu_output = self.npu_op_exec_tensor_need_to_npu(npu_input1, (1,1), (1,1),(0,0),(1,1))
+            cpu_output = self.cpu_op_exec(cpu_input1.float(), (1, 1), (1, 1), (0, 0), (1, 1)).half()
+            npu_output = self.npu_op_exec(npu_input1, (1, 1), (1, 1), (0, 0), (1, 1))
             self.assertRtolEqual(cpu_output, npu_output)
+
 
 if __name__ == "__main__":
     run_tests()
-    
--- torch_npu/csrc/aten/ops/Im2colBackwardKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/Im2colBackwardKernelNpu.cpp
@@ -58,29 +58,28 @@ at::Tensor& NPUNativeFunctions::im2col_backward_out(
     at::IntArrayRef padding,
     at::IntArrayRef stride,
     at::Tensor& grad_input) {
-  c10::SmallVector<int64_t, SIZE> outputSize = {
-    grad_output.size(0),
-    grad_output.size(1) / (kernel_size[0] * kernel_size[1]),
+  at::Tensor grad_output_cp = grad_output.dim() == 2 ? at::unsqueeze(grad_output, 0) : grad_output;
+  c10::SmallVector<int64_t, SIZE> output_size = {
+    grad_output_cp.size(0),
+    grad_output_cp.size(1) / (kernel_size[0] * kernel_size[1]),
     input_size[0],
-    input_size[1]};
+    input_size[1]
+    };
   OpPreparation::CheckOut(
-      {grad_output},
+      {grad_output_cp},
       grad_input,
-      grad_output,
-      outputSize);
+      grad_output_cp,
+      output_size);
   OpPipeWithDefinedOut pipe;
-  return pipe.CheckMemory({grad_output}, {grad_input})
-    .Func([&grad_output, &input_size, &kernel_size, &dilation, &padding, &stride]
-    (at::Tensor& grad_input)
-    {im2col_backward_out_npu_nocheck(
-        grad_input,
-        grad_output,
-        input_size,
-        kernel_size,
-        dilation,
-        padding,
-        stride);})
-    .Call(grad_input);
+  pipe.CheckMemory({grad_output_cp}, {grad_input})
+      .Func([&grad_output_cp, &input_size, &kernel_size, &dilation, &padding, &stride](at::Tensor& grad_input) {
+        im2col_backward_out_npu_nocheck(
+            grad_input, grad_output_cp, input_size, kernel_size, dilation, padding, stride);})
+      .Call(grad_input);
+  if (grad_output.dim() == 2) {
+    grad_input = at::squeeze(grad_input, 0);
+  }
+  return grad_input;
 }
 
 at::Tensor NPUNativeFunctions::im2col_backward(
@@ -90,20 +89,25 @@ at::Tensor NPUNativeFunctions::im2col_backward(
     at::IntArrayRef dilation,
     at::IntArrayRef padding,
     at::IntArrayRef stride) {
-  c10::SmallVector<int64_t, SIZE> outputSize = {
-    grad_output.size(0),
-    grad_output.size(1) / (kernel_size[0] * kernel_size[1]),
+  at::Tensor grad_output_cp = grad_output.dim() == 2 ? at::unsqueeze(grad_output, 0) : grad_output;
+  c10::SmallVector<int64_t, SIZE> output_size = {
+    grad_output_cp.size(0),
+    grad_output_cp.size(1) / (kernel_size[0] * kernel_size[1]),
     input_size[0],
-    input_size[1]};
-  at::Tensor grad_input = OpPreparation::ApplyTensor(grad_output, outputSize);
+    input_size[1]
+    };
+  at::Tensor grad_input = OpPreparation::ApplyTensor(grad_output_cp, output_size);
   im2col_backward_out_npu_nocheck(
       grad_input,
-      grad_output,
+      grad_output_cp,
       input_size,
       kernel_size,
       dilation,
       padding,
       stride);
+  if (grad_output.dim() == 2) {
+    grad_input = at::squeeze(grad_input, 0);
+  }
   return grad_input;
 }
 
