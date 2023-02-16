2cdb9c44e0d0311f35c48c7686585ca2f37c85af
liangyunfan <liangyunfan@huawei.com>
2023-02-16 14:59:11 +0800

    Test npu_indexing, npu_slice and npu_stride_add.

---
+++ test/test_custom_ops/test_npu_slice.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2020, Huawei Technologies.All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import numpy
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.decorator import Dtypes, instantiate_tests
+
+
+@instantiate_tests
+class TestNpuSlice(TestCase):
+    def split_npu_slice(self, input1, offset, sizes):
+        input_dim = input1.size()
+        num_dim = len(input_dim)
+        for i in range(num_dim):
+            input_index = [j for j in range(offset[i], offset[i] + sizes[i])]
+            input1 = torch.index_select(input=input1,
+                                        dim=i,
+                                        index=torch.tensor(input_index, device="npu"))
+        return input1
+
+    def split_npu_slice_out(self, input1, offset, sizes,out=None):
+        input_dim = input1.size()
+        num_dim = len(input_dim)
+        for i in range(num_dim):
+            input_index = [j for j in range(offset[i], offset[i] + sizes[i])]
+            input1 = torch.index_select(input=input1,
+                                        dim=i,
+                                        index=torch.tensor(input_index, device="npu"))
+        out = input1.clone()
+        return out
+
+    def npu_op_exec(self, input1, offset, sizes):
+        output = torch.npu_slice(input1, offset, sizes)
+        output = output.to("cpu")
+        output = output.numpy()
+        return output
+
+    def split_op_exec(self, input1, offset, sizes):
+        output = self.split_npu_slice(input1, offset, sizes)
+        output = output.to("cpu")
+        output = output.numpy()
+        return output
+
+    @Dtypes(torch.float, torch.half, torch.int32, torch.uint8, torch.int8, torch.int16, torch.long)
+    def test_slice(self, dtype):
+        input_data = torch.tensor([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]).npu().to(dtype)
+        split_out = self.split_op_exec(input_data, [0, 0], [2, 2])
+        exp_out = self.npu_op_exec(input_data, [0, 0], [2, 2])
+        self.assertRtolEqual(split_out, exp_out)
+
+        input_data = torch.tensor([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                                   [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                                   [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]]).npu().to(dtype)
+        split_out = self.split_op_exec(input_data, [0, 0, 0], [2, 2, 2])
+        exp_out = self.npu_op_exec(input_data, [0, 0, 0], [2, 2, 2])
+        self.assertRtolEqual(split_out, exp_out)
+
+
+if __name__ == '__main__':
+    run_tests()
+
---
+++ test/test_custom_ops/test_npu_stride_add.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2020, Huawei Technologies.All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import numpy as np
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.decorator import Dtypes, instantiate_tests, graph_mode
+
+
+@instantiate_tests
+class TestNpuStrideAdd(TestCase):
+    def _split_npu_stride_add(self, x1, x2, offset1, offset2, c1_len):
+        x1 = x1[:, offset1:, :, :, :]
+        x2 = x2[:, offset2:, :, :, :]
+        x1_size = list(x1.size())
+        x2_size = list(x2.size())
+        x1_pad_size = [x1_size[0]] + [16 * c1_len - x1_size[1]] + x1_size[2:]
+        x2_pad_size = [x2_size[0]] + [16 * c1_len - x2_size[1]] + x2_size[2:]
+        x1_pad = torch.cat((x1, torch.zeros(*x1_pad_size, device='npu')), 1)
+        x2_pad = torch.cat((x2, torch.zeros(*x2_pad_size, device='npu')), 1)
+        return torch.add(x1_pad, x2_pad)
+
+    def npu_op_exec(self, input1, input2, offset1, offset2, c1_len):
+        output = torch.npu_stride_add(input1, input2, offset1, offset2, c1_len)
+        output = output.to("cpu")
+        output = output.numpy()
+        return output
+
+    def split_npu_op_exec(self, input1, input2, offset1, offset2, c1_len):
+        output = self._split_npu_stride_add(input1, input2, offset1, offset2, c1_len)
+        output = output.to("cpu")
+        output = output.numpy()
+        return output
+
+    @graph_mode
+    def test_StrideAdd(self):
+        input1 = torch.tensor([[[[[1.]]]]]).npu()
+        input2 = input1
+        exoutput = self.npu_op_exec(input1, input2, 0, 0, 1)
+        output = self.split_npu_op_exec(input1, input2, 0, 0, 1)
+        self.assertRtolEqual(exoutput, output)
+
+
+if __name__ == "__main__":
+    run_tests()
+
