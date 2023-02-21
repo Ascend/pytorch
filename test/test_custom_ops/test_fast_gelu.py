4b224862544b33bc0dd6e0c414e92d9f01e35d3e
叶灵煜 <yelingyu@huawei.com>
2023-02-21 11:38:09 +0000

    Add testcases for custom torch_npu ops.

---
+++ test/test_custom_ops/test_fast_gelu.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2023 Huawei Technologies Co., Ltd
+# All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import torch
+
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+
+
+class TestFastGelu(TestCase):
+
+    def supported_op_exec(self, input1):
+        attr = 1.702
+        attr_half = attr / 2
+        abs_input1 = torch.abs(input1)
+        numerator = input1 * torch.exp((attr_half * input1) * (input1 - abs_input1))
+        denominator = 1.0 + torch.exp(- attr * abs_input1)
+        output = numerator / denominator
+        return output.cpu().detach()
+
+    def custom_op_exec(self, input1):
+        output = torch_npu.fast_gelu(input1)
+        return output.cpu().detach()
+
+    def test_fast_gelu(self, device="npu"):
+        item = [np.float32, 0, [3, 16, 32]]
+        _, npu_input = create_common_tensor(item, 0, 100)
+
+        supported_output = self.supported_op_exec(npu_input)
+        custom_output = self.custom_op_exec(npu_input)
+        self.assertRtolEqual(supported_output, custom_output)
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ test/test_custom_ops/test_npu_fused_attention_layernorm_qkv_fwd.py
@@ -0,0 +1,83 @@
+## Copyright (c) 2023 Huawei Technologies Co., Ltd
+# All rights reserved.
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import torch
+
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+
+
+class TestFusedAttentionQKV(TestCase):
+    
+    def confusion_transpose(self, x, new_shape):
+        return x.view(new_shape).permute(0, 2, 1, 3).npu_format_cast(29)
+
+    def supported_op_exec(self, ln_input, q_kernel, k_kernel, v_kernel, gamma, beta, q_bias, k_bias, v_bias):
+        q_kernel = q_kernel.t().contiguous().npu_format_cast(29)
+        k_kernel = k_kernel.t().contiguous().npu_format_cast(29)
+        v_kernel = v_kernel.t().contiguous().npu_format_cast(29)
+
+        norm_shape = (1024,)
+        norm, mean, variance = torch.native_layer_norm(ln_input, norm_shape, gamma, beta, eps=1e-05)
+        q_layer = self.confusion_transpose(torch.nn.functional.linear(norm, q_kernel, q_bias), (24, 512, 16, 64))
+        k_layer = self.confusion_transpose(torch.nn.functional.linear(norm, k_kernel, k_bias), (24, 512, 16, 64))
+        v_layer = self.confusion_transpose(torch.nn.functional.linear(norm, v_kernel, v_bias), (24, 512, 16, 64))
+        return norm.cpu(), mean.cpu(), variance.cpu(), q_layer.cpu(), k_layer.cpu(), v_layer.cpu()
+
+    def custom_op_exec(self, hidden_states, q_kernel, k_kernel, v_kernel, gamma, beta, q_bias, k_bias, v_bias):
+        hidden_states = hidden_states.npu_format_cast(29)
+        q_kernel = q_kernel.npu_format_cast(29)
+        k_kernel = k_kernel.npu_format_cast(29)
+        v_kernel = v_kernel.npu_format_cast(29)
+        gamma = gamma.npu_format_cast(1)
+        beta = beta.npu_format_cast(1)
+
+        seq_len = 512
+        num_heads = 16
+        norm, q_layer, k_layer, v_layer, mean, variance = torch_npu.npu_fused_attention_layernorm_qkv_fwd(
+            hidden_states, q_kernel, k_kernel, v_kernel, gamma, beta, q_bias, k_bias, v_bias, seq_len, num_heads)
+        return norm.cpu(), mean.cpu(), variance.cpu(), q_layer.cpu(), k_layer.cpu(), v_layer.cpu()
+
+    def test_npu_fused_attention_layernorm_qkv_fwd(self, device="npu"):
+        ln_input = torch.rand(12288, 1024).uniform_(-6, 6).half().npu()
+        q_weight = torch.rand(1024, 1024).uniform_(-0.1, 0.1).half().npu()
+        k_weight = torch.rand(1024, 1024).uniform_(-0.1, 0.1).half().npu()
+        v_weight = torch.rand(1024, 1024).uniform_(-0.1, 0.1).half().npu()
+        gamma = torch.rand(1024).half().npu()
+        beta = torch.rand(1024).half().npu()
+        q_bias = torch.rand(1024).half().npu()
+        k_bias = torch.rand(1024).half().npu()
+        v_bias = torch.rand(1024).half().npu()
+
+        supported_norm, supported_mean, supported_variance, \
+        supported_q, supported_k, supported_v = self.supported_op_exec(
+            ln_input, q_weight, k_weight, v_weight, gamma, beta, q_bias, k_bias, v_bias)
+
+        custom_norm, custom_mean, custom_variance, \
+        custom_q, custom_k, custom_v = self.custom_op_exec(
+            ln_input, q_weight, k_weight, v_weight, gamma, beta, q_bias, k_bias, v_bias)
+
+        self.assertRtolEqual(supported_norm, custom_norm)
+        self.assertRtolEqual(supported_mean, custom_mean)
+        self.assertRtolEqual(supported_variance, custom_variance)
+        self.assertRtolEqual(supported_q, custom_q, prec16=0.003)
+        self.assertRtolEqual(supported_k, custom_k, prec16=0.003)
+        self.assertRtolEqual(supported_v, custom_v, prec16=0.003)
+
+
+if __name__ == '__main__':
+    run_tests()
