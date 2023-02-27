df2f34de5b5e56d8866b5b3c563b33342fc5c153
尤安升 <youansheng@huawei.com>
2023-02-27 14:35:49 +0800

    Refactor codegen & make sure clean of setup succeed.

--- generate_code.sh
+++ generate_code.sh
@@ -16,21 +16,20 @@
 # limitations under the License.
 
 CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
-NDIR="$CDIR/.."
 
-cd $NDIR/scripts
+cd $CDIR
 
 build_libtorch="$1"
 
 python3 -m codegen.gen_backend_stubs  \
-  --output_dir="$NDIR/torch_npu/csrc/aten/" \
-  --source_yaml="$NDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
-  --impl_path="$NDIR/torch_npu/csrc/aten"  # Used to double-check the yaml file definitions.
+  --output_dir="$CDIR/torch_npu/csrc/aten/" \
+  --source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
+  --impl_path="$CDIR/torch_npu/csrc/aten"  # Used to double-check the yaml file definitions.
 
 if [[ ${build_libtorch} != "True" ]]; then
   python3 -m codegen.gen_python_functions  \
-    --output_dir="$NDIR/torch_npu/csrc/aten/" \
-    --source_yaml="$NDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
-    --native_yaml="$NDIR/scripts/codegen/native_functions.yaml" \
-    --template_path="$NDIR/scripts/codegen/templates"
+    --output_dir="$CDIR/torch_npu/csrc/aten/" \
+    --source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
+    --native_yaml="$CDIR/codegen/native_functions.yaml" \
+    --template_path="$CDIR/codegen/templates"
 fi
