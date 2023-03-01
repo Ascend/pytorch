38a0e62a3e8b1ad83cb7d0de8638c56aa47c4467
zhang-aurora <zhangyingxin6@huawei.com>
2023-03-01 15:42:17 +0800

    Combined onnx ops export and testcases

--- test/test_combined_onnx_ops.py
+++ test/test_combined_onnx_ops.py
@@ -27,6 +27,7 @@ import torch_npu.onnx
 
 
 class TestOnnxOps(TestCase):
+
     test_onnx_path = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), "test_onnx_combined")
 
@@ -250,6 +251,378 @@ class TestOnnxOps(TestCase):
             self.onnx_export(model, input_, onnx_model_name, ["input_"])
 
         onnx_model_name = "model_npu_conv3d.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_stride_copy(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+
+            def forward(self, input_):
+                shape = (2, 2)
+                stride = (1, 2)
+                storage_offset = 0
+                return torch_npu.npu_stride_copy(input_, shape, stride, storage_offset)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand(3, 3).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_stride_copy.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path, 
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_sort_v2(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+
+            def forward(self, input_):
+                dim = -1
+                descending = False
+                return torch_npu.npu_sort_v2(input_, dim, descending)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.randn(3, 4).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_sort_v2.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path, 
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_layer_norm_eval(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+                normalized_shape = 4
+                self.elementwise_affine = True
+                if self.elementwise_affine:
+                    self.weight = Parameter(torch.empty(normalized_shape))
+                    self.bias = Parameter(torch.empty(normalized_shape))
+                else:
+                    self.register_parameter('weight', None)
+                    self.register_parameter('bias', None)
+
+                self.reset_parameters()
+
+            def reset_parameters(self) -> None:
+                if self.elementwise_affine:
+                    init.ones_(self.weight)
+                    init.zeros_(self.bias)
+
+            def forward(self, input_):
+                normalized_shape = input_.size()[1:]
+                eps = 1e-5
+                return torch_npu.npu_layer_norm_eval(input_, normalized_shape,
+                                                     self.weight, self.bias, eps)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((6, 4), dtype=torch.float32).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_layer_norm_eval.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path, 
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_reshape(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+
+            def forward(self, input_):
+                shape = (4, 4)
+                can_refresh = False
+                return torch_npu.npu_reshape(input_, shape, can_refresh)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand(2, 8).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_reshape.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path, 
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_pad(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+
+            def forward(self, input_):
+                paddings = [1, 1, 1, 1]
+                return torch_npu.npu_pad(input_, paddings)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.tensor([[20, 20, 10, 10]],
+                                  dtype=torch.float16).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"], ["output_name"])
+
+        onnx_model_name = "model_npu_pad.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_convolution(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.weight = Parameter(torch.empty([64, 32, 3, 3]))
+                bias = True
+                if bias:
+                    self.bias = Parameter(torch.empty([64]))
+                else:
+                    self.register_parameter('bias', None)
+                self.reset_parameters()
+
+            def reset_parameters(self):
+                init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+                if self.bias is not None:
+                    fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+                    if fan_in != 0:
+                        bound = 1 / math.sqrt(fan_in)
+                        init.uniform_(self.bias, -bound, bound)
+
+            def forward(self, input_):
+                stride, paddings, dilation, groups = \
+                    [1, 1], [1, 1], [1, 1], 1
+                return torch_npu.npu_convolution(input_, self.weight, self.bias,
+                                                 stride, paddings, dilation, groups)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand([16, 32, 24, 24]).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_convolution.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_convolution_transpose(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.weight = Parameter(torch.empty([3, 2, 3, 3]))
+                bias = True
+                if bias:
+                    self.bias = Parameter(torch.empty([2]))
+                else:
+                    self.register_parameter('bias', None)
+                self.reset_parameters()
+
+            def reset_parameters(self):
+                init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+                if self.bias is not None:
+                    fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+                    if fan_in != 0:
+                        bound = 1 / math.sqrt(fan_in)
+                        init.uniform_(self.bias, -bound, bound)
+
+            def forward(self, input_):
+                padding, output_padding, stride, dilation, groups = \
+                    [1, 1], [0, 0], [1, 1], [1, 1], 1
+                return torch_npu.npu_convolution_transpose(input_, self.weight, self.bias, padding,
+                                                           output_padding, stride, dilation, groups)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand([1, 3, 3, 3]).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_convolution_transpose.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path, 
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_confusion_transpose(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                perm = (0, 2, 1, 3)
+                shape = [1, 576, 32, 80]
+                transpose_first = False
+                return torch_npu.npu_confusion_transpose(input_, perm, shape, transpose_first)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand([1, 576, 2560]).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_confusion_transpose.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_max(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                dim = 2
+                return torch_npu.npu_max(input_, dim)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((2, 2, 2, 2)).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, 
+                             ["input_"], ["values", "indices"])
+
+        onnx_model_name = "model_npu_max.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_bmmV2(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_, mat2):
+                output_sizes = []
+                return torch_npu.npu_bmmV2(input_, mat2, output_sizes)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((10, 3, 4)).npu()
+            mat2 = torch.rand((10, 4, 5)).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, (input_, mat2),
+                             onnx_model_name, ["input_", "mat2"])
+
+        onnx_model_name = "model_npu_bmmV2.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_dtype_cast(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                return torch_npu.npu_dtype_cast(input_, torch.int32)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((8, 10)).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_dtype_cast.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_silu(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                return torch_npu.npu_silu(input_)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.randn(5, 5).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_silu.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_mish(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                return torch_npu.npu_mish(input_)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.randn(5, 5).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, ["input_"])
+
+        onnx_model_name = "model_npu_mish.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_min(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_):
+                dim = 2
+                return torch_npu.npu_min(input_, dim)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((2, 2, 2, 2)).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, input_, onnx_model_name, 
+                             ["input_"], ["values", "indices"])
+
+        onnx_model_name = "model_npu_min.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_dropout_with_add_softmax(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_1, input_2):
+                alpha = 0.1
+                prob = 0
+                dim = -1
+                return torch_npu.npu_dropout_with_add_softmax(input_1, input_2,
+                                                              alpha, prob, dim)
+
+        def export_onnx(onnx_model_name):
+            input_1 = torch.rand((4, 3, 64, 64)).npu()
+            input_2 = torch.rand((4, 3, 64, 64)).npu()
+            model = Model().to("npu")
+            self.onnx_export(model, (input_1, input_2), onnx_model_name,
+                             ["input_1", "input_2"])
+
+        onnx_model_name = "model_npu_dropout_with_add_softmax.onnx"
+        export_onnx(onnx_model_name)
+        assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
+                                            onnx_model_name)))
+
+    def test_wrapper_npu_scaled_masked_softmax(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, input_, mask):
+                scale = 0.56
+                fixed_triu_mask = False
+                return torch_npu.npu_scaled_masked_softmax(input_, mask,
+                                                           scale, fixed_triu_mask)
+
+        def export_onnx(onnx_model_name):
+            input_ = torch.rand((4, 3, 64, 64)).npu()
+            mask = torch.rand((4, 3, 64, 64)).npu() > 0
+            model = Model().to("npu")
+            self.onnx_export(model, (input_, mask), onnx_model_name,
+                             ["input_", "mask"])
+
+        onnx_model_name = "model_npu_scaled_masked_softmax.onnx"
         export_onnx(onnx_model_name)
         assert (os.path.isfile(os.path.join(TestOnnxOps.test_onnx_path,
                                             onnx_model_name)))
