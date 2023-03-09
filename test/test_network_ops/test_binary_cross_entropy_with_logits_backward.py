9203c39321167cf11dd0ca3ab759bef6fd7b0d12
xiaxia3 <wangxiaxia3@huawei.com>
2023-03-09 10:38:37 +0800

    [fix] fix binary_cross_entropy_with_logits_backward bug

--- test/test_network_ops/test_binary_cross_entropy_with_logits_backward.py
+++ test/test_network_ops/test_binary_cross_entropy_with_logits_backward.py
@@ -32,6 +32,11 @@ def generate_data(min1, max1, shape, dtype):
     return output, target
 
 class TestBinaryCrossEntropyWithLogitsBackward(TestCase):
+    def generate_one_input(self, lower, upper, shape, dtype):
+        x = np.random.uniform(lower, upper, shape).astype(dtype)
+        npu_input = torch.from_numpy(x)
+        return npu_input
+
     def cpu_op_exec(self, input1, target):
         input1.requires_grad_(True)
         output = torch.nn.functional.binary_cross_entropy_with_logits(input1, target)
@@ -53,7 +58,32 @@ class TestBinaryCrossEntropyWithLogitsBackward(TestCase):
         res = res.numpy()
         return input_npu, res
 
-    def test_binary_cross_entropy_with_logits_backward_fp32(self, device="npu"):
+    def cpu_op_exec_module(self, input1, target, weight=None, pos_weight=None, reduction="mean"):
+        input1.requires_grad_(True)
+        criterion  = torch.nn.BCEWithLogitsLoss(weight=weight, pos_weight=pos_weight,
+                                                                   reduction=reduction)
+        res = criterion(input1, target)
+        res.sum().backward()
+        return res.detach().numpy(), input1.grad.numpy()
+
+    def npu_op_exec_module(self, input1, target, weight=None, pos_weight=None, reduction="mean"):
+        input1 = input1.to("npu")
+        target = target.to("npu")
+        input1.requires_grad_(True)
+        if weight is not None:
+            weight = weight.to("npu")
+        if pos_weight is not None:
+            pos_weight = pos_weight.to("npu")
+
+        criterion  = torch.nn.BCEWithLogitsLoss(weight=weight, pos_weight=pos_weight,
+                                                                   reduction=reduction)
+        criterion = criterion.to("npu")
+        res = criterion(input1, target)
+        res.sum().backward()                                                        
+        res = res.to("cpu")
+        return res.detach().numpy(), input1.grad.cpu().numpy()
+
+    def test_binary_cross_entropy_with_logits_backward_fp32(self):
         npu_input1, npu_target = generate_data(0, 100, (5, 3), np.float32)
         cpu_input1 = copy.deepcopy(npu_input1)
         cpu_target = copy.deepcopy(npu_target)
@@ -62,7 +92,7 @@ class TestBinaryCrossEntropyWithLogitsBackward(TestCase):
         self.assertRtolEqual(cpu_output, npu_output)
         self.assertRtolEqual(cpu_grad_output, npu_grad_output)
 
-    def test_binary_cross_entropy_with_logits_backward_fp16(self, device="npu"):
+    def test_binary_cross_entropy_with_logits_backward_fp16(self):
         npu_input1, npu_target = generate_data(0, 100, (5, 3), np.float16)
         cpu_input1 = copy.deepcopy(npu_input1)
         cpu_target = copy.deepcopy(npu_target)
@@ -75,6 +105,85 @@ class TestBinaryCrossEntropyWithLogitsBackward(TestCase):
         self.assertRtolEqual(cpu_output, npu_output)
         self.assertRtolEqual(cpu_grad_output, npu_grad_output)
 
+    def test_binary_cross_with_logits_backward_module_float32(self):
+        for shape, weight_shape, pos_weight_shape, reduction in [
+           ((10, 64), None, None, "mean"),
+           ((10, 64), (10, 1), None, "mean"),
+           ((10, 64), None, (64,), "mean"),
+           ((10, 64), None, None, "none"),
+           ((10, 64), (10, 1), None, "none"),
+           ((10, 64), None, (64,), "none"),
+           ((10, 64), None, None, "sum"),
+           ((10, 64), (10, 1), None, "sum"),
+           ((10, 64), None, (64,), "sum"),
+           ((10, 64), (10, 64), (10, 64), "sum"),
+           ((10, 64), (10, 64), (10, 64), "mean"),
+           ((10, 64), (10, 64), (10, 64), "none") 
+        ]:
+            input1 = self.generate_one_input(0, 10, shape, np.float32)
+            target = torch.empty(shape, dtype=torch.float32).random_(2)
+            weight = None
+            pos_weight = None
+            
+            if weight_shape is not None:
+                weight = self.generate_one_input(0, 10, weight_shape, np.float32)
+            if pos_weight_shape is not None:
+                pos_weight = self.generate_one_input(0, 10, pos_weight_shape, np.float32)
+
+            npu_output, npu_grad = self.npu_op_exec_module(input1, target,
+                                                           weight=weight,
+                                                           pos_weight=pos_weight,
+                                                           reduction=reduction)
+            cpu_output, cpu_grad = self.cpu_op_exec_module(input1, target,
+                                                           weight=weight,
+                                                           pos_weight=pos_weight,
+                                                           reduction=reduction)
+            self.assertRtolEqual(cpu_output, npu_output)
+            self.assertRtolEqual(cpu_grad, npu_grad)
+
+    def test_binary_cross_with_logits_backward_module_float16(self):
+        for shape, weight_shape, pos_weight_shape, reduction in [
+           ((10, 64), None, None, "mean"),
+           ((10, 64), (10, 1), None, "mean"),
+           ((10, 64), None, (64,), "mean"),
+           ((10, 64), None, None, "none"),
+           ((10, 64), (10, 1), None, "none"),
+           ((10, 64), None, (64,), "none"),
+           ((10, 64), None, None, "sum"),
+           ((10, 64), (10, 1), None, "sum"),
+           ((10, 64), None, (64,), "sum"),
+           ((10, 64), (10, 64), (10, 64), "sum"),
+           ((10, 64), (10, 64), (10, 64), "mean"),
+           ((10, 64), (10, 64), (10, 64), "none") 
+        ]:
+            input1 = self.generate_one_input(0, 10, shape, np.float16)
+            target = torch.empty(shape, dtype=torch.float16).random_(2)
+            input_32 = input1.type(torch.float32)
+            target_32 = target.type(torch.float32)
+            weight = None
+            weight_32 = None
+            pos_weight = None
+            pos_weight_32 = None
+            
+            if weight_shape is not None:
+                weight = self.generate_one_input(0, 10, weight_shape, np.float16)
+                weight_32 = weight.type(torch.float32)
+            if pos_weight_shape is not None:
+                pos_weight = self.generate_one_input(0, 10, pos_weight_shape, np.float16)
+                pos_weight_32 = pos_weight.type(torch.float32)
+                
+            npu_output, npu_grad = self.npu_op_exec_module(input1, target,
+                                                           weight=weight,
+                                                           pos_weight=pos_weight,
+                                                           reduction=reduction)
+            cpu_output, cpu_grad = self.cpu_op_exec_module(input_32, target_32,
+                                                           weight=weight_32,
+                                                           pos_weight=pos_weight_32,
+                                                           reduction=reduction)
+            cpu_output = cpu_output.astype(np.float16)
+            cpu_grad = cpu_grad.astype(np.float16)
+            self.assertRtolEqual(cpu_output, npu_output)
+            self.assertRtolEqual(cpu_grad, npu_grad)
 
 if __name__ == "__main__":
     run_tests()
