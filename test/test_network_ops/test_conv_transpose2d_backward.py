46dba976a5cb84ab75b703f140764df7ffa4b532
pipihugh <jifang5@h-partners.com>
2023-03-20 10:08:22 +0800

    Fix the outputpadding of conv_transpose2d.

--- test/test_network_ops/test_conv_transpose2d_backward.py
+++ test/test_network_ops/test_conv_transpose2d_backward.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2020 Huawei Technologies Co., Ltd
-# Copyright (c) 2019, Facebook CORPORATION. 
+# Copyright (c) 2019, Facebook CORPORATION.
 # All rights reserved.
 #
 # Licensed under the BSD 3-Clause License  (the "License");
@@ -14,92 +14,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import copy
+
 import torch
 import torch.nn as nn
 import numpy as np
-import torch_npu
 
+import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.common_utils import create_common_tensor
 from torch_npu.testing.decorator import graph_mode
 
 
 class TestConvTranspose2dBackward(TestCase):
-    weight_grad = []
-    input_grad = []
 
-    def get_weight_grad(self, grad):
-        self.weight_grad.append(grad.to("cpu"))
-
-    def get_input_grad(self, grad):
-        self.input_grad.append(grad.to("cpu"))
-    
-    def cpu_op_exec(self, input1, weight, bias1):
+    def cpu_op_exec(self, input1, weight, bias):
         input1.requires_grad = True
-        input1.register_hook(lambda grad: self.get_input_grad(grad))
         weight.requires_grad = True
-        weight.register_hook(lambda grad: self.get_weight_grad(grad))
-        bias1.requires_grad = True
+        bias.requires_grad = True
 
-        res_forward = nn.functional.conv_transpose2d(input1, weight, padding=1, bias=bias1)
-        grads = torch.ones_like(res_forward).float()
+        res_forward = nn.functional.conv_transpose2d(input1, weight, padding=1, bias=bias)
+        grads = torch.ones_like(res_forward)
         res_forward.backward(grads, retain_graph=True)
-        return res_forward
-    
-    def npu_op_exec(self, input1, weight, bias1):
+        input_grad = input1.grad
+        weight_grad = weight.grad
+        return res_forward, input_grad, weight_grad
+
+    def npu_op_exec(self, input1, weight, bias):
         input1.requires_grad = True
-        input1.register_hook(lambda grad: self.get_input_grad(grad))
         weight.requires_grad = True
-        weight.register_hook(lambda grad: self.get_weight_grad(grad))
-        bias1 = bias1.to("npu")
-        bias1.requires_grad = True
+        bias.requires_grad = True
 
-        res_forward = nn.functional.conv_transpose2d(input1, weight, padding=1, bias=bias1)
-        grads = torch.ones_like(res_forward).float()
-        grads = grads.to("npu")
+        res_forward = nn.functional.conv_transpose2d(input1, weight, padding=1, bias=bias)
+        grads = torch.ones_like(res_forward).npu()
         res_forward.backward(grads, retain_graph=True)
-        res_forward = res_forward.to("cpu")
-        return res_forward
-    
+        res_forward = res_forward.cpu()
+        input_grad = input1.grad.cpu()
+        weight_grad = weight.grad.cpu()
+        return res_forward, input_grad, weight_grad
+
     def conv_transpose2d_backward_result(self, shape_format):
-        cpu_bias = torch.randn(4)
-        npu_bias = copy.deepcopy(cpu_bias)
-
         for item in shape_format:
-            self.weight_grad.clear()
-            self.input_grad.clear()
             cpu_input1, npu_input1 = create_common_tensor(item[0], -2, 2)
-            if cpu_input1.dtype == torch.float16:
-                cpu_input1 = cpu_input1.to(torch.float32)
             cpu_input2, npu_input2 = create_common_tensor(item[1], -2, 2)
-            if cpu_input2.dtype == torch.float16:
-                cpu_input2 = cpu_input2.to(torch.float32)
-            kernel_size = (item[1][2][2], item[1][2][3])
-            cpu_output = self.cpu_op_exec(cpu_input1, cpu_input2, bias1=cpu_bias)
-            npu_output = self.npu_op_exec(npu_input1, npu_input2, bias1=npu_bias)
+            cpu_bias, npu_bias = create_common_tensor((item[0][0], 0, 4), -100, 100)
+            if item[0][0] == np.float16:
+                cpu_input1 = cpu_input1.float()
+                cpu_input2 = cpu_input2.float()
+                cpu_bias = cpu_bias.float()
+            else:
+                # Synchronous accuracy loss: The operator converts fp32 to fp16 for calculation.
+                cpu_input1 = cpu_input1.half().float()
+                cpu_input2 = cpu_input2.half().float()
+                cpu_bias = cpu_bias.half().float()
+            cpu_output, cpu_input_grad, cpu_weight_grad = self.cpu_op_exec(cpu_input1, cpu_input2, bias=cpu_bias)
+            npu_output, npu_input_grad, npu_weight_grad = self.npu_op_exec(npu_input1, npu_input2, bias=npu_bias)
+            if item[0][0] == np.float16:
+                cpu_output = cpu_output.half()
+                cpu_input_grad = cpu_input_grad.half()
+                cpu_weight_grad = cpu_weight_grad.half()
 
-            cpu_output = cpu_output.to(torch.float16)
-            npu_output = npu_output.to(torch.float16)
-            self.input_grad[0] = self.input_grad[0].to(torch.float16)
-            self.input_grad[1] = self.input_grad[1].to(torch.float16)
-            self.weight_grad[0] = self.weight_grad[0].to(self.weight_grad[1].dtype)
-
-            self.assertRtolEqual(cpu_output.detach().numpy(), npu_output.detach().numpy())
-            self.assertRtolEqual(self.input_grad[0].numpy(), self.input_grad[1].numpy())
-            self.assertRtolEqual(self.weight_grad[0].numpy(), self.weight_grad[1].numpy())
+            self.assertRtolEqual(cpu_output.detach(), npu_output.detach(), prec=1e-3)
+            self.assertRtolEqual(cpu_input_grad, npu_input_grad, prec=1e-3)
+            self.assertRtolEqual(cpu_weight_grad, npu_weight_grad, prec=1e-3)
 
     @graph_mode
-    def test_conv_transpose2d_backward_shape_format_fp16(self):
+    def test_conv_transpose2d_backward_shape_format(self):
         shape_format = [
-            [[np.float16, 0, [1, 4, 5, 5]], [np.float16, 0, [4, 4, 3, 3]]]
-        ]
-        self.conv_transpose2d_backward_result(shape_format)
-
-    def test_conv_transpose2d_backward_shape_format_fp32(self):
-        shape_format = [
+            [[np.float16, 0, [1, 4, 5, 5]], [np.float16, 0, [4, 4, 3, 3]]],
             [[np.float32, 0, [1, 4, 5, 5]], [np.float32, 0, [4, 4, 3, 3]]]
         ]
+        self.conv_transpose2d_backward_result(shape_format)
 
     def test_conv_transpose2d_backward_allow_hf32(self):
         torch.npu.conv.allow_hf32 = True
@@ -108,6 +92,7 @@ class TestConvTranspose2dBackward(TestCase):
         ]
         self.conv_transpose2d_backward_result(shape_format)
         torch.npu.conv.allow_hf32 = False
+
 
 if __name__ == "__main__":
     run_tests()
