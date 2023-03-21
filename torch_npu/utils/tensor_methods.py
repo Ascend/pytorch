731ebffb811d1b7d1d152e2d62cd5fcb6c63a821
dujinhang <dujinhang@huawei.com>
2023-03-21 22:32:27 +0800

    [Fix] add _lazy_init in torch_device_guard.

--- torch_npu/utils/tensor_methods.py
+++ torch_npu/utils/tensor_methods.py
@@ -104,50 +104,32 @@ def _storage(self):
 
 @torch_device_guard
 def _new_empty(self, *args, **kwargs):
-    if args and isinstance(args[0], int):
-        list_args = list(args)
-        sizes = []
-        for item in list_args:
-            if not isinstance(item, int):
-                break
-            sizes.append(item)
-        args = tuple([tuple(sizes)] + list_args[len(sizes):])
-    return torch_npu._C.new_empty(self, *args, **kwargs)
+    return torch._C._TensorBase.new_empty(self, *args, **kwargs)
 
 
 @torch_device_guard
 def _new_empty_strided(self, *args, **kwargs):
-    return torch_npu._C.new_empty_strided(self, *args, **kwargs)
+    return torch._C._TensorBase.new_empty_strided(self, *args, **kwargs)
 
 
 @torch_device_guard
 def _new_full(self, *args, **kwargs):
-    return torch_npu._C.new_full(self, *args, **kwargs)
+    return torch._C._TensorBase.new_full(self, *args, **kwargs)
 
 
 @torch_device_guard
 def _new_ones(self, *args, **kwargs):
-
     return torch._C._TensorBase.new_ones(self, *args, **kwargs)
 
 
 @torch_device_guard
 def _new_tensor(self, *args, **kwargs):
-
     return torch._C._TensorBase.new_tensor(self, *args, **kwargs)
 
 
 @torch_device_guard
 def _new_zeros(self, *args, **kwargs):
-    if args and isinstance(args[0], int):
-        list_args = list(args)
-        sizes = []
-        for item in list_args:
-            if not isinstance(item, int):
-                break
-            sizes.append(item)
-        args = tuple([tuple(sizes)] + list_args[len(sizes):])
-    return torch_npu._C.new_zeros(self, *args, **kwargs)
+    return torch._C._TensorBase.new_zeros(self, *args, **kwargs)
 
 
 @property
