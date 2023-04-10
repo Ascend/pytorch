7773589d29929600d6fb18bd70d0c974b658c792
wbigat <1093025676@qq.com>
2023-04-10 15:38:18 +0800

    fixed c180c88 from https://gitee.com/wbigat/pytorch_master_wq/pulls/2905 [Fix] During shutdown, removing operations for device resources such as events and stream.

--- torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
+++ torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
@@ -214,12 +214,9 @@ NpuSysCtrl::NpuSysCtrl() : init_flag_(false), device_id_(0) {}
     }
 
     this->RegisterReleaseFn([=]() ->void {
-          c10_npu::NPUEventManager::GetInstance().ClearEvent();
-          auto stream = c10_npu::getCurrentNPUStream();
-          (void)aclrtDestroyStream(stream);
           C10_NPU_CHECK(ge::GEFinalize());
-          C10_NPU_CHECK(aclrtResetDevice(device_id_));
           C10_NPU_CHECK(aclFinalize());
+          C10_NPU_CHECK(aclrtResetDevice(device_id_));
         }, ReleasePriority::PriorityLast);
 
     init_flag_ = false;
