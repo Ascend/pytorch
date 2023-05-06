edb08e418c98807710893f6862cc30e4f7335e39
shibo <18207133434@163.com>
2023-05-06 08:53:14 +0800

    remove tensor str

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -18,7 +18,6 @@ from torch.serialization import register_package
 from .module import apply_module_patch
 from .tensor_methods import add_tensor_methods
 from .serialization import save, load, _npu_tag, _npu_deserialize
-from ._tensor_str import add_str_methods
 from .storage import add_storage_methods
 from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
 
