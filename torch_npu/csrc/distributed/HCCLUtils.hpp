ee999ffce66513b8a56895a5f15616ebec891383
chenzijian <chenzijian9@huawei.com>
2023-05-09 10:39:31 +0800

    add hcclresult err msg details and complement error code map

--- torch_npu/csrc/distributed/HCCLUtils.hpp
+++ torch_npu/csrc/distributed/HCCLUtils.hpp
@@ -18,6 +18,7 @@
 
 #include "torch_npu/csrc/core/npu/npu_log.h"
 #include "torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.h"
+#include "torch_npu/csrc/core/npu/NPUErrorCodes.h"
 #include <memory>
 
 #include "hccl/hccl.h"
@@ -28,7 +29,15 @@
     HcclResult error = cmd;                                         \
     if (error != HCCL_SUCCESS) {                                    \
       std::string err = "HCCL error in: " + std::string(__FILE__) + \
-          ":" + std::to_string(__LINE__) + "\n" +                   \
+          ":" + std::to_string(__LINE__) + ".\n" +                  \
+          " HCCL error, error code is " +                           \
+          std::to_string(error) + ". " +                            \
+          (c10_npu::acl::hcclresult_code_map.find(error) !=         \
+          c10_npu::acl::hcclresult_code_map.end() ?                 \
+          " [error]: " +                                            \
+          c10_npu::acl::hcclresult_code_map[error] +                \
+          "\n" : " " )  +                                           \
+          "And see details in Ascend logs.\n" +                     \
           c10_npu::acl::AclGetErrMsg();                             \
       throw std::runtime_error(err);                                \
     }                                                               \
