711df330a0e4669d5240d4c6b200b619a05c84f2
Rayyyy <Uedalan@163.com>
2023-05-10 19:14:45 +0800

    Add checkout and check_match in bmm_out、cosh_out、cumsum_out、cumprod_out、erfinv_out、expm1_out.

--- torch_npu/csrc/aten/ops/CoshKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/CoshKernelNpu.cpp
@@ -14,39 +14,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "torch_npu/csrc/framework/utils/CalcuOpUtil.h"
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 
 namespace at_npu {
 namespace native {
- at::Tensor& NPUNativeFunctions::cosh_out(const  at::Tensor& self,  at::Tensor& result) {
+
+at::Tensor& cosh_out_npu_nocheck(at::Tensor& result, const at::Tensor& self) {
   OpCommand cmd;
   cmd.Name("Cosh")
-     .Input(self)
-     .Output(result)
-     .Run();
-
+      .Input(self)
+      .Output(result)
+      .Run();
   return result;
 }
 
- at::Tensor& NPUNativeFunctions::cosh_( at::Tensor& self) {
-  OpPreparation::CheckMemory({self}, {self});
+at::Tensor& NPUNativeFunctions::cosh_out(const at::Tensor& self, at::Tensor& result) {
+  OpPreparation::CheckOut(
+      {self},
+      result,
+      CalcuOpUtil::GetTensorNpuFormat(result),
+      self.scalar_type(),
+      self.sizes());
 
-  if (!NpuUtils::check_match(&self)) {
-     at::Tensor contiguousSelf = NpuUtils::format_contiguous(self);
-     at::Tensor result = NPUNativeFunctions::cosh_out(contiguousSelf, contiguousSelf);
-    NpuUtils::format_fresh_view(self, result);
+  if (!NpuUtils::check_match(&result)) {
+    at::Tensor contiguous_result = NpuUtils::format_contiguous(result);
+    cosh_out_npu_nocheck(contiguous_result, self);
+    NpuUtils::format_fresh_view(result, contiguous_result);
   } else {
-    NPUNativeFunctions::cosh_out(self, self);
+    cosh_out_npu_nocheck(result, self);
   }
-
-  return self;
+  return result;
 }
 
- at::Tensor NPUNativeFunctions::cosh(const  at::Tensor& self) {
-   at::Tensor result = OpPreparation::ApplyTensor(self);
-  // calculate the output result of the NPU
-  NPUNativeFunctions::cosh_out(self, result);
+at::Tensor& NPUNativeFunctions::cosh_(at::Tensor& self) {
+  return NPUNativeFunctions::cosh_out(self, self);
+}
+
+at::Tensor NPUNativeFunctions::cosh(const at::Tensor& self) {
+  at::Tensor result = OpPreparation::ApplyTensor(self);
+  cosh_out_npu_nocheck(result, self);
   return result;
 }
 
