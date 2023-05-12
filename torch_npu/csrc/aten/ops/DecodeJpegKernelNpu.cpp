405a0e7c232d29e8211cc8f3b93f88b67e3258e2
c00659642 <chenyingyan2@hisilicon.com>
2023-05-12 11:28:16 +0800

    [DVPP adaptation] decode_jpeg add parameter try_recover_truncated

--- torch_npu/csrc/aten/ops/DecodeJpegKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/DecodeJpegKernelNpu.cpp
@@ -1,17 +1,3 @@
-// Copyright (c) Huawei Technologies Co., Ltd. 2022. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License  (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/framework/utils/CalcuOpUtil.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
@@ -22,7 +8,6 @@ namespace native {
 namespace {
   constexpr int64_t ratio = 1;
   constexpr bool fancyUpscaling = true;
-  constexpr bool tryRecoverTruncated = false;
   constexpr float acceptableFraction = 1.0;
   const std::string dctMethod = "";
   const std::string dstImgFormat = "CHW";
@@ -31,6 +16,7 @@ namespace {
 at::Tensor &decode_jpeg_out(
     const at::Tensor &self,
     int64_t channels,
+    bool try_recover_truncated,
     at::Tensor &result)
 {
   OpCommand cmd;
@@ -40,7 +26,7 @@ at::Tensor &decode_jpeg_out(
       .Attr("channels", channels)
       .Attr("ratio", ratio)
       .Attr("fancy_upscaling", fancyUpscaling)
-      .Attr("try_recover_truncated", tryRecoverTruncated)
+      .Attr("try_recover_truncated", try_recover_truncated)
       .Attr("acceptable_fraction", acceptableFraction)
       .Attr("dct_method", dctMethod)
       .Attr("dst_img_format", dstImgFormat)
@@ -52,7 +38,8 @@ at::Tensor &decode_jpeg_out(
 at::Tensor NPUNativeFunctions::decode_jpeg(
     const at::Tensor &self,
     at::IntArrayRef image_shape,
-    int64_t channels)
+    int64_t channels,
+    bool try_recover_truncated)
 {
   // calculate the output size
   auto outputSize = decode_jpeg_npu_output_size(image_shape, channels);
@@ -64,7 +51,7 @@ at::Tensor NPUNativeFunctions::decode_jpeg(
       ACL_FORMAT_ND);
 
   // calculate the output result of the NPU
-  decode_jpeg_out(self, channels, result);
+  decode_jpeg_out(self, channels, try_recover_truncated, result);
 
   return result;
 }
