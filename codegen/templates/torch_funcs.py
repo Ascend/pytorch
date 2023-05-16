953e85bb51188ba10366aaa487d2dbcb35a8cfc7
刘嘉巍 <843972097@qq.com>
2023-05-16 10:50:40 +0800

    Add warning for custom torch & tensor methods

--- codegen/templates/torch_funcs.py
+++ codegen/templates/torch_funcs.py
@@ -14,11 +14,21 @@
 # limitations under the License.
 
 import dis
-import torch
+from functools import wraps
 
+import torch
 
 import torch_npu
 from torch_npu.utils.device_guard import torch_device_guard
+
+
+def wrap_torch_warning_func(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Warning: torch.{func.__name__} is deprecated and will be removed in future version. "
+              f"Use torch_npu.{func.__name__} instead.")
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @torch_device_guard
@@ -46,6 +56,7 @@ def _arange(*args, **kwargs):
     return torch_npu.arange(*args, **kwargs)
 
 
+@wrap_torch_warning_func
 @torch_device_guard
 def _empty_with_format(*args, **kwargs):
     return torch_npu.empty_with_format(*args, **kwargs)
