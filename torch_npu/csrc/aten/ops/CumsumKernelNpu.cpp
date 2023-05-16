3fa81d0b0a88dc5acafbb9346571246cac922f61
Rayyyy <Uedalan@163.com>
2023-05-16 11:23:39 +0800

    Add checkout and check_match in bmm_out/cosh_out/cumprod_out/cumsum_out/erfinv_out/expm1_out.

--- torch_npu/csrc/aten/ops/CumsumKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/CumsumKernelNpu.cpp
@@ -11,36 +11,52 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 
 namespace at_npu {
 namespace native {
 
-at::Tensor& NPUNativeFunctions::_cumsum_out(const at::Tensor& self, int64_t dim, at::Tensor& result) {
+at::Tensor& cumsum_out_nocheck(at::Tensor& result, const at::Tensor& self, int64_t dim) {
   OpCommand cmd;
   // if dim = 0, performance in Aicpu is better than Aicore
   // if dim > INT32_MAX, we should use long to store dim for ensuring function correctness.
   // use host memory instead of scalar to improve delivery performance
-  at::Scalar dimScalar(dim);
+  at::Scalar dim_scalar(dim);
   cmd.Name("Cumsum")
-    .Input(self);
+      .Input(self);
   if (dim == 0 || dim > INT32_MAX) {
-    cmd.Input(dimScalar, at::kLong, CompileType::MEMORY_HOST_COMPILE_DEPENDENT);
+    cmd.Input(dim_scalar, at::kLong, CompileType::MEMORY_HOST_COMPILE_DEPENDENT);
   } else {
-    cmd.Input(dimScalar, at::kInt, CompileType::MEMORY_HOST_COMPILE_DEPENDENT);
+    cmd.Input(dim_scalar, at::kInt, CompileType::MEMORY_HOST_COMPILE_DEPENDENT);
   }
   cmd.Output(result)
-    .Run();
+      .Run();
+  return result;
+}
 
+at::Tensor& NPUNativeFunctions::_cumsum_out(const at::Tensor& self, int64_t dim, at::Tensor& result) {
+  OpPreparation::CheckOut(
+      {self},
+      result,
+      CalcuOpUtil::GetTensorNpuFormat(result),
+      self.scalar_type(),
+      self.sizes());
+
+  if (!NpuUtils::check_match(&result)) {
+    at::Tensor contiguous_result = NpuUtils::format_contiguous(result);
+    cumsum_out_nocheck(contiguous_result, self, dim);
+    NpuUtils::format_fresh_view(result, contiguous_result);
+  } else {
+    cumsum_out_nocheck(result, self, dim);
+  }
   return result;
 }
 
 at::Tensor NPUNativeFunctions::_cumsum(const at::Tensor& self, int64_t dim) {
-  auto outputSize = input_same_output_size(self);
-  at::Tensor result = OpPreparation::ApplyTensor(self, outputSize);
-  NPUNativeFunctions::_cumsum_out(self, dim, result);
-
+  at::Tensor result = OpPreparation::ApplyTensor(self);
+  cumsum_out_nocheck(result, self, dim);
   return result;
 }
 
