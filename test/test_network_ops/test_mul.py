a7eae332b9ad0a71e02176360b8951316aefaec6
pipihugh <jifang5@h-partners.com>
2023-05-18 21:23:33 +0800

    Fix the output type of mul_out.

--- test/test_network_ops/test_mul.py
+++ test/test_network_ops/test_mul.py
@@ -11,14 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 import torch
 import numpy as np
-import torch_npu
 
+import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.common_utils import create_common_tensor
 
+
 class TestMuls(TestCase):
+
     def cpu_op_exec(self, input1, input2):
         output = torch.mul(input1, input2)
         output = output.numpy()
@@ -29,27 +33,27 @@ class TestMuls(TestCase):
         output = output.to("cpu")
         output = output.numpy()
         return output
-    
+
     def cpu_op_out_exec(self, input1, input2, input3):
-        torch.mul(input1, input2, out = input3)
+        torch.mul(input1, input2, out=input3)
         input3 = input3.numpy()
         return input3
 
     def npu_op_out_exec(self, input1, input2, input3):
-        torch.mul(input1, input2, out = input3)
+        torch.mul(input1, input2, out=input3)
         input3 = input3.to("cpu")
         input3 = input3.numpy()
         return input3
-    
+
     def cpu_inp_op_exec(self, input1, input2):
         input1 *= input2
         return input1
-    
+
     def npu_inp_op_exec(self, input1, input2):
         input1 *= input2
         return input1.cpu()
 
-    def test_muls_shape_format_fp16(self, device="npu"):
+    def test_muls_shape_format_fp16(self):
         format_list = [0, 3, 4, 29]
         shape_list = [1, (64, 10), (32, 3, 3), (256, 2048, 7, 7), (2, 0, 2)]
         shape_format = [
@@ -65,7 +69,7 @@ class TestMuls(TestCase):
             cpu_output = cpu_output.astype(npu_output.dtype)
             self.assertRtolEqual(cpu_output, npu_output)
 
-    def test_muls_shape_format_fp32(self, device="npu"):
+    def test_muls_shape_format_fp32(self):
         format_list = [0, 3, 4, 29]
         shape_list = [1, (64, 10), (32, 3, 3), (256, 2048, 7, 7)]
         shape_format = [
@@ -78,7 +82,7 @@ class TestMuls(TestCase):
             npu_output = self.npu_op_exec(npu_input1, npu_input2)
             self.assertRtolEqual(cpu_output, npu_output)
 
-    def test_muls_shape_format_bool(self, device="npu"):
+    def test_muls_shape_format_bool(self):
         format_list = [0]
         shape_list = [1, (64, 10), (32, 3, 3), (256, 2048, 7, 7)]
         shape_format = [
@@ -90,8 +94,8 @@ class TestMuls(TestCase):
             cpu_output = self.cpu_op_exec(cpu_input1 > 50, cpu_input2 > 50)
             npu_output = self.npu_op_exec(npu_input1 > 50, npu_input2 > 50)
             self.assertRtolEqual(cpu_output, npu_output)
-    
-    def test_muls_shape_format_out_fp32(self, device="npu"):
+
+    def test_muls_shape_format_out(self):
         format_list = [0]
         shape_list = [1, (64, 10), (32, 3, 3), (256, 2048, 7, 7)]
         shape_format = [
@@ -106,22 +110,28 @@ class TestMuls(TestCase):
             npu_output = self.npu_op_out_exec(npu_input1, npu_input2, npuout)
             self.assertRtolEqual(cpu_output, npu_output)
 
-    def test_muls_mix_dtype(self, device="npu"):
-        npu_input1, npu_input2 = create_common_tensor([np.int32, 0, (2, 3)], 1, 100)
-        npu_input3, npu_input4 = create_common_tensor([np.float32, 0, (2, 3)], 1, 100)
-        cpu_output = self.cpu_op_exec(npu_input1, npu_input3)
-        npu_output = self.npu_op_exec(npu_input2, npu_input4)
+    def test_muls_mix_dtype(self):
+        cpu_input1, npu_input1 = create_common_tensor([np.int32, 0, (2, 3)], 1, 100)
+        cpu_input2, npu_input2 = create_common_tensor([np.float32, 0, (2, 3)], 1, 100)
+        cpu_out = torch.randn(2, 3).half()
+        npu_out = cpu_out.npu()
+        cpu_output = self.cpu_op_exec(cpu_input1, cpu_input2)
+        npu_output = self.npu_op_exec(npu_input1, npu_input2)
         self.assertRtolEqual(cpu_output, npu_output)
 
-    def test_muls_scalar_dtype(self, device="npu"):
+        cpu_output = self.cpu_op_out_exec(cpu_input1, cpu_input2, cpu_out)
+        npu_output = self.npu_op_out_exec(npu_input1, npu_input2, npu_out)
+        self.assertRtolEqual(cpu_output, npu_output)
+
+    def test_muls_scalar_dtype(self):
         shape_format = [
-                        [np.int8, 0, (2, 3, 8)],
-                        [np.int16, 0, (2, 3)],
-                        [np.int32, 0, (2, 3, 8)],
-                        [np.int64, 0, (2, 32, 8, 16)],
-                        [np.uint8, 0, (8)],
-                        [np.bool, 0, (2, 2, 3, 8)],
-                       ]
+            [np.int8, 0, (2, 3, 8)],
+            [np.int16, 0, (2, 3)],
+            [np.int32, 0, (2, 3, 8)],
+            [np.int64, 0, (2, 32, 8, 16)],
+            [np.uint8, 0, (8)],
+            [np.bool, 0, (2, 2, 3, 8)],
+        ]
         for item in shape_format:
             cpu_input1, npu_input1 = create_common_tensor(item, 1, 100)
             cpu_output = self.cpu_op_exec(cpu_input1, 0.5)
@@ -136,7 +146,7 @@ class TestMuls(TestCase):
         # PR: https://gitee.com/ascend/pytorch/pulls/849
         _, npu_input2 = create_common_tensor([np.float16, 0, (2, 3)], 1, 100)
         npu_output2 = self.npu_op_exec(npu_input2, 65536)
-    
+
     def test_mul_different_dtype_inputs(self):
         shape_list = [1, (64, 10), (32, 3, 3), (256, 2048, 7, 7)]
         shape_format = [
@@ -150,6 +160,16 @@ class TestMuls(TestCase):
             cpu_input2.mul_(cpu_input1)
             npu_input2.mul_(npu_input1)
             self.assertEqual(cpu_input2, npu_input2.cpu())
+
+    def test_mul_out_error_dtype(self):
+        npu_input1 = torch.randn(10, 23).npu()
+        npu_input2 = npu_input1.int()
+        npu_out = torch.randn(2, 3).long()
+        try:
+            self.npu_op_out_exec(npu_input1, npu_input2, npu_out)
+        except RuntimeError as e:
+            self.assertRegex(
+                str(e), "result type Float can't be cast to the desired output type Long")
 
 
 if __name__ == "__main__":
