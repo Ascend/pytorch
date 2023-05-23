1ae130315a9064c4e3a215ff74e3c7c1f6b7afac
pipihugh <jifang5@h-partners.com>
2023-05-23 16:32:39 +0800

    Remove the PadV3 of reflection_pad2d and add reflection_pad1d_backward.

--- torch_npu/csrc/aten/ops/ReflectionPad2dKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/ReflectionPad2dKernelNpu.cpp
@@ -49,23 +49,12 @@ at::Tensor& reflection_pad2d_out_npu_nocheck(
   }
   c10::SmallVector<int64_t, N> value_tensor = {(int64_t)0};
   OpCommand cmd;
-  if(self.dtype() == at::kHalf) {
-    cmd.Name("PadV3")
-        .Input(self)
-        .Input(vectorInt, at::kInt)
-        .Input(value_tensor, self.scalar_type())
-        .Output(out)
-        .Attr("mode", (string)"reflect")
-        .Attr("paddings_contiguous", true)
-        .Run();
-  } else {
-    cmd.Name("MirrorPad")
-        .Input(self)
-        .Input(vectorInt, at::kInt)
-        .Output(out)
-        .Attr("mode", (string)"REFLECT")
-        .Run();
-  }
+  cmd.Name("MirrorPad")
+      .Input(self)
+      .Input(vectorInt, at::kInt)
+      .Output(out)
+      .Attr("mode", (string)"REFLECT")
+      .Run();
   return out;
 }
 
