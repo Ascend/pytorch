b0d8eac925d3aaa9a3f9d20428457f5f245b111c
刘嘉巍 <843972097@qq.com>
2023-05-24 11:07:16 +0800

    Fix bug when raising error in _empty_with_format

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -25,6 +25,7 @@ import warnings
 
 from builtins import isinstance as builtin_isinstance
 from typing import Set, Type
+from functools import wraps
 
 import torch
 import torch_npu
@@ -54,7 +55,6 @@ from torch_npu.contrib.module import npu_modules
 from torch_npu.utils import apply_module_patch, add_tensor_methods, add_torch_funcs, \
      serialization_patches, add_storage_methods, add_str_methods, add_dataloader_method, \
      add_fx_methods, add_checkpoint_methods, add_dynamo_patch
-from torch_npu.utils.torch_funcs import wrap_torch_error_func
 from torch_npu.distributed.hccl_dtype_wraper import wrap_dtype_for_hccl
 from torch_npu.npu.amp.autocast_mode import apply_autocast_patch
 
@@ -150,6 +150,13 @@ builtins.isinstance = _isinstance
 
 
 __all__ = []
+
+def wrap_torch_error_func(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        raise RuntimeError(f"torch.{func.__name__} is deprecated and will be removed in future version. "
+                           f"Use torch_npu.{func.__name__} instead.")
+    return wrapper
 
 
 for name in dir(torch_npu._C._VariableFunctions):
