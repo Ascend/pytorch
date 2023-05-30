8b38aee7b9fb783036327bdcd4c527a3b274976d
wangxiao <wangxiao99@huawei.com>
2023-05-30 19:11:31 +0800

    Adapter of native_dropout.

--- test/test_nn/test_dropout_layers.py
+++ test/test_nn/test_dropout_layers.py
@@ -43,6 +43,29 @@ class TestDropoutLayers(TestCase):
         output = m(input1)
         self.assertEqual(output is not None, True)
 
+    def test_native_dropout(self):
+        for train in [True, False]:
+            for p in [0.0, 1.0, 0.5]:
+                cpu_x = torch.randn(5, 5)
+                x = cpu_x.npu()
+                x_ref = cpu_x.npu()
+                x.requires_grad = True
+                x_ref.requires_grad = True
+                torch.manual_seed(123)
+                npu_o = torch.native_dropout(x, p, train)
+                o = npu_o[0]
+                o.sum().backward()
+                torch.manual_seed(123)
+                o_ref = torch.dropout(x_ref, p, train)
+                o_ref.sum().backward()
+                self.assertRtolEqual(o.cpu().detach(), o_ref.cpu().detach())
+                self.assertRtolEqual(x.grad.cpu(), x_ref.grad.cpu())
+                if p in [0.0, 1.0]:
+                    cpu_o = torch.native_dropout(cpu_x, p, train)
+                    self.assertRtolEqual(cpu_o[0], npu_o[0].cpu())
+                    self.assertRtolEqual(cpu_o[1], npu_o[1].cpu())
+
+
 if __name__ == "__main__":
     torch.npu.set_device(0)
     run_tests()
