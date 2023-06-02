00e342e83e0759089e629cd712483772f12875f5
pipihugh <jifang5@h-partners.com>
2023-06-02 17:35:57 +0800

    Optimized copy_ to support complex inputs.

--- test/test_network_ops/test_copy_.py
+++ test/test_network_ops/test_copy_.py
@@ -14,31 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import torch
 import numpy as np
-import torch_npu
 
+import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.common_utils import create_common_tensor
 
+
 class TestCopy(TestCase):
+
     def cpu_op_exec(self, input1, input2):
-        output = input1.copy_(input2);
-        output = output.numpy()
-        return output
+        input1.copy_(input2)
+        return input1
 
     def npu_op_exec(self, input1, input2):
-        input1 = input1.to("npu")
-        input2 = input2.to("npu")
-        output = input1.copy_(input2);
-        output = output.to("cpu")
-        output = output.numpy()
-        return output
+        input1.copy_(input2)
+        return input1.cpu()
 
     def test_copy__(self):
         format_list = [0]
         shape_list = [(4, 1), (4, 3, 1)]
-        dtype_list = [np.float32, np.int32, np.float16]
+        dtype_list = [np.float32, np.int32, np.float16, np.cfloat]
         shape_format = [
             [i, j, k] for i in dtype_list for j in format_list for k in shape_list
         ]
@@ -47,7 +45,11 @@ class TestCopy(TestCase):
             cpu_input2, npu_input2 = create_common_tensor(item, 0, 100)
             cpu_output = self.cpu_op_exec(cpu_input1, cpu_input2)
             npu_output = self.npu_op_exec(npu_input1, npu_input2)
+            if item[0] == np.cfloat:
+                cpu_output = cpu_output.float()
+                npu_output = npu_output.float()
             self.assertRtolEqual(cpu_output, npu_output)
+
 
 if __name__ == "__main__":
     run_tests()
