3dbaf4d9d8dfb3a803a4b9f9558a87e438ca3579
liangyunfan <liangyunfan@huawei.com>
2023-06-20 09:55:29 +0800

    [DFX]add fixed device num check.

--- torch_npu/npu/utils.py
+++ torch_npu/npu/utils.py
@@ -26,6 +26,7 @@ import torch._six
 
 import torch_npu
 import torch_npu._C
+from torch_npu.utils.device_guard import check_is_valid_ordinal
 
 _initialized = False
 _tls = threading.local()
@@ -142,6 +143,7 @@ def device_count():
 
 
 def set_device(device):
+    check_is_valid_ordinal(device)
     if isinstance(device, str) and device.startswith("npu"):
         device = device.replace('npu', torch_npu.npu.native_device)
     if isinstance(device, (torch_npu._C.device, torch._C.device)):
@@ -150,6 +152,7 @@ def set_device(device):
         torch_npu._C._npu_setDevice(device)
     elif torch.device(str(device)):
         device_index = torch.device(str(device)).index
+        check_is_valid_ordinal(device_index)
         torch_npu._C._npu_setDevice(device_index)
     else:
         raise AssertionError("input can not convert to torch.device")
