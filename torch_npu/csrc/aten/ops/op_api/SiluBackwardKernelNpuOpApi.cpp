29c3e52220778c825c4401422ef4025410d0f49d
ruimingluo <luoruiming1@hisilicon.com>
2023-06-20 10:19:19 +0800

    add silu_backward PTA

---
+++ torch_npu/csrc/aten/ops/op_api/SiluBackwardKernelNpuOpApi.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// Copyright (c) 2023, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "torch_npu/csrc/aten/NPUNativeOpApiFunctions.h"
+#include "torch_npu/csrc/aten/NPUNativeFunctions.h"
+#include "torch_npu/csrc/aten/ops/op_api/op_api_common.h"
+
+namespace at_npu {
+namespace native {
+
+/* Note: This is the pre-released op_api-version PTA for silu_backward. To avoid build problem, op_api namespace of
+  defined function(NPUNativeOpApiFunctions) is removed here. When old-version PTA for silu_backward is refined and
+  uploaded, the op_api namespace should be added (e.g. NPUNativeOpApiFunctions::silu_backward_out()), and the macro
+  for compatibility(e.g. DO_COMPATIBILITY(aclnnSiluBackward, NPUNativeFunctions::silu_backward(grad_output, self)))
+  should also be added in all defined functions.
+*/ 
+
+at::Tensor& silu_backward_out(const at::Tensor& grad_output, const at::Tensor& self,
+                              at::Tensor& result) {
+  OpPreparation::CheckOut({grad_output, self}, result, grad_output);
+  EXEC_NPU_CMD(aclnnSiluBackward, grad_output, self, result);
+  return result;
+}
+
+at::Tensor silu_backward(const at::Tensor& grad_output, const at::Tensor& self) {
+  at::Tensor grad_input = OpPreparation::ApplyTensor(grad_output);
+  EXEC_NPU_CMD(aclnnSiluBackward, grad_output, self, grad_input);
+  return grad_input;
+}
+
+} // namespace native
+} // namespace at_npu
--- torch_npu/csrc/aten/ops/op_api/SiluKernelNpuOpApi.cpp
+++ torch_npu/csrc/aten/ops/op_api/SiluKernelNpuOpApi.cpp
@@ -13,19 +13,33 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "torch_npu/csrc/aten/NPUNativeFunctions.h"
+
 #include "torch_npu/csrc/aten/NPUNativeOpApiFunctions.h"
+#include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 #include "torch_npu/csrc/aten/ops/op_api/op_api_common.h"
 
 namespace at_npu {
 namespace native {
 
-at::Tensor &NPUNativeOpApiFunctions::silu_out(const at::Tensor &self, at::Tensor &out) {
-  DO_COMPATIBILITY(aclnnSilu, NPUNativeFunctions::silu_out(self, out));
-  OpPreparation::CheckOut({self}, out, self);
-  EXEC_NPU_CMD(aclnnSilu, self, out);
-  return out;
+at::Tensor& NPUNativeOpApiFunctions::silu_out(const at::Tensor& self, at::Tensor& result) {
+  DO_COMPATIBILITY(aclnnSilu, NPUNativeFunctions::silu_out(self, result));
+  OpPreparation::CheckOut({self}, result, self);
+  EXEC_NPU_CMD(aclnnSilu, self, result);
+  return result;
 }
 
-}  // namespace native
-}  // namespace at_npu
+at::Tensor NPUNativeOpApiFunctions::silu(const at::Tensor& self) {
+  DO_COMPATIBILITY(aclnnSilu, NPUNativeFunctions::silu(self));
+  at::Tensor result = OpPreparation::ApplyTensor(self);
+  EXEC_NPU_CMD(aclnnSilu, self, result);
+  return result;
+}
+
+at::Tensor& NPUNativeOpApiFunctions::silu_(at::Tensor& self) {
+  DO_COMPATIBILITY(aclnnSilu, NPUNativeFunctions::silu_(self));
+  NPUNativeOpApiFunctions::silu_out(self, self);
+  return self;
+}
+
+} // namespace native
+} // namespace at_npu
