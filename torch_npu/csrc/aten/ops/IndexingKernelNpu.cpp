be3d7b1fb4fe125e36ecd953f8d1f5905cb846c7
YUANCHUN <Uedalan@163.com>
2023-06-26 16:48:59 +0800

    Add complex dtype in div and npu_indexing.

--- torch_npu/csrc/aten/ops/IndexingKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/IndexingKernelNpu.cpp
@@ -16,11 +16,14 @@
 
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
+#include "torch_npu/csrc/aten/common/InnerNpuNativeFunction.h"
 
 namespace at_npu {
 namespace native {
 
-at::Tensor& NPUNativeFunctions::npu_indexing_out(const at::Tensor& self,
+at::Tensor& npu_indexing_out_nocheck(
+    at::Tensor& result,
+    const at::Tensor& self,
     c10::IntArrayRef begin,
     c10::IntArrayRef end,
     c10::IntArrayRef strides,
@@ -28,9 +31,7 @@ at::Tensor& NPUNativeFunctions::npu_indexing_out(const at::Tensor& self,
     int64_t end_mask,
     int64_t ellipsis_mask,
     int64_t new_axis_mask,
-    int64_t shrink_axis_mask,
-    at::Tensor& result) {
-
+    int64_t shrink_axis_mask) {
   OpCommand cmd;
   cmd.Name("StridedSlice")
       .Input(self)
@@ -47,6 +48,39 @@ at::Tensor& NPUNativeFunctions::npu_indexing_out(const at::Tensor& self,
   return result;
 }
 
+at::Tensor& NPUNativeFunctions::npu_indexing_out(
+    const at::Tensor& self,
+    c10::IntArrayRef begin,
+    c10::IntArrayRef end,
+    c10::IntArrayRef strides,
+    int64_t begin_mask,
+    int64_t end_mask,
+    int64_t ellipsis_mask,
+    int64_t new_axis_mask,
+    int64_t shrink_axis_mask,
+    at::Tensor& result) {
+  if (self.is_complex() && self.scalar_type() != c10::ScalarType::ComplexDouble) {
+    c10::SmallVector<at::Tensor, N> self_splite = complex_compute_split(self);
+
+    at::Tensor self_left = self_splite[0].squeeze(-1);
+    at::Tensor self_right = self_splite[1].squeeze(-1);
+    at::Tensor left_result = OpPreparation::ApplyTensor(result, self_left.options());
+    at::Tensor right_result = OpPreparation::ApplyTensor(result, self_right.options());
+
+    npu_indexing_out_nocheck(left_result, self_left, begin, end, strides, begin_mask, end_mask, ellipsis_mask,
+        new_axis_mask, shrink_axis_mask);
+    npu_indexing_out_nocheck(right_result, self_right, begin, end, strides, begin_mask, end_mask, ellipsis_mask,
+        new_axis_mask, shrink_axis_mask);
+
+    auto result_coeffs = NPUNativeFunctions::stack({left_result, right_result}, -1);
+    result = at::native::view_as_complex(result_coeffs);
+  } else {
+    npu_indexing_out_nocheck(result, self, begin, end, strides, begin_mask, end_mask, ellipsis_mask,
+        new_axis_mask, shrink_axis_mask);
+  }
+  return result;
+}
+
 at::Tensor NPUNativeFunctions::npu_indexing(
     const at::Tensor& self,
     c10::IntArrayRef begin,
@@ -57,18 +91,16 @@ at::Tensor NPUNativeFunctions::npu_indexing(
     int64_t ellipsis_mask,
     int64_t new_axis_mask,
     int64_t shrink_axis_mask) {
-  // calculate the output size
-  c10::SmallVector<int64_t, SIZE> outputSize;
+  c10::SmallVector<int64_t, SIZE> output_size;
   for (int i = 0; i < self.dim(); i++) {
     TORCH_CHECK(strides[i] != 0, "stride should not be 0");
-    outputSize.emplace_back((end[i] + strides[i] - 1 - begin[i]) / strides[i]);
+    output_size.emplace_back((end[i] + strides[i] - 1 - begin[i]) / strides[i]);
   }
-  // construct the output tensor of the NPU
-  at::Tensor result = OpPreparation::ApplyTensor(self, outputSize);
-  npu_indexing_out(self, begin, end, strides,begin_mask, end_mask,
-                   ellipsis_mask, new_axis_mask, shrink_axis_mask, result);
+  at::Tensor result = OpPreparation::ApplyTensor(self, output_size);
+  npu_indexing_out(self, begin, end, strides, begin_mask, end_mask, ellipsis_mask, new_axis_mask,
+      shrink_axis_mask, result);
   return result;
 }
 
 } // namespace native
-} // namespace at
+} // namespace at_npu
