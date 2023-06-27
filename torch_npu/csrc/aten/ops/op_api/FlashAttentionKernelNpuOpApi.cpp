9023e1a8d5a6bd7528d498296f75877f4d45f88f
田宏民 <tianhongmin@huawei.com>
2023-06-27 08:16:17 +0000

    !4317 bit_mask and transpose * update flash attention score

--- torch_npu/csrc/aten/ops/op_api/FlashAttentionKernelNpuOpApi.cpp
+++ torch_npu/csrc/aten/ops/op_api/FlashAttentionKernelNpuOpApi.cpp
@@ -15,92 +15,198 @@
 // limitations under the License.
 
 #include <torch/csrc/autograd/custom_function.h>
+
+#include "torch_npu/csrc/core/npu/SecondaryStreamGuard.h"
+#include "torch_npu/csrc/core/npu/NPUCachingAllocator.h"
 #include "torch_npu/csrc/framework/utils/OpAdapter.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 #include "torch_npu/csrc/aten/ops/op_api/op_api_common.h"
+#include "torch_npu/csrc/aten/NPUGeneratorImpl.h"
 
 namespace at_npu {
 namespace native {
 using torch::autograd::AutogradContext;
 using torch::autograd::Function;
 
+at::Tensor format_trans(const at::Tensor &at_tensor) {
+    return at_tensor.defined() ? NPUNativeFunctions::npu_format_cast(at_tensor, ACL_FORMAT_ND) : at_tensor;
+}
+
+at::Tensor dropout_gen_mask_impl(const at::Tensor &self, const at::Scalar &prob, const at::Scalar &seed,
+    const int64_t &offset, const int64_t &length) {
+  at::Tensor mask;
+  auto original_stream = c10_npu::getCurrentNPUStream();
+  {
+    // During the life cycle of this raii instance, the calcu stream is set as the
+    // secondary stream, and tasks are distributed to the secondary stream. At the
+    // same time, according to the one-stream-one-pool principle, memory is also
+    // alloced from the pool of the secondary stream.
+    c10_npu::SecondaryStreamGuard guard(c10_npu::getCurrentSecondaryStream());
+    c10::TensorOptions options = self.options();
+    mask = OpPreparation::ApplyTensorWithFormat(at::IntArrayRef{length}, options.dtype(at::kByte),
+                                                ACL_FORMAT_ND);
+    at::SmallVector<int64_t, N> offsetList = {0, offset};
+    const int64_t seed1 = 0;
+    OpCommand cmd;
+    cmd.Name("StatelessDropOutGenMask")
+      .Input(at::IntArrayRef{length})
+      .Input(prob, self.scalar_type(), CompileType::MEMORY_HOST_COMPILE_DEPENDENT)
+      .Input(seed, at::ScalarType::Int)
+      .Input(at::Scalar(seed1), at::ScalarType::Int)
+      .Input(offsetList, at::kLong, CompileType::MEMORY_HOST_COMPILE_INDEPENDENT)
+      .Output(mask)
+      .Run();
+  }
+  // When tasks on multiple streams read and write the same block of memory,
+  // recordStream needs to be called to ensure the correctness of memory reuse.
+  c10_npu::NPUCachingAllocator::recordStream(mask.storage().data_ptr(), original_stream);
+  return mask;
+}
+
+at::Tensor dropout_gen_mask(const at::Tensor &self, double p, int64_t head_num,
+    int64_t &seed, int64_t &offset, int64_t &length) {
+  const auto gen = at_npu::detail::getDefaultNPUGenerator();
+  auto pair = at::check_generator<NPUGeneratorImpl>(gen)->philox_engine_inputs(10);
+  seed = pair.first;
+  offset = pair.second;
+  int64_t numels = self.size(0) * head_num * self.size(1) * self.size(1); // [B,N,S,S]
+  length = (numels + 128 - 1) / 128 * 16; // 先对齐到128，然后按Byte缩放8倍
+  length += 32;
+  at::Scalar prob = at::Scalar(1. - p);
+  return dropout_gen_mask_impl(self, prob, at::Scalar(seed), offset, length);
+}
+
+std::vector<at::Tensor> npu_flash_attention_backward(
+    const at::Tensor &query,
+    const at::Tensor &key,
+    const at::Tensor &value,
+    const at::Tensor &dy,
+    int64_t head_num,
+    const c10::optional<at::Tensor> &pse,
+    const c10::optional<at::Tensor> &drop_mask,
+    const c10::optional<at::Tensor> &padding_mask,
+    const c10::optional<at::Tensor> &atten_mask,
+    const c10::optional<at::Tensor> &softmax_max,
+    const c10::optional<at::Tensor> &softmax_sum,
+    const c10::optional<at::Tensor> &softmax_in,
+    const c10::optional<at::Tensor> &attention_in,
+    double scale_value,
+    double keep_prob,
+    int64_t pre_tockens,
+    int64_t next_tockens)
+{
+  bool is_flash = true;
+  const at::Tensor &pse_const = pse.value_or(at::Tensor());
+  const at::Tensor &drop_mask_const = drop_mask.value_or(at::Tensor());
+  const at::Tensor &padding_mask_const = padding_mask.value_or(at::Tensor());
+  const at::Tensor &atten_mask_const = atten_mask.value_or(at::Tensor());
+  const at::Tensor &softmax_max_const = softmax_max.value_or(at::Tensor());
+  const at::Tensor &softmax_sum_const = softmax_sum.value_or(at::Tensor());
+  const at::Tensor &softmax_const = softmax_in.value_or(at::Tensor());
+  const at::Tensor &attention_const = attention_in.value_or(at::Tensor());
+
+  at::Tensor format_query = NPUNativeFunctions::npu_format_cast(query, ACL_FORMAT_ND);
+  at::Tensor format_key = NPUNativeFunctions::npu_format_cast(key, ACL_FORMAT_ND);
+  at::Tensor format_value = NPUNativeFunctions::npu_format_cast(value, ACL_FORMAT_ND);
+  at::Tensor format_dy = NPUNativeFunctions::npu_format_cast(dy, ACL_FORMAT_ND);
+
+  at::Tensor format_pse = format_trans(pse_const);
+  at::Tensor format_drop_mask = format_trans(drop_mask_const);
+  at::Tensor format_padding_mask = format_trans(padding_mask_const);
+  at::Tensor format_atten_mask = format_trans(atten_mask_const);
+  at::Tensor format_softmax_max = format_trans(softmax_max_const);
+  at::Tensor format_softmax_sum = format_trans(softmax_sum_const);
+  at::Tensor format_softmax = format_trans(softmax_const);
+  at::Tensor format_attention = format_trans(attention_const);
+
+  at::Tensor dq = OpPreparation::ApplyTensor(format_query);
+  at::Tensor dk = OpPreparation::ApplyTensor(format_key);
+  at::Tensor dv = OpPreparation::ApplyTensor(format_value);
+
+  EXEC_NPU_CMD(
+      aclnnFlashAttentionScoreGrad, format_query, format_key, format_value, format_dy,
+      format_pse, format_drop_mask, format_padding_mask, format_atten_mask,
+      format_softmax_max, format_softmax_sum, format_softmax, format_attention, scale_value, keep_prob,
+      pre_tockens, next_tockens, is_flash, head_num, dq, dk, dv);
+
+  return {dq, dk, dv,
+          at::Tensor(), at::Tensor(), at::Tensor(), at::Tensor(),
+          at::Tensor(), at::Tensor(), at::Tensor(), at::Tensor()};
+}
+
 class NPUFlashAttentionFunction : public torch::autograd::Function<NPUFlashAttentionFunction> {
 public:
   static std::vector<at::Tensor> forward(
-      AutogradContext *ctx, const at::Tensor &query_layer, const at::Tensor &key_layer,
-      const at::Tensor &value_layer, const c10::optional<at::Tensor> &pse_opt,
-      const c10::optional<at::Tensor> &drop_mask_opt, const c10::optional<at::Tensor> &padding_mask_opt,
-      const c10::optional<at::Tensor> &atten_mask_opt, bool query_transpose, bool key_transpose, bool value_transpose,
-      double scale, double keep_prob, bool is_transpose_out, int64_t pre_tockens, int64_t next_tockens)
+      AutogradContext *ctx, const at::Tensor &query, const at::Tensor &key,
+      const at::Tensor &value, int64_t head_num, const c10::optional<at::Tensor> &pse_opt,
+      const c10::optional<at::Tensor> &padding_mask_opt, const c10::optional<at::Tensor> &atten_mask_opt,
+      double scale, double keep_prob, int64_t pre_tockens, int64_t next_tockens)
   {
     const at::Tensor &pse = pse_opt.value_or(at::Tensor());
     const at::Tensor &padding_mask = padding_mask_opt.value_or(at::Tensor());
     const at::Tensor &atten_mask = atten_mask_opt.value_or(at::Tensor());
-    const at::Tensor &drop_mask = drop_mask_opt.value_or(at::Tensor());
-    at::Tensor attention_score;
-    if (is_transpose_out) {
-      attention_score = OpPreparation::ApplyTensor(query_layer,
-          {query_layer.size(0), query_layer.size(2), query_layer.size(1) * query_layer.size(3)});
-    } else {
-      attention_score = OpPreparation::ApplyTensor(query_layer);
-    }
 
-    at::Tensor format_query = NPUNativeFunctions::npu_format_cast(query_layer, ACL_FORMAT_ND);
-    at::Tensor format_key = NPUNativeFunctions::npu_format_cast(key_layer, ACL_FORMAT_ND);
-    at::Tensor format_value = NPUNativeFunctions::npu_format_cast(value_layer, ACL_FORMAT_ND);
+    TORCH_CHECK(query.dim() == 3, "The shapes of the input query should be 3-dimensional, but got ", query.dim(), "-dimensional");
+    TORCH_CHECK(key.dim() == 3, "The shapes of the input key should be 3-dimensional, but got ", key.dim(), "-dimensional");
+    TORCH_CHECK(value.dim() == 3, "The shapes of the input value should be 3-dimensional, but got ", value.dim(), "-dimensional");
+    at::Tensor attention_score = OpPreparation::ApplyTensor(query);
 
-    at::Tensor format_pse = pse.defined() ? NPUNativeFunctions::npu_format_cast(pse, ACL_FORMAT_ND) : pse;
-    at::Tensor format_padding_mask =
-        padding_mask.defined() ? NPUNativeFunctions::npu_format_cast(padding_mask, ACL_FORMAT_ND) : padding_mask;
-    at::Tensor format_atten_mask =
-        atten_mask.defined() ? NPUNativeFunctions::npu_format_cast(atten_mask, ACL_FORMAT_ND) : atten_mask;
-    at::Tensor format_drop_mask =
-        drop_mask.defined() ? NPUNativeFunctions::npu_format_cast(drop_mask, ACL_FORMAT_ND) : drop_mask;
+    at::Tensor format_query = NPUNativeFunctions::npu_format_cast(query, ACL_FORMAT_ND);
+    at::Tensor format_key = NPUNativeFunctions::npu_format_cast(key, ACL_FORMAT_ND);
+    at::Tensor format_value = NPUNativeFunctions::npu_format_cast(value, ACL_FORMAT_ND);
 
-    size_t dim = (query_layer.scalar_type() == at::ScalarType::Float) ? 8 : 16;
-    at::Tensor softmax_max = OpPreparation::ApplyTensor(query_layer,
-        {query_layer.size(0), query_layer.size(1), query_layer.size(2), dim});
-    at::Tensor softmax_sum = OpPreparation::ApplyTensor(query_layer,
-        {query_layer.size(0), query_layer.size(1), query_layer.size(2), dim});
+    at::Tensor format_pse = format_trans(pse);
+    at::Tensor format_padding_mask = format_trans(padding_mask);
+    at::Tensor format_atten_mask = format_trans(atten_mask);
+
+    int64_t seed;
+    int64_t offset;
+    int64_t length;
+    at::Tensor format_drop_mask = dropout_gen_mask(format_query, keep_prob, head_num, seed, offset, length);
+
+    size_t dim = (query.scalar_type() == at::ScalarType::Float) ? 8 : 16;
+    at::Tensor softmax_max = OpPreparation::ApplyTensor(query,
+        {query.size(0), head_num, query.size(1), dim}); // [B, N, S, dim]
+    at::Tensor softmax_sum = OpPreparation::ApplyTensor(query,
+        {query.size(0), head_num, query.size(1), dim}); // [B, N, S, dim]
 
     bool is_flash = true;
     at::Tensor softmax_out;
 
-    EXEC_NPU_CMD_CLEAR_WORKSPACE(aclnnFlashAttentionScore, format_query, format_key, format_value,
+    EXEC_NPU_CMD(aclnnFlashAttentionScore, format_query, format_key, format_value,
         format_pse, format_drop_mask, format_padding_mask, format_atten_mask,
-        query_transpose, key_transpose, value_transpose,
-        scale, keep_prob, is_transpose_out, pre_tockens, next_tockens, is_flash,
+        scale, keep_prob, pre_tockens, next_tockens, head_num, is_flash,
         softmax_max, softmax_sum, softmax_out, attention_score);
 
     at::AutoNonVariableTypeMode g;
 
     ctx->save_for_backward({format_query, format_key, format_value, softmax_max, softmax_sum, softmax_out,
-                            format_pse, format_drop_mask, format_padding_mask, format_atten_mask, attention_score});
+                            format_pse, format_padding_mask, format_atten_mask, attention_score});
 
-    ctx->saved_data["is_flash"] = is_flash;
+    ctx->saved_data["head_num"] = head_num;
     ctx->saved_data["scale"] = scale;
     ctx->saved_data["keep_prob"] = keep_prob;
-    ctx->saved_data["query_transpose"] = query_transpose;
-    ctx->saved_data["key_transpose"] = key_transpose;
     ctx->saved_data["pre_tockens"] = pre_tockens;
     ctx->saved_data["next_tockens"] = next_tockens;
-    ctx->saved_data["value_transpose"] = value_transpose;
-    ctx->saved_data["is_transpose_out"] = is_transpose_out;
+    ctx->saved_data["is_flash"] = is_flash;
+    ctx->saved_data["length"] = length;
+    ctx->saved_data["seed"] = seed;
+    ctx->saved_data["offset"] = offset;
 
     return {attention_score};
   }
 
   static std::vector<at::Tensor> backward(AutogradContext *ctx, std::vector<at::Tensor> grad_outputs)
   {
+    auto head_num = ctx->saved_data["head_num"].toInt();
     auto scale = ctx->saved_data["scale"].toDouble();
     auto keep_prob = ctx->saved_data["keep_prob"].toDouble();
-    auto query_transpose = ctx->saved_data["query_transpose"].toBool();
-    auto key_transpose = ctx->saved_data["key_transpose"].toBool();
-    auto value_transpose = ctx->saved_data["value_transpose"].toBool();
-    auto is_transpose_out = ctx->saved_data["is_transpose_out"].toBool();
     auto pre_tockens = ctx->saved_data["pre_tockens"].toInt();
     auto next_tockens = ctx->saved_data["next_tockens"].toInt();
     auto is_flash = ctx->saved_data["is_flash"].toBool();
+    auto length = ctx->saved_data["length"].toInt();
+    auto offset = ctx->saved_data["offset"].toInt();
+    auto seed = ctx->saved_data["seed"].toInt();
     auto saved = ctx->get_saved_variables();
 
     auto query = saved[0];
@@ -110,30 +216,62 @@ public:
     auto softmax_sum = saved[4];
     auto softmax_out = saved[5];
     auto pse = saved[6];
-    auto drop_mask = saved[7];
-    auto padding_mask = saved[8];
-    auto atten_mask = saved[9];
-    auto attention_score = saved[10];
+    auto padding_mask = saved[7];
+    auto atten_mask = saved[8];
+    auto attention_score = saved[9];
 
-    bool dy_transpose = false;
-    return NPUNativeFunctions::npu_flash_attention_grad(query,
-        key, value, grad_outputs[0], pse, drop_mask, padding_mask, atten_mask,
+    at::Scalar prob = at::Scalar(1. - keep_prob);
+    at::Tensor drop_mask = dropout_gen_mask_impl(query, prob, at::Scalar(seed), offset, length);
+
+    return npu_flash_attention_backward(query,
+        key, value, grad_outputs[0], head_num, pse, drop_mask, padding_mask, atten_mask,
         softmax_max, softmax_sum, softmax_out, attention_score, scale,
-        keep_prob, query_transpose, key_transpose, value_transpose,
-        dy_transpose, is_transpose_out, pre_tockens, next_tockens);
+        keep_prob, pre_tockens, next_tockens);
   }
 };
 
-std::vector<at::Tensor> NPUNativeFunctions::npu_flash_attention(
-    const at::Tensor &query_layer, const at::Tensor &key_layer,
-    const at::Tensor &value_layer, const c10::optional<at::Tensor> &pse,
-    const c10::optional<at::Tensor> &drop_mask, const c10::optional<at::Tensor> &padding_mask,
-    const c10::optional<at::Tensor> &atten_mask, bool query_transpose, bool key_transpose, bool value_transpose,
-    double scale, double keep_prob, bool is_transpose_out, int64_t pre_tockens, int64_t next_tockens)
+std::vector<at::Tensor> NPUNativeFunctions::npu_flash_attention_grad(
+    const at::Tensor &query,
+    const at::Tensor &key,
+    const at::Tensor &value,
+    const at::Tensor &dy,
+    int64_t head_num,
+    const c10::optional<at::Tensor> &pse,
+    const c10::optional<at::Tensor> &padding_mask,
+    const c10::optional<at::Tensor> &atten_mask,
+    const c10::optional<at::Tensor> &softmax_max,
+    const c10::optional<at::Tensor> &softmax_sum,
+    const c10::optional<at::Tensor> &softmax_in,
+    const c10::optional<at::Tensor> &attention_in,
+    double scale_value,
+    double keep_prob,
+    int64_t pre_tockens,
+    int64_t next_tockens)
 {
-  return NPUFlashAttentionFunction::apply(query_layer, key_layer, value_layer, pse, drop_mask, padding_mask,
-      atten_mask, query_transpose, key_transpose, value_transpose, scale, keep_prob,
-      is_transpose_out, pre_tockens, next_tockens);
+  TORCH_CHECK(query.dim() == 3, "The shapes of the input query should be 3-dimensional, but got ", query.dim(), "-dimensional");
+  TORCH_CHECK(key.dim() == 3, "The shapes of the input key should be 3-dimensional, but got ", key.dim(), "-dimensional");
+  TORCH_CHECK(value.dim() == 3, "The shapes of the input value should be 3-dimensional, but got ", value.dim(), "-dimensional");
+  TORCH_CHECK(dy.dim() == 3, "The shapes of the input dy should be 3-dimensional, but got ", dy.dim(), "-dimensional");
+  int64_t seed;
+  int64_t offset;
+  int64_t length;
+  at::Tensor drop_mask = dropout_gen_mask(query, keep_prob, head_num, seed, offset, length);
+
+  return npu_flash_attention_backward(query,
+      key, value, dy, head_num, pse, drop_mask, padding_mask, atten_mask,
+      softmax_max, softmax_sum, softmax_in, attention_in, scale_value,
+      keep_prob, pre_tockens, next_tockens);
+}
+
+std::vector<at::Tensor> NPUNativeFunctions::npu_flash_attention(
+    const at::Tensor &query, const at::Tensor &key,
+    const at::Tensor &value, int64_t head_num,
+    const c10::optional<at::Tensor> &pse, const c10::optional<at::Tensor> &padding_mask,
+    const c10::optional<at::Tensor> &atten_mask,
+    double scale, double keep_prob, int64_t pre_tockens, int64_t next_tockens)
+{
+  return NPUFlashAttentionFunction::apply(query, key, value, head_num, pse, padding_mask,
+      atten_mask, scale, keep_prob, pre_tockens, next_tockens);
 }
 } // namespace native
 } // namespace at_npu
