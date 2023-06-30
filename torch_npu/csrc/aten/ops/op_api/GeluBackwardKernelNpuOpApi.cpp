9ed8a922311cff64f2ba0ddcbd69b8e856b47de9
w00565603 <wangjiacheng13@huawei.com>
2023-06-30 14:38:35 +0800

    bug fix gelu_backward add promote & broadcast

--- torch_npu/csrc/aten/ops/op_api/GeluBackwardKernelNpuOpApi.cpp
+++ torch_npu/csrc/aten/ops/op_api/GeluBackwardKernelNpuOpApi.cpp
@@ -15,13 +15,20 @@
 #include "torch_npu/csrc/aten/NPUNativeOpApiFunctions.h"
 #include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 #include "torch_npu/csrc/aten/ops/op_api/op_api_common.h"
+#include "torch_npu/csrc/framework/utils/OpAdapter.h"
 
 namespace at_npu {
 namespace native {
 
 at::Tensor NPUNativeOpApiFunctions::gelu_backward(const at::Tensor& grad, const at::Tensor& self) {
   DO_COMPATIBILITY(aclnnGeluBackward, NPUNativeFunctions::gelu_backward(grad, self));
-  at::Tensor grad_input = OpPreparation::ApplyTensor(self);
+  // calculate the output size
+  auto output_size = broadcast_ops_npu_output_size(grad, self);
+  // dtype promotion
+  auto output_dtype = at::native::result_type(grad, self);
+  // construct the output tensor of the NPU
+  at::Tensor grad_input = OpPreparation::ApplyTensor(output_size, self.options().dtype(output_dtype), self);
+  // dispatch hostAPI
   EXEC_NPU_CMD(aclnnGeluBackward, grad, self, grad_input);
   return grad_input;
 }
