012bfa7c094a5731768a36dac1eb3e538614d61b
pipihugh <jifang5@h-partners.com>
2023-07-07 21:06:23 +0800

    Change unique_consecutive's output[1] to static output.

--- torch_npu/csrc/aten/ops/UniqueConsecutiveKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/UniqueConsecutiveKernelNpu.cpp
@@ -27,16 +27,16 @@ std::tuple<at::Tensor&, at::Tensor&, at::Tensor&> unique_consecutive_out_npu(
     const bool return_inverse, 
     const bool return_counts, 
     c10::optional<int64_t> dim) {
-  at::Tensor selfCopy = self;
+  at::Tensor self_copy = self;
   if (self.scalar_type() == at::ScalarType::Half) {
-    selfCopy = NPUNativeFunctions::npu_dtype_cast(self, at::ScalarType::Float);
+    self_copy = NPUNativeFunctions::npu_dtype_cast(self, at::ScalarType::Float);
     output = NPUNativeFunctions::npu_dtype_cast(output, at::ScalarType::Float);
   }
-  c10::SmallVector<int64_t, N> output_sync_idx = {0, 1, 2};
+  c10::SmallVector<int64_t, N> output_sync_idx = {0, 2};
   OpCommand cmd;
   cmd.Sync(output_sync_idx)
      .Name("UniqueConsecutive")
-     .Input(selfCopy)
+     .Input(self_copy)
      .Output(output)
      .Output(inverse_indices)
      .Output(counts)
