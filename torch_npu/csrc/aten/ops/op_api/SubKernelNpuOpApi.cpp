9e608d08a8f02457294e1f46719ec05c77865c14
Veronica Lam <lamveronica@huawei.com>
2023-07-14 09:44:09 +0800

    [Host API] Add PTA for InplaceSub, InplaceSubs

--- torch_npu/csrc/aten/ops/op_api/SubKernelNpuOpApi.cpp
+++ torch_npu/csrc/aten/ops/op_api/SubKernelNpuOpApi.cpp
@@ -22,30 +22,47 @@
 namespace at_npu {
 namespace native {
 
-at::Tensor &sub_out_npu_nocheck(
+inline void alpha_check_npu(const at::ScalarType dtype, at::Scalar alpha) {
+  TORCH_CHECK(isFloatingType(dtype) || alpha.isIntegral(true),
+              "For integral input tensors, argument alpha must not be a floating point number.");
+}
+
+static at::Tensor &sub_out_npu_nocheck(
     const at::Tensor &self,
     const at::Tensor &other,
     const at::Scalar alpha,
     at::Tensor &result) {
-  // executing the NPU operator
-  if (other.dim() == 0 && !at_npu::key::isDeviceTensor(other)) {
-    c10::Scalar others = at_npu::native::CalcuOpUtil::ConvertTensorToScalar(other);
-    EXEC_NPU_CMD(aclnnSubs, self, others, alpha, result);
+  if (OpPreparation::IsCPUScalar(other)) {
+    c10::Scalar other_scalar = at_npu::native::CalcuOpUtil::ConvertTensorToScalar(other);
+    EXEC_NPU_CMD(aclnnSubs, self, other_scalar, alpha, result);
   } else {
     EXEC_NPU_CMD(aclnnSub, self, other, alpha, result);
   }
   return result;
 }
 
-static at::Tensor self_tensor_to_device(const at::Tensor &tensor, const at::ScalarType resultType) {
+static at::Tensor& inplace_sub_out_npu_no_check(
+    at::Tensor& self,
+    const at::Tensor& other,
+    const at::Scalar& alpha) {
+  if (OpPreparation::IsCPUScalar(other)) {
+    c10::Scalar other_scalar = at_npu::native::CalcuOpUtil::ConvertTensorToScalar(other);
+    EXEC_NPU_CMD(aclnnInplaceSubs, self, other_scalar, alpha);
+  } else {
+    EXEC_NPU_CMD(aclnnInplaceSub, self, other, alpha);
+  }
+  return self;
+}
+
+static at::Tensor self_tensor_to_device(const at::Tensor &tensor, const at::ScalarType result_type) {
   if (at_npu::native::CalcuOpUtil::IsScalarWrappedToTensor(tensor)) {
     at::Scalar scalar = CalcuOpUtil::ConvertTensorToScalar(tensor);
-    return CalcuOpUtil::CopyScalarToDevice(scalar, resultType);
+    return CalcuOpUtil::CopyScalarToDevice(scalar, result_type);
   }
   return tensor;
 }
 
-at::Tensor sub_dest_output(const at::Tensor& self, const at::Tensor& other) {
+static at::Tensor sub_dest_output(const at::Tensor& self, const at::Tensor& other) {
   bool is_self_wrapped = CalcuOpUtil::IsScalarWrappedToTensor(self);
   return is_self_wrapped ? other : self;
 }
@@ -57,36 +74,44 @@ at::Tensor &NPUNativeOpApiFunctions::sub_out(
     at::Tensor &result) {
   DO_COMPATIBILITY(aclnnSub, NPUNativeFunctions::sub_out(self, other, alpha, result));
   DO_COMPATIBILITY(aclnnSubs, NPUNativeFunctions::sub_out(self, other, alpha, result));
-  at::Tensor output_tensor = sub_dest_output(self, other);
+  alpha_check_npu(self.scalar_type(), alpha);
   auto output_size = broadcast_ops_npu_output_size(self, other);
   at::ScalarType result_type = at::native::result_type(self, other);
   at::Tensor self_converted = self_tensor_to_device(self, result_type);
+
   OpPreparation::CheckOut(
       {self},
       result,
-      CalcuOpUtil::GetTensorNpuFormat(output_tensor),
-      result.scalar_type(),
+      result,
       output_size);
 
+  CalcuOpUtil::CheckMemoryOverLaps({self, other}, {result});
   sub_out_npu_nocheck(self_converted, other, alpha, result);
   return result;
 }
 
-at::Tensor NPUNativeOpApiFunctions::sub(const at::Tensor &self, const at::Tensor &other, const at::Scalar &alpha) {
+at::Tensor NPUNativeOpApiFunctions::sub(
+    const at::Tensor &self,
+    const at::Tensor &other,
+    const at::Scalar &alpha) {
   DO_COMPATIBILITY(aclnnSub, NPUNativeFunctions::sub(self, other, alpha));
   DO_COMPATIBILITY(aclnnSubs, NPUNativeFunctions::sub(self, other, alpha));
+  alpha_check_npu(self.scalar_type(), alpha);
   at::Tensor output_tensor = sub_dest_output(self, other);
   auto output_size = broadcast_ops_npu_output_size(self, other);
   at::ScalarType result_type = at::native::result_type(self, other);
   at::Tensor self_converted = self_tensor_to_device(self, result_type);
-
   auto result = OpPreparation::ApplyTensorWithoutFormat(output_size, output_tensor.options().dtype(result_type));
   sub_out_npu_nocheck(self_converted, other, alpha, result);
   return result;
 }
 
-at::Tensor NPUNativeOpApiFunctions::sub(const at::Tensor &self, const at::Scalar &other, const at::Scalar &alpha) {
+at::Tensor NPUNativeOpApiFunctions::sub(
+    const at::Tensor &self,
+    const at::Scalar &other,
+    const at::Scalar &alpha) {
   DO_COMPATIBILITY(aclnnSubs, NPUNativeFunctions::sub(self, other, alpha));
+  alpha_check_npu(self.scalar_type(), alpha);
   auto output_size = input_same_output_size(self);
   at::ScalarType result_type = at::native::result_type(self, other);
   auto result = OpPreparation::ApplyTensorWithoutFormat(output_size, self.options().dtype(result_type));
@@ -94,5 +119,29 @@ at::Tensor NPUNativeOpApiFunctions::sub(const at::Tensor &self, const at::Scalar
   return result;
 }
 
+at::Tensor &NPUNativeOpApiFunctions::sub_(
+    at::Tensor &self,
+    const at::Tensor &other,
+    const at::Scalar &alpha) {
+  DO_COMPATIBILITY(aclnnInplaceSub, NPUNativeFunctions::sub_(self, other, alpha));
+  DO_COMPATIBILITY(aclnnInplaceSubs, NPUNativeFunctions::sub_(self, other, alpha));
+  alpha_check_npu(self.scalar_type(), alpha);
+  c10::SmallVector<at::Tensor, N> inputs = {self, other};
+  c10::SmallVector<at::Tensor, N> outputs = {self};
+  CalcuOpUtil::CheckMemoryOverLaps(inputs, outputs);
+  inplace_sub_out_npu_no_check(self, other, alpha);
+  return self;
+}
+
+at::Tensor &NPUNativeOpApiFunctions::sub_(
+    at::Tensor &self,
+    const at::Scalar &other,
+    const at::Scalar &alpha) {
+  DO_COMPATIBILITY(aclnnInplaceSubs, NPUNativeFunctions::sub_(self, other, alpha));
+  alpha_check_npu(self.scalar_type(), alpha);
+  EXEC_NPU_CMD(aclnnInplaceSubs, self, other, alpha);
+  return self;
+}
+  
 } // namespace native
 } // namespace at_npu
