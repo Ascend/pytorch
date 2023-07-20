98c7e4aa9388d000f5c1c58744ee149671c3bfe0
w00809619 <wanjunwei4@huawei.com>
2023-07-20 16:47:55 +0800

    [feat]add torch_npu api, support current_blas_handle and can_device_access_peer.

--- test/test_npu/test_torch_npu.py
+++ test/test_npu/test_torch_npu.py
@@ -18,19 +18,12 @@ import collections
 import torch
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import skipIfUnsupportMultiNPU
 
-class TorchNPUApiTestCase(TestCase):
+class TorchNPUDeviceApiTestCase(TestCase):
     def test_npu_current_device(self):
         res = torch_npu.npu.current_device()
         self.assertIsInstance(res, int)
-
-    def test_npu_current_stream(self):
-        res = torch_npu.npu.current_stream()
-        self.assertIsInstance(res, torch_npu.npu.streams.Stream)
-    
-    def test_npu_default_stream(self):
-        res = torch_npu.npu.default_stream()
-        self.assertIsInstance(res, torch_npu.npu.streams.Stream)
 
     def test_npu_device(self):
         res = torch_npu.npu.device("npu:0")
@@ -40,10 +33,105 @@ class TorchNPUApiTestCase(TestCase):
         res = torch_npu.npu.device_count()
         self.assertIsInstance(res, int)
 
-    def test_npu_device_of(self):
-        x = torch.Tensor([1,2,3]).to("npu")
-        res = torch_npu.npu.device_of(x)
-        self.assertIsInstance(res, torch_npu.npu.device_of)
+    @skipIfUnsupportMultiNPU(2)
+    def test_npu_can_device_access_peer_multi_npu(self):
+        res = torch_npu.npu.can_device_access_peer(0, 1)
+        self.assertEqual(res, True)
+
+    def test_npu_can_device_access_peer(self):
+        res = torch_npu.npu.can_device_access_peer(0, 0)
+        self.assertEqual(res, False)
+        with self.assertRaisesRegex(AssertionError, "Invalid devide id"):
+            torch_npu.npu.can_device_access_peer(-1, 0)
+        with self.assertRaisesRegex(AssertionError, "Invalid peer devide id"):
+            torch_npu.npu.can_device_access_peer(0, -1)
+
+    def test_npu_get_device_name(self):
+        res = torch_npu.npu.get_device_name(0)
+        self.assertIsInstance(res, str)
+        res = torch_npu.npu.get_device_name()
+        self.assertIsInstance(res, str)
+        res = torch_npu.npu.get_device_name("npu:0")
+        self.assertIsInstance(res, str)
+        device = torch.device("npu:0")
+        res = torch_npu.npu.get_device_name(device)
+        self.assertIsInstance(res, str)
+
+    def test_npu_get_device_properties(self):
+        name = torch_npu.npu.get_device_properties(0).name
+        self.assertIsInstance(name, str)
+        total_memory = torch_npu.npu.get_device_properties(0).total_memory
+        self.assertIsInstance(total_memory, int)
+
+    def test_npu_get_device_capability(self):
+        res = torch_npu.npu.get_device_capability()
+        self.assertEqual(res, None)
+        res = torch_npu.npu.get_device_capability(0)
+        self.assertEqual(res, None)
+        name = torch_npu.npu.get_device_properties(0).name
+        res = torch_npu.npu.get_device_capability(name)
+        self.assertEqual(res, None)
+        device = torch_npu.npu.device("npu")
+        res = torch_npu.npu.get_device_capability(device)
+        self.assertEqual(res, None)
+
+class TorchNPUMemoryApiTestCase(TestCase):
+    def test_npu_memory_stats(self):
+        res = torch_npu.npu.memory_stats()
+        self.assertIsInstance(res, collections.OrderedDict)
+
+    def test_npu_memory_summary(self):
+        res = torch_npu.npu.memory_summary()
+        self.assertIsInstance(res, str)
+
+    def test_npu_memory_snapshot(self):
+        res = torch_npu.npu.memory_snapshot()
+        self.assertIsInstance(res, list)
+
+    def test_npu_memory_allocated(self):
+        res = torch_npu.npu.memory_allocated()
+        self.assertIsInstance(res, int)
+
+    def test_npu_max_memory_allocated(self):
+        res = torch_npu.npu.max_memory_allocated()
+        self.assertIsInstance(res, int)
+
+    def test_npu_reset_max_memory_allocated(self):
+        res = torch_npu.npu.reset_max_memory_allocated()
+        self.assertIsNone(res)
+
+    def test_npu_memory_reserved(self):
+        res = torch_npu.npu.memory_reserved()
+        self.assertIsInstance(res, int)
+
+    def test_npu_max_memory_reserved(self):
+        res = torch_npu.npu.max_memory_reserved()
+        self.assertIsInstance(res, int)
+
+    def test_npu_memory_cached(self):
+        res = torch_npu.npu.memory_cached()
+        self.assertIsInstance(res, int)
+
+    def test_npu_max_memory_cached(self):
+        res = torch_npu.npu.max_memory_cached()
+        self.assertIsInstance(res, int)
+
+    def test_npu_reset_max_memory_cached(self):
+        res = torch_npu.npu.reset_max_memory_cached()
+        self.assertIsNone(res)
+
+class TorchNPUApiTestCase(TestCase):
+    def test_npu_current_stream(self):
+        res = torch_npu.npu.current_stream()
+        self.assertIsInstance(res, torch_npu.npu.streams.Stream)
+
+    def test_npu_default_stream(self):
+        res = torch_npu.npu.default_stream()
+        self.assertIsInstance(res, torch_npu.npu.streams.Stream)
+
+    def test_npu_current_blas_handle(self):
+        res = torch_npu.npu.current_blas_handle()
+        self.assertIsNone(res)
 
     def test_npu_init(self):
         res = torch_npu.npu.init()
@@ -121,79 +209,6 @@ class TorchNPUApiTestCase(TestCase):
     def test_npu_empty_cache(self):
         res = torch_npu.npu.empty_cache()
         self.assertIsNone(res)
-
-    def test_npu_memory_stats(self):
-        res = torch_npu.npu.memory_stats()
-        self.assertIsInstance(res, collections.OrderedDict)
-
-    def test_npu_memory_summary(self):
-        res = torch_npu.npu.memory_summary()
-        self.assertIsInstance(res, str)
-
-    def test_npu_memory_snapshot(self):
-        res = torch_npu.npu.memory_snapshot()
-        self.assertIsInstance(res, list)
-
-    def test_npu_memory_allocated(self):
-        res = torch_npu.npu.memory_allocated()
-        self.assertIsInstance(res, int)
-
-    def test_npu_max_memory_allocated(self):
-        res = torch_npu.npu.max_memory_allocated()
-        self.assertIsInstance(res, int)
-
-    def test_npu_reset_max_memory_allocated(self):
-        res = torch_npu.npu.reset_max_memory_allocated()
-        self.assertIsNone(res)
-
-    def test_npu_memory_reserved(self):
-        res = torch_npu.npu.memory_reserved()
-        self.assertIsInstance(res, int)
-
-    def test_npu_max_memory_reserved(self):
-        res = torch_npu.npu.max_memory_reserved()
-        self.assertIsInstance(res, int)
-
-    def test_npu_memory_cached(self):
-        res = torch_npu.npu.memory_cached()
-        self.assertIsInstance(res, int)
-
-    def test_npu_max_memory_cached(self):
-        res = torch_npu.npu.max_memory_cached()
-        self.assertIsInstance(res, int)
-
-    def test_npu_reset_max_memory_cached(self):
-        res = torch_npu.npu.reset_max_memory_cached()
-        self.assertIsNone(res)
-
-    def test_npu_get_device_name(self):
-        res = torch_npu.npu.get_device_name(0)
-        self.assertIsInstance(res, str)
-        res = torch_npu.npu.get_device_name()
-        self.assertIsInstance(res, str)
-        res = torch_npu.npu.get_device_name("npu:0")
-        self.assertIsInstance(res, str)
-        device = torch.device("npu:0")
-        res = torch_npu.npu.get_device_name(device)
-        self.assertIsInstance(res, str)
-
-    def test_npu_get_device_properties(self):
-        name = torch_npu.npu.get_device_properties(0).name
-        self.assertIsInstance(name, str)
-        total_memory = torch_npu.npu.get_device_properties(0).total_memory
-        self.assertIsInstance(total_memory, int)
-
-    def test_npu_get_device_capability(self):
-        res = torch_npu.npu.get_device_capability()
-        self.assertEqual(res, None)
-        res = torch_npu.npu.get_device_capability(0)
-        self.assertEqual(res, None)
-        name = torch_npu.npu.get_device_properties(0).name
-        res = torch_npu.npu.get_device_capability(name)
-        self.assertEqual(res, None)
-        device = torch_npu.npu.device("npu")
-        res = torch_npu.npu.get_device_capability(device)
-        self.assertEqual(res, None)
 
     def test_npu_get_aclnn_version(self):
         res = torch_npu.npu.aclnn.version()
