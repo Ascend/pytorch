d96bc0232e5b7519fc863b0705974f16d56d4f9c
13739288457 <guanlongfeng1@huawei.com>
2023-07-26 15:58:46 +0800

    add sdp and hf32 interface

--- torch_npu/npu/backends.py
+++ torch_npu/npu/backends.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2023 Huawei Technologies Co., Ltd
-# All rights reserved.
+# Copyright (c) 2023, Huawei Technologies.All rights reserved.
 #
 # Licensed under the BSD 3-Clause License  (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,4 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .backends import version, allow_hf32
+import contextlib
+import torch
+import torch_npu._C
+import warnings
+
+from typing import Union
+
+
+def preferred_linalg_library(backend: Union[None, str, torch._C._LinalgBackend] = None) -> torch._C._LinalgBackend:
+    """Currently, the linalg lib is not available and does not support it.
+    By default, it returns Default type.
+    """
+    warnings.warn("torch.npu.preferred_linalg_library isn't implemented!")
+    return torch._C._LinalgBackend.Default
