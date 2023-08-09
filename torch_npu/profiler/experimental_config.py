99f0a7418d0f8089f04150a814449dcda8971f0f
裘凯达 <kdaqiu@163.com>
2023-08-09 15:27:56 +0800

    Add record_op_args in ExperimentalConfig.

--- torch_npu/profiler/experimental_config.py
+++ torch_npu/profiler/experimental_config.py
@@ -33,17 +33,20 @@ class _ExperimentalConfig:
     def __init__(self,
                  profiler_level: int = Constant.LEVEL0,
                  aic_metrics: int = Constant.AicMetricsNone,
-                 l2_cache: bool = False):
+                 l2_cache: bool = False,
+                 record_op_args: bool = False):
         self._profiler_level = profiler_level
         self._aic_metrics = aic_metrics
         if self._profiler_level != Constant.LEVEL0 and self._aic_metrics == Constant.AicMetricsNone:
             self._aic_metrics = Constant.AicPipeUtilization
         self._l2_cache = l2_cache
+        self.record_op_args = record_op_args
 
     def __call__(self) -> torch_npu._C._profiler._ExperimentalConfig:
         return torch_npu._C._profiler._ExperimentalConfig(trace_level=self._profiler_level,
                                                           metrics=self._aic_metrics,
-                                                          l2_cache=self._l2_cache)
+                                                          l2_cache=self._l2_cache,
+                                                          record_op_args=self.record_op_args)
 
     def profiler_level(self):
         return self._profiler_level
