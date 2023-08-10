f9c842ff4d12ea44cadf660f4a04aa09596552d6
zhang-xianglong-3 <zhangxianglong3@huawei.com>
2023-08-10 17:20:07 +0800

    change pta for _unique2

--- torch_npu/csrc/aten/ops/op_api/Unique2KernelNpuOPApi.cpp
+++ torch_npu/csrc/aten/ops/op_api/Unique2KernelNpuOPApi.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2023 Huawei Technologies Co., Ltd
-// Copyright (c) 2019, Facebook CORPORATION.
+// Copyright (c) 2023, Facebook CORPORATION.
 // All rights reserved.
 //
 // Licensed under the BSD 3-Clause License  (the "License");
@@ -14,25 +14,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "torch_npu/csrc/framework/utils/OpAdapter.h"
-#include "torch_npu/csrc/aten/NPUNativeFunctions.h"
 #include "torch_npu/csrc/aten/NPUNativeOpApiFunctions.h"
 #include "torch_npu/csrc/aten/ops/op_api/op_api_common.h"
+#include "torch_npu/csrc/framework/utils/KernelNpuOutputSize.h"
 
 namespace at_npu {
 namespace native {
 
-tuple<at::Tensor, at::Tensor, at::Tensor> NPUNativeOpApiFunctions::_unique2(
-    const at::Tensor& selfOp,
+std::tuple<at::Tensor, at::Tensor, at::Tensor> NPUNativeOpApiFunctions::_unique2(
+    const at::Tensor& self,
     bool sorted,
     bool return_inverse,
     bool return_counts) {
 
-  DO_COMPATIBILITY(aclnnUnique2, NPUNativeFunctions::_unique2(selfOp, sorted, return_inverse, return_counts));
+  DO_COMPATIBILITY(aclnnUnique2, NPUNativeFunctions::_unique2(self, sorted, return_inverse, return_counts));
   
-  at::Tensor y = OpPreparation::ApplyTensorWithoutFormat(selfOp, selfOp.numel());
-  at::Tensor yInverse = OpPreparation::ApplyTensorWithoutFormat(selfOp.sizes(), selfOp.options().dtype(at::kLong));
-  at::Tensor yCounts = OpPreparation::ApplyTensorWithoutFormat(selfOp.numel(), selfOp.options().dtype(at::kLong));
+  at::Tensor y = OpPreparation::ApplyTensorWithoutFormat(self, self.numel());
+  at::Tensor y_inverse = (return_inverse || return_counts) 
+                            ? OpPreparation::ApplyTensorWithoutFormat(self.sizes(), self.options().dtype(at::kLong)) 
+                            : OpPreparation::ApplyTensorWithoutFormat({0}, self.options().dtype(at::kLong));
+  at::Tensor y_counts = return_counts ? OpPreparation::ApplyTensorWithoutFormat(self.numel(), self.options().dtype(at::kLong)) 
+                                      : OpPreparation::ApplyTensorWithoutFormat({0}, self.options().dtype(at::kLong));
   
   static auto opApiFuncAddr = [](){
     auto ret = GetOpApiFuncAddr("aclGetViewShape");
@@ -42,19 +44,23 @@ tuple<at::Tensor, at::Tensor, at::Tensor> NPUNativeOpApiFunctions::_unique2(
   using aclGetViewShapeFunc = int (*)(const aclTensor* tensor, int64_t** view_dims, uint64_t* view_dims_num);
   auto aclGetViewShape = reinterpret_cast<aclGetViewShapeFunc>(opApiFuncAddr);
 
-  auto npuAclParams = EXEC_NPU_CMD_SYNC(aclnnUnique2, selfOp, sorted, return_inverse, return_counts, y, yInverse, yCounts);
+  auto npuAclParams = EXEC_NPU_CMD_SYNC(aclnnUnique2, self, sorted, return_inverse, return_counts, y, y_inverse, y_counts);
+
   int64_t* view_dims = nullptr;
   uint64_t view_dim_num = 0;
-  auto ret = aclGetViewShape(npuAclParams.Get<4>(), &view_dims, &view_dim_num);
+  constexpr int64_t Y_IDX = 4;
+  auto ret = aclGetViewShape(npuAclParams.Get<Y_IDX>(), &view_dims, &view_dim_num);
   TORCH_CHECK(ret == 0, "aclGetViewShape failed.");
   c10::SmallVector<int64_t, SIZE> output_size(view_dims, view_dims + view_dim_num);
 
   y.resize_(output_size);
-  yCounts.resize_(output_size);
+  if (return_counts) {
+    y_counts.resize_(output_size);
+  }
   delete view_dims;
   view_dims = nullptr;
 
-  return std::tuple<at::Tensor, at::Tensor, at::Tensor>(y, yInverse, yCounts);
+  return std::tuple<at::Tensor, at::Tensor, at::Tensor>(y, y_inverse, y_counts);
 }
 
 } // namespace native
