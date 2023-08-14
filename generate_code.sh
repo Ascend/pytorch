fd6f1befb263cf96a5ecd586ef254068eebe017c
chuboning <chuboning@foxmail.com>
2023-08-14 11:25:15 +0800

    Update python support to 3.10

--- generate_code.sh
+++ generate_code.sh
@@ -20,21 +20,22 @@ CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
 cd $CDIR
 
 build_libtorch="$1"
+python_execute="$2"
 
-python3 -m codegen.gen_backend_stubs  \
+${python_execute} -m codegen.gen_backend_stubs  \
   --output_dir="$CDIR/torch_npu/csrc/aten/" \
   --source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
   --impl_path="$CDIR/torch_npu/csrc/aten"  # Used to double-check the yaml file definitions.
 
   
-python3 -m codegen.autograd.gen_autograd \
+${python_execute} -m codegen.autograd.gen_autograd \
   --native_functions_dir="$CDIR/codegen/native_functions.yaml" \
   --out_dir="$CDIR/torch_npu/csrc/aten/" \
   --autograd_dir="$CDIR/codegen/autograd/" \
   --npu_native_function_dir="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml"
 
 if [[ ${build_libtorch} != "True" ]]; then
-  python3 -m codegen.gen_python_functions  \
+  ${python_execute} -m codegen.gen_python_functions  \
     --output_dir="$CDIR/torch_npu/csrc/aten/" \
     --source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
     --native_yaml="$CDIR/codegen/native_functions.yaml" \
