ede936d01dfee90d7dc5a1db564bef379c1396a4
shaoyf <shaoyifan1@huawei.com>
2023-08-17 03:56:45 +0000

    Use op_plugin yaml to generate code

--- generate_code.sh
+++ generate_code.sh
@@ -17,14 +17,22 @@ if [ -f "${file}" ]; then
   bash ${file} ${pytorch_version} ${python_execute}
 fi
 
+op_plugin_config_path=$CDIR/third_party/op-plugin/op_plugin/config/$pytorch_dir
+source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml"
+
+op_plugin_functions_yaml_path="$op_plugin_config_path/npu_native_functions.yaml"
+if [ -f "${op_plugin_functions_yaml_path}" ]; then
+  cp -f $op_plugin_functions_yaml_path $source_yaml
+fi
+
 ${python_execute} -m codegen.gen_backend_stubs  \
   --output_dir="torch_npu/csrc/aten" \
-  --source_yaml="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml" \
+  --source_yaml="$source_yaml" \
   --impl_path="$CDIR/torch_npu/csrc/aten" \
   --op_plugin_impl_path="$CDIR/third_party/op-plugin/op_plugin/ops" \
-  --op_plugin_yaml_path="$CDIR/third_party/op-plugin/op_plugin/config/$pytorch_dir/op_plugin_functions.yaml"
+  --op_plugin_yaml_path="$op_plugin_config_path/op_plugin_functions.yaml"
 
 ${python_execute} -m codegen.autograd.gen_autograd \
   --out_dir="$CDIR/torch_npu/csrc/aten" \
   --autograd_dir="$CDIR/codegen/autograd" \
-  --npu_native_function_dir="$CDIR/torch_npu/csrc/aten/npu_native_functions.yaml"
+  --npu_native_function_dir="$source_yaml"
