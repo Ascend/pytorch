3d419ddbe6c6c58c213e5d5e75123d4edacc2990
闫鹏全 <yanpengquan@huawei.com>
2023-08-28 11:20:22 +0800

    Fix torch.load with different income parameters

--- torch_npu/utils/serialization.py
+++ torch_npu/utils/serialization.py
@@ -5,7 +5,8 @@ from typing import Any, Optional
 import torch
 
 from torch.serialization import _check_dill_version, _open_file_like, _is_zipfile,\
-    FILE_LIKE, MAP_LOCATION, DEFAULT_PROTOCOL
+    _open_zipfile_reader, _is_torchscript_zip, _weights_only_unpickler,\
+    _legacy_load, _load, FILE_LIKE, MAP_LOCATION, DEFAULT_PROTOCOL
 
 
 def _remap_result(cpu_result, map_location):
@@ -95,22 +96,72 @@ def load(
     **pickle_load_args: Any
 ) -> Any:
 
+    torch._C._log_api_usage_once("torch.load")
+    UNSAFE_MESSAGE = (
+        "Weights only load failed. Re-running `torch.load` with `weights_only` set to `False`"
+        " will likely succeed, but it can result in arbitrary code execution."
+        "Do it only if you get the file from a trusted source. WeightsUnpickler error: "
+    )
+    # Add ability to force safe only weight loads via environment variable
+    if os.getenv("TORCH_FORCE_WEIGHTS_ONLY_LOAD", "0").lower() in ['1', 'y', 'yes', 'true']:
+        weights_only = True
+
+    if weights_only:
+        if pickle_module is not None:
+            raise RuntimeError("Can not safely load weights when explicit pickle_module is specified")
+    else:
+        if pickle_module is None:
+            pickle_module = pickle
+
+    # make flipping default BC-compatible
+    if mmap is None:
+        mmap = False
     _check_dill_version(pickle_module)
+
+    if 'encoding' not in pickle_load_args.keys():
+        pickle_load_args['encoding'] = 'utf-8'
 
     with _open_file_like(f, 'rb') as opened_file:
         if _is_zipfile(opened_file):
-            return torch.serialization.load(f, map_location, pickle_module, weights_only, mmap, **pickle_load_args)
+            orig_position = opened_file.tell()
+            overall_storage = None
+            with _open_zipfile_reader(opened_file) as opened_zipfile:
+                if _is_torchscript_zip(opened_zipfile):
+                    print(f"Warning: 'torch.load' received a zip file that looks like a TorchScript archive"
+                                  " dispatching to 'torch.jit.load' (call 'torch.jit.load' directly to silence this warning)")
+                    opened_file.seek(orig_position)
+                    return torch.jit.load(opened_file, map_location=map_location)
+                if mmap:
+                    if not isinstance(f, str):
+                        raise ValueError("f must be a string filename in order to use mmap argument")
+                    size = os.path.getsize(f)
+                    overall_storage = torch.UntypedStorage.from_file(f, False, size)
+                if weights_only:
+                    try:
+                        return _load(opened_zipfile, map_location, _weights_only_unpickler, overall_storage=overall_storage, **pickle_load_args)
+                    except RuntimeError as e:
+                        raise pickle.UnpicklingError(UNSAFE_MESSAGE + str(e)) from None
+                return _load(opened_zipfile, map_location, pickle_module, overall_storage=overall_storage, **pickle_load_args)
         else:
+            if mmap:
+                raise RuntimeError("mmap can only be used with files saved with `torch.save(_use_new_zipfile_serialization=True), ",
+                               "please torch.save your checkpoint with this option in order to use mmap.")
+            if weights_only:
+                try:
+                    return _legacy_load(opened_file, map_location, _weights_only_unpickler, **pickle_load_args)
+                except RuntimeError as e:
+                    raise pickle.UnpicklingError(UNSAFE_MESSAGE + str(e)) from None
+
             print(f"Warning: since the loaded file is not a zipfile, only \"torch.device\" and \"str\" type parameters are currently supported for parameter types of map_location")
             if map_location is not None and isinstance(map_location, (torch.device, str)):
-                cpu_result = torch.serialization.load(opened_file, "cpu", pickle_module, **pickle_load_args)
+                cpu_result = _legacy_load(opened_file, "cpu", pickle_module, **pickle_load_args)
                 if (isinstance(map_location, str) and "cpu" in map_location) or (isinstance(map_location, torch.device) and "cpu" in map_location.type):
                     return cpu_result
                 return _remap_result(cpu_result, map_location)
             else:
                 print(f"Warning: parameter types of map_location is \"Callable[[torch.Tensor, str], torch.Tensor]\" or \"Dict[str, str]\", which is only support for zipfile."
                                "All tensors are currently loaded onto the CPU, which may introduce problems")
-                return torch.serialization.load(opened_file, "cpu", pickle_module, **pickle_load_args)
+                return _legacy_load(opened_file, "cpu", pickle_module, **pickle_load_args)
 
 
 def save(
@@ -124,7 +175,7 @@ def save(
     if _use_new_zipfile_serialization is False:
         print(f"Warning: legacy save is not recommended for npu tensor, which may bring unexpected errors and hopefully set \"_use_new_zipfile_serialization = True\"",
                         "If it is necessary to use a unzipfile, convert the npu tensor to cpu tensor for saving")
-    return torch.serialization.save(obj, f, pickle_module,pickle_protocol, _use_new_zipfile_serialization, _disable_byteorder_record)
+    return torch.serialization.save(obj, f, pickle_module,pickle_protocol, True, _disable_byteorder_record)
 
 
 def add_serialization_methods():
