7dfa5d464b69b49d50e2504768bb9c72bc5a4b41
chuboning <chuboning@foxmail.com>
2023-09-01 18:03:07 +0800

    Default build with git sha

--- setup.py
+++ setup.py
@@ -45,6 +45,9 @@ def generate_torch_npu_version():
     flags = os.O_WRONLY | os.O_CREAT | os.O_EXCL
     modes = stat.S_IWUSR | stat.S_IRUSR
     sha = get_sha(torch_npu_root)
+    if os.getenv("BUILD_WITHOUT_SHA") is None:
+        global VERSION
+        VERSION += "+git" + sha[:7]
     with os.fdopen(os.open(version_path, flags, modes), 'w') as f:
         f.write("__version__ = '{version}'\n".format(version=VERSION))
         f.write("git_version = {}\n".format(repr(sha)))
