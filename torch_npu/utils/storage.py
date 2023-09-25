4ae6df840e2430fa9293d2da6871482c31b314c1
闫鹏全 <yanpengquan@huawei.com>
2023-09-25 01:56:38 +0000

    !6118 [FIX] Compatible with older version files. Merge pull request !6118 from 闫鹏全/serialization_add

--- torch_npu/utils/storage.py
+++ torch_npu/utils/storage.py
@@ -1,5 +1,25 @@
 import torch
 import torch_npu
+from . import serialization as se
+
+
+def _rebuild_npu_tensor(storage, storage_offset, size, stride, requires_grad, backward_hooks, npu_storage_info):
+    warn_massages = (
+        "Warning: The current version of the file storing weights is old,"
+        "and in the future we will deprecate the loading support for this type of file,"
+        "please use 2.1 and newer torch to re-store the weight file."
+    )
+    se._warn_legacy_serialization(warn_massages, "oldfile")
+    tensor = torch.tensor([], dtype=storage.dtype, device=storage.device)
+    tensor.set_(storage, storage_offset, size, stride)
+    tensor.requires_grad = requires_grad
+    tensor._backward_hooks = backward_hooks
+    if not se.RE_MAP_CPU:
+        if isinstance(npu_storage_info, bool):
+            tensor = tensor.npu()
+        else:
+            tensor = torch_npu.npu_format_cast(tensor.npu(), npu_storage_info)
+    return tensor
 
 
 def _cpu(self):
