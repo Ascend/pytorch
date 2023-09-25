bb7b86914014661413ba9da536bb719227711200
汪飞 <wangfei136@huawei.com>
2023-09-25 12:02:31 +0000

    !6145 modify scale to scale_value for FAG Merge pull request !6145 from 汪飞/v1.11.0

--- torch_npu/contrib/function/fusion_attention.py
+++ torch_npu/contrib/function/fusion_attention.py
@@ -26,10 +26,10 @@ def npu_fusion_attention(query, key, value, head_num, input_layout, pse=None, pa
 
 def npu_fusion_attention_grad(query, key, value, dy, head_num, input_layout, pse=None, padding_mask=None,
                               atten_mask=None, softmax_max=None, softmax_sum=None, softmax_in=None, attention_in=None,
-                              scale=1., keep_prob=1., pre_tockens=2147483647, next_tockens=2147483647,
+                              scale_value=1., keep_prob=1., pre_tockens=2147483647, next_tockens=2147483647,
                               inner_precise=0, gen_mask_parallel=True, sync=False):
     return torch_npu.npu_flash_attention_grad(
         query, key, value, dy, head_num, input_layout, pse=pse, padding_mask=padding_mask, atten_mask=atten_mask,
         softmax_max=softmax_max, softmax_sum=softmax_sum, softmax_in=softmax_in, attention_in=attention_in,
-        scale=scale, keep_prob=keep_prob, pre_tockens=pre_tockens, next_tockens=next_tockens,
+        scale_value=scale_value, keep_prob=keep_prob, pre_tockens=pre_tockens, next_tockens=next_tockens,
         inner_precise=inner_precise, gen_mask_parallel=gen_mask_parallel, sync=sync)
