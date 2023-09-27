a96f058906b2fe6259c69ddf92ff3e0ce1c26960
刘嘉巍 <843972097@qq.com>
2023-09-27 06:24:44 +0000

    !6202 Expose NpuStorageOffsetGuard to op-plugin. Merge pull request !6202 from 刘嘉巍/master-dev

--- torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h
+++ torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h
@@ -3,3 +3,4 @@
 #include "torch_npu/csrc/framework/StorageDescHelper.h"
 #include "torch_npu/csrc/framework/FormatHelper.h"
 #include "torch_npu/csrc/aten/common/InnerNpuNativeFunction.h"
+#include "torch_npu/csrc/framework/utils/NpuStorageOffsetGuard.h"
