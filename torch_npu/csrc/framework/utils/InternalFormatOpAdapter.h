8f7a5743a9a8d902c81a0831d673c709441ced5d
王姜奔 <wangjiangben@huawei.com>
2023-10-11 04:49:30 +0000

    !6487 回退 'Pull Request !6267 : [Fix] Give correct offset for AsStrided.' Merge pull request !6487 from 王姜奔/revert-merge-6267-v1.11.0-5.0.rc3

--- torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h
+++ torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h
@@ -18,4 +18,3 @@
 #include "torch_npu/csrc/framework/StorageDescHelper.h"
 #include "torch_npu/csrc/framework/FormatHelper.h"
 #include "torch_npu/csrc/aten/common/InnerNpuNativeFunction.h"
-#include "torch_npu/csrc/framework/utils/NpuStorageOffsetGuard.h"
