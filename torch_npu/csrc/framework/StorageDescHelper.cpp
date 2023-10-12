3f2305c3d5f0583170199df47b4463adf1c682ec
刘嘉巍 <843972097@qq.com>
2023-10-12 02:34:13 +0000

    !6238 [Clean code] Finish circulation safely. Merge pull request !6238 from 刘嘉巍/master-opt

--- torch_npu/csrc/framework/StorageDescHelper.cpp
+++ torch_npu/csrc/framework/StorageDescHelper.cpp
@@ -153,7 +153,7 @@ namespace at_npu
 
         auto str_to_small_vector = [](std::string str) -> c10::SmallVector<int64_t, 5> {
             int start = 0;
-            while (str[start++] != '/');
+            while ((start < str.size()) && (str[start++] != '/'));
             int end = start;
             c10::SmallVector<int64_t, 5> vec;
             while (end < str.size()) {
