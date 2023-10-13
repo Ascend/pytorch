c29f107524aeae2fade49d06acb09eed6eddea9a
刘嘉巍 <843972097@qq.com>
2023-10-13 02:16:29 +0000

    !6532 [Clean Code] Cleancode for torch_npu.onnx Merge pull request !6532 from 刘嘉巍/v2.0.1-dev

--- torch_npu/onnx/register_aten_ops_to_onnx.py
+++ torch_npu/onnx/register_aten_ops_to_onnx.py
@@ -9,18 +9,19 @@ from torch.onnx.symbolic_opset9 import sub, mul, add, pow, sqrt, reciprocal
 
 _onnx_symbolic = functools.partial(registration.onnx_symbolic, opset=11)
 
+
 @_onnx_symbolic("aten::native_layer_norm")
 @symbolic_helper.quantized_args(True, False, False, False)
 @symbolic_helper.parse_args("v", "is", "v", "v", "f")
 @_beartype.beartype
-def native_layer_norm(g, input, normalized_shape, weight, bias, eps):
+def native_layer_norm(g, inputs, normalized_shape, weight, bias, eps):
     axes = [-i for i in range(len(normalized_shape), 0, -1)]
 
     two_cst = symbolic_helper._generate_wrapped_number(g, 2.0)
     eps_cst = symbolic_helper._generate_wrapped_number(g, eps)
 
-    mean = g.op("ReduceMean", input, axes_i=axes)
-    numerator = sub(g, input, mean)
+    mean = g.op("ReduceMean", inputs, axes_i=axes)
+    numerator = sub(g, inputs, mean)
 
     # variance = e((x - e(x))^2), and (x - e(x)) is the numerator in the layer_norm formula
     variance = g.op("ReduceMean", pow(g, numerator, two_cst), axes_i=axes)
