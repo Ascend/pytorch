3cfbc9acb64062c62c7cdbbaec6e1865be1b9b3a
赖长铃 <laichangling@huawei.com>
2023-10-14 06:36:12 +0000

    !6596 restore function for mmcv Merge pull request !6596 from 赖长铃/restore_file1013

--- torch_npu/csrc/framework/utils/OpAdapter.h
+++ torch_npu/csrc/framework/utils/OpAdapter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "torch_npu/csrc/framework/utils/KernelNpuOutputSize.h"
 #include "torch_npu/csrc/framework/utils/OpPreparation.h"
 #include "torch_npu/csrc/framework/OpCommand.h"
 #include "torch_npu/csrc/aten/CustomFunctions.h"
