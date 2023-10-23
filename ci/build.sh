2f60999261a0f12e40ec1eeddd900317cf3d90a7
weili10 <liwei386@huawei.com>
2023-10-23 12:08:18 +0000

    delete disable_install_torchair env
    Signed-off-by: weili10 <liwei386@huawei.com>

--- ci/build.sh
+++ ci/build.sh
@@ -6,8 +6,6 @@ CUR_DIR=$(dirname $(readlink -f $0))
 SUPPORTED_PY_VERSION=(3.8 3.9 3.10)
 PY_VERSION='3.8'                     # Default supported python version is 3.8
 DEFAULT_SCRIPT_ARGS_NUM=1            # Default supported input parameters
-export DISABLE_INSTALL_TORCHAIR=TRUE
-export DISABLE_RPC_FRAMEWORK=TRUE
 
 # Parse arguments inside script
 function parse_script_args() {
