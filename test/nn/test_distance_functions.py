492813cffc8577ad25f653023dc314fe2cd0c78f
yuhaiyan <yuhaiyan8@huawei.com>
2023-11-02 09:02:28 +0000

    !6954 Rename test files: test_nn and test_nn_func Merge pull request !6954 from yuhaiyan/pt2.1-dev2

--- test/nn/test_distance_functions.py
+++ test/nn/test_distance_functions.py
@@ -1,5 +1,7 @@
 import copy
+
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch_npu
 
@@ -7,6 +9,20 @@ from torch_npu.testing.testcase import TestCase, run_tests
 
 
 class TestDistanceFunctions(TestCase):
+    def test_CosineSimilarity(self):
+        input1 = torch.randn(100, 128).npu()
+        input2 = torch.randn(100, 128).npu()
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6).npu()
+        output = cos(input1, input2)
+        self.assertEqual(output is not None, True)
+
+    def test_PairwiseDistance(self):
+        pdist = nn.PairwiseDistance(p=2).npu()
+        input1 = torch.randn(100, 128).npu()
+        input2 = torch.randn(100, 128).npu()
+        output = pdist(input1, input2)
+        self.assertEqual(output is not None, True)
+
     def test_pairwise_distance(self):
         input1 = torch.randn(2, 3)
         input2 = torch.randn(2, 3)
@@ -40,4 +56,5 @@ class TestDistanceFunctions(TestCase):
 
 
 if __name__ == "__main__":
+    torch.npu.set_device(0)
     run_tests()
