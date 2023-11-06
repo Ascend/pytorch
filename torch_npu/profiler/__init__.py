0f4e6db3f9a81d55bd48cdc3c2bc315d370f3821
MooYeh <yangtao279@huawei.com>
2023-11-06 08:37:00 +0000

    !7016 Add _KinetoProfiler, add_metadata and export_stacks interfaces for PyTorch Profiler Merge pull request !7016 from MooYeh/v2.1.0

--- torch_npu/profiler/__init__.py
+++ torch_npu/profiler/__init__.py
@@ -1,7 +1,10 @@
-from .profiler import profile
-from .msprofiler_c_interface import ProfilerActivity
-from .msprofiler_c_interface import supported_ms_activities as supported_activities
-from .profiler import tensorboard_trace_handler
+from torch_npu._C._profiler import ProfilerActivity
+
+from .profiler import (
+    profile,
+    _KinetoProfile,
+    supported_activities,
+    tensorboard_trace_handler)
 from .scheduler import Schedule as schedule
 from .scheduler import ProfilerAction
 from .experimental_config import _ExperimentalConfig, supported_profiler_level, supported_ai_core_metrics, \
--- torch_npu/profiler/analysis/prof_common_func/constant.py
+++ torch_npu/profiler/analysis/prof_common_func/constant.py
@@ -48,6 +48,7 @@ class Constant(object):
     # framework
     TENSORBOARD_TRACE_HANDLER = "tensorboard_trace_handler"
     EXPORT_CHROME_TRACE = "export_chrome_trace"
+    EXPORT_STACK = "export_stack"
 
     ACL_OP_EXE_NAME = ("AscendCL@aclopCompileAndExecute".lower(), "AscendCL@aclopCompileAndExecuteV2".lower())
     AI_CORE = "AI_CORE"
@@ -94,6 +95,10 @@ class Constant(object):
     StartCnt = "start_cnt"
     StartMonotonic = "start_monotonic"
     SyscntEable = "syscnt_enable"
+
+    # metric
+    METRIC_CPU_TIME = "self_cpu_time_total"
+    METRIC_NPU_TIME = "self_npu_time_total"
 
 
 def print_info_msg(message: str):
