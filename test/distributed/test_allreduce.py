e8626b94ba4444b7fc48e945c31571a678ca94e5
邵非凡 <shaofeifan2@huawei.com>
2023-11-07 11:27:12 +0000

    !6977 transfer uint8 to int32 for allreduce and reduce in ProcessGroupHCCL.cpp Merge pull request !6977 from 邵非凡/wrap_hccl_2.1

--- test/distributed/test_allreduce.py
+++ test/distributed/test_allreduce.py
@@ -82,6 +82,19 @@ class HcomAllReduceTest(TestCase):
             self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
                                     HcomAllReduceTest._init_dist_hccl, expected, input1, world_size)
 
+    @skipIfUnsupportMultiNPU(2)
+    def test_dist_all_reduce_uint8(self):
+        # CI currently supports only 2 devices
+        ranks = [2]
+        shape_format = [np.uint8, 2, [2, 3, 16]]
+        for world_size in ranks:
+            exp_input, input1 = create_common_tensor(shape_format, 0, 10)
+            expected = 0
+            for _ in range(world_size):
+                expected += exp_input
+            self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
+                                    HcomAllReduceTest._init_dist_hccl, expected, input1, world_size)
+
 
 if __name__ == '__main__':
     run_tests()
