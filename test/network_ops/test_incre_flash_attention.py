1824c49a12685792dd17a64581b671632899ba08
唐浩 <tanghao71@hisilicon.com>
2023-11-07 13:39:20 +0000

    !7125 fix: PFA/IFA, fix existing failed ut Merge pull request !7125 from 唐浩/master

--- test/network_ops/test_incre_flash_attention.py
+++ test/network_ops/test_incre_flash_attention.py
@@ -1,10 +1,13 @@
 import math
+import unittest
 import torch
 import numpy as np
 import torch_npu
 import torch_npu.npu.utils as utils
 
 from torch_npu.testing.testcase import TestCase, run_tests
+
+DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
 
 
 class TestIncreFlashAttention(TestCase):
@@ -27,6 +30,8 @@ class TestIncreFlashAttention(TestCase):
         scale = 1 / 0.0078125
         return torch_npu.npu_incre_flash_attention(q, k, v, num_heads=32, input_layout="BSH", scale_value=scale)
 
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
+        "OP `IncreFlashAttention` is only supported on 910B, skip this ut for this device type!")
     def test_op_exec(self):
         q = torch.randn(1, 32, 1, 128, dtype=torch.float16).npu()
         k = torch.randn(1, 32, 2048, 128, dtype=torch.float16).npu()
