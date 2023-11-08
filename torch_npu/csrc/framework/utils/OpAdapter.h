c7388e96ae935fa74a9be242dc20c2b41bc4c334
赖长铃 <laichangling@huawei.com>
2023-11-08 02:00:11 +0000

    !7087 remove unused file "KernelNpuOutputSize" Merge pull request !7087 from 赖长铃/remove_1106

--- torch_npu/csrc/framework/utils/OpAdapter.h
+++ torch_npu/csrc/framework/utils/OpAdapter.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "torch_npu/csrc/framework/utils/KernelNpuOutputSize.h"
 #include "torch_npu/csrc/framework/utils/OpPreparation.h"
 #include "torch_npu/csrc/framework/OpCommand.h"
 #include "torch_npu/csrc/aten/CustomFunctions.h"
