5648471756b74ab5eb56c3562bf3e66374ec2ed5
郭光浩 <guoguanghao@huawei.com>
2023-11-08 14:59:46 +0000

    !7176 [bugfix] add synchronize when init ddp Merge pull request !7176 from 郭光浩/v1.11.0-5.0.0

--- torch_npu/utils/module.py
+++ torch_npu/utils/module.py
@@ -27,6 +27,7 @@ from torch.nn.parameter import Parameter, UninitializedParameter, UninitializedB
 from torch.nn.modules.batchnorm import _NormBase, _LazyNormBase
 from torch.nn.modules.module import Module
 from torch.nn.parallel._functions import _streams
+from torch.nn.parallel import DistributedDataParallel
 from torch.nn.utils.rnn import PackedSequence
 from torch._C._nn import _parse_to as torch_parse_to
 
@@ -36,6 +37,7 @@ from torch_npu.utils.syncbatchnorm import SyncBatchNorm as sync_batch_norm
 from torch_npu.utils.tensor_methods import torch_device_guard
 
 logger = logging.getLogger(__name__)
+origin_ddp_init = DistributedDataParallel.__init__
 
 
 def npu(self, device=None):
@@ -220,6 +222,11 @@ def ddp_forward(self, *inputs, **kwargs):
             self.require_forward_param_sync = False
 
     return output
+
+
+def ddp_init(self, *args, **kwargs):
+    origin_ddp_init(self, *args, **kwargs)
+    torch_npu.npu.synchronize()
 
 
 def lstm_forward(self, input1, hx=None):
@@ -719,6 +726,7 @@ def apply_module_patch():
     torch.nn.parallel.DistributedDataParallel._register_builtin_comm_hook = ddp_register_builtin_comm_hook
     torch.nn.parallel.DistributedDataParallel._set_static_graph = ddp_set_static_graph
     torch.nn.parallel.DistributedDataParallel.forward = ddp_forward
+    torch.nn.parallel.DistributedDataParallel.__init__ = ddp_init
     torch.nn.modules.rnn.LSTM.forward = lstm_forward
     torch.nn.modules.rnn.GRU.forward = gru_forward
     torch.nn.utils.rnn.pad_packed_sequence = pad_packed_sequence
