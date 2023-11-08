d985c6c143d324597368da71cf3499a162010d8e
sunjiayang <sunxinlei1@huawei.com>
2023-11-08 14:26:14 +0000

    !7196 save_async Merge pull request !7196 from sunjiayang/async_save_210

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -4,7 +4,7 @@ from .storage import add_storage_methods
 from .optim import add_optim_method
 from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
 from ._device import apply_device_patch
-from .serialization import add_serialization_methods
+from .serialization import add_serialization_methods, save_async
 from .npu_intercept import cann_package_check, add_intercept_methods
 from .dtensor import register_ops_under_dtensor_rules
 
