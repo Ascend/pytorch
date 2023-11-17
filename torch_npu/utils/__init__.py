ef43be9ffc636bd34c6f0bc25b5e03c927183578
sunjiayang <sunxinlei1@huawei.com>
2023-11-17 03:01:26 +0000

    !7323 torch.version.cann Merge pull request !7323 from sunjiayang/get_cann_version_210

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -7,5 +7,6 @@ from ._device import apply_device_patch
 from .serialization import add_serialization_methods, save_async
 from .npu_intercept import cann_package_check, add_intercept_methods
 from .dtensor import register_ops_under_dtensor_rules
+from .collect_env import add_collect_env_methods
 
 __all__ = []
