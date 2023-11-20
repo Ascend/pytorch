29cafe17e5cb11d4c0219c0ef2c547bf44596cee
sunjiayang <sunxinlei1@huawei.com>
2023-11-20 07:12:24 +0000

    !7245 async save bug fix Merge pull request !7245 from sunjiayang/asave_111

--- torch_npu/utils/serialization.py
+++ torch_npu/utils/serialization.py
@@ -28,7 +28,7 @@ import torch_npu
 
 DEFAULT_PROTOCOL = 2
 RE_MAP_CPU = False
-second_stream = None
+save_async_stream_map = {}
 
 
 def _npu_tag(obj):
@@ -142,9 +142,6 @@ def save_async(
                            set \"_use_new_zipfile_serialization = True\"",
                            "if it is necessary to use this, please convert the npu tensor to cpu tensor for saving")
     
-    if not isinstance(obj, torch.Tensor) and not isinstance(model, torch.nn.Module):
-        raise RuntimeError("Error: When 'obj' is not a torch.Tensor, 'model' must be a torch.nn.Module.")
-    
     _check_dill_version(pickle_module)
     save_args = (obj, f, pickle_module, pickle_protocol, _use_new_zipfile_serialization, _disable_byteorder_record)
 
@@ -156,19 +153,22 @@ def save_async(
 def save_data_thread(save_args,
                      device,
                      model: torch.nn.Module = None):
-    global second_stream
+    global save_async_stream_map
     torch.npu.set_device(device)
 
     def hook_fn(*args):
-        torch.npu.current_stream().wait_stream(second_stream)
+        torch.npu.current_stream().wait_stream(save_async_stream_map.get(device))
 
-    if second_stream is None:
-        second_stream = torch.npu.Stream()
+    if device not in save_async_stream_map:
+        save_async_stream = torch.npu.Stream()
+        save_async_stream_map[device] = save_async_stream
         if isinstance(model, torch.nn.Module):
             model.register_full_backward_hook(hook_fn)
+    else:
+        save_async_stream = save_async_stream_map[device]
 
     obj, f, pickle_module, pickle_protocol, _use_new_zipfile_serialization, _disable_byteorder_record = save_args
-    with torch.npu.stream(second_stream):
+    with torch.npu.stream(save_async_stream):
         data_value, serialized_storages = _save(obj, pickle_module, pickle_protocol)
         storage_value = []
         for key in sorted(serialized_storages.keys()):
@@ -242,7 +242,11 @@ def _save(obj, pickle_module, pickle_protocol):
     pickler = pickle_module.Pickler(data_buf, protocol=pickle_protocol)
     pickler.persistent_id = persistent_id
     if isinstance(obj, torch.nn.Module):
+        hook_handle = obj._backward_hooks.copy()
         obj._backward_hooks.clear()
-    pickler.dump(obj)
+        pickler.dump(obj)
+        obj._backward_hooks.update(hook_handle)
+    else:
+        pickler.dump(obj)
     data_value = data_buf.getvalue()
     return data_value, serialized_storages
