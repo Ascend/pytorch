2eee1249ad05a8a9e5b7a4dfa74c4e7aeaae1553
sunjiayang <sunxinlei1@huawei.com>
2023-11-21 08:54:41 +0000

    !7292 save_async bug fix Merge pull request !7292 from sunjiayang/asave_201_500

--- test/test_utils/test_save_async.py
+++ test/test_utils/test_save_async.py
@@ -1,5 +1,6 @@
 import os
 import time
+import copy
 
 import torch
 import torch.nn as nn
@@ -8,7 +9,6 @@ import torch.optim as optim
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.utils.path_manager import PathManager
-
 
 
 class TestAsyncSave(TestCase):
@@ -24,14 +24,22 @@ class TestAsyncSave(TestCase):
         time.sleep(5)
         PathManager.remove_path_safety(TestAsyncSave.test_save_path)
 
-        
-    def test_async_save_tensor(self):
-        save_tensor = torch.zeros(1024, dtype=torch.float32).npu()
-        save_path = os.path.join(TestAsyncSave.test_save_path, "save_tensor.pt")
-        torch_npu.utils.save_async(save_tensor, save_path)
+    def test_save_async_tensor(self):
+        save_tensor = torch.rand(1024, dtype=torch.float32).npu()
+        async_save_path = os.path.join(TestAsyncSave.test_save_path, "async_save_tensor.pt")
+        torch_npu.utils.save_async(save_tensor, async_save_path)
+
+        torch.npu.synchronize()
+        tensor_async = torch.load(async_save_path)
+
+        self.assertEqual(save_tensor, tensor_async)
     
-    def test_async_save_model(self):
-        model = nn.Sequential(
+    def test_save_async(self):
+        loss1 = [1.6099495, 1.6099086, 1.6098710]
+        loss2 = []
+        model_list = []
+        checkpoint_list = []
+        model_origin = nn.Sequential(
             nn.Linear(100, 50),
             nn.ReLU(),
             nn.Linear(50, 20),
@@ -39,64 +47,55 @@ class TestAsyncSave(TestCase):
             nn.Linear(20, 5),
             nn.ReLU()
         )
-        model = model.npu()
+
+        input_data = torch.ones(6400, 100).npu()
+        labels = torch.arange(5).repeat(1280).npu()
 
         criterion = nn.CrossEntropyLoss()
-        optimerizer = optim.SGD(model.parameters(), lr=0.01)
-        for epoch in range(5):
-            for step in range(5):
-                input_data = torch.ones(6400, 100).npu()
-                labels = torch.randint(0, 5, (6400,)).npu()
+        model = model_origin.npu()
+        optimerizer = optim.SGD(model.parameters(), lr=0.1)
+        for step in range(3):
+            outputs = model(input_data)
+            loss = criterion(outputs, labels)
 
-                outputs = model(input_data)
-                loss = criterion(outputs, labels)
+            optimerizer.zero_grad()
+            loss.backward()
 
-                optimerizer.zero_grad()
-                loss.backward()
-
-                optimerizer.step()
+            optimerizer.step()
             
-            save_path = os.path.join(TestAsyncSave.test_save_path, f"model_{epoch}_{step}.path")
-
-            torch_npu.utils.save_async(model, save_path, model=model)
-    
-    def test_async_save_checkpoint(self):
-        model = nn.Sequential(
-            nn.Linear(100, 50),
-            nn.ReLU(),
-            nn.Linear(50, 20),
-            nn.ReLU(),
-            nn.Linear(20, 5),
-            nn.ReLU()
-        )
-        model = model.npu()
-
-        criterion = nn.CrossEntropyLoss()
-        optimerizer = optim.SGD(model.parameters(), lr=0.01)
-        for epoch in range(5):
-            for step in range(5):
-                input_data = torch.ones(6400, 100).npu()
-                labels = torch.randint(0, 5, (6400,)).npu()
-
-                outputs = model(input_data)
-                loss = criterion(outputs, labels)
-
-                optimerizer.zero_grad()
-                loss.backward()
-
-                optimerizer.step()
+            loss2.append(loss)
             checkpoint = {
                 "model" : model.state_dict(),
-                "optimizer" : optimerizer.state_dict(),
-                "epoch" : epoch,
-                "step" : step
+                "optimizer" : optimerizer.state_dict()
             }
+            checkpoint_list.append(copy.deepcopy(checkpoint))
+            model_list.append(copy.deepcopy(model))
+            checkpoint_async_path = os.path.join(TestAsyncSave.test_save_path, f"checkpoint_async_{step}.path")
+            model_async_path = os.path.join(TestAsyncSave.test_save_path, f"model_async_{step}.path")
+            torch_npu.utils.save_async(checkpoint, checkpoint_async_path, model=model)
+            torch_npu.utils.save_async(model, model_async_path, model=model)
 
-            save_path = os.path.join(TestAsyncSave.test_save_path, f"checkpoint_{epoch}_{step}.path")
+            time.sleep(1)
 
-            torch_npu.utils.save_async(checkpoint, save_path, model=model)
+        torch.npu.synchronize()
 
+        for i in range(3):
+            self.assertEqual(loss1[i], loss2[i].item())
+            checkpoint_async_path = os.path.join(TestAsyncSave.test_save_path, f"checkpoint_async_{i}.path")
+            checkpoint_async = torch.load(checkpoint_async_path)
+            self.assertEqual(checkpoint_list[i], checkpoint_async, prec=2e-3)
+            model_async_path = os.path.join(TestAsyncSave.test_save_path, f"model_async_{i}.path")
+            model_async = torch.load(model_async_path)
 
+            state_dict_sync = model_list[i].state_dict()
+            state_dict_async = model_async.state_dict()
+
+            key_sync = sorted(state_dict_sync.keys())
+            key_async = sorted(state_dict_async.keys())
+
+            self.assertEqual(key_sync, key_async)
+            for key in key_async:
+                self.assertEqual(state_dict_async[key], state_dict_sync[key], prec=2e-3)
 
 if __name__ == '__main__':
     torch.npu.set_device(0)
