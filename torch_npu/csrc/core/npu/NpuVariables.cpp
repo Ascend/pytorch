1cbe07f509c3d31633ccb0f71334491cb5dcd767
xudaohong <xudaohong@huawei.com>
2023-11-23 09:22:32 +0000

    !7595 [fix] change soc version that supports inf_nan mode Merge pull request !7595 from xudaohong/cherry-pick-1700559134

--- torch_npu/csrc/core/npu/NpuVariables.cpp
+++ torch_npu/csrc/core/npu/NpuVariables.cpp
@@ -70,8 +70,9 @@ void SetSocVersion(const char* const socVersion) {
 const SocVersion& GetSocVersion() { return g_curSocVersion; }
 
 bool IsSupportInfNan() {
-  return c10_npu::option::OptionsManager::CheckInfNanModeEnable() &&
-         (GetSocVersion() >= SocVersion::Ascend910B1);
+    return c10_npu::option::OptionsManager::CheckInfNanModeEnable() &&
+           (((GetSocVersion() >= SocVersion::Ascend910B1) && (GetSocVersion() < SocVersion::Ascend310B1)) ||
+           (GetSocVersion() >= SocVersion::Ascend910C1));
 }
 }  // namespace c10_npu
 
