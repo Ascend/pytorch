295a45892088776102ba12c69e3e7bb34685d6c6
z00581707 <zhangshufan@huawei.com>
2023-11-23 19:35:35 +0800

    performance optimization: Log switch using ASCEND_GLOBAL_LOG_LEVEL

--- torch_npu/csrc/core/npu/npu_log.h
+++ torch_npu/csrc/core/npu/npu_log.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <stdio.h>
 #include "third_party/acl/inc/acl/acl_base.h"
-
+#include "torch_npu/csrc/core/npu/register/OptionsManager.h"
 
 #define NPUStatus std::string
 #define SUCCESS "SUCCESS"
@@ -13,12 +13,28 @@
 #define ALLOC_ERROR "ALLOC_ERROR"
 #define FAILED "FAILED"
 
-#define ASCEND_LOGE(fmt, ...) \
-  aclAppLog(ACL_ERROR, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__)
-#define ASCEND_LOGW(fmt, ...) \
-  aclAppLog(ACL_WARNING, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__)
-#define ASCEND_LOGI(fmt, ...) \
-  aclAppLog(ACL_INFO, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__)
-#define ASCEND_LOGD(fmt, ...) \
-  aclAppLog(ACL_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__)
+#define ASCEND_LOGE(fmt, ...)                                                                           \
+    do {                                                                                                \
+        if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_ERROR)) {                             \
+            aclAppLog(ACL_ERROR, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);    \
+        }                                                                                               \
+    } while (0);
+#define ASCEND_LOGW(fmt, ...)                                                                           \
+    do {                                                                                                \
+        if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_WARNING)) {                           \
+            aclAppLog(ACL_WARNING, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);  \
+        }                                                                                               \
+    } while (0);
+#define ASCEND_LOGI(fmt, ...)                                                                           \
+    do {                                                                                                \
+        if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_INFO)) {                              \
+            aclAppLog(ACL_INFO, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);     \
+        }                                                                                               \
+    } while (0);
+#define ASCEND_LOGD(fmt, ...)                                                                           \
+    do {                                                                                                \
+        if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_DEBUG)) {                             \
+            aclAppLog(ACL_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);    \
+        }                                                                                               \
+    } while (0);
 
--- torch_npu/csrc/core/npu/register/OptionsManager.h
+++ torch_npu/csrc/core/npu/register/OptionsManager.h
@@ -5,28 +5,31 @@
 #include <unordered_map>
 
 #include "torch_npu/csrc/core/npu/NPUException.h"
+#include "torch_npu/csrc/core/npu/NPUMacros.h"
 
 namespace c10_npu {
 namespace option {
 
 class OptionsManager {
 public:
-  static bool IsResumeModeEnable();
-  static bool IsMultiStreamMemoryReuse();
-  static bool CheckInfNanModeEnable();
-  static bool CheckBlockingEnable();
-  static bool CheckQueueEnable();
-  static bool CheckCombinedOptimizerEnable();
-  static bool CheckTriCombinedOptimizerEnable();
-  static bool CheckAclDumpDateEnable();
-  static uint32_t GetHCCLExecTimeout();
-  static std::string CheckDisableDynamicPath();
-  static int32_t GetACLExecTimeout();
-  static const char* GetAclConfigJsonPath();
-  static uint32_t CheckUseHcclAsyncErrorHandleEnable();
-  static uint32_t CheckUseDesyncDebugEnable();
+    static bool IsResumeModeEnable();
+    static bool IsMultiStreamMemoryReuse();
+    static bool CheckInfNanModeEnable();
+    static bool CheckBlockingEnable();
+    static bool CheckQueueEnable();
+    static bool CheckCombinedOptimizerEnable();
+    static bool CheckTriCombinedOptimizerEnable();
+    static bool CheckAclDumpDateEnable();
+    static uint32_t GetHCCLExecTimeout();
+    static std::string CheckDisableDynamicPath();
+    static int32_t GetACLExecTimeout();
+    static const char* GetAclConfigJsonPath();
+    static uint32_t CheckUseHcclAsyncErrorHandleEnable();
+    static uint32_t CheckUseDesyncDebugEnable();
+    C10_NPU_API static bool isACLGlobalLogOn(aclLogLevel level);
+
 private:
-  static int GetBoolTypeOption(const char* env_str, int defaultVal = 0);
+    static int GetBoolTypeOption(const char* env_str, int defaultVal = 0);
 };
 
 } // namespace option
