b45d546bd03f12c09200d1a964eb22368b87db04
liujie379908 <liujie81@huawei.com>
2023-11-24 10:14:00 +0000

    !7686 Add watchdog UT use case Merge pull request !7686 from liujie379908/v2.1.0

---
+++ test/distributed/test_watchdog.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+import torch.distributed.run as launch
+from torch_npu.testing.testcase import run_tests, TestCase
+from torch_npu.testing.common_distributed import skipIfUnsupportMultiNPU
+
+
+def path(script):
+    return os.path.join(os.path.dirname(__file__), script)
+
+
+class ElasticLaunchTest(TestCase):
+    @skipIfUnsupportMultiNPU(2)
+    def test_communicate_npu_watchdog_timeout(self):
+        try:
+            error = None
+            launch.main(
+                [
+                    "--run-path",
+                    "--nnodes=1",
+                    "--nproc-per-node=2",
+                    "--monitor-interval=1",
+                    path("watchdog/watchdog_base.py"),
+                ]
+            )
+        except Exception as e:
+            error = e
+        finally:
+            if error is None:
+                raise RuntimeError("Test case fail")
+
+
+if __name__ == "__main__":
+    run_tests()
