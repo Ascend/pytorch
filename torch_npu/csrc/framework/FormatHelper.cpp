a0aec26fe4d11778f98c20862c1dd2b21d4991ed
邵非凡 <shaofeifan2@huawei.com>
2023-11-27 11:49:50 +0000

    !7723 bugfix, modify aclnn route logic Merge pull request !7723 from 邵非凡/base_format20-5.0.0

--- torch_npu/csrc/framework/FormatHelper.cpp
+++ torch_npu/csrc/framework/FormatHelper.cpp
@@ -144,8 +144,9 @@ namespace at_npu
       if (!torch_npu::utils::is_npu(tensor)) {
         return true;
       }
-      const auto &desc = torch_npu::NPUBridge::GetNpuStorageImplDesc(tensor);
-      return desc.origin_format_ == desc.npu_format_;
+      const auto format = torch_npu::NPUBridge::GetNpuStorageImplDesc(tensor).npu_format_;
+      return (format == ACL_FORMAT_ND) || (format == ACL_FORMAT_NCHW) ||
+             (format == ACL_FORMAT_NHWC) || (format == ACL_FORMAT_NCDHW);
     }
 
     bool FormatHelper::IsOpInputBaseFormat(
