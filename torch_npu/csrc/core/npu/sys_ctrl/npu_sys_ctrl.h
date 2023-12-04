b9f0c17ff0cfb87fe9325ae5feace218b4294587
郭光浩 <guoguanghao@huawei.com>
2023-12-04 11:19:51 +0000

    !8023 [revert ]'Pull Request !7855 : Support 'to' operator to copied tensor to any npu device in a single process' Merge pull request !8023 from 郭光浩/revert-merge-7856-v2.0.1

--- torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.h
+++ torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.h
@@ -4,7 +4,6 @@
 #include <map>
 #include <string>
 #include <vector>
-#include <unordered_set>
 #include <functional>
 #include "c10/macros/Export.h"
 #include "torch_npu/csrc/core/npu/NPUMacros.h"
@@ -65,9 +64,7 @@ private:
 private:
     bool init_flag_;
     int device_id_;
-    uint32_t device_count_;
     std::map<ReleasePriority, std::vector<ReleaseFn>> release_fn_;
-    std::unordered_set<int> used_devices;
 };
 } // namespace c10_npu
 
