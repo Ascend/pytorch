3c4bbc17b3fc32044d0c0c2265c689f0858cb638
张磊 <zhanglei121@huawei.com>
2023-12-25 08:31:04 +0000

    !8426 [FAG] adapt seqLens input params for FAG Merge pull request !8426 from 张磊/v1.11.0

--- torch_npu/contrib/function/fusion_attention.py
+++ torch_npu/contrib/function/fusion_attention.py
@@ -17,22 +17,24 @@ import torch_npu
 
 def npu_fusion_attention(query, key, value, head_num, input_layout, pse=None, padding_mask=None, atten_mask=None,
                          scale=1., keep_prob=1., pre_tockens=2147483647, next_tockens=2147483647, 
-                         inner_precise=0, prefix=None, sparse_mode=0, gen_mask_parallel=True, sync=False):
+                         inner_precise=0, prefix=None, actual_seq_qlen=None, actual_seq_kvlen=None, sparse_mode=0,
+                         gen_mask_parallel=True, sync=False):
     return torch_npu.npu_flash_attention(
         query, key, value, head_num, input_layout, pse=pse, padding_mask=padding_mask, atten_mask=atten_mask,
         scale=scale, keep_prob=keep_prob, pre_tockens=pre_tockens, next_tockens=next_tockens,
-        inner_precise=inner_precise, prefix=prefix, sparse_mode=sparse_mode,
-        gen_mask_parallel=gen_mask_parallel, sync=sync)
+        inner_precise=inner_precise, prefix=prefix, actual_seq_qlen=actual_seq_qlen, actual_seq_kvlen=actual_seq_kvlen,
+        sparse_mode=sparse_mode, gen_mask_parallel=gen_mask_parallel, sync=sync)
 
 
 def npu_fusion_attention_grad(query, key, value, dy, head_num, input_layout, pse=None, padding_mask=None,
                               atten_mask=None, softmax_max=None, softmax_sum=None, softmax_in=None, attention_in=None,
                               scale_value=1., keep_prob=1., pre_tockens=2147483647, next_tockens=2147483647,
-                              inner_precise=0, seed=0, offset=0, numels=0, prefix=None, sparse_mode=0,
-                              gen_mask_parallel=True, sync=False):
+                              inner_precise=0, seed=0, offset=0, numels=0, prefix=None, actual_seq_qlen=None,
+                              actual_seq_kvlen=None, sparse_mode=0, gen_mask_parallel=True, sync=False):
     return torch_npu.npu_flash_attention_grad(
         query, key, value, dy, head_num, input_layout, pse=pse, padding_mask=padding_mask, atten_mask=atten_mask,
         softmax_max=softmax_max, softmax_sum=softmax_sum, softmax_in=softmax_in, attention_in=attention_in,
         scale_value=scale_value, keep_prob=keep_prob, pre_tockens=pre_tockens, next_tockens=next_tockens,
-        inner_precise=inner_precise, seed=seed, offset=offset, numels=numels, prefix=prefix, sparse_mode=sparse_mode,
+        inner_precise=inner_precise, seed=seed, offset=offset, numels=numels, prefix=prefix,
+        actual_seq_qlen=actual_seq_qlen, actual_seq_kvlen=actual_seq_kvlen, sparse_mode=sparse_mode,
         gen_mask_parallel=gen_mask_parallel, sync=sync)
