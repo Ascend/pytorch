f4bac8be56c97b29b8261b135b8de45bb555f146
裘凯达 <kdaqiu@163.com>
2023-12-27 01:07:39 +0000

    !8562 Bugfix for syscnt or monotonic in Ascend PyTorch Profiler. Merge pull request !8562 from 裘凯达/pta111_syscnt

--- torch_npu/csrc/framework/interface/LibAscendHal.cpp
+++ torch_npu/csrc/framework/interface/LibAscendHal.cpp
@@ -16,50 +16,49 @@ REGISTER_LIBRARY(libascend_hal)
 LOAD_FUNCTION(halGetDeviceInfo)
 LOAD_FUNCTION(halGetAPIVersion)
 constexpr uint32_t KHZTOMHZ = 1000U;
-constexpr uint32_t RES_OK = 0;
-constexpr uint32_t ERR_FREQ = 1;
+constexpr uint32_t DRV_ERROR_NONE = 0;
+constexpr uint32_t ERR_FREQ = 0;
 constexpr uint32_t ERR_VER = 0;
 constexpr uint32_t FREQ_CONFIG = 24;
 
 int64_t getFreq() {
-  using getReqFun = int32_t (*)(uint32_t, int32_t, int32_t, int64_t*);
-  static getReqFun getFreqInfo = nullptr;
-  if (getFreqInfo == nullptr) {
-      getFreqInfo = (getReqFun)GET_FUNC(halGetDeviceInfo);
-  }
-  int64_t freq = ERR_FREQ;
-  if (getFreqInfo == nullptr) {
-    TORCH_WARN("Failed to find function halGetDeviceInfo.");
+    using getReqFun = int32_t (*)(uint32_t, int32_t, int32_t, int64_t*);
+    static getReqFun getFreqInfo = nullptr;
+    if (getFreqInfo == nullptr) {
+        getFreqInfo = (getReqFun)GET_FUNC(halGetDeviceInfo);
+        if (getFreqInfo == nullptr) {
+            TORCH_WARN("Failed to find function halGetDeviceInfo.");
+            return ERR_FREQ;
+        }
+    }
+    int64_t freq = ERR_FREQ;
+    if (getFreqInfo(0, 0, FREQ_CONFIG, &freq) == DRV_ERROR_NONE && freq > 0) {
+        return freq / KHZTOMHZ;
+    }
     return ERR_FREQ;
-  }
-  int32_t ret = getFreqInfo(0, 0, FREQ_CONFIG, &freq);
-  if (ret != RES_OK) {
-    TORCH_WARN("Failed to find freq.");
-    return ERR_FREQ;
-  }
-  return freq / KHZTOMHZ;
 }
+
 int64_t getVer() {
-  using getReqFun = int32_t (*)(int32_t*);
-  static getReqFun getVerInfo = nullptr;
-  if (getVerInfo == nullptr) {
-      getVerInfo = (getReqFun)GET_FUNC(halGetAPIVersion);
-  }
-  int32_t ver = ERR_VER;
-  if (getVerInfo == nullptr) {
-    TORCH_WARN("Failed to find function halGetAPIVersion.");
-    return ERR_VER;
-  }
-  int32_t ret = getVerInfo(&ver);
-  if (ret != RES_OK) {
-    TORCH_WARN("Failed to find version.");
-    return ERR_VER;
-  }
-  return ver;
+    using getReqFun = int32_t (*)(int32_t*);
+    static getReqFun getVerInfo = nullptr;
+    if (getVerInfo == nullptr) {
+        getVerInfo = (getReqFun)GET_FUNC(halGetAPIVersion);
+        if (getVerInfo == nullptr) {
+            TORCH_WARN("Failed to find function halGetAPIVersion.");
+            return ERR_VER;
+        }
+    }
+    int32_t ver = ERR_VER;
+    if (getVerInfo(&ver) != DRV_ERROR_NONE) {
+        TORCH_WARN("Failed to find version.");
+        return ERR_VER;
+    }
+    return ver;
 }
+
 bool isSyscntEnable() {
-  constexpr int32_t supportVersion = 0x071905;
-  return getVer() >= supportVersion;
+    constexpr int32_t supportVersion = 0x071905;
+    return getVer() >= supportVersion && getFreq() != ERR_FREQ;
 }
 
 } // namespace native
