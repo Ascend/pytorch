754e8fa8c787670d67f7f67fefa2e31c67f6bf23
yuhaiyan <yuhaiyan8@huawei.com>
2023-12-29 09:35:16 +0000

    !8606 Fixed the failed test cases. Merge pull request !8606 from yuhaiyan/v2.1.0-dev4

--- test/contrib/test_multiclass_nms.py
+++ test/contrib/test_multiclass_nms.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 import torch
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
@@ -7,62 +8,68 @@ from torch_npu.contrib.function import npu_multiclass_nms, \
 
 
 class TestMultiClassNms(TestCase):
-    @unittest.skip("skip test_npu_multiclass_nms_1 now")
     def test_npu_multiclass_nms_1(self):
-        boxes = torch.randint(1, 255, size=(1000, 4))
-        scores = torch.randn(1000, 81)
+        np.random.seed(123)
+        data1 = np.random.randint(low=1, high=255, size=(1000, 4))
+        boxes = torch.tensor(data1, dtype=torch.int64)
+        data2 = np.random.randn(1000, 81)
+        scores = torch.tensor(data2, dtype=torch.float32)
         boxes = boxes.npu().half()
         scores = scores.npu().half()
         det_bboxes, det_labels = npu_multiclass_nms(boxes, scores, score_thr=0.3, nms_thr=0.5, max_num=3)
-        expedt_det_bboxes = torch.tensor([[57.0000, 198.8750, 45.9688, 221.8750, 4.1484],
-                                          [215.0000, 155.0000, 236.8750, 137.0000, 3.9023],
-                                          [208.8750, 221.0000, 228.0000, 17.0000, 3.8867]],
-                                         dtype=torch.float16)
-        expedt_det_labels = torch.tensor([59., 3., 75.], dtype=torch.float16)
-        self.assertRtolEqual(expedt_det_bboxes, det_bboxes.cpu())
-        self.assertRtolEqual(expedt_det_labels, det_labels.cpu())
-    
-    @unittest.skip("skip test_npu_multiclass_nms_2 now")
+        expect_det_bboxes = torch.tensor([[81.9375, 183.8750, 35.0000, 172.0000, 4.1797],
+                                          [52.0000, 47.0000, 196.8750, 67.0000, 3.8750],
+                                          [76.0000, 140.0000, 42.0000, 132.0000, 3.8613]], dtype=torch.float16)
+        expect_det_labels = torch.tensor([69., 31., 48.], dtype=torch.float16)
+        self.assertRtolEqual(expect_det_bboxes, det_bboxes.cpu())
+        self.assertRtolEqual(expect_det_labels, det_labels.cpu())
+
     def test_npu_multiclass_nms_2(self):
-        boxes = torch.randn(1000, 4)
-        scores = torch.randn(1000, 81)
+        np.random.seed(123)
+        data1 = np.random.randn(1000, 4)
+        boxes = torch.tensor(data1, dtype=torch.float32)
+        data2 = np.random.randn(1000, 81)
+        scores = torch.tensor(data2, dtype=torch.float32)
         boxes = boxes.npu().half()
         scores = scores.npu().half()
         det_bboxes, det_labels = npu_multiclass_nms(boxes, scores, score_thr=0.3, nms_thr=0.5, max_num=3)
-        expedt_det_bboxes = torch.tensor([[0.2231, -1.6943, -0.1172, -1.0547, 4.1484],
-                                          [0.2891, 0.4897, -0.3809, -0.7129, 3.9023],
-                                          [0.6694, -1.2266, -0.3027, 0.4639, 3.8867]],
-                                         dtype=torch.float16)
-        expedt_det_labels = torch.tensor([59., 3., 75.], dtype=torch.float16)
-        self.assertRtolEqual(expedt_det_bboxes, det_bboxes.cpu())
-        self.assertRtolEqual(expedt_det_labels, det_labels.cpu())
-    
-    @unittest.skip("skip test_npu_batched_multiclass_nms_1 now")
+        expect_det_bboxes = torch.tensor([[0.1643, 0.7480, 0.1807, -0.2734, 4.1836],
+                                          [-0.9736, -0.1373, 0.9473, 1.0938, 4.1641],
+                                          [1.5234, -0.6831, -1.8359, 1.1035, 4.0664]], dtype=torch.float16)
+        expect_det_labels = torch.tensor([46., 8., 69.], dtype=torch.float16)
+        self.assertRtolEqual(expect_det_bboxes, det_bboxes.cpu())
+        self.assertRtolEqual(expect_det_labels, det_labels.cpu())
+
     def test_npu_batched_multiclass_nms_1(self):
-        boxes = torch.randint(1, 255, size=(4, 200, 80, 4))
-        scores = torch.randn(4, 200, 81)
+        np.random.seed(339)
+        data1 = np.random.randint(low=1, high=255, size=(4, 200, 80, 4))
+        boxes = torch.tensor(data1, dtype=torch.int64)
+        data2 = np.random.randn(4, 200, 81)
+        scores = torch.tensor(data2, dtype=torch.float32)
         boxes = boxes.npu().half()
         scores = scores.npu().half()
         det_bboxes, det_labels = npu_batched_multiclass_nms(boxes, scores, score_thr=0.3, nms_thr=0.5, max_num=3)
-        expedt_det_bboxes = torch.tensor([[[221.8750, 60.0000, 183.0000, 22.0000, 3.8867],
-                                           [167.0000, 250.0000, 136.0000, 144.0000, 3.6445],
-                                           [45.9688, 147.0000, 67.0000, 241.8750, 3.4844]],
-                                          [[5.0000, 178.0000, 243.8750, 138.0000, 3.7344],
-                                           [238.0000, 132.0000, 47.0000, 84.0000, 3.6836],
-                                           [32.0000, 110.0000, 131.0000, 73.0000, 3.6309]],
-                                          [[111.9375, 120.9375, 54.0000, 231.0000, 3.9219],
-                                           [147.0000, 162.0000, 78.0000, 1.0010, 3.9219],
-                                           [157.0000, 118.0000, 57.0000, 115.0000, 3.6523]],
-                                          [[80.0000, 126.9375, 54.0000, 246.8750, 3.7344],
-                                           [31.0000, 253.8750, 19.0000, 138.0000, 3.6328],
-                                           [54.0000, 253.8750, 78.0000, 75.0000, 3.5586]]],
-                                         dtype=torch.float16)
-        expedt_det_labels = torch.tensor([[76., 3., 32.],
-                                          [26., 66., 25.],
-                                          [34., 41., 30.],
-                                          [22., 27., 46.]], dtype=torch.float16)
-        self.assertRtolEqual(expedt_det_bboxes, det_bboxes.cpu())
-        self.assertRtolEqual(expedt_det_labels, det_labels.cpu())
+        expect_det_bboxes = torch.tensor([[[195.0000, 133.0000, 123.0000, 36.0000, 4.3984],
+                                           [70.0000, 113.0000, 149.0000, 22.9844, 3.8184],
+                                           [203.0000, 250.0000, 9.0000, 81.9375, 3.5996]],
+
+                                          [[60.0000, 19.0000, 233.8750, 142.0000, 3.7852],
+                                           [147.0000, 218.0000, 223.0000, 86.9375, 3.6426],
+                                           [19.0000, 113.0000, 91.9375, 238.8750, 3.5449]],
+
+                                          [[163.8750, 47.9688, 221.8750, 218.0000, 3.8281],
+                                           [5.9961, 81.9375, 110.0000, 171.0000, 3.7734],
+                                           [155.0000, 133.0000, 138.0000, 108.0000, 3.4844]],
+
+                                          [[238.8750, 78.0000, 188.0000, 17.0000, 3.9121],
+                                           [79.0000, 115.9375, 83.0000, 246.0000, 3.5879],
+                                           [11.0000, 94.0000, 243.0000, 226.8750, 3.4805]]], dtype=torch.float16)
+        expect_det_labels = torch.tensor([[27., 36., 78.],
+                                          [19., 27., 39.],
+                                          [54., 18., 27.],
+                                          [72., 43., 31.]], dtype=torch.float16)
+        self.assertRtolEqual(expect_det_bboxes, det_bboxes.cpu())
+        self.assertRtolEqual(expect_det_labels, det_labels.cpu())
 
 
 if __name__ == "__main__":
