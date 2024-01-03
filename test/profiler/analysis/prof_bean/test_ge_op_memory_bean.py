bf1f16b5e376fcbbee16b502f157e97427a7a71a
MooYeh <yangtao279@huawei.com>
2024-01-03 03:33:51 +0000

    !8733 Add UTs for profiler prof_bean Merge pull request !8733 from MooYeh/master_pta_ut_prof_bean

---
+++ test/profiler/analysis/prof_bean/test_ge_op_memory_bean.py
@@ -0,0 +1,49 @@
+from torch_npu.profiler.analysis.prof_bean.ge_op_memory_bean import GeOpMemoryBean
+from torch_npu.testing.testcase import TestCase, run_tests
+
+
+class TestGeOpMemoryBean(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_cases = [
+            {
+                "Name": "Graph_21",
+                "Size(KB)": "192",
+                "Allocation Time(us)": "16995.950\t",
+                "Duration(us)": "92.6",
+                "Allocation Total Allocated(KB)": "192",
+                "Allocation Total Reserved(KB)": "502336",
+                "Release Total Allocated(KB)": "0",
+                "Release Total Reserved(KB)": "502336",
+                "Device": "NPU:0",
+                # The following is the auxiliary data
+                "row": ["cann::Graph_21", "192", "16995.950\t", "17088.550\t", "92.6", 0.1875, 490.5625, 0.0, 490.5625, "NPU:0"],
+                "name": "cann::Graph_21",
+                "release_time": "17088.550\t",
+                "total_alloc": 0.1875,
+                "total_reserve": 490.5625,
+                "release_total_alloc": 0,
+                "release_total_reserve": 490.5625
+            },
+        ]
+
+    def test_property(self):
+        for test_case in self.test_cases:
+            ge_op_mem = GeOpMemoryBean(test_case)
+            self.assertEqual(test_case.get("row"), ge_op_mem.row)
+            self.assertEqual(test_case.get("name"), ge_op_mem.name)
+            self.assertEqual(test_case.get("Size(KB)"), ge_op_mem.size)
+            self.assertEqual(test_case.get("Allocation Time(us)"), ge_op_mem.allocation_time)
+            self.assertEqual(test_case.get("Duration(us)"), ge_op_mem.dur)
+            self.assertEqual(test_case.get("release_time"), ge_op_mem.release_time)
+            self.assertEqual(test_case.get("total_alloc"), ge_op_mem.allocation_total_allocated)
+            self.assertEqual(test_case.get("total_reserve"), ge_op_mem.allocation_total_reserved)
+            self.assertEqual(test_case.get("release_total_alloc"), ge_op_mem.release_total_allocated)
+            self.assertEqual(test_case.get("release_total_reserve"), ge_op_mem.release_total_reserved)
+            self.assertEqual(test_case.get("Device"), ge_op_mem.device)
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ test/profiler/analysis/prof_bean/test_node_info_bean.py
@@ -0,0 +1,48 @@
+import random
+from collections import namedtuple
+
+from torch_npu.profiler.analysis.prof_bean.node_info_bean import NodeInfoBean
+from torch_npu.profiler.analysis.prof_common_func.constant import convert_us2ns
+from torch_npu.testing.testcase import TestCase, run_tests
+
+
+class TestNodeInfoBean(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.kernel_num = 3
+        cls.sample_num = 3
+        cls.samples = cls.generate_samples()
+
+    @classmethod
+    def generate_samples(cls):
+        samples = []
+        for _ in range(cls.sample_num):
+            kernel_list = []
+            Kernel = namedtuple("Kernel", ["ts", "dur", "is_ai_core"])
+            for _ in range(cls.kernel_num):
+                ts = random.randint(0, 2**31)
+                dur = random.random() * 100
+                is_ai_core = True if random.choice([0, 1]) else False
+                kernel = Kernel(ts, dur, is_ai_core)
+                kernel_list.append(kernel)
+            samples.append(kernel_list)
+        return samples
+
+    def test_property(self):
+        for sample in self.samples:
+            node_indo_bean = NodeInfoBean(sample)
+            device_dur = sum([float(kernel.dur) for kernel in sample])
+            device_dur_with_ai_core = sum([float(kernel.dur) for kernel in sample if kernel.is_ai_core])
+            min_start = min([kernel.ts for kernel in sample])
+            max_end = max([kernel.ts + convert_us2ns(kernel.dur) for kernel in sample])
+            self.assertEqual(sample, node_indo_bean.kernel_list)
+            self.assertEqual(device_dur, node_indo_bean.device_dur)
+            self.assertEqual(device_dur_with_ai_core, node_indo_bean.device_dur_with_ai_core)
+            self.assertEqual(min_start, node_indo_bean.min_start)
+            self.assertEqual(max_end, node_indo_bean.max_end)
+
+
+if __name__ == "__main__":
+    run_tests()
