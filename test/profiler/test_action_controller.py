4d894431bc532f2d5e945ab31e2cba974554c4f2
MooYeh <yangtao279@huawei.com>
2024-01-03 03:33:08 +0000

    !8729 Add UTs for Ascend PyTorch Profiler Merge pull request !8729 from MooYeh/v2.1.0

---
+++ test/profiler/test_action_controller.py
@@ -0,0 +1,54 @@
+from unittest.mock import MagicMock
+
+from torch_npu.profiler.profiler_action_controller import ProfActionController
+from torch_npu.profiler.profiler_interface import ProfInterface
+from torch_npu.profiler import ProfilerAction
+from torch_npu.testing.testcase import TestCase, run_tests
+
+
+class TestActionController(TestCase):
+
+    def setUp(self):
+        self.prof_if = ProfInterface()
+        self.prof_if.init_trace = MagicMock(name='init_trace')
+        self.prof_if.start_trace = MagicMock(name='start_trace')
+        self.prof_if.stop_trace = MagicMock(name='stop_trace')
+        self.prof_if.finalize_trace = MagicMock(name='finalize_trace')
+        self.on_trace_ready = MagicMock(name='on_trace_ready')
+        self.action_controller = ProfActionController(self.prof_if, self.on_trace_ready)
+        self.test_cases = [
+            # call_cnt_list: [init_trace, start_trace, stop_trace, finalize_trace, on_trace_ready]
+            [ProfilerAction.NONE, ProfilerAction.NONE, [0, 0, 0, 0, 0]],
+            [ProfilerAction.NONE, ProfilerAction.WARMUP, [1, 0, 0, 0, 0]],
+            [ProfilerAction.NONE, ProfilerAction.RECORD, [2, 1, 0, 0, 0]],
+            [ProfilerAction.NONE, ProfilerAction.RECORD_AND_SAVE, [3, 2, 0, 0, 0]],
+            [ProfilerAction.WARMUP, ProfilerAction.NONE, [3, 3, 1, 1, 0]],
+            [ProfilerAction.WARMUP, ProfilerAction.WARMUP, [3, 3, 1, 1, 0]],
+            [ProfilerAction.WARMUP, ProfilerAction.RECORD, [3, 4, 1, 1, 0]],
+            [ProfilerAction.WARMUP, ProfilerAction.RECORD_AND_SAVE, [3, 5, 1, 1, 0]],
+            [ProfilerAction.RECORD, ProfilerAction.NONE, [3, 5, 2, 2, 0]],
+            [ProfilerAction.RECORD, ProfilerAction.WARMUP, [3, 5, 3, 3, 0]],
+            [ProfilerAction.RECORD, ProfilerAction.RECORD, [3, 5, 3, 3, 0]],
+            [ProfilerAction.RECORD, ProfilerAction.RECORD_AND_SAVE, [3, 5, 3, 3, 0]],
+            [ProfilerAction.RECORD_AND_SAVE, ProfilerAction.NONE, [3, 5, 4, 4, 1]],
+            [ProfilerAction.RECORD_AND_SAVE, ProfilerAction.WARMUP, [4, 5, 5, 5, 2]],
+            [ProfilerAction.RECORD_AND_SAVE, ProfilerAction.RECORD, [5, 6, 6, 6, 3]],
+            [ProfilerAction.RECORD_AND_SAVE, ProfilerAction.RECORD_AND_SAVE, [6, 7, 7, 7, 4]],
+            [ProfilerAction.WARMUP, None, [6, 8, 8, 8, 4]],
+            [ProfilerAction.RECORD, None, [6, 8, 9, 9, 5]],
+            [ProfilerAction.RECORD_AND_SAVE, None, [6, 8, 10, 10, 6]],
+            [None, None, [6, 8, 10, 10, 6]],
+        ]
+
+    def test_transit_action(self):
+        for prev, current, call_cnt_list in self.test_cases:
+            self.action_controller.transit_action(prev, current)
+            self.assertEqual(self.prof_if.init_trace.call_count, call_cnt_list[0])
+            self.assertEqual(self.prof_if.start_trace.call_count, call_cnt_list[1])
+            self.assertEqual(self.prof_if.stop_trace.call_count, call_cnt_list[2])
+            self.assertEqual(self.prof_if.finalize_trace.call_count, call_cnt_list[3])
+            self.assertEqual(self.on_trace_ready.call_count, call_cnt_list[4])
+
+
+if __name__ == "__main__":
+    run_tests()
