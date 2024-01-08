9919eee091a94aaf8ef8f93a7efa1ab3a4809c66
shaoyf <shaoyifan1@huawei.com>
2024-01-08 12:57:23 +0000

    !8818 add patch for optim Merge pull request !8818 from shaoyf/optim/210_patch

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -8,5 +8,6 @@ from .npu_intercept import cann_package_check, add_intercept_methods
 from .dtensor import register_ops_under_dtensor_rules
 from .collect_env import add_collect_env_methods
 from ._dynamo import add_dynamo_methods
+from ._optim import add_optim_method
 
 __all__ = ["save_async"]
