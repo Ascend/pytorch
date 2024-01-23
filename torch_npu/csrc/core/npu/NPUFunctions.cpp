1978755509a92dc7f75e7d5f8dc7078d76d33c22
chuboning <chuboning@foxmail.com>
2024-01-23 11:30:26 +0000

    !9066 Update device management functions Merge pull request !9066 from chuboning/v2.2.0

--- torch_npu/csrc/core/npu/NPUFunctions.cpp
+++ torch_npu/csrc/core/npu/NPUFunctions.cpp
@@ -3,9 +3,34 @@
 #include "torch_npu/csrc/core/npu/NPUFunctions.h"
 
 namespace c10_npu {
+    static uint32_t dev_count = 0;
     static thread_local int local_device = -1;
     static std::unordered_map<int8_t, aclrtContext> used_devices;
     std::mutex mtx;
+
+    c10::DeviceIndex device_count() noexcept
+    {
+        // initialize number of devices only once
+        if (dev_count == 0) {
+            aclError error = aclrtGetDeviceCount(&dev_count);
+            if (error != ACL_ERROR_NONE) {
+                ASCEND_LOGE("get device count of NPU failed");
+                return 0;
+            }
+            return static_cast<c10::DeviceIndex>(dev_count);
+        }
+        return static_cast<c10::DeviceIndex>(dev_count);
+    }
+
+    c10::DeviceIndex device_count_ensure_non_zero()
+    {
+        unsigned int count = 0;
+
+        NPU_CHECK_ERROR(aclrtGetDeviceCount(&count));
+        TORCH_CHECK(count, "No NPUs are available");
+
+        return static_cast<c10::DeviceIndex>(count);
+    }
 
     aclError GetDevice(int32_t *device)
     {
@@ -70,5 +95,22 @@ namespace c10_npu {
             return nullptr;
         }
         return used_devices[device];
+    }
+
+    void set_device(c10::DeviceIndex device)
+    {
+        NPU_CHECK_ERROR(c10_npu::SetDevice(device));
+    }
+
+    void device_synchronize()
+    {
+        NPU_CHECK_ERROR(aclrtSynchronizeDevice());
+    }
+
+    int ExchangeDevice(int device)
+    {
+        NPU_CHECK_ERROR(SetDevice(device));
+
+        return device;
     }
 }
