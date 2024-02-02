0b9f8993d846f7813b6075a9bef3c788b0553417
chuboning <chuboning@foxmail.com>
2024-02-02 02:15:07 +0000

    !9298 Hiding functions that should not be exposed Merge pull request !9298 from chuboning/master

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -4,7 +4,7 @@ from .storage import add_storage_methods
 from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
 from .serialization import add_serialization_methods
 from .npu_intercept import cann_package_check, add_intercept_methods
-from .dtensor import register_ops_under_dtensor_rules
+from .dtensor import _register_ops_under_dtensor_rules
 from .collect_env import add_collect_env_methods
 from ._dynamo import add_dynamo_methods
 from ._dynamo_device import _dynamo_register_interface_for_device
