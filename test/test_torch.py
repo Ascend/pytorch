bc3f876a33e880fd961b379f743dd7fe4d17e463
yuhaiyan <yuhaiyan8@huawei.com>
2024-02-02 02:14:04 +0000

    !9241 Skip the failed test cases in test_torch.py. Merge pull request !9241 from yuhaiyan/v2.2.0-dev3

--- test/test_torch.py
+++ test/test_torch.py
@@ -61,7 +61,7 @@ from torch.testing._internal.common_dtype import (
     get_all_qint_dtypes,
 )
 
-# Protects against includes accidentally setting the default dtype
+# Protects against includes accidentally setting the default dtype 
 assert torch.get_default_dtype() is torch.float32
 
 # load_tests from torch.testing._internal.common_utils is used to automatically filter tests for
