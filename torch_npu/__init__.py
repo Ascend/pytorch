b2168470dd9cad85160f7be5c9462a4a5d936f7d
王姜奔 <wangjiangben@huawei.com>
2024-02-05 13:03:01 +0000

    !9454 Use raise instead of exit to ensure that errors can be captured. Merge pull request !9454 from 王姜奔/v1.11.0

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -32,25 +32,17 @@ try:
     import torch_npu.npu
 except ImportError as e:
     if "libhccl.so" in str(e):
-        ei = sys.exc_info()
-        if ("ASCEND_OPP_PATH" in os.environ):
-            newErr = ImportError(str(ei[1]) + ". Please check that the compiler package is installed. " \
-                                 "Please run 'source set_env.sh' in the CANN installation path.")
-        else :
-            newErr = ImportError(str(ei[1]) + ". Please check that the cann package is installed. " \
-                                 "Please run 'source set_env.sh' in the CANN installation path.")
-        traceback.print_exception(ei[0], newErr, ei[2])
-        sys.exit()
-
-    if "libascendcl.so" in str(e):
-        ei = sys.exc_info()
-        newErr = ImportError(str(ei[1]) + ". Please check that the runtime package is installed. " \
-                             "Please run 'source set_env.sh' in the CANN installation path.")
-        traceback.print_exception(ei[0], newErr, ei[2])
-        sys.exit()
-
-    else:
-        traceback.print_exc()
+        if "ASCEND_OPP_PATH" in os.environ:
+            e.msg += ". Please check that the compiler package is installed. "\
+                       "Please run 'source set_env.sh' in the CANN installation path."
+        else:
+            e.msg += ". Please check that the cann package is installed. "\
+                       "Please run 'source set_env.sh' in the CANN installation path."
+    elif "libascendcl.so" in str(e):
+        e.msg += ". Please check that the runtime package is installed. "\
+                   "Please run 'source set_env.sh' in the CANN installation path."
+    raise
+    
 import torch_npu.npu.amp
 import torch_npu.npu.aclnn
 import torch_npu.distributed
