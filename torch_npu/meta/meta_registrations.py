137bcc130da016964471c60e6464c8a4f78e4b85
提军伟 <tijunwei@huawei.com>
2024-02-05 13:04:48 +0000

    !9451 ifa suport a8w8 quant in v2.2.0 Merge pull request !9451 from 提军伟/v2.2.0

--- torch_npu/meta/meta_registrations.py
+++ torch_npu/meta/meta_registrations.py
@@ -13,7 +13,17 @@ m = Library("npu", "IMPL", "Meta")
 
 @impl(m, "npu_incre_flash_attention")
 def npu_incre_flash_attention_forward(query, key, value, *, padding_mask=None, atten_mask=None, pse_shift=None, actual_seq_lengths=None,
-                                      num_heads=1, scale_value=1.0, input_layout="BSH", num_key_value_heads=0):
+                                      antiquant_scale=None, antiquant_offset=None, block_table=None,
+                                      dequant_scale1=None, quant_scale1=None, dequant_scale2=None, quant_scale2=None,
+                                      quant_offset2=None, num_heads=1, scale_value=1.0, input_layout="BSH",
+                                      num_key_value_heads=0, block_size=0, inner_precise=1):
+    if quant_scale2 is not None:
+        return torch.empty_like(query, dtype=torch.int8)
+    elif query.dtype == torch.int8:
+        return torch.empty_like(query, dtype=torch.half)
+    else:
+        return torch.empty_like(query)
+
     return torch.empty_like(query)
 
 
