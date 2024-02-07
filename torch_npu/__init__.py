adc7212ba1d516deb50156c84a5b352538d95cda
shaoyf <shaoyifan1@huawei.com>
2024-02-07 07:21:07 +0000

    !9505 Add test cases and fix possible problems with cp in some environments Merge pull request !9505 from shaoyf/doc/210

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -45,6 +45,7 @@ from torch_npu.distributed.optim.zero_redundancy_optimizer import _get_optimizer
 from .version import __version__ as __version__
 from .meta import meta_registrations
 from . import _op_plugin_docs
+del _op_plugin_docs
 
 
 cann_package_check()
