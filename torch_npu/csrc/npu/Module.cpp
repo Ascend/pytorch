dba8413e20efe73a5675a9686b13e0454e7a0e32
dongwenbo6 <dongwenbo6@huawei.com>
2024-02-27 12:11:07 +0000

    !9730 add device synchronize for finalize_dump Merge pull request !9730 from dongwenbo6/v2.1.0

--- torch_npu/csrc/npu/Module.cpp
+++ torch_npu/csrc/npu/Module.cpp
@@ -700,6 +700,7 @@ PyObject* THNPModule_setDump(PyObject* _unused, PyObject* arg)
 
 PyObject* THNPModule_finalizeDump(PyObject* _unused, PyObject* noargs)
 {
+    c10_npu::npuSynchronizeDevice();
     HANDLE_TH_ERRORS
     pybind11::gil_scoped_release no_gil;
     NPU_CHECK_ERROR(aclmdlFinalizeDump());
