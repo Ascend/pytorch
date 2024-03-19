25e9bc2d8a40a7e7b5a057103a57a7bfb52aae40
关龙锋 <guanlongfeng1@huawei.com>
2024-03-19 07:51:36 +0000

    !10338 support to conv.allow_hf32 to aclnn.allow_hf32 Merge pull request !10338 from 关龙锋/cherry-pick-1710759350

--- torch_npu/npu/aclnn/backends.py
+++ torch_npu/npu/aclnn/backends.py
@@ -1,8 +1,4 @@
-import sys
 import warnings
-
-from torch.backends import ContextProp, PropModule
-import torch_npu._C
 
 
 def version():
@@ -11,30 +7,3 @@ def version():
     """
     warnings.warn("torch.npu.aclnn.version isn't implemented!")
     return None
-
-
-def _set_allow_conv_hf32(_enabled: bool):
-    r"""Set the device supports conv operation hf32.
-    Args:
-        Switch for hf32.
-    """
-    option = {"ALLOW_CONV_HF32": "enable" if _enabled else "disable"}
-    torch_npu._C._npu_setOption(option)
-
-
-def _get_allow_conv_hf32() -> bool:
-    r"""Return the device supports conv operation hf32 is enabled or not.
-    """
-    hf32_value = torch_npu._C._npu_getOption("ALLOW_CONV_HF32")
-    return (hf32_value is None) or (hf32_value.decode() == "") or (hf32_value.decode() == "enable")
-
-
-class AclnnModule(PropModule):
-    def __init__(self, m, name):
-        super().__init__(m, name)
-
-    allow_hf32 = ContextProp(_get_allow_conv_hf32, _set_allow_conv_hf32)
-
-
-sys.modules[__name__] = AclnnModule(sys.modules[__name__], __name__)
-allow_hf32: bool
