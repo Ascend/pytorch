03a3bb12eb4f167756112755ceaf7d3d041cbcf9
刘嘉巍 <843972097@qq.com>
2024-03-21 14:00:48 +0000

    !10372 [Fix] Modify empty api and add check for complex type. Merge pull request !10372 from 刘嘉巍/v2.1.0-6.0.rc1

--- torch_npu/csrc/aten/common/TensorFactories.cpp
+++ torch_npu/csrc/aten/common/TensorFactories.cpp
@@ -95,42 +95,45 @@ at::Tensor NPUNativeFunctions::empty(c10::IntArrayRef size,
                                      c10::optional<c10::Device> device_opt,
                                      c10::optional<bool> pin_memory_opt,
                                      c10::optional<c10::MemoryFormat> memory_format_opt) {
-  auto device_ = c10::device_or_default(device_opt);
-  AT_ASSERT(device_.type() == c10::DeviceType::PrivateUse1, OPS_ERROR(ErrCode::PARAM));
-  torch_npu::utils::maybe_initialize_npu(device_);
-  TORCH_CHECK(!pinned_memory_or_default(pin_memory_opt), "Only dense CPU tensors can be pinned",
-              OPS_ERROR(ErrCode::NOT_SUPPORT));
-  check_size_nonnegative(size);
-  c10_npu::NPUGuard guard(device_);
-  c10::Allocator *allocator = c10_npu::NPUCachingAllocator::get();
-  int64_t nelements = c10::multiply_integers(size);
-  auto dtype = c10::scalarTypeToTypeMeta(dtype_or_default(dtype_opt));
-  int64_t size_bytes = nelements * dtype.itemsize();
-  c10::intrusive_ptr<c10::StorageImpl> storage_impl = c10::make_intrusive<torch_npu::NPUStorageImpl>(
-      c10::StorageImpl::use_byte_size_t(),
-      size_bytes,
-      allocator->allocate(size_bytes),
-      allocator,
-      true);
+    auto device_ = c10::device_or_default(device_opt);
+    AT_ASSERT(device_.type() == c10::DeviceType::PrivateUse1, OPS_ERROR(ErrCode::PARAM));
+    torch_npu::utils::maybe_initialize_npu(device_);
+    TORCH_CHECK(!(at::isComplexType(dtype_or_default(dtype_opt)) && !at_npu::native::env::CheckJitDisable()),
+                "Current settings do not support Complex dtype. Please try again with jit_compile=False.",
+                OPS_ERROR(ErrCode::NOT_SUPPORT));
+    TORCH_CHECK(!pinned_memory_or_default(pin_memory_opt), "Only dense CPU tensors can be pinned",
+                OPS_ERROR(ErrCode::NOT_SUPPORT));
+    check_size_nonnegative(size);
+    c10_npu::NPUGuard guard(device_);
+    c10::Allocator *allocator = c10_npu::NPUCachingAllocator::get();
+    int64_t nelements = c10::multiply_integers(size);
+    auto dtype = c10::scalarTypeToTypeMeta(dtype_or_default(dtype_opt));
+    int64_t size_bytes = nelements * dtype.itemsize();
+    c10::intrusive_ptr<c10::StorageImpl> storage_impl = c10::make_intrusive<torch_npu::NPUStorageImpl>(
+        c10::StorageImpl::use_byte_size_t(),
+        size_bytes,
+        allocator->allocate(size_bytes),
+        allocator,
+        true);
 
-  auto tensor =
-      at::detail::make_tensor<torch_npu::NPUTensorImpl>(storage_impl, dtype);
+    auto tensor =
+        at::detail::make_tensor<torch_npu::NPUTensorImpl>(storage_impl, dtype);
 
-  // Default at::TensorImpl has size [0]
-  if (size.size() != 1 || size[0] != 0)
-  {
-    tensor.unsafeGetTensorImpl()->set_sizes_contiguous(size);
-  }
-  auto memory_format =
-      memory_format_opt.value_or(c10::MemoryFormat::Contiguous);
-  TORCH_CHECK(
-      memory_format == c10::MemoryFormat::Contiguous,
-      "Only c10::MemoryFormat::Contiguous is supported for creating a npu tensor",
-      OPS_ERROR(ErrCode::NOT_SUPPORT));
-  tensor.unsafeGetTensorImpl()->empty_tensor_restride(memory_format);
-  StorageDescHelper::SetDesc(tensor, size, tensor.strides());
+    // Default at::TensorImpl has size [0]
+    if (size.size() != 1 || size[0] != 0)
+    {
+        tensor.unsafeGetTensorImpl()->set_sizes_contiguous(size);
+    }
+    auto memory_format =
+        memory_format_opt.value_or(c10::MemoryFormat::Contiguous);
+    TORCH_CHECK(
+        memory_format == c10::MemoryFormat::Contiguous,
+        "Only c10::MemoryFormat::Contiguous is supported for creating a npu tensor",
+        OPS_ERROR(ErrCode::NOT_SUPPORT));
+    tensor.unsafeGetTensorImpl()->empty_tensor_restride(memory_format);
+    StorageDescHelper::SetDesc(tensor, size, tensor.strides());
 
-  return tensor;
+    return tensor;
 }
 
 at::Tensor empty_like_npu(
@@ -273,37 +276,40 @@ at::Tensor NPUNativeFunctions::empty_with_format(c10::IntArrayRef size,
                                                  c10::optional<c10::Device> device_opt,
                                                  c10::optional<bool> pin_memory_opt,
                                                  int64_t dst_format) {
-  auto device_ = c10::device_or_default(device_opt);
-  torch_npu::utils::torch_check_npu(device_);
-  torch_npu::utils::maybe_initialize_npu(device_);
-  TORCH_CHECK(!pinned_memory_or_default(pin_memory_opt), "Only dense CPU tensors can be pinned",
-              OPS_ERROR(ErrCode::NOT_SUPPORT));
-  check_size_nonnegative(size);
-  c10_npu::NPUGuard guard(device_);
-  c10::Allocator *allocator = c10_npu::NPUCachingAllocator::get();
-  // when the shape and format are not match, fix format here.
-  aclFormat format = InferFormat::GuessStorageFormat(size, (aclFormat)dst_format);
-  int64_t nelements = StorageDescHelper::GetMemorySize(size, format);
-  auto dtype = c10::scalarTypeToTypeMeta(dtype_or_default(dtype_opt));
-  int64_t size_bytes = nelements * dtype.itemsize();
-  c10::intrusive_ptr<c10::StorageImpl> storage_impl = c10::make_intrusive<torch_npu::NPUStorageImpl>(
-      c10::StorageImpl::use_byte_size_t(),
-      size_bytes,
-      allocator->allocate(size_bytes),
-      allocator,
-      true);
+    auto device_ = c10::device_or_default(device_opt);
+    torch_npu::utils::torch_check_npu(device_);
+    torch_npu::utils::maybe_initialize_npu(device_);
+    TORCH_CHECK(!(at::isComplexType(dtype_or_default(dtype_opt)) && !at_npu::native::env::CheckJitDisable()),
+                "Current settings do not support Complex dtype. Please try again with jit_compile=False.",
+                OPS_ERROR(ErrCode::NOT_SUPPORT));
+    TORCH_CHECK(!pinned_memory_or_default(pin_memory_opt), "Only dense CPU tensors can be pinned",
+                OPS_ERROR(ErrCode::NOT_SUPPORT));
+    check_size_nonnegative(size);
+    c10_npu::NPUGuard guard(device_);
+    c10::Allocator *allocator = c10_npu::NPUCachingAllocator::get();
+    // when the shape and format are not match, fix format here.
+    aclFormat format = InferFormat::GuessStorageFormat(size, (aclFormat)dst_format);
+    int64_t nelements = StorageDescHelper::GetMemorySize(size, format);
+    auto dtype = c10::scalarTypeToTypeMeta(dtype_or_default(dtype_opt));
+    int64_t size_bytes = nelements * dtype.itemsize();
+    c10::intrusive_ptr<c10::StorageImpl> storage_impl = c10::make_intrusive<torch_npu::NPUStorageImpl>(
+        c10::StorageImpl::use_byte_size_t(),
+        size_bytes,
+        allocator->allocate(size_bytes),
+        allocator,
+        true);
 
-  auto tensor =
-      at::detail::make_tensor<torch_npu::NPUTensorImpl>(storage_impl, dtype);
+    auto tensor =
+        at::detail::make_tensor<torch_npu::NPUTensorImpl>(storage_impl, dtype);
 
-  // Default NPUTensorImpl has size [0]
-  if (size.size() != 1 || size[0] != 0)
-  {
-    tensor.unsafeGetTensorImpl()->set_sizes_contiguous(size);
-  }
-  tensor.unsafeGetTensorImpl()->empty_tensor_restride(c10::MemoryFormat::Contiguous);
-  StorageDescHelper::SetDesc(tensor, size, tensor.strides(), format);
-  return tensor;
+    // Default NPUTensorImpl has size [0]
+    if (size.size() != 1 || size[0] != 0)
+    {
+        tensor.unsafeGetTensorImpl()->set_sizes_contiguous(size);
+    }
+    tensor.unsafeGetTensorImpl()->empty_tensor_restride(c10::MemoryFormat::Contiguous);
+    StorageDescHelper::SetDesc(tensor, size, tensor.strides(), format);
+    return tensor;
 }
 
 at::Tensor NPUNativeFunctions::unsafe_empty_with_format(c10::IntArrayRef size,
