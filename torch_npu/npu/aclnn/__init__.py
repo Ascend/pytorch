4ab240282209a7251dfdf139f8b9f8fd5f2c4e1b
关龙锋 <guanlongfeng1@huawei.com>
2024-03-22 02:08:00 +0000

    !10277 support to conv.allow_hf32 to aclnn.allow_hf32 Merge pull request !10277 from 关龙锋/v2.1.0

--- torch_npu/npu/aclnn/__init__.py
+++ torch_npu/npu/aclnn/__init__.py
@@ -3,4 +3,56 @@ __all__ = [
     "allow_hf32",
 ]
 
-from .backends import version, allow_hf32
+import sys
+
+from contextlib import contextmanager
+from torch.backends import __allow_nonbracketed_mutation, ContextProp, PropModule
+
+import torch_npu._C
+from .backends import version
+
+
+def _set_allow_conv_hf32(_enabled: bool):
+    r"""Set the device supports conv operation hf32.
+    Args:
+        Switch for hf32.
+    """
+    option = {"ALLOW_CONV_HF32": "enable" if _enabled else "disable"}
+    torch_npu._C._npu_setOption(option)
+
+
+def _get_allow_conv_hf32() -> bool:
+    r"""Return the device supports conv operation hf32 is enabled or not.
+    """
+    hf32_value = torch_npu._C._npu_getOption("ALLOW_CONV_HF32")
+    return (hf32_value is None) or (hf32_value.decode() == "") or (hf32_value.decode() == "enable")
+
+
+def set_flags(_allow_hf32=None, ):
+    orig_flags = (_get_allow_conv_hf32(),)
+    if _allow_hf32 is not None:
+        _set_allow_conv_hf32(_allow_hf32)
+    return orig_flags
+
+
+@contextmanager
+def flags(allow_hf32=True, ):
+    with __allow_nonbracketed_mutation():
+        orig_flags = set_flags(allow_hf32)
+    try:
+        yield
+    finally:
+        # recover the previous values
+        with __allow_nonbracketed_mutation():
+            set_flags(*orig_flags)
+
+
+class AclnnModule(PropModule):
+    def __init__(self, m, name):
+        super().__init__(m, name)
+
+    allow_hf32 = ContextProp(_get_allow_conv_hf32, _set_allow_conv_hf32)
+
+
+sys.modules[__name__] = AclnnModule(sys.modules[__name__], __name__)
+allow_hf32: bool
