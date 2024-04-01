d1000e91547ac7b3925171019bdaad853b2f4345
dannyAys <anyunshan@huawei.com>
2024-04-01 13:45:12 +0000

    !10882 FIA op enables PTA - Meta Infer & Onnx Merge pull request !10882 from dannyAys/v2.1.0

--- torch_npu/meta/meta_registrations.py
+++ torch_npu/meta/meta_registrations.py
@@ -37,6 +37,21 @@ def npu_prompt_flash_attention_forward(query, key, value, *, padding_mask=None, 
         return torch.empty_like(query, dtype=query.dtype)
 
 
+@impl(m, "npu_fused_infer_attention_score")
+def npu_fused_infer_attention_score_forward(query, key, value, *, pse_shift = None, atten_mask = None, actual_seq_lengths = None, actual_seq_lengths_kv = None,
+                                      dequant_scale1 = None, quant_scale1 = None, dequant_scale2 = None, quant_scale2 = None,
+                                      quant_offset2 = None, antiquant_scale = None, antiquant_offset = None, block_table = None,
+                                      query_padding_size = None, kv_padding_size = None,
+                                      num_heads = 1, scale = 1.0, pre_tokens = 2147483647, next_tokens = 2147483647, input_layout = "BSH", num_key_value_heads = 0,
+                                      sparse_mode = 0, inner_precise = 0, block_size = 0, antiquant_mode = 0, softmax_lse_flag = False):
+    if quant_scale2 is not None:
+        return (torch.empty_like(query, dtype=torch.int8), torch.empty_like(query))
+    elif query.dtype == torch.int8:
+        return (torch.empty_like(query, dtype=torch.half), torch.empty_like(query))
+    else:
+        return (torch.empty_like(query), torch.empty_like(query))
+
+
 @impl(m, "npu_fusion_attention")
 def npu_fusion_attention_forward(query, key, value, head_num, input_layout, pse=None, padding_mask=None,
                                 atten_mask=None, scale=1.0, keep_prob=1.0, pre_tockens=2147483647, next_tockens=2147483647,
