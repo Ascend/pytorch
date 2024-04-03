00bda56d300f35c22bf8eabd26c52484c60887f5
cheng <wuzicheng2@huawei.com>
2024-04-03 10:37:20 +0000

    !10895 modify the process_number of Pool in online analyze scene Merge pull request !10895 from cheng/v1.11.0

--- torch_npu/profiler/analysis/npu_profiler.py
+++ torch_npu/profiler/analysis/npu_profiler.py
@@ -35,7 +35,8 @@ class NpuProfiler:
             return
         # 多profiling数据的解析
         multiprocessing.set_start_method("fork", force=True)
-        process_number = kwargs.get("max_process_number", Constant.DEFAULT_PROCESS_NUMBER)
+        process_number = min(kwargs.get("max_process_number", Constant.DEFAULT_PROCESS_NUMBER),
+                             len(profiler_path_list))
         pool = NoDaemonProcessPool(processes=process_number)
         for profiler_path in profiler_path_list:
             PathManager.check_directory_path_writeable(profiler_path)
