9dde65a75e11a7f1659586dab585499b255dc6a1
梅飞要 <meifeiyao@huawei.com>
2024-04-23 09:50:45 +0000

    !11277 [PROF]delete e2e profiling residual code Merge pull request !11277 from 梅飞要/dbmain

--- third_party/acl/inc/acl/acl_msprof.h
+++ third_party/acl/inc/acl/acl_msprof.h
@@ -31,43 +31,6 @@ typedef enum {
     ACL_PROF_ARGS_MAX,
 } aclprofConfigType;
 
-ACL_FUNC_VISIBILITY void *aclprofCreateStamp();
-
-/**
-
-@ingroup AscendCL
-@yutong zhang destroy aclprofStamp pointer
-@retval void
-*/
-ACL_FUNC_VISIBILITY void aclprofDestroyStamp(void *stamp);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetStampTagName(void *stamp, const char *tagName, uint16_t len);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetCategoryName(uint32_t category, const char *categoryName);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetStampCategory(void *stamp, uint32_t category);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetStampPayload(void *stamp, const int32_t type, void *value);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetStampTraceMessage(void *stamp, const char *msg, uint32_t msgLen);
-
-ACL_FUNC_VISIBILITY aclError aclprofSetStampCallStack(void *stamp, const char *callStack, uint32_t len);
-
-ACL_FUNC_VISIBILITY aclError aclprofMsproftxSwitch(bool isOpen);
-
-ACL_FUNC_VISIBILITY aclError aclprofMark(void *stamp);
-
-ACL_FUNC_VISIBILITY aclError aclprofPush(void *stamp);
-
-ACL_FUNC_VISIBILITY aclError aclprofPop();
-
-ACL_FUNC_VISIBILITY aclError aclprofRangeStart(void *stamp, uint32_t *rangeId);
-
-ACL_FUNC_VISIBILITY aclError aclprofRangeStop(uint32_t rangeId);
-
-ACL_FUNC_VISIBILITY aclError aclprofReportStamp(
-    const char *tag, unsigned int tagLen, unsigned char *data, unsigned int dataLen);
-
 ACL_FUNC_VISIBILITY aclError aclprofSetConfig(aclprofConfigType configType, const char* config, size_t configLength);
 
 #ifdef __cplusplus
