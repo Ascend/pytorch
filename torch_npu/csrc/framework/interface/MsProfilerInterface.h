989256ef531188a363cc950c8feabf86a1b195f8
梅飞要 <meifeiyao@huawei.com>
2024-04-23 09:50:52 +0000

    !11278 [PROF]delete e2e profiling residual code Merge pull request !11278 from 梅飞要/db1.11.0

--- torch_npu/csrc/framework/interface/MsProfilerInterface.h
+++ torch_npu/csrc/framework/interface/MsProfilerInterface.h
@@ -22,39 +22,6 @@ namespace at_npu {
 namespace native {
 
 
-void *AclprofCreateStamp();
-
-void AclprofDestroyStamp(void *stamp);
-
-aclError AclprofSetStampTagName(void *stamp, const char *tagName, uint16_t len);
-
-aclError AclprofSetCategoryName(uint32_t category, const char *categoryName);
-
-aclError AclprofSetStampCategory(void *stamp, uint32_t category);
-
-aclError AclprofSetStampPayload(void *stamp, const int32_t type, void *value);
-
-aclError AclprofSetStampTraceMessage(void *stamp, const char *msg, uint32_t msgLen);
-
-aclError AclprofSetStampCallStack(void *stamp, const char *callStack, uint32_t len);
-
-aclError AclprofMsproftxSwitch(bool isOpen);
-
-aclError AclprofMark(void *stamp);
-
-aclError AclprofPush(void *stamp);
-
-aclError AclprofPop();
-
-aclError AclprofRangeStart(void *stamp, uint32_t *rangeId);
-
-aclError AclprofRangeStop(uint32_t rangeId);
-
-aclError AclprofReportStamp(const char *tag, unsigned int tagLen,
-                            unsigned char *data, unsigned int dataLen);
-
-bool CheckInterfaceReportStamp();
-
 aclError AclprofSetConfig(aclprofConfigType configType, const char* config, size_t configLength);
 
 }
