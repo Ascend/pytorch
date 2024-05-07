2882750a3ede460e54e0686a385d8939982b88da
tangpeiqi96 <tangpeiqi1@huawei.com>
2024-05-07 06:45:32 +0000

    !11486 Add groupRanks interface Merge pull request !11486 from tangpeiqi96/master_extended_api

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -158,10 +158,11 @@ def _new_process_group_hccl_helper(dist_backend_opts, pg_options):
     store = dist_backend_opts.store
     group_rank = dist_backend_opts.group_rank
     group_size = dist_backend_opts.group_size
-    timeout = dist_backend_opts.timeout
-    # global_ranks_in_group is used for further processGroup code updates and is not currently used.
-    global_ranks_in_group = dist_backend_opts.global_ranks_in_group
-    return torch_npu._C._distributed_c10d.ProcessGroupHCCL(store, group_rank, group_size, timeout)
+    pg_options = torch_npu._C._distributed_c10d.ProcessGroupHCCL.Options()
+    pg_options.is_high_priority_stream = False
+    pg_options._timeout = dist_backend_opts.timeout
+    pg_options.global_ranks_in_group = dist_backend_opts.global_ranks_in_group
+    return torch_npu._C._distributed_c10d.ProcessGroupHCCL(store, group_rank, group_size, pg_options)
 
 
 # init and register hccl backend
