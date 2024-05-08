6127972abb569663df5eff43d8de112f94200c64
张明宇 <zhangmingyu15@huawei.com>
2024-05-08 07:39:07 +0000

    !11358 [Feature] Add api_statistic.csv into ASCEND_PROFILER_OUTPUT when using L1 or L2 Merge pull request !11358 from 张明宇/v2.1.0

---
+++ test/profiler/analysis/prof_bean/test_api_statistic_bean.py
@@ -0,0 +1,31 @@
+from torch_npu.profiler.analysis.prof_bean.api_statistic_bean import ApiStatisticBean
+from torch_npu.testing.testcase import TestCase, run_tests
+
+
+class TestApiStatisticBean(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_cases = [
+            {
+                "Level": "acl",
+                "API Name": "aclnnMm",
+                "Time(us)": "37.77",
+                "Count": "1",
+                "Avg(us)": "37.77",
+                "Min(us)": "37.77",
+                "Max(us)": "37.77",
+                "Variance": "0"
+            },
+        ]
+
+    def test_property(self):
+        for test_case in self.test_cases:
+            api_statistic_bean = ApiStatisticBean(test_case)
+            self.assertEqual(set(test_case.keys()), set(api_statistic_bean.headers))
+            self.assertEqual(set(test_case.values()), set(api_statistic_bean.row))
+
+
+if __name__ == "__main__":
+    run_tests()
