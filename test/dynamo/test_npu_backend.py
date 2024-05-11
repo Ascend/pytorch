3366be0bda0d70c558d0d58f862146f161c02a92
杜承昆 <duchengkun1@huawei.com>
2024-05-11 09:33:45 +0000

    !11344 【torchair】support lazy init of torchair Merge pull request !11344 from 杜承昆/lazy_init_v2.2.0

---
+++ test/dynamo/test_npu_backend.py
@@ -0,0 +1,22 @@
+# Owner(s): ["module: dynamo"]
+import torch
+from torch._dynamo.test_case import TestCase
+
+import torch_npu
+
+
+class TestNpuBackend(TestCase):
+    def test_optimize_npu(self):
+        func = torch.ops.aten.relu.default
+        dynamo_func = torch._dynamo.optimize('npu')(func)
+
+        x = torch.randn(10).npu()
+        eager_result = func(x)
+        dynamo_result = dynamo_func(x)
+        self.assertEqual(eager_result, dynamo_result)
+
+
+if __name__ == "__main__":
+    from torch._dynamo.test_case import run_tests
+
+    run_tests()
