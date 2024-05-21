2021c8f15e4f5b94c8416378da9c681a8ac44e8b
郭光浩 <guoguanghao@huawei.com>
2024-05-21 06:50:55 +0000

    update setup.py.
    Signed-off-by: 郭光浩 <guoguanghao@huawei.com>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ from setuptools.command.egg_info import egg_info
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 THIRD_PARTY_PATH = os.path.join(BASE_DIR, "third_party")
-VERSION = '1.11.0.post10'
+VERSION = '1.11.0.post12'
 UNKNOWN = "Unknown"
 BUILD_PERMISSION = stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
 call_once_flag = 0
