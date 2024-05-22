5d05065929a0f7c898c9e8e2eb413da33a5bd39b
刘嘉巍 <843972097@qq.com>
2024-05-22 09:08:23 +0000

    !11913 [Feature] Add test_namedtensor & resize_ support named. Merge pull request !11913 from 刘嘉巍/cherry-pick-1716296235

--- torch_npu/csrc/aten/common/ResizeNpu.cpp
+++ torch_npu/csrc/aten/common/ResizeNpu.cpp
@@ -8,19 +8,46 @@
 namespace at_npu {
 namespace native {
 
+inline const at::Tensor& resize_named_tensor_(
+    const at::Tensor& self,
+    c10::IntArrayRef size,
+    c10::optional<c10::MemoryFormat> format)
+{
+    TORCH_INTERNAL_ASSERT(self.has_names());
+    TORCH_CHECK(
+        self.sizes() == size,
+        "Cannot resize named tensor with resize_ or resize_as_ (tried to resize "
+        "Tensor",
+        self.names(),
+        " with size ",
+        self.sizes(),
+        " to ",
+        size,
+        "). This may be caused by passing a named tensor ",
+        "as an `out=` argument; please ensure that the sizes are the same. ");
+    TORCH_CHECK(
+        !format.has_value(),
+        "Unsupported memory format for named tensor resize ",
+        format.value());
+    return self;
+}
+
 const at::Tensor& NPUNativeFunctions::resize_(
     const at::Tensor& self,
     c10::IntArrayRef size,
     c10::optional<c10::MemoryFormat> format) {
-  // because of resize _impl_npu_ only support at base format, so
-  // no need to reflush NpuStorageDesc here.
-  at::Tensor temp_self = self;
-  if (!FormatHelper::IsBaseFormatType(self)) {
-    NPUNativeFunctions::npu_format_cast_(temp_self, FormatHelper::GetBaseFormat(self));
-  }
-  auto* self_ = self.unsafeGetTensorImpl();
-  resize_impl_npu_(self_, size, c10::nullopt);
-  return self;
+    if (self.has_names()) {
+        return resize_named_tensor_(self, size, format);
+    }
+    // because of resize _impl_npu_ only support at base format, so
+    // no need to reflush NpuStorageDesc here.
+    at::Tensor temp_self = self;
+    if (!FormatHelper::IsBaseFormatType(self)) {
+        NPUNativeFunctions::npu_format_cast_(temp_self, FormatHelper::GetBaseFormat(self));
+    }
+    auto* self_ = self.unsafeGetTensorImpl();
+    resize_impl_npu_(self_, size, c10::nullopt);
+    return self;
 }
 
 const at::Tensor& NPUNativeFunctions::resize_as_(
