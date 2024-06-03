89d2b304a9fc7cc98752a5e7d1cbfe67d067b494
yuhaiyan <yuhaiyan8@huawei.com>
2024-06-03 02:46:42 +0000

    !11992 Add test files for the onnx module: test_fx_type_promotion.py/test_models_onnxruntime.py/test_models_quantized_onnxruntime.py... Merge pull request !11992 from yuhaiyan/v2.1.0-dev5

--- ci/access_control_test.py
+++ ci/access_control_test.py
@@ -26,7 +26,8 @@ SLOW_TEST_BLOCKLIST = [
     'test_ops_gradients',
     'test_reductions',
     'test_unary_ufuncs',
-    'test_ops_jit'
+    'test_ops_jit',
+    'onnx/test_op_consistency'
 ]
 
 # exclude some not run directly test files
