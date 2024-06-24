0e24b7d9467280518297a58a9b38fc7c158436fb
yuhaiyan <yuhaiyan8@huawei.com>
2024-06-24 09:02:55 +0000

    !12569 Add test files for the onnx module: test_pytorch_jit_onnx.py/test_pytorch_onnx_no_runtime.py/... Merge pull request !12569 from yuhaiyan/master-dev7

--- ci/access_control/constants.py
+++ ci/access_control/constants.py
@@ -14,7 +14,8 @@ SLOW_TEST_BLOCKLIST = [
     'test_unary_ufuncs',
     'test_ops_jit',
     'onnx/test_fx_op_consistency',
-    "onnx/test_op_consistency"
+    "onnx/test_op_consistency",
+    'onnx/test_pytorch_onnx_onnxruntime'
 ]
 
 # exclude some not run directly test files
--- test/onnx/test_fx_to_onnx_with_onnxruntime.py
+++ test/onnx/test_fx_to_onnx_with_onnxruntime.py
@@ -542,7 +542,7 @@ class TestFxToOnnxWithOnnxRuntime(onnx_test_common._TestONNXRuntime):
             additional_test_inputs=[((x2,),)],
         )
 
-    @pytorch_test_common.skipIfNoCuda
+    @pytorch_test_common.skipIfNoNPU
     def test__scaled_dot_product_flash_attention(self):
         class Foo(torch.nn.Module):
             def forward(self, x):
