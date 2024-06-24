c47718a21dcd774e49aba2641cc08f7eccfb4b0d
杜金航 <15990042527@163.com>
2024-06-24 06:31:10 +0000

    !12558 arm displays only the python stack. Merge pull request !12558 from 杜金航/v1.11.0-5.0.0

--- torch_npu/npu/memory.py
+++ torch_npu/npu/memory.py
@@ -21,6 +21,7 @@ import pickle
 import sys
 import os
 import stat
+import platform
 from typing import Any, Dict, Optional, Tuple, Union
 
 import torch_npu
@@ -583,6 +584,10 @@ def _record_memory_history_impl(
     max_entries: int = sys.maxsize,
     device=None,
 ):
+    if platform.machine() == "aarch64" and stacks == "all":
+        warnings.warn("Currently 'aarch64' does not support the display of c++ stacks, " \
+                      "changed to display only python.")
+        stacks = "python"
     torch_npu._C._npu_record_memory_history(enabled, context, stacks, max_entries)
 
 
