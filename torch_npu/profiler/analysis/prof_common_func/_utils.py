39a6a7992a4afd696dc34394adeaecc7a994ffdc
孔德硕 <1670690897@qq.com>
2024-07-24 09:01:27 +0000

    !13135 [PROF]实现环境变量采集和落盘 Merge pull request !13135 from 孔德硕/f_m

---
+++ torch_npu/profiler/analysis/prof_common_func/_utils.py
@@ -0,0 +1,16 @@
+import os
+
+
+def collect_env_vars():
+
+    collected_env_vars = {
+        "ASCEND_GLOBAL_LOG_LEVEL": os.environ.get("ASCEND_GLOBAL_LOG_LEVEL", ""),
+        "HCCL_RDMA_TC": os.environ.get("HCCL_RDMA_TC", ""),
+        "HCCL_RDMA_SL": os.environ.get("HCCL_RDMA_SL", ""),
+        "ACLNN_CACHE_LIMIT": os.environ.get("ACLNN_CACHE_LIMIT", ""),
+        "HOST_CACHE_CAPACITY": os.environ.get("HOST_CACHE_CAPACITY", ""),
+        "ASCEND_ENHANCE_ENABLE": os.environ.get("ASCEND_ENHANCE_ENABLE", ""),
+        "PYTORCH_NPU_ALLOC_CONF": os.environ.get("PYTORCH_NPU_ALLOC_CONF", "")
+    }
+
+    return {"ENV_VARIABLES": collected_env_vars}
