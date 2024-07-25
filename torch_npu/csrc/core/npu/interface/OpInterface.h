7435a3d5a265360c01e1c1ee6c3135b37ef0e781
王超 <wangchao430@huawei.com>
2024-07-25 09:45:12 +0000

    !13051 silent check v2 Merge pull request !13051 from 王超/v2.4.0_check

---
+++ torch_npu/csrc/core/npu/interface/OpInterface.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace c10_npu {
+namespace opapi {
+/**
+ * This API is used to check whether aclnnSilentCheck exist.
+*/
+bool IsExistAclnnSilentCheck();
+
+} // namespace opapi
+} // namespace c10_npu
