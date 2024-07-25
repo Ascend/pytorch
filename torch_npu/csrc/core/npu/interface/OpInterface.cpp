0ee89a514b717a967a3c8496530e5c7be666ecca
王超 <wangchao430@huawei.com>
2024-07-25 09:43:56 +0000

    !13052 silent check v2 Merge pull request !13052 from 王超/master_check

---
+++ torch_npu/csrc/core/npu/interface/OpInterface.cpp
@@ -0,0 +1,24 @@
+#include "OpInterface.h"
+#include "torch_npu/csrc/core/npu/register/FunctionLoader.h"
+
+namespace c10_npu {
+
+namespace opapi {
+#undef LOAD_FUNCTION
+#define LOAD_FUNCTION(funcName) \
+  REGISTER_FUNCTION(libopapi, funcName)
+#undef GET_FUNC
+#define GET_FUNC(funcName)           \
+  GET_FUNCTION(libopapi, funcName)
+
+REGISTER_LIBRARY(libopapi)
+LOAD_FUNCTION(aclnnSilentCheck)
+
+bool IsExistAclnnSilentCheck()
+{
+    static auto func = GET_FUNC(aclnnSilentCheck);
+    return func != nullptr;
+}
+
+} // namespace acl
+} // namespace c10
