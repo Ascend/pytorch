0017cdfc8d6d713bc46d308f65c184a87afd47af
jiangpengfei <jiangpengfei24@huawei.com>
2024-07-27 01:30:35 +0000

    !13112 Remove judgement of done in watchdog to slove the stucking problem of error exit Merge pull request !13112 from jiangpengfei/v2.2.0

--- test/distributed/test_watchdog.py
+++ test/distributed/test_watchdog.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import time
 import torch.distributed.run as launch
 from torch_npu.testing._testcase import run_tests, TestCase
 from torch_npu.testing._internal.common_distributed import skipIfUnsupportMultiNPU
@@ -11,15 +12,12 @@ def path(script):
 
 class ElasticLaunchTest(TestCase):
     @skipIfUnsupportMultiNPU(2)
-    def test_communicate_npu_watchdog_timeout(self):
+    def test_npu_watchdog_timeout(self):
         try:
             error = None
             launch.main(
                 [
-                    "--run-path",
-                    "--nnodes=1",
                     "--nproc-per-node=2",
-                    "--monitor-interval=1",
                     path("watchdog/watchdog_base.py"),
                 ]
             )
@@ -29,6 +27,26 @@ class ElasticLaunchTest(TestCase):
             if error is None:
                 raise RuntimeError("Test case fail")
 
+
+    @skipIfUnsupportMultiNPU(2)
+    def test_npu_watchdog_quick_exit(self):
+        start_time = time.time()
+        try:
+            launch.main(
+                [
+                    "--nproc-per-node=2",
+                    path("watchdog/watchdog_quick_exit.py"),
+                ]
+            )
+        except Exception:
+            print("Program fail and exit")
+            
+        end_time = time.time()
+        excution_time = end_time - start_time
+        if excution_time > 120:
+            print(f"Excution time using time.time(): {excution_time} seconds")
+            raise RuntimeError("Test case fail")
+            
 
 if __name__ == "__main__":
     run_tests()
