26d41ea69bd44ff7533c05b8fbd54373b8b78101
王超 <wangchao430@huawei.com>
2024-07-30 09:06:04 +0000

    !13313 add tensor check with silentCheckV2 Merge pull request !13313 from 王超/v2.1.0_fix

--- torch_npu/utils/_step.py
+++ torch_npu/utils/_step.py
@@ -204,8 +204,9 @@ def _custom_call(self, *args, **kwargs):
 
     if asd_enable:
         if hasattr(self, "outer") and self.outer:
-            if tmp.requires_grad:
-                tmp.register_hook(output_hook)
+            if isinstance(tmp, torch.Tensor):
+                if tmp.requires_grad:
+                    tmp.register_hook(output_hook)
             input_hook_flag = True
             weight_hook_flag = True
             first_forward = True
