1399db17febba4e2b05e585d78edf93b6c5d576b
liyou_b <2953090824@qq.com>
2024-08-07 10:28:40 +0000

    !13552 Update dynamic profiling monitor process for v2.1.0 Merge pull request !13552 from liyou_b/dyn_mon_prof_v2_10

---
+++ torch_npu/profiler/_dynamic_profiler/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['logger', 'DynamicProfilerMonitor', 'init_logger']
+
+from ._dynamic_profiler_log import logger, init_logger
+from ._dynamic_profiler_monitor import DynamicProfilerMonitor
---
+++ torch_npu/profiler/_dynamic_profiler/_dynamic_profiler_log.py
@@ -0,0 +1,26 @@
+import os
+import socket
+import logging
+import logging.handlers
+from ...utils.path_manager import PathManager
+
+logger = logging.getLogger("DynamicProfiler")
+logger_monitor = logging.getLogger("DynamicProfilerMonitor")
+
+
+def init_logger(logger_: logging.Logger, path: str, is_monitor_process=False):
+    path = os.path.join(path, 'log')
+    if not os.path.exists(path):
+        PathManager.make_dir_safety(path)
+    worker_name = "{}".format(socket.gethostname())
+    log_name = "dp_{}_{}.log".format(worker_name, os.getpid())
+    if is_monitor_process:
+        log_name = "monitor_" + log_name
+    log_file = os.path.join(path, log_name)
+    if not os.path.exists(log_file):
+        PathManager.create_file_safety(log_file)
+    handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] [%(process)d] %(filename)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger_.setLevel(logging.DEBUG)
+    logger_.addHandler(handler)
