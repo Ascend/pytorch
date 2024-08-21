24e2cabc219bc366a70cfcd45de8cf4c386646ed
张向龙3 <zhangxianglong3@huawei.com>
2024-08-21 12:13:39 +0000

    !13345 Remove foreach patch of optim Merge pull request !13345 from 张向龙3/optim4each_21

--- torch_npu/utils/_optim.py
+++ torch_npu/utils/_optim.py
@@ -1,70 +1,62 @@
-from functools import partial, partialmethod, wraps
+from functools import wraps
 
 import torch
-from torch.optim.sgd import sgd as src_sgd
-from torch.optim.rprop import rprop as src_rprop
-from torch.optim.rmsprop import rmsprop as src_rmsprop
-from torch.optim.radam import radam as src_radam
-from torch.optim.nadam import nadam as src_nadam
-from torch.optim.asgd import asgd as src_asgd
-from torch.optim.adamw import adamw as src_adamw
-from torch.optim.adamax import adamax as src_adamax
-from torch.optim.adam import adam as src_adam
-from torch.optim.adagrad import adagrad as src_adagrad
-from torch.optim.adadelta import adadelta as src_adadelta
+from torch.optim.sgd import sgd, SGD
+from torch.optim.rprop import rprop, Rprop
+from torch.optim.rmsprop import rmsprop, RMSprop
+from torch.optim.radam import radam, RAdam
+from torch.optim.nadam import nadam, NAdam
+from torch.optim.asgd import asgd, ASGD
+from torch.optim.adamw import adamw, AdamW
+from torch.optim.adamax import adamax, Adamax
+from torch.optim.adam import adam, Adam
+from torch.optim.adagrad import adagrad, Adagrad
+from torch.optim.adadelta import adadelta, Adadelta
+import torch_npu
 
 from .utils import should_print_warning
 
 
-def wrap_optim_warning_func(func, name):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not wrapper.warned and should_print_warning():
-            print(f"Warning: NPU does not support argument 'foreach' in torch.optim.{name}, "
-                  f"we set foreach=False by default. Please do not set any value for this argument.")
-            wrapper.warned = True
-        return func(*args, **kwargs)
-    wrapper.warned = False
-    return wrapper
+def is_support_foreach():
+    device_name = torch_npu.npu.get_device_name(0)
+    return device_name > "Ascend910B" and device_name < "Ascend910P"
 
 
-def partial_class(cls, *args, **kwargs):
-    cls.__init__ = partialmethod(cls.__init__, *args, **kwargs)
-
-
-sgd = wrap_optim_warning_func(partial(src_sgd, foreach=False), 'sgd')
-rprop = wrap_optim_warning_func(partial(src_rprop, foreach=False), 'rprop')
-rmsprop = wrap_optim_warning_func(partial(src_rmsprop, foreach=False), 'rmsprop')
-radam = wrap_optim_warning_func(partial(src_radam, foreach=False), 'radam')
-nadam = wrap_optim_warning_func(partial(src_nadam, foreach=False), 'nadam')
-asgd = wrap_optim_warning_func(partial(src_asgd, foreach=False), 'asgd')
-adamw = wrap_optim_warning_func(partial(src_adamw, foreach=False), 'adamw')
-adamax = wrap_optim_warning_func(partial(src_adamax, foreach=False), 'adamax')
-adam = wrap_optim_warning_func(partial(src_adam, foreach=False), 'adam')
-adagrad = wrap_optim_warning_func(partial(src_adagrad, foreach=False), 'adagrad')
-adadelta = wrap_optim_warning_func(partial(src_adadelta, foreach=False), 'adadelta')
+def monkey_patch_optimizer(optimizer):
+    @wraps(optimizer)
+    def new_optimizer(*args, **kwargs):
+        if not is_support_foreach():
+            if should_print_warning():
+                print(f"Warning: NPU does not support argument 'foreach' in this device type, "
+                      f"we set foreach=False by default. Please do not set any value for this argument.")
+            kwargs['foreach'] = False
+        if 'foreach' in kwargs:
+            kwargs['foreach'] = is_support_foreach() and kwargs['foreach']
+        return optimizer(*args, **kwargs)
+    
+    return new_optimizer
 
 
 def add_optim_method():
-    partial_class(torch.optim.SGD, foreach=False)
-    torch.optim.sgd = sgd
-    partial_class(torch.optim.Rprop, foreach=False)
-    torch.optim.rprop = rprop
-    partial_class(torch.optim.RMSprop, foreach=False)
-    torch.optim.rmsprop = rmsprop
-    partial_class(torch.optim.RAdam, foreach=False)
-    torch.optim.radam = radam
-    partial_class(torch.optim.NAdam, foreach=False)
-    torch.optim.nadam = nadam
-    partial_class(torch.optim.ASGD, foreach=False)
-    torch.optim.asgd = asgd
-    partial_class(torch.optim.AdamW, foreach=False)
-    torch.optim.adamw = adamw
-    partial_class(torch.optim.Adamax, foreach=False)
-    torch.optim.adamax = adamax
-    partial_class(torch.optim.Adam, foreach=False)
-    torch.optim.adam = adam
-    partial_class(torch.optim.Adagrad, foreach=False)
-    torch.optim.adagrad = adagrad
-    partial_class(torch.optim.Adadelta, foreach=False)
-    torch.optim.adadelta = adadelta
+    torch.optim.SGD = monkey_patch_optimizer(SGD)
+    torch.optim.sgd = monkey_patch_optimizer(sgd)
+    torch.optim.Rprop = monkey_patch_optimizer(Rprop)
+    torch.optim.rprop = monkey_patch_optimizer(rprop)
+    torch.optim.RMSprop = monkey_patch_optimizer(RMSprop)
+    torch.optim.rmsprop = monkey_patch_optimizer(rmsprop)
+    torch.optim.RAdam = monkey_patch_optimizer(RAdam)
+    torch.optim.radam = monkey_patch_optimizer(radam)
+    torch.optim.NAdam = monkey_patch_optimizer(NAdam)
+    torch.optim.nadam = monkey_patch_optimizer(nadam)
+    torch.optim.ASGD = monkey_patch_optimizer(ASGD)
+    torch.optim.asgd = monkey_patch_optimizer(asgd)
+    torch.optim.AdamW = monkey_patch_optimizer(AdamW)
+    torch.optim.adamw = monkey_patch_optimizer(adamw)
+    torch.optim.Adamax = monkey_patch_optimizer(Adamax)
+    torch.optim.adamax = monkey_patch_optimizer(adamax)
+    torch.optim.Adam = monkey_patch_optimizer(Adam)
+    torch.optim.adam = monkey_patch_optimizer(adam)
+    torch.optim.Adagrad = monkey_patch_optimizer(Adagrad)
+    torch.optim.adagrad = monkey_patch_optimizer(adagrad)
+    torch.optim.Adadelta = monkey_patch_optimizer(Adadelta)
+    torch.optim.adadelta = monkey_patch_optimizer(adadelta)
