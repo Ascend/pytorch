e1f0cd9e52f57bbd3c88ad038ec61bc903ba71db
chuboning <chuboning@foxmail.com>
2024-08-26 12:44:28 +0000

    !13489 Profiling adds external query interface Merge pull request !13489 from chuboning/v2.2.0

--- torch_npu/csrc/core/npu/npu_log.h
+++ torch_npu/csrc/core/npu/npu_log.h
@@ -16,24 +16,24 @@
 #define ASCEND_LOGE(fmt, ...)                                                                           \
     do {                                                                                                \
         if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_ERROR)) {                             \
-            aclAppLog(ACL_ERROR, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);    \
+            aclAppLog(ACL_ERROR, __FILE__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);        \
         }                                                                                               \
     } while (0);
 #define ASCEND_LOGW(fmt, ...)                                                                           \
     do {                                                                                                \
         if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_WARNING)) {                           \
-            aclAppLog(ACL_WARNING, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);  \
+            aclAppLog(ACL_WARNING, __FILE__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);      \
         }                                                                                               \
     } while (0);
 #define ASCEND_LOGI(fmt, ...)                                                                           \
     do {                                                                                                \
         if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_INFO)) {                              \
-            aclAppLog(ACL_INFO, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);     \
+            aclAppLog(ACL_INFO, __FILE__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);         \
         }                                                                                               \
     } while (0);
 #define ASCEND_LOGD(fmt, ...)                                                                           \
     do {                                                                                                \
         if (c10_npu::option::OptionsManager::isACLGlobalLogOn(ACL_DEBUG)) {                             \
-            aclAppLog(ACL_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);    \
+            aclAppLog(ACL_DEBUG, __FILE__, __FUNCTION__, __LINE__, "[PTA]:"#fmt, ##__VA_ARGS__);        \
         }                                                                                               \
     } while (0);
