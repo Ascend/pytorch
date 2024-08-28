115a43886e2eedbecefcb7066724dbdee56fc484
王广斌 <wgb_strive@163.com>
2024-08-28 15:46:42 +0000

    !14154 Fix optim patch to solve performance problem Merge pull request !14154 from 王广斌/v2.2.0

--- torch_npu/utils/_optim.py
+++ torch_npu/utils/_optim.py
@@ -1,16 +1,18 @@
 import torch
 import torch.optim.optimizer as opt
 import torch_npu
-from .utils import should_print_warning
+
+
+_device_name = None
 
 
 def patch_supported_devices():
-    device_name = torch_npu.npu.get_device_name(0)
-    if device_name > "Ascend910B" and device_name < "Ascend910P":
+    global _device_name
+    _device_name = _device_name if _device_name is not None else torch_npu.npu.get_device_name(0)
+
+    if _device_name > "Ascend910B" and _device_name < "Ascend910P":
         return ["cuda", "xpu", torch._C._get_privateuse1_backend_name()]
-    if should_print_warning():
-        print(f"Warning: NPU does not support argument 'foreach' in this device type, "
-              f"we set foreach=False by default. Please do not set any value for this argument.")
+    
     return ["cuda", "xpu"] 
 
 
