b1a6972b3616e367de0d5efb8bb80f31db90ab0b
jiangpengfei <jiangpengfei24@huawei.com>
2024-08-30 02:23:51 +0000

    !14050 Patch clip_grad_norm for deterministic trainig. Merge pull request !14050 from jiangpengfei/v2.1.0

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -45,7 +45,7 @@ from torch_npu.contrib.function import npu_functional
 from torch_npu.contrib.module import npu_modules
 from torch_npu.utils import _apply_module_patch, _add_tensor_methods, _add_collect_env_methods, add_perf_dump_patch,\
      _add_storage_methods, _add_serialization_methods, apply_device_patch, add_dynamo_methods, add_optim_method,\
-     _apply_npu_show_warning
+     _apply_npu_show_warning, _apply_clip_grad_norm_patch
 import torch_npu.utils.custom_ops
 import torch_npu.distributed.rpc
 from torch_npu.distributed.rpc.backend_registry import _rpc_backend_registry
@@ -147,6 +147,7 @@ def _apply_class_patches():
     _apply_dcp_patch()
     _apply_sharded_grad_scaler_patch()
     add_perf_dump_patch()
+    _apply_clip_grad_norm_patch()
 
 
 torch.utils.rename_privateuse1_backend("npu")
