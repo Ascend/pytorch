ba5472171ca64ba5b730aa4217590355c0833fb5
sunjiayang <sunxinlei1@huawei.com>
2024-09-20 02:17:03 +0000

    !14747 only last error Merge pull request !14747 from sunjiayang/last_919_master

--- third_party/acl/libs/acl.cpp
+++ third_party/acl/libs/acl.cpp
@@ -51,6 +51,9 @@ aclError aclrtGetMemInfo(aclrtMemAttr attr, size_t *free, size_t *total){return 
 aclError aclrtGetMemUceInfo(int32_t deviceId, aclrtMemUceInfo* memUceInfoArray, size_t arraySize, size_t *retSize){return 0;}
 aclError aclrtMemUceRepair(int32_t deviceId, aclrtMemUceInfo* memUceInfoArray, size_t arraySize){return 0;}
 aclError aclrtCmoAsync(void *src, size_t size, aclrtCmoType cmoType, aclrtStream stream){return 0;}
+aclError aclrtGetLastError(int32_t flag){return 0;}
+aclError aclrtPeekAtLastError(int32_t flag){return 0;}
+
 
 // op相关操作
 aclopAttr *aclopCreateAttr(){return NULL;}
--- torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
+++ torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
@@ -1488,12 +1488,15 @@ void ProcessGroupHCCL::workEnqueue(c10::intrusive_ptr<ProcessGroupHCCL::WorkHCCL
     if (uce_error_flag) {
         uce_error_flag = false;
         c10_npu::set_has_throw_error(true);
-        throw std::runtime_error("UCE ERROR.");
+        ASCEND_LOGE("uce_error_flag is true when workEnqueue, throw UCE ERROR.");
+        throw std::runtime_error("UCE ERROR." + PTA_ERROR(ErrCode::ACL));
         return;
     }
     if (force_stop_error_flag) {
         force_stop_error_flag = false;
-        CHECK_AND_THROW_FORCE_STOP(ACL_ERROR_RT_DEVICE_TASK_ABORT);
+        c10_npu::set_has_throw_error(true);
+        ASCEND_LOGE("force_stop_error_flag is true when workEnqueue, throw FORCE STOP.");
+        throw std::runtime_error("FORCE STOP." + PTA_ERROR(ErrCode::ACL));
         return;
     }
     if (watchdogStatus == WatchdogStatus::STOP) {
