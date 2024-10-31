c66f358a9bf68c8c3103795c9976dfa39dcb14c6
叶子凡 <vscience@qq.com>
2024-10-31 13:36:58 +0000

    !15627 The key name 'hcclBufferSize' in hccl_config has changed to 'hccl_buffer_size'. Merge pull request !15627 from 叶子凡/v2.4.0_hccl_config_new

--- torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
+++ torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
@@ -1726,8 +1726,8 @@ HcclCommConfig ProcessGroupHCCL::createHcclCommConfigWithOptions()
 {
     HcclCommConfig config;
     HcclCommConfigInit(&config);
-    if (options_->hccl_config.find("hcclBufferSize") != options_->hccl_config.end()) {
-        config.hcclBufferSize = options_->hccl_config["hcclBufferSize"];
+    if (options_->hccl_config.find("hccl_buffer_size") != options_->hccl_config.end()) {
+        config.hcclBufferSize = options_->hccl_config["hccl_buffer_size"];
     }
     return config;
 }
