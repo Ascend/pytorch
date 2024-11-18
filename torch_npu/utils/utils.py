a398a7a21caf5be2abc177333fed22aaad27c4d3
xuzhubin <11206414+xuzhubin@user.noreply.gitee.com>
2024-11-18 12:37:37 +0000

    !15704 [PROF]add forward dataloader step and save_checkpoint keyfunc mstx Merge pull request !15704 from xuzhubin/v2.3.1

--- torch_npu/utils/utils.py
+++ torch_npu/utils/utils.py
@@ -1,8 +1,33 @@
 import os
+import time
 import warnings
 from warnings import _showwarnmsg_impl
 
 __all__ = ["should_print_warning"]
+
+
+class _LogLevel:
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    INFO = "INFO"
+
+
+def _print_log(level: str, msg: str):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+    pid = os.getpid()
+    print(f"{current_time}({pid})-[{level}] {msg}")
+
+
+def _print_info_log(info_msg: str):
+    _print_log(_LogLevel.INFO, info_msg)
+
+
+def _print_warn_log(warn_msg: str):
+    _print_log(_LogLevel.WARNING, warn_msg)
+
+
+def _print_error_log(error_msg: str):
+    _print_log(_LogLevel.ERROR, error_msg)
 
 
 def should_print_warning():
