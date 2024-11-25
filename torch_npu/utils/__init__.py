84dfadc5a978ea7548607f0dd80a88345f98d0b9
邵非凡 <shaofeifan2@huawei.com>
2024-11-25 06:27:53 +0000

    !16058 rename _should_print_warning Merge pull request !16058 from 邵非凡/warn25

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -12,12 +12,11 @@ from ._dynamo_device import _dynamo_register_interface_for_device
 from ._inductor import _inductor_register_device_op_overrides
 from ._optim import add_optim_method
 from .asd_detector import set_asd_loss_scale, register_asd_hook
-from .utils import print_error_log, print_warn_log, print_info_log, _apply_npu_show_warning, should_print_warning
+from .utils import print_error_log, print_warn_log, print_info_log, _apply_npu_show_warning, _should_print_warning
 from ._step import add_perf_dump_patch
 
 __all__ = ["npu_combine_tensors", "get_part_combined_tensor", "is_combined_tensor_valid",
-           "register_asd_hook", "set_asd_loss_scale", "print_error_log", "print_warn_log", "print_info_log",
-           "should_print_warning"]
+           "register_asd_hook", "set_asd_loss_scale", "print_error_log", "print_warn_log", "print_info_log"]
 
 
 # init flopcount
