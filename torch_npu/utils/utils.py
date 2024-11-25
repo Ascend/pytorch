3b173261dfee321939aff0af89e91f0deeab5c7a
邵非凡 <shaofeifan2@huawei.com>
2024-11-25 06:27:32 +0000

    !16124 rename _should_print_warning Merge pull request !16124 from 邵非凡/warn23

--- torch_npu/utils/utils.py
+++ torch_npu/utils/utils.py
@@ -3,8 +3,6 @@ import time
 import warnings
 from warnings import _showwarnmsg_impl
 
-__all__ = ["should_print_warning"]
-
 
 class _LogLevel:
     ERROR = "ERROR"
@@ -30,7 +28,7 @@ def _print_error_log(error_msg: str):
     _print_log(_LogLevel.ERROR, error_msg)
 
 
-def should_print_warning():
+def _should_print_warning():
     disabled_warning = os.environ.get("TORCH_NPU_DISABLED_WARNING", "0")
     if disabled_warning == "1":
         return False
@@ -43,7 +41,7 @@ def should_print_warning():
 def _apply_npu_show_warning():
     def npu_show_warning(message, category, filename, lineno, file=None, line=None):
         npu_path = os.path.dirname(os.path.dirname(__file__))
-        if not should_print_warning() and npu_path in filename:
+        if not _should_print_warning() and npu_path in filename:
             return
         msg = warnings.WarningMessage(message, category, filename, lineno, file, line)
         _showwarnmsg_impl(msg)
