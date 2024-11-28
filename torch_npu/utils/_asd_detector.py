351f6bb34215b2be542e0a05b4ff134f0a069417
杜金航 <15990042527@163.com>
2024-11-28 13:22:17 +0000

    !16405 Change to a private interface Merge pull request !16405 from 杜金航/v2.1.0

---
+++ torch_npu/utils/_asd_detector.py
@@ -0,0 +1,18 @@
+import torch
+import torch_npu
+from torch_npu.asd.asd import _silent_fault_detector
+
+
+def set_asd_loss_scale(loss_scale=1.0):
+    if torch_npu._C._npu_support_silentClientV2():
+        return
+    _silent_fault_detector.set_asd_loss_scale(loss_scale)
+    return
+
+
+def register_asd_hook(x, weight):
+    if torch_npu._C._npu_support_silentClientV2():
+        return
+    if x is not None and x.requires_grad and x._backward_hooks is None:
+        x.register_hook(_silent_fault_detector.silent_fault_check_hook(weight))
+    return
