69181d98a59489072d6c40a2f962187ee30d5890
李飞265 <lifei265@h-partners.com>
2024-11-28 12:59:54 +0000

    !16277 npu_quant_matmul out int32 Merge pull request !16277 from 李飞265/out_int32_v2.4.0

--- torch_npu/contrib/module/linear_a8w8_quant.py
+++ torch_npu/contrib/module/linear_a8w8_quant.py
@@ -84,7 +84,9 @@ class LinearA8W8Quant(nn.Module):
         scale_quant = self.scale
         first_last_dim = self.weight.dim() - 1
         second_last_dim = self.weight.dim() - 2
-        if self.scale.dtype == torch.float32 and self.pertoken_scale is None and self.output_dtype != torch.bfloat16:
+        is_check_dtype_ok = (self.scale.dtype == torch.float32 and 
+                             self.output_dtype not in [torch.bfloat16, torch.int32])
+        if self.pertoken_scale is None and is_check_dtype_ok:
             scale_quant = torch_npu.npu_trans_quant_param(self.scale, self.offset)
 
         return torch_npu.npu_quant_matmul(linear_quant_input, self.weight.transpose(second_last_dim, first_last_dim),
