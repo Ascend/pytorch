750d5d12667eeaff684c0246cf92194ae30980cc
huangyunlong <huangyunlong4@huawei.com>
2024-11-28 12:46:46 +0000

    !16304 update soc Merge pull request !16304 from huangyunlong/2.1rc1soc

--- torch_npu/csrc/core/npu/NpuVariables.h
+++ torch_npu/csrc/core/npu/NpuVariables.h
@@ -21,11 +21,7 @@ enum class SocVersion {
   Ascend310B1 = 240,
   Ascend310B2,
   Ascend310B3,
-  Ascend310B4,
-  Ascend910C1 = 250,
-  Ascend910C2,
-  Ascend910C3,
-  Ascend910C4
+  Ascend310B4
 };
 
 void SetSocVersion(const char* const socVersion);
