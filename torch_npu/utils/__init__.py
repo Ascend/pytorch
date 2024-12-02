94376bcb7e2b3fcd23bff299c83b690a464b9fd6
叶子凡 <vscience@qq.com>
2024-12-02 12:59:20 +0000

    !16369 Expose torch_npu.utils.FlopsCounter Merge pull request !16369 from 叶子凡/master_FLOPS

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -14,9 +14,10 @@ from ._optim import add_optim_method
 from .asd_detector import set_asd_loss_scale, register_asd_hook
 from .utils import print_error_log, print_warn_log, print_info_log, _apply_npu_show_warning, _should_print_warning
 from ._step import add_perf_dump_patch
+from .flops_count import _FlopsCounter as FlopsCounter
 
 __all__ = ["npu_combine_tensors", "get_part_combined_tensor", "is_combined_tensor_valid",
-           "print_error_log", "print_warn_log", "print_info_log"]
+           "print_error_log", "print_warn_log", "print_info_log", "FlopsCounter"]
 
 
 # init flopcount
