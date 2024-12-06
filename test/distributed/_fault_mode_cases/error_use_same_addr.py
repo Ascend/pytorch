903fadc4dff3b8deedd3a027bf8a2181064c197b
yuhaiyan <yuhaiyan8@huawei.com>
2024-12-06 10:05:11 +0000

    !16681 Fixed for the public API. Merge pull request !16681 from yuhaiyan/v2.1.0-dev1

--- test/distributed/_fault_mode_cases/error_use_same_addr.py
+++ test/distributed/_fault_mode_cases/error_use_same_addr.py
@@ -14,7 +14,7 @@ def same_addr():
     torch.npu.set_device(rank)
     input_a = torch.randn(800, 100).npu()
     input_b = torch.randn(100, 600).npu()
-    for _ in range(200):
+    for _ in range(1000):
         result = torch.matmul(input_a, input_b)
         result_mean = result.mean()
     dist.all_reduce(input_a)
