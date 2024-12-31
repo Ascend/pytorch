ef2cc8a05c00d2182b5c0cb2ce008935ede4686b
shaojieMike <tanshaojie1@huawei.com>
2024-12-31 09:32:37 +0000

    !16792 Lazyinit aclsetcompileopt and related settings Merge pull request !16792 from shaojieMike/v2.5.1_PR

---
+++ torch_npu/csrc/framework/LazyInitAclops.h
@@ -0,0 +1,13 @@
+#ifndef AT_NPU_ACOPLS_LAZYINITACLOPS_H_
+#define AT_NPU_ACOPLS_LAZYINITACLOPS_H_
+
+namespace at_npu {
+namespace aclops {
+
+void LazyInitAclops();
+void InitializeJitCompilationMode();
+
+}  // namespace aclops
+}  // namespace at_npu
+
+#endif  // AT_NPU_ACOPLS_LAZYINITACLOPS_H_
