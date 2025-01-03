97b80840e03b12c5a27ecfe47fa8f6a8de0eba5e
王夏夏 <wangxiaxia3@huawei.com>
2025-01-03 02:23:29 +0000

    !16764 [Bugfix] Copy operator misses memory_format. Merge pull request !16764 from 王夏夏/master

--- torch_npu/csrc/aten/common/TensorProperties.cpp
+++ torch_npu/csrc/aten/common/TensorProperties.cpp
@@ -8,29 +8,29 @@ namespace at_npu {
 namespace native {
 
 at::Tensor NPUNativeFunctions::contiguous(const at::Tensor& self, c10::MemoryFormat memory_format) {
-  if (self.is_contiguous(memory_format)) {
-    return self;
-  }
+    if (self.is_contiguous(memory_format)) {
+        return self;
+    }
 
-  TORCH_CHECK(
-      memory_format == c10::MemoryFormat::Contiguous,
-      "NPU contiguous operator only supportted contiguous memory format.", OPS_ERROR(ErrCode::NOT_SUPPORT));
-  return self.clone();
+    TORCH_CHECK(
+        memory_format == c10::MemoryFormat::Contiguous,
+        "NPU contiguous operator only supportted contiguous memory format.", OPS_ERROR(ErrCode::NOT_SUPPORT));
+    return self.clone(memory_format);
 }
 
 bool NPUNativeFunctions::is_set_to(const at::Tensor& self, const at::Tensor& src) {
-  if (self.storage().unsafeGetStorageImpl() == src.storage().unsafeGetStorageImpl() &&
-      self.storage_offset() == src.storage_offset() && self.dim() == src.dim() &&
-      NPUNativeFunctions::get_storage_size(self) == NPUNativeFunctions::get_storage_size(src) &&
-      NPUNativeFunctions::get_npu_format(self) == NPUNativeFunctions::get_npu_format(src)) {
-    for (const auto d : c10::irange(self.dim())) {
-      if (self.size(d) != src.size(d) || self.stride(d) != src.stride(d)) {
-        return false;
-      }
+    if (self.storage().unsafeGetStorageImpl() == src.storage().unsafeGetStorageImpl() &&
+        self.storage_offset() == src.storage_offset() && self.dim() == src.dim() &&
+        NPUNativeFunctions::get_storage_size(self) == NPUNativeFunctions::get_storage_size(src) &&
+        NPUNativeFunctions::get_npu_format(self) == NPUNativeFunctions::get_npu_format(src)) {
+        for (const auto d : c10::irange(self.dim())) {
+            if (self.size(d) != src.size(d) || self.stride(d) != src.stride(d)) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
-  return false;
+    return false;
 }
 
 } // namespace native
