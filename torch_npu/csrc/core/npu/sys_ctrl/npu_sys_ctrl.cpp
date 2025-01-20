cf26c0b257fb1dc66234a7e6d1fe7b76d6b45c88
邵非凡 <shaofeifan2@huawei.com>
2025-01-20 02:10:01 +0000

    !17321 codecheck Merge pull request !17321 from 邵非凡/codecheck23

--- torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
+++ torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
@@ -49,18 +49,19 @@ const size_t kMaxPathLen = 4096U;
 
 void MakeCompileCacheDirAndSetOption()
 {
-    char* compile_cache_mode_val = std::getenv("ACL_OP_COMPILER_CACHE_MODE");
-    std::string compile_cache_mode = (compile_cache_mode_val == nullptr) ? std::string("enable")
-                                                                        : std::string(compile_cache_mode_val);
+    char *compile_cache_mode_val = std::getenv("ACL_OP_COMPILER_CACHE_MODE");
+    std::string compile_cache_mode =
+        (compile_cache_mode_val == nullptr) ? std::string("enable") : std::string(compile_cache_mode_val);
     if (compile_cache_mode != "enable" && compile_cache_mode != "disable" && compile_cache_mode != "force") {
         compile_cache_mode = std::string("enable");
     }
     auto compile_mode = c10_npu::option::GetOption("ACL_OP_COMPILER_CACHE_MODE");
     if (!compile_mode.has_value() || compile_mode.value() == "") {
-        c10_npu::option::register_options::OptionRegister::GetInstance()->Set("ACL_OP_COMPILER_CACHE_MODE", compile_cache_mode);
+        c10_npu::option::register_options::OptionRegister::GetInstance()->Set("ACL_OP_COMPILER_CACHE_MODE",
+                                                                              compile_cache_mode);
     }
 
-    char* compile_cache_dir_val = std::getenv("ACL_OP_COMPILER_CACHE_DIR");
+    char *compile_cache_dir_val = std::getenv("ACL_OP_COMPILER_CACHE_DIR");
     if (compile_cache_dir_val != nullptr) {
         std::string compile_cache_dir = std::string(compile_cache_dir_val);
         // mode : 750
@@ -73,7 +74,8 @@ void MakeCompileCacheDirAndSetOption()
         }
         auto compile_dir = c10_npu::option::GetOption("ACL_OP_COMPILER_CACHE_DIR");
         if (!compile_dir.has_value() || compile_dir.value() == "") {
-            c10_npu::option::register_options::OptionRegister::GetInstance()->Set("ACL_OP_COMPILER_CACHE_DIR", compile_cache_dir);
+            c10_npu::option::register_options::OptionRegister::GetInstance()->Set("ACL_OP_COMPILER_CACHE_DIR",
+                                                                                  compile_cache_dir);
         }
     }
 }
@@ -85,19 +87,19 @@ void GetAndSetDefaultJitCompileByAcl()
         return;
     }
 
-  auto opt_size = at_npu::native::AclGetCompileoptSize(ACL_OP_JIT_COMPILE);
-  if (!opt_size.has_value()) {
-    ASCEND_LOGW("Get ACL JitCompile default value size failed, use PTA default value: True");
-    return;
-  }
-  TORCH_CHECK(opt_size.value() != 0, "AclGetCompileoptSize opt_size.value() = 0 !", PTA_ERROR(ErrCode::ACL));
-  char value_name[opt_size.value()];
-  auto ret = at_npu::native::AclGetCompileopt(ACL_OP_JIT_COMPILE, value_name, opt_size.value());
-  // Get func success but get value failed, throw error
-  TORCH_CHECK(ret == ACL_SUCCESS, "Get ACL JitCompile default value failed.", PTA_ERROR(ErrCode::ACL));
-  std::string value_str(value_name);
-  c10_npu::option::SetOption("jitCompile", value_str);
-  ASCEND_LOGI("Get ACL JitCompile default value %s and set", value_str.c_str());
+    auto opt_size = at_npu::native::AclGetCompileoptSize(ACL_OP_JIT_COMPILE);
+    if (!opt_size.has_value()) {
+        ASCEND_LOGW("Get ACL JitCompile default value size failed, use PTA default value: True");
+        return;
+    }
+    TORCH_CHECK(opt_size.value() != 0, "AclGetCompileoptSize opt_size.value() = 0 !", PTA_ERROR(ErrCode::ACL));
+    char value_name[opt_size.value()];
+    auto ret = at_npu::native::AclGetCompileopt(ACL_OP_JIT_COMPILE, value_name, opt_size.value());
+    // Get func success but get value failed, throw error
+    TORCH_CHECK(ret == ACL_SUCCESS, "Get ACL JitCompile default value failed.", PTA_ERROR(ErrCode::ACL));
+    std::string value_str(value_name);
+    c10_npu::option::SetOption("jitCompile", value_str);
+    ASCEND_LOGI("Get ACL JitCompile default value %s and set", value_str.c_str());
 }
 
 void SetDefaultAllowInternalFromatDisable()
@@ -111,28 +113,32 @@ void SetDefaultAllowInternalFromatDisable()
     ASCEND_LOGI("Set ALLOW_INTERNAL_FORMAT default value disable.");
 }
 
-void SetHF32DefaultValue() {
-  // The default value of the flag used to control whether HF32 is allowed on conv is True.
-  // The default value of the flag used to control whether HF32 is allowed on matmul is True,
-  // but this flag defaults to False in PyTorch 1.12 and later.
+void SetHF32DefaultValue()
+{
+    // The default value of the flag used to control whether HF32 is allowed on conv is True.
+    // The default value of the flag used to control whether HF32 is allowed on matmul is True,
+    // but this flag defaults to False in PyTorch 1.12 and later.
 
-  // When the flag of matmul is False, and the flag of conv is True,
-  // the value of option "ACL_ALLOW_HF32" should be set to "10";
-  std::string allow_hf32 = "10";
-  auto ret = at_npu::native::AclSetCompileopt(aclCompileOpt::ACL_ALLOW_HF32, allow_hf32.c_str());
-  if (ret == ACL_SUCCESS) {
-    ASCEND_LOGI("Set ACL option ACL_ALLOW_HF32 default value to %s.", allow_hf32.c_str());
-  } else if (ret == ACL_ERROR_INTERNAL_ERROR) {
-    // Used to solve version compatibility issues, when ASCEND have not been updated.
-    ASCEND_LOGW("Failed to set default value of ACL option ACL_ALLOW_HF32, which is unsupported by current version.");
-  } else {
-    TORCH_CHECK(0, "Failed to set compile option ACL_ALLOW_HF32, result = ", ret, ", set value ", allow_hf32, PTA_ERROR(ErrCode::ACL));
-  }
+    // When the flag of matmul is False, and the flag of conv is True,
+    // the value of option "ACL_ALLOW_HF32" should be set to "10";
+    std::string allow_hf32 = "10";
+    auto ret = at_npu::native::AclSetCompileopt(aclCompileOpt::ACL_ALLOW_HF32, allow_hf32.c_str());
+    if (ret == ACL_SUCCESS) {
+        ASCEND_LOGI("Set ACL option ACL_ALLOW_HF32 default value to %s.", allow_hf32.c_str());
+    } else if (ret == ACL_ERROR_INTERNAL_ERROR) {
+        // Used to solve version compatibility issues, when ASCEND have not been updated.
+        ASCEND_LOGW(
+            "Failed to set default value of ACL option ACL_ALLOW_HF32, which is unsupported by current version.");
+    } else {
+        TORCH_CHECK(0, "Failed to set compile option ACL_ALLOW_HF32, result = ", ret, ", set value ", allow_hf32,
+                    PTA_ERROR(ErrCode::ACL));
+    }
 }
 
 #ifndef BUILD_LIBTORCH
-std::string GetTorchNpuFile() {
-    PyObject* file_attr = nullptr;
+std::string GetTorchNpuFile()
+{
+    PyObject *file_attr = nullptr;
     {
         pybind11::gil_scoped_acquire get_gil;
         auto torch = THPObjectPtr(PyImport_ImportModule("torch"));
@@ -142,7 +148,7 @@ std::string GetTorchNpuFile() {
         file_attr = PyObject_GetAttrString(torch, "__file__");
     }
     if (file_attr) {
-        const char* file_path = PyUnicode_AsUTF8(file_attr);
+        const char *file_path = PyUnicode_AsUTF8(file_attr);
         std::string file_path_str = std::string(file_path);
         std::string key_word = "torch";
         size_t pos = file_path_str.rfind(key_word);
@@ -155,21 +161,22 @@ std::string GetTorchNpuFile() {
 }
 #endif
 
-std::string GetAclConfigJsonPath() {
+std::string GetAclConfigJsonPath()
+{
 #ifndef BUILD_LIBTORCH
-  std::string npu_path = GetTorchNpuFile();
-  if (npu_path == "") {
-    ASCEND_LOGW("Failed to get npu path!");
-    return "";
-  }
-  std::string json_path = npu_path.append("torch_npu/acl.json");
-  std::string json_path_str = torch_npu::toolkit::profiler::Utils::RealPath(json_path);
-  if (json_path_str == "") {
-    ASCEND_LOGW("this path:%s is not exist!", json_path.c_str());
-  }
-  return json_path_str;
+    std::string npu_path = GetTorchNpuFile();
+    if (npu_path == "") {
+        ASCEND_LOGW("Failed to get npu path!");
+        return "";
+    }
+    std::string json_path = npu_path.append("torch_npu/acl.json");
+    std::string json_path_str = torch_npu::toolkit::profiler::Utils::RealPath(json_path);
+    if (json_path_str == "") {
+        ASCEND_LOGW("this path:%s is not exist!", json_path.c_str());
+    }
+    return json_path_str;
 #else
-  return "";
+    return "";
 #endif
 }
 } // namespace
@@ -179,9 +186,10 @@ namespace c10_npu {
 NpuSysCtrl::NpuSysCtrl() : repeat_init_acl_flag_(true), init_flag_(false), device_id_(0) {}
 
 // Get NpuSysCtrl singleton instance
- NpuSysCtrl& NpuSysCtrl::GetInstance() {
-  static NpuSysCtrl instance;
-  return instance;
+NpuSysCtrl &NpuSysCtrl::GetInstance()
+{
+    static NpuSysCtrl instance;
+    return instance;
 }
 
 // Environment Initialize, return Status: SUCCESS, FAILED
@@ -246,8 +254,8 @@ NpuSysCtrl::SysStatus NpuSysCtrl::Initialize(int device_id)
     }
 
     // set ACL_PRECISION_MODE by SocVersion("allow_fp32_to_fp16" or "must_keep_origin_dtype").
-    auto precision_mode = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1 ?
-        "must_keep_origin_dtype" : "allow_fp32_to_fp16";
+    auto precision_mode =
+        c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1 ? "must_keep_origin_dtype" : "allow_fp32_to_fp16";
     NPU_CHECK_ERROR(at_npu::native::AclSetCompileopt(aclCompileOpt::ACL_PRECISION_MODE, precision_mode));
 
     // set default compile cache mode and dir for users to improve op compile time
@@ -266,13 +274,13 @@ NpuSysCtrl::SysStatus NpuSysCtrl::Initialize(int device_id)
     NPU_CHECK_SUPPORTED_OR_ERROR(c10_npu::acl::AclrtSetOpExecuteTimeOut(kMaxOpExecuteTimeOut));
 
     // lazy call for the setoption
-    for (const auto& iter : lazy_fn_) {
+    for (const auto &iter: lazy_fn_) {
         ASCEND_LOGD("start setoption for the lazy call.");
-        const auto& call_ = iter.first;
-        const auto& in = iter.second;
+        const auto &call_ = iter.first;
+        const auto &in = iter.second;
         call_(in);
     }
-    
+
     lazy_fn_.clear();
 
     GetAffinityInfo();
@@ -290,35 +298,40 @@ NpuSysCtrl::SysStatus NpuSysCtrl::ExchangeDevice(int device)
     return INIT_SUCC;
 }
 
- NpuSysCtrl::SysStatus NpuSysCtrl::BackwardsInit() {
+NpuSysCtrl::SysStatus NpuSysCtrl::BackwardsInit()
+{
     NPU_CHECK_ERROR(c10_npu::SetDevice(device_id_));
     return INIT_SUCC;
 }
 
- NpuSysCtrl::SysStatus NpuSysCtrl::OverflowSwitchEnable() {
-   if (!c10_npu::IsSupportInfNan()) {
-     c10_npu::acl::AclrtSetStreamOverflowSwitch(c10_npu::getCurrentNPUStream(), 1);
-     ASCEND_LOGI("Npu overflow check switch set successfully.");
-   }
-   return INIT_SUCC;
+NpuSysCtrl::SysStatus NpuSysCtrl::OverflowSwitchEnable()
+{
+    if (!c10_npu::IsSupportInfNan()) {
+        c10_npu::acl::AclrtSetStreamOverflowSwitch(c10_npu::getCurrentNPUStream(), 1);
+        ASCEND_LOGI("Npu overflow check switch set successfully.");
+    }
+    return INIT_SUCC;
 }
 
 // Environment Finalize, return SysStatus
- NpuSysCtrl::SysStatus NpuSysCtrl::Finalize() {
+NpuSysCtrl::SysStatus NpuSysCtrl::Finalize()
+{
     if (!init_flag_) {
         return FINALIZE_SUCC;
     }
 
-    this->RegisterReleaseFn([=]() ->void {
-        c10_npu::NPUEventManager::GetInstance().ClearEvent();
-        NPU_CHECK_WARN(c10_npu::DestroyUsedStreams());
-        NPU_CHECK_WARN(c10_npu::ResetUsedDevices());
-        // Maintain a basic point of view, who applies for the resource, the resource is released by whom.
-        // If aclInit is not a PTA call, then aclFinalize should not be a PTA call either.
-        if (repeat_init_acl_flag_) {
-            NPU_CHECK_WARN(aclFinalize());
-        }
-        }, ReleasePriority::PriorityLast);
+    this->RegisterReleaseFn(
+        [=]() -> void {
+            c10_npu::NPUEventManager::GetInstance().ClearEvent();
+            NPU_CHECK_WARN(c10_npu::DestroyUsedStreams());
+            NPU_CHECK_WARN(c10_npu::ResetUsedDevices());
+            // Maintain a basic point of view, who applies for the resource, the resource is released by whom.
+            // If aclInit is not a PTA call, then aclFinalize should not be a PTA call either.
+            if (repeat_init_acl_flag_) {
+                NPU_CHECK_WARN(aclFinalize());
+            }
+        },
+        ReleasePriority::PriorityLast);
 
     init_flag_ = false;
 
@@ -327,9 +340,9 @@ NpuSysCtrl::SysStatus NpuSysCtrl::ExchangeDevice(int device)
     }
 
     // call release fn by priotity
-    for (const auto& iter : release_fn_) {
-        const auto& fn_vec = iter.second;
-        for (const auto& fn : fn_vec) {
+    for (const auto &iter: release_fn_) {
+        const auto &fn_vec = iter.second;
+        for (const auto &fn: fn_vec) {
             fn();
         }
     }
@@ -339,11 +352,12 @@ NpuSysCtrl::SysStatus NpuSysCtrl::ExchangeDevice(int device)
     return FINALIZE_SUCC;
 }
 
- bool NpuSysCtrl::GetInitFlag() {
+bool NpuSysCtrl::GetInitFlag()
+{
     return init_flag_;
 }
 
-void NpuSysCtrl::RegisterLazyFn(const option::OptionCallBack& call_, const std::string& in)
+void NpuSysCtrl::RegisterLazyFn(const option::OptionCallBack &call_, const std::string &in)
 {
     lazy_fn_.emplace_back(std::make_pair(call_, in));
 }
@@ -357,9 +371,9 @@ int NpuSysCtrl::InitializedDeviceID()
     return -1;
 }
 
-void NpuSysCtrl::RegisterReleaseFn(ReleaseFn release_fn,
-                                   ReleasePriority priority) {
-    const auto& iter = this->release_fn_.find(priority);
+void NpuSysCtrl::RegisterReleaseFn(ReleaseFn release_fn, ReleasePriority priority)
+{
+    const auto &iter = this->release_fn_.find(priority);
     if (iter != release_fn_.end()) {
         release_fn_[priority].emplace_back(release_fn);
     } else {
@@ -367,12 +381,13 @@ void NpuSysCtrl::RegisterReleaseFn(ReleaseFn release_fn,
     }
 }
 
-aclError SetCurrentDevice() {
-  if (c10_npu::NpuSysCtrl::GetInstance().GetInitFlag()) {
-    c10_npu::SetDevice(c10_npu::NpuSysCtrl::GetInstance().InitializedDeviceID());
-    return ACL_SUCCESS;
-  }
-  TORCH_CHECK(false, "npu device has not been inited.", PTA_ERROR(ErrCode::INTERNAL));
+aclError SetCurrentDevice()
+{
+    if (c10_npu::NpuSysCtrl::GetInstance().GetInitFlag()) {
+        c10_npu::SetDevice(c10_npu::NpuSysCtrl::GetInstance().InitializedDeviceID());
+        return ACL_SUCCESS;
+    }
+    TORCH_CHECK(false, "npu device has not been inited.", PTA_ERROR(ErrCode::INTERNAL));
 }
 
 } // namespace c10_npu
