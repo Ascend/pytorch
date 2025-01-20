f54a26d7258377aa3e95babda03d6548ddeaab61
邵非凡 <shaofeifan2@huawei.com>
2025-01-20 02:08:26 +0000

    !17315 codecheck Merge pull request !17315 from 邵非凡/codecheck21

--- torch_npu/csrc/core/npu/impl/NPUGuardImpl.cpp
+++ torch_npu/csrc/core/npu/impl/NPUGuardImpl.cpp
@@ -12,14 +12,16 @@ constexpr c10::DeviceType NPUGuardImpl::static_type;
 
 C10_REGISTER_GUARD_IMPL(PrivateUse1, NPUGuardImpl);
 
-#define REGISTER_PRIVATEUSE1_BACKEND(name)                                                      \
-  int rename_privateuse1_backend() {                                                            \
-    c10::register_privateuse1_backend(#name);                                                   \
-    c10::SetStorageImplCreate(c10::DeviceType::PrivateUse1, &torch_npu::make_npu_storage_impl); \
-    torch::jit::TensorBackendMetaRegistry(c10::DeviceType::PrivateUse1, &torch_npu::npu_info_serialization, &torch_npu::npu_info_deserialization); \
-    return 0;                                                                                   \
-  }                                                                                             \
-  static const int _temp_##name = rename_privateuse1_backend();
+#define REGISTER_PRIVATEUSE1_BACKEND(name)                                                                             \
+    int rename_privateuse1_backend()                                                                                   \
+    {                                                                                                                  \
+        c10::register_privateuse1_backend(#name);                                                                      \
+        c10::SetStorageImplCreate(c10::DeviceType::PrivateUse1, &torch_npu::make_npu_storage_impl);                    \
+        torch::jit::TensorBackendMetaRegistry(c10::DeviceType::PrivateUse1, &torch_npu::npu_info_serialization,        \
+                                              &torch_npu::npu_info_deserialization);                                   \
+        return 0;                                                                                                      \
+    }                                                                                                                  \
+    static const int _temp_##name = rename_privateuse1_backend();
 
 REGISTER_PRIVATEUSE1_BACKEND(npu)
 
