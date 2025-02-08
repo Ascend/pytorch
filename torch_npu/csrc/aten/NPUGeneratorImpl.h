73e7f1f58f656bcbfb3bd93ad2e8f7d1db99f5db
SCh-zx <1325467101@qq.com>
2025-02-08 08:29:00 +0000

    !17407 [cleancode]left brace and four spaces Merge pull request !17407 from SCh-zx/cleancode2.6

--- torch_npu/csrc/aten/NPUGeneratorImpl.h
+++ torch_npu/csrc/aten/NPUGeneratorImpl.h
@@ -87,70 +87,69 @@ namespace at_npu {
 
 // Stores state values. Passed as a kernel argument. See "Usage:" above.
 struct PhiloxNpuState {
-  PhiloxNpuState() = default;
-  PhiloxNpuState(const PhiloxNpuState&) = default;
-  // Called if graph capture is not underway
-  PhiloxNpuState(uint64_t seed,
-                  uint64_t offset) {
-    seed_ = seed;
-    offset_.val = offset;
-  }
-  // Called if graph capture is underway
-  PhiloxNpuState(uint64_t seed,
-                  int64_t* offset_extragraph,
-                  uint32_t offset_intragraph) {
-    seed_ = seed;
-    offset_.ptr = offset_extragraph;
-    offset_intragraph_ = offset_intragraph;
-    captured_ = true;
-  }
+    PhiloxNpuState() = default;
+    PhiloxNpuState(const PhiloxNpuState&) = default;
+    // Called if graph capture is not underway
+    PhiloxNpuState(uint64_t seed, uint64_t offset)
+    {
+        seed_ = seed;
+        offset_.val = offset;
+    }
+    // Called if graph capture is underway
+    PhiloxNpuState(uint64_t seed, int64_t* offset_extragraph, uint32_t offset_intragraph)
+    {
+        seed_ = seed;
+        offset_.ptr = offset_extragraph;
+        offset_intragraph_ = offset_intragraph;
+        captured_ = true;
+    }
 
-  // Public members, directly accessible by at::Npu::philox::unpack.
-  // If we made them private with getters/setters, the getters/setters
-  // would have to be __device__, and we can't declare __device__ in ATen.
-  union Payload {
-    uint64_t val;
-    int64_t* ptr;
-  };
+    // Public members, directly accessible by at::Npu::philox::unpack.
+    // If we made them private with getters/setters, the getters/setters
+    // would have to be __device__, and we can't declare __device__ in ATen.
+    union Payload {
+        uint64_t val;
+        int64_t* ptr;
+    };
 
-  uint64_t seed_;
-  Payload offset_;
-  uint32_t offset_intragraph_{0};
-  bool captured_ = false;
+    uint64_t seed_;
+    Payload offset_;
+    uint32_t offset_intragraph_{0};
+    bool captured_ = false;
 };
 
 struct TORCH_NPU_API NPUGeneratorImpl : public c10::GeneratorImpl {
-  // Constructors
-  NPUGeneratorImpl(c10::DeviceIndex device_index = -1);
-  ~NPUGeneratorImpl() = default;
+    // Constructors
+    NPUGeneratorImpl(c10::DeviceIndex device_index = -1);
+    ~NPUGeneratorImpl() = default;
 
-  // NPUGeneratorImpl methods
-  std::shared_ptr<NPUGeneratorImpl> clone() const;
-  void set_current_seed(uint64_t seed) override;
-  void set_offset(uint64_t offset) override;
-  uint64_t get_offset() const override;
-  uint64_t current_seed() const override;
-  uint64_t seed() override;
-  void set_state(const c10::TensorImpl& new_state) override;
-  c10::intrusive_ptr<c10::TensorImpl> get_state() const override;
-  void set_philox_offset_per_thread(uint64_t offset);
-  uint64_t philox_offset_per_thread() const;
-  void capture_prologue(int64_t* offset_extragraph);
-  uint64_t capture_epilogue();
-  PhiloxNpuState philox_npu_state(uint64_t increment);
+    // NPUGeneratorImpl methods
+    std::shared_ptr<NPUGeneratorImpl> clone() const;
+    void set_current_seed(uint64_t seed) override;
+    void set_offset(uint64_t offset) override;
+    uint64_t get_offset() const override;
+    uint64_t current_seed() const override;
+    uint64_t seed() override;
+    void set_state(const c10::TensorImpl& new_state) override;
+    c10::intrusive_ptr<c10::TensorImpl> get_state() const override;
+    void set_philox_offset_per_thread(uint64_t offset);
+    uint64_t philox_offset_per_thread() const;
+    void capture_prologue(int64_t* offset_extragraph);
+    uint64_t capture_epilogue();
+    PhiloxNpuState philox_npu_state(uint64_t increment);
 
-  // Temporarily accommodates call sites that use philox_engine_inputs.
-  // Allows incremental refactor of call sites to use philox_npu_state.
-  std::pair<uint64_t, uint64_t> philox_engine_inputs(uint64_t increment);
-  static c10::DeviceType device_type();
+    // Temporarily accommodates call sites that use philox_engine_inputs.
+    // Allows incremental refactor of call sites to use philox_npu_state.
+    std::pair<uint64_t, uint64_t> philox_engine_inputs(uint64_t increment);
+    static c10::DeviceType device_type();
 
 private:
-  NPUGeneratorImpl* clone_impl() const override;
-  uint64_t seed_ = c10::default_rng_seed_val;
-  uint64_t philox_offset_per_thread_ = 0;
-  int64_t* offset_extragraph_ = nullptr;
-  uint32_t offset_intragraph_ = 0;
-  bool graph_expects_this_gen_ = false;
+    NPUGeneratorImpl* clone_impl() const override;
+    uint64_t seed_ = c10::default_rng_seed_val;
+    uint64_t philox_offset_per_thread_ = 0;
+    int64_t* offset_extragraph_ = nullptr;
+    uint32_t offset_intragraph_ = 0;
+    bool graph_expects_this_gen_ = false;
 };
 
 namespace detail {
