d743d3c289085ca4b7bb7e0e438b60afd99e5bbd
zhanglei <george.zhanglei@huawei.com>
2025-02-08 08:34:42 +0000

    !17747 支持配置FFT PLAN Merge pull request !17747 from zhanglei/master

--- torch_npu/npu/backends.py
+++ torch_npu/npu/backends.py
@@ -5,4 +5,8 @@ This file will be discarded.
 from torch_npu.npu._backends import flash_sdp_enabled, enable_flash_sdp, mem_efficient_sdp_enabled, \
     enable_mem_efficient_sdp, math_sdp_enabled, enable_math_sdp, sdp_kernel, preferred_linalg_library
 
+from torch_npu.npu._fft_plan_cache import NPUFFTPlanCache
+
 __all__ = []
+
+fft_plan_cache = NPUFFTPlanCache()
