cfb19ef5eca56613ced4a8b2d8a8c3576b570c22
SCh-zx <1325467101@qq.com>
2025-02-08 09:09:26 +0000

    !17405 [cleancode]left brace and four spaces Merge pull request !17405 from SCh-zx/cleancode2.4

--- torch_npu/csrc/aten/AutoCastOps.cpp
+++ torch_npu/csrc/aten/AutoCastOps.cpp
@@ -17,13 +17,11 @@ at::Tensor binary_cross_entropy_banned(const at::Tensor &, const at::Tensor &, c
              "or torch.nn.BCEWithLogitsLoss.  binary_cross_entropy_with_logits and BCEWithLogits are\n"
              "safe to autocast.");
 }
-TORCH_LIBRARY_IMPL(_, AutocastPrivateUse1, m)
-{
+TORCH_LIBRARY_IMPL(_, AutocastPrivateUse1, m) {
     m.fallback(torch::CppFunction::makeFallthrough());
 }
 
-TORCH_LIBRARY_IMPL(aten, AutocastPrivateUse1, m)
-{
+TORCH_LIBRARY_IMPL(aten, AutocastPrivateUse1, m) {
     // lower_precision_fp
     KERNEL_PRIVATEUSEONE(_convolution, deprecated, lower_precision_fp)
     KERNEL_PRIVATEUSEONE(_convolution, lower_precision_fp)
