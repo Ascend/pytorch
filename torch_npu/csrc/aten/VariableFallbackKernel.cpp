ef06714bfaa0cafd316dabcb20265b8b476dff06
SCh-zx <1325467101@qq.com>
2025-02-08 09:08:25 +0000

    !17400 [cleancode]left brace and four spaces Merge pull request !17400 from SCh-zx/cleancode2.3

--- torch_npu/csrc/aten/VariableFallbackKernel.cpp
+++ torch_npu/csrc/aten/VariableFallbackKernel.cpp
@@ -34,40 +34,43 @@ namespace {
 
 // (Ascend) TORCH_LIBRARY_IMPL
 TORCH_LIBRARY_IMPL(_, AutogradPrivateUse1, m) {
-  m.fallback(torch::CppFunction::makeFallthrough());
+    m.fallback(torch::CppFunction::makeFallthrough());
 }
 
-bool has_op_name_warned(const std::string& op_name) {
-  static std::unordered_set<std::string> _op_lists = {};
-  if (_op_lists.find(op_name) != _op_lists.end()) {
-    return true;
-  }
-  _op_lists.insert(op_name);
-  return false;
+bool has_op_name_warned(const std::string& op_name)
+{
+    static std::unordered_set<std::string> _op_lists = {};
+    if (_op_lists.find(op_name) != _op_lists.end()) {
+        return true;
+    }
+    _op_lists.insert(op_name);
+    return false;
 }
 
-void npu_cpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
-  if (!has_op_name_warned(c10::toString(op.schema().operator_name()))) {
-    TORCH_NPU_WARN("CAUTION: The operator '",
-                   op.schema().operator_name(),
-                   "' is not currently supported ",
-                   "on the NPU backend and will fall back to run on the CPU.",
-                   " This may have performance implications.");
-  }
+void npu_cpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack)
+{
+    if (!has_op_name_warned(c10::toString(op.schema().operator_name()))) {
+        TORCH_NPU_WARN("CAUTION: The operator '",
+                       op.schema().operator_name(),
+                       "' is not currently supported ",
+                       "on the NPU backend and will fall back to run on the CPU.",
+                       " This may have performance implications.");
+    }
   at::native::cpu_fallback(op, stack);
 }
 
 TORCH_LIBRARY_IMPL(_, PrivateUse1, m) {
-  m.fallback(torch::CppFunction::makeFromBoxedFunction<&npu_cpu_fallback>());
+    m.fallback(torch::CppFunction::makeFromBoxedFunction<&npu_cpu_fallback>());
 }
 
-void npu_Sparse_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
-  TORCH_CHECK(false, "CAUTION: The operator '",
-              op.schema().operator_name(),
-              "' is not currently supported on the NPU backend.", OPS_ERROR(ErrCode::NOT_SUPPORT))
+void npu_Sparse_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack)
+{
+    TORCH_CHECK(false, "CAUTION: The operator '",
+                op.schema().operator_name(),
+                "' is not currently supported on the NPU backend.", OPS_ERROR(ErrCode::NOT_SUPPORT))
 }
 
 TORCH_LIBRARY_IMPL(_, SparsePrivateUse1, m) {
-  m.fallback(torch::CppFunction::makeFromBoxedFunction<&npu_Sparse_fallback>());
+    m.fallback(torch::CppFunction::makeFromBoxedFunction<&npu_Sparse_fallback>());
 }
 }
