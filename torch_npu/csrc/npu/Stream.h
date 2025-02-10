3de06890e220ead9d7999ff0894d3a774af9195c
闫鹏全 <yanpengquan@huawei.com>
2025-02-10 10:16:51 +0000

    !17781 clean code for csrc/npu folder Merge pull request !17781 from 闫鹏全/v2.5.1_cleancode

--- torch_npu/csrc/npu/Stream.h
+++ torch_npu/csrc/npu/Stream.h
@@ -1,20 +1,21 @@
 #ifndef THNP_STREAM_INC
 #define THNP_STREAM_INC
 
-#include "torch_npu/csrc/core/npu/NPUMacros.h"
-#include "torch_npu/csrc/core/npu/NPUStream.h"
 #include <torch/csrc/Stream.h>
 #include <torch/csrc/python_headers.h>
 
+#include "torch_npu/csrc/core/npu/NPUMacros.h"
+#include "torch_npu/csrc/core/npu/NPUStream.h"
+
 struct THNPStream : THPStream {
-  c10_npu::NPUStream npu_stream;
+    c10_npu::NPUStream npu_stream;
 };
 extern PyObject *THNPStreamClass;
 
 TORCH_NPU_API void THNPStream_init(PyObject *module);
 
 inline bool THNPStream_Check(PyObject* obj) {
-  return THNPStreamClass && PyObject_IsInstance(obj, THNPStreamClass);
+    return THNPStreamClass && PyObject_IsInstance(obj, THNPStreamClass);
 }
 
 TORCH_NPU_API std::vector<c10::optional<c10_npu::NPUStream>> THNPUtils_PySequence_to_NPUStreamList(PyObject* obj);
