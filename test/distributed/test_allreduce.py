27a3a723279075e65ca78346df2f3b7e1b569580
zhangqiongwen <zhangqiongwen@huawei.com>
2025-02-13 06:45:28 +0000

    !17926 支持reduceop.avg Merge pull request !17926 from zhangqiongwen/v2.5.1_reduceop_avg

--- test/distributed/test_allreduce.py
+++ test/distributed/test_allreduce.py
@@ -23,14 +23,16 @@ class HcomAllReduceTest(TestCase):
         return dist
 
     @classmethod
-    def _test_all_reduce(cls, rank, input1, world_size, init_pg, c2p):
+    # pylint:disable=huawei-too-many-arguments
+    def _test_all_reduce(cls, rank, input1, world_size, init_pg, c2p, reduce_op=dist.ReduceOp.SUM):
         dist_group = init_pg(rank, world_size)
         dst = 0
         input1 = input1.npu()
-        dist_group.all_reduce(input1)
+        dist_group.all_reduce(input1, reduce_op)
         c2p.put((rank, dst, input1.cpu()))
 
-    def _test_multiprocess(self, f, init_pg, expected, input1, world_size):
+    # pylint:disable=huawei-too-many-arguments
+    def _test_multiprocess(self, f, init_pg, expected, input1, world_size, reduce_op=dist.ReduceOp.SUM):
         ctx = mp.get_context('spawn')
         c2p = ctx.Queue(world_size)
 
@@ -38,7 +40,7 @@ class HcomAllReduceTest(TestCase):
         for i in range(world_size):
             p = ctx.Process(
                 target=f,
-                args=(i, input1.cpu(), world_size, init_pg, c2p))
+                args=(i, input1.cpu(), world_size, init_pg, c2p, reduce_op))
             p.start()
             ps.append(p)
 
@@ -46,10 +48,24 @@ class HcomAllReduceTest(TestCase):
             rank, dst, output = c2p.get()
             if rank == dst:
                 self.assertEqual(output, expected,
-                                 ("rank {} Expect receive tensor {} but got {}.").format(rank, expected, output))
+                                 "rank {} world_size {} dtype {} shape {} Expect receive tensor {} but got {}.".format(
+                                     rank, world_size, expected.dtype, expected.shape, expected, output))
 
         for p in ps:
             p.join()
+
+    def _construct_excepted_result(self, inputs, world_size, dtype=np.float32, reduce_op=dist.ReduceOp.SUM):
+        expected = 0
+        for _ in range(world_size):
+            expected += inputs
+
+        if reduce_op == dist.ReduceOp.AVG:
+            if dtype in [np.int32, np.int8, np.int64, np.uint8]:
+                expected //= world_size
+            else:
+                expected /= world_size
+
+        return expected
 
     @skipIfUnsupportMultiNPU(2)
     def test_dist_all_reduce(self):
@@ -63,9 +79,7 @@ class HcomAllReduceTest(TestCase):
         for world_size in ranks:
             for shape in shape_format:
                 exp_input, input1 = create_common_tensor(shape, -10, 10)
-                expected = 0
-                for _ in range(world_size):
-                    expected += exp_input
+                expected = self._construct_excepted_result(exp_input, world_size)
                 self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
                                         HcomAllReduceTest._init_dist_hccl, expected, input1, world_size)
 
@@ -76,9 +90,7 @@ class HcomAllReduceTest(TestCase):
         shape_format = [np.int64, 2, [1]]
         for world_size in ranks:
             exp_input, input1 = create_common_tensor(shape_format, -10, 10)
-            expected = 0
-            for _ in range(world_size):
-                expected += exp_input
+            expected = self._construct_excepted_result(exp_input, world_size)
             self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
                                     HcomAllReduceTest._init_dist_hccl, expected, input1, world_size)
 
@@ -91,11 +103,48 @@ class HcomAllReduceTest(TestCase):
                 if len(shape[2]) == 1:
                     continue
                 exp_input, input1 = create_common_tensor(shape, 0, 10)
-                expected = 0
-                for _ in range(world_size):
-                    expected += exp_input
+                expected = self._construct_excepted_result(exp_input, world_size)
                 self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
                                         HcomAllReduceTest._init_dist_hccl, expected, input1, world_size)
+
+    @skipIfUnsupportMultiNPU(2)
+    def test_dist_all_reduce_avg(self):
+        # CI currently supports only 2 devices
+        ranks = [2]
+        dtype_list = [np.float32, np.int32]
+        shape_format = [
+            [i, 2, [3, 16]] for i in dtype_list
+        ]
+        for world_size in ranks:
+            for shape in shape_format:
+                exp_input, input1 = create_common_tensor(shape, -10, 10)
+                expected = self._construct_excepted_result(exp_input, world_size, shape[0], dist.ReduceOp.AVG)
+                self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
+                                        HcomAllReduceTest._init_dist_hccl, expected, input1, world_size,
+                                        dist.ReduceOp.AVG)
+
+    @skipIfUnsupportMultiNPU(2)
+    def test_dist_all_reduce_int64_avg(self):
+        # CI currently supports only 2 devices
+        ranks = [2]
+        shape_format = [np.int64, 2, [1]]
+        for world_size in ranks:
+            exp_input, input1 = create_common_tensor(shape_format, -10, 10)
+            expected = self._construct_excepted_result(exp_input, world_size, np.int64, dist.ReduceOp.AVG)
+            self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
+                                    HcomAllReduceTest._init_dist_hccl, expected, input1, world_size, dist.ReduceOp.AVG)
+
+    @skipIfUnsupportMultiNPU(2)
+    def test_dist_all_reduce_uint8_avg(self):
+        ranks = [2]
+        shape_format = [[np.uint8, 2, [3, 16]]]
+        for world_size in ranks:
+            for shape in shape_format:
+                exp_input, input1 = create_common_tensor(shape, 0, 10)
+                expected = self._construct_excepted_result(exp_input, world_size, shape[0], dist.ReduceOp.AVG)
+                self._test_multiprocess(HcomAllReduceTest._test_all_reduce,
+                                        HcomAllReduceTest._init_dist_hccl, expected, input1, world_size,
+                                        dist.ReduceOp.AVG)
 
 
 if __name__ == '__main__':
