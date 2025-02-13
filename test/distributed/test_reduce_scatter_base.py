20f67fa7027b4208759f92486bf0f7728c8ee661
zhangqiongwen <zhangqiongwen@huawei.com>
2025-02-13 06:45:53 +0000

    !17929 支持reduceop.avg Merge pull request !17929 from zhangqiongwen/v2.1.0_reduceop_avg

--- test/distributed/test_reduce_scatter_base.py
+++ test/distributed/test_reduce_scatter_base.py
@@ -17,14 +17,16 @@ from test_reduce_scatter import HcclReduceScatterTestBase
 class HcclReduceScatterBaseTest(HcclReduceScatterTestBase):
 
     @classmethod
-    def _test_reduce_scatter_base(cls, rank, input_list, world_size, init_pg, c2p):
+    # pylint:disable=huawei-too-many-arguments
+    def _test_reduce_scatter_base(cls, rank, input_list, world_size, init_pg, c2p, p2c, reduce_op=dist.ReduceOp.SUM):
         pg = init_pg(rank, world_size)
         input_list_npu = [input.npu() for input in input_list]
         input_tensor = torch.cat(input_list_npu)
         output = torch.empty_like(input_list_npu[rank])
-        pg._reduce_scatter_base(output, input_tensor)
+        pg._reduce_scatter_base(output, input_tensor, reduce_op)
         c2p.put((rank, output.cpu()))
         pg.barrier()
+        p2c.get()
 
     @skipIfUnsupportMultiNPU(2)
     def test_reduce_scatter_base(self):
@@ -63,6 +65,23 @@ class HcclReduceScatterBaseTest(HcclReduceScatterTestBase):
                 expected = self._construct_excepted_result(input_list, world_size, dist._reduce_scatter_base)
                 self._test_multiprocess(HcclReduceScatterBaseTest._test_reduce_scatter_base,
                                         HcclReduceScatterBaseTest._init_dist_hccl, expected, input_list, world_size)
+
+    @skipIfUnsupportMultiNPU(2)
+    def test_reduce_scatter_base_avg(self):
+        ranks = [2]
+        dtype_list = [np.float32, np.float16, np.int32, np.int8, np.int64]
+        shape_format = [[i, 2, [4, 9]] for i in dtype_list]
+        for world_size in ranks:
+            for shape in shape_format:
+                if shape[0] == np.int8:
+                    shape[1] = 0
+                input_list = []
+                for _ in range(world_size):
+                    _, input1 = create_common_tensor(shape, -10, -10)
+                    input_list.append(input1.cpu())
+                expected = self._construct_excepted_result(input_list, world_size, dist._reduce_scatter_base, dist.ReduceOp.AVG)
+                self._test_multiprocess(HcclReduceScatterBaseTest._test_reduce_scatter_base,
+                                        HcclReduceScatterBaseTest._init_dist_hccl, expected, input_list, world_size, dist.ReduceOp.AVG)
 
 
 if __name__ == '__main__':
