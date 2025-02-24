96092809251a5144bc6998fae6134dc008854915
will-devil <wangyicheng16@huawei.com>
2025-02-24 03:41:23 +0000

    !17218 [Task] Adapt to the aten::_to_copy instead of the aten::to.dtype_layout. Merge pull request !17218 from will-devil/remove_to_kernel_test

--- torch_npu/csrc/aten/common/ToKernelNpu.cpp
+++ torch_npu/csrc/aten/common/ToKernelNpu.cpp
@@ -58,50 +58,72 @@ static inline at::Tensor to_impl_npu(
   return r;
 }
 
-at::Tensor NPUNativeFunctions::to(
-    const at::Tensor &self,
+at::Tensor NPUNativeFunctions::_to_copy(
+    const at::Tensor& self,
     c10::optional<at::ScalarType> dtype,
     c10::optional<c10::Layout> layout,
     c10::optional<c10::Device> device,
     c10::optional<bool> pin_memory,
     bool non_blocking,
-    bool copy,
     c10::optional<c10::MemoryFormat> optional_memory_format)
 {
+    c10::TensorOptions options_ = c10::TensorOptions()
+        .dtype(dtype)
+        .layout(layout)
+        .device(device);
+
+    auto options = self.options().merge_in(options_);
+
+    if (layout.has_value()) {
+        TORCH_CHECK(
+            self.layout() == layout.value(),
+            "to(options) doesn't support converting to a different layout, "
+            "but got self.layout being ",
+            self.layout(),
+            " and options.layout set as ",
+            layout.value(), OPS_ERROR(ErrCode::NOT_SUPPORT));
+    }
+
+    if (device.has_value()) {
+        options = options.device(ensure_has_index(device.value()));
+    }
+
     if (optional_memory_format.has_value()) {
         TORCH_CHECK(
             optional_memory_format.value() == c10::MemoryFormat::Preserve ||
             optional_memory_format.value() == c10::MemoryFormat::Contiguous,
             "Only contiguous_format or preserve_format is supported.", OPS_ERROR(ErrCode::NOT_SUPPORT));
+        options = options.memory_format(optional_memory_format.value());
+    } else {
+        options = options.memory_format(c10::MemoryFormat::Contiguous);
     }
-
-    c10::TensorOptions options_ = c10::TensorOptions().dtype(dtype).layout(layout).device(device);
-    TORCH_CHECK(
-        !(options_.has_memory_format() && optional_memory_format.has_value()),
-        "Cannot set memory_format both in c10::TensorOptions and explicit argument; please delete "
-        "the redundant setter.", OPS_ERROR(ErrCode::PARAM));
-    auto options =
-        options_.merge_in(c10::TensorOptions().memory_format(optional_memory_format));
-
     TORCH_CHECK(
         options.requires_grad_opt() == c10::nullopt,
         "to(options) expects unset requires_grad flag, but got "
         "options.requires_grad set as ",
         options.requires_grad(), OPS_ERROR(ErrCode::PARAM));
 
-    TORCH_CHECK(
-        !options.has_layout() || self.layout() == options.layout(),
-        "to(options) doesn't support converting to a different layout, "
-        "but got self.layout being ",
-        self.layout(),
-        " and options.layout set as ",
-        options.layout(), OPS_ERROR(ErrCode::NOT_SUPPORT));
+    bool pin_out = non_blocking && torch_npu::utils::is_npu(self) && options.device().is_cpu() &&
+                    (options.layout() == c10::kStrided);
 
-    if (options.has_device()) {
-        options = options.device(ensure_has_index(options.device()));
+    c10::MemoryFormat memory_format = options.memory_format_opt().value_or(c10::MemoryFormat::Contiguous);
+    if (memory_format == c10::MemoryFormat::Preserve) {
+        if (self.is_non_overlapping_and_dense()) {
+            // Copy all strides
+            auto r = at::empty_strided(
+                self.sizes(), self.strides(), options.memory_format(c10::nullopt).pinned_memory(pin_out));
+            r.copy_(self, non_blocking);
+            return r;
+        } else {
+            memory_format = self.suggest_memory_format();
+        }
     }
-    auto specified_options = self.options().merge_in(options);
-    return to_impl_npu(self, specified_options, non_blocking, copy);
+    
+    // See Note [Explicit nullopt c10::MemoryFormat argument]
+    auto r = at::empty(
+        self.sizes(), options.memory_format(memory_format).pinned_memory(pin_out), c10::nullopt);
+    r.copy_(self, non_blocking);
+    return r;
 }
 
 at::Tensor NPUNativeFunctions::to(
@@ -110,7 +132,8 @@ at::Tensor NPUNativeFunctions::to(
     at::ScalarType dtype,
     bool non_blocking,
     bool copy,
-    c10::optional<c10::MemoryFormat> optional_memory_format) {
+    c10::optional<c10::MemoryFormat> optional_memory_format)
+{
   device = ensure_has_index(device);
   return to_impl_npu(
       self,
