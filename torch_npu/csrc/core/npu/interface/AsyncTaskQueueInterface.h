10127fc6ce1b226b03a8af304d993b77109b5fea
jiangpengfei <jiangpengfei24@huawei.com>
2025-03-07 03:11:38 +0000

    !18392 cleancode Merge pull request !18392 from jiangpengfei/v2.4.0_cleancode

--- torch_npu/csrc/core/npu/interface/AsyncTaskQueueInterface.h
+++ torch_npu/csrc/core/npu/interface/AsyncTaskQueueInterface.h
@@ -7,29 +7,29 @@
 namespace c10_npu {
 namespace queue {
 struct CopyParas {
-  void *dst = nullptr;
-  size_t dstLen = 0;
-  void *src = nullptr;
-  size_t srcLen = 0;
-  aclrtMemcpyKind kind = ACL_MEMCPY_HOST_TO_HOST;
-  void Copy(CopyParas& other);
-  static std::map<int64_t, std::string> COPY_PARAS_MAP;
+    void *dst = nullptr;
+    size_t dstLen = 0;
+    void *src = nullptr;
+    size_t srcLen = 0;
+    aclrtMemcpyKind kind = ACL_MEMCPY_HOST_TO_HOST;
+    void Copy(CopyParas& other);
+    static std::map<int64_t, std::string> COPY_PARAS_MAP;
 };
 
 enum EventAllocatorType {
-  HOST_ALLOCATOR_EVENT = 1,
-  NPU_ALLOCATOR_EVENT = 2,
-  RESERVED = -1,
+    HOST_ALLOCATOR_EVENT = 1,
+    NPU_ALLOCATOR_EVENT = 2,
+    RESERVED = -1,
 };
 
 struct EventParas {
-  explicit EventParas(aclrtEvent aclEvent, EventAllocatorType allocatorType)
-      : event(aclEvent), eventAllocatorType(allocatorType) {}
-  EventParas() = default;
-  aclrtEvent event = nullptr;
-  void Copy(EventParas& other);
-  EventAllocatorType eventAllocatorType = RESERVED;
-  static std::map<int64_t, std::string> EVENT_PARAS_MAP;
+    explicit EventParas(aclrtEvent aclEvent, EventAllocatorType allocatorType)
+        : event(aclEvent), eventAllocatorType(allocatorType) {}
+    EventParas() = default;
+    aclrtEvent event = nullptr;
+    void Copy(EventParas& other);
+    EventAllocatorType eventAllocatorType = RESERVED;
+    static std::map<int64_t, std::string> EVENT_PARAS_MAP;
 };
 
 enum QueueParamType {
@@ -43,13 +43,13 @@ enum QueueParamType {
 };
 
 struct QueueParas {
-  QueueParas(QueueParamType type, size_t len, void *val) : paramType(type), paramLen(len), paramVal(val) {}
-  aclrtStream paramStream = nullptr;
-  QueueParamType paramType = COMPILE_AND_EXECUTE;
-  size_t paramLen = 0;
-  void* paramVal = nullptr;
-  static std::atomic<uint64_t> g_correlation_id;
-  uint64_t correlation_id = 0;
+    QueueParas(QueueParamType type, size_t len, void *val) : paramType(type), paramLen(len), paramVal(val) {}
+    aclrtStream paramStream = nullptr;
+    QueueParamType paramType = COMPILE_AND_EXECUTE;
+    size_t paramLen = 0;
+    void* paramVal = nullptr;
+    static std::atomic<uint64_t> g_correlation_id;
+    uint64_t correlation_id = 0;
 };
 
 aclError LaunchAsyncCopyTask(void* dst, size_t dstLen, void* src, size_t srcLen, aclrtMemcpyKind kind);
--- torch_npu/csrc/core/npu/register/FunctionLoader.cpp
+++ torch_npu/csrc/core/npu/register/FunctionLoader.cpp
@@ -7,80 +7,80 @@ namespace c10_npu {
 namespace option {
 
 FunctionLoader::FunctionLoader(const std::string& name) {
-  this->fileName = name + ".so";
+    this->fileName = name + ".so";
 }
 
 FunctionLoader::~FunctionLoader() {
-  if (this->handle != nullptr) {
-    dlclose(this->handle);
-  }
+    if (this->handle != nullptr) {
+        dlclose(this->handle);
+    }
 }
 
 void FunctionLoader::Set(const std::string& name) {
-  this->registry[name] = nullptr;
+    this->registry[name] = nullptr;
 }
 
 void* FunctionLoader::Get(const std::string& name) {
-  if (this->handle == nullptr) {
-    auto handle = dlopen(this->fileName.c_str(), RTLD_LAZY | RTLD_GLOBAL);
-    if (handle == nullptr) {
-      AT_ERROR(dlerror());
-      return nullptr;
+    if (this->handle == nullptr) {
+        auto handle = dlopen(this->fileName.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+        if (handle == nullptr) {
+            AT_ERROR(dlerror());
+            return nullptr;
+        }
+        this->handle = handle;
     }
-    this->handle = handle;
-  }
 
-  auto itr = registry.find(name);
-  if (itr == registry.end()) {
-    AT_ERROR("function(", name, ") is not registered.");
-    return nullptr;
-  }
+    auto itr = registry.find(name);
+    if (itr == registry.end()) {
+        AT_ERROR("function(", name, ") is not registered.");
+        return nullptr;
+    }
 
-  if (itr->second != nullptr) {
-    return itr->second;
-  }
+    if (itr->second != nullptr) {
+        return itr->second;
+    }
 
-  auto func = dlsym(this->handle, name.c_str());
-  if (func == nullptr) {
-    return nullptr;
-  }
-  this->registry[name] = func;
-  return func;
+    auto func = dlsym(this->handle, name.c_str());
+    if (func == nullptr) {
+        return nullptr;
+    }
+    this->registry[name] = func;
+    return func;
 }
 
 namespace register_function {
-  FunctionRegister* FunctionRegister::GetInstance() {
-    static FunctionRegister instance;
-    return &instance;
-  }
-  void FunctionRegister::Register(const std::string& name, ::std::unique_ptr<FunctionLoader>& ptr) {
-    std::lock_guard<std::mutex> lock(mu_);
-    registry.emplace(name, std::move(ptr));
-  }
-
-  void FunctionRegister::Register(const std::string& name, const std::string& funcName) {
-    auto itr = registry.find(name);
-    if (itr == registry.end()) {
-      AT_ERROR(name, " library should register first.");
-      return;
+    FunctionRegister* FunctionRegister::GetInstance() {
+        static FunctionRegister instance;
+        return &instance;
     }
-    itr->second->Set(funcName);
-  }
-
-  void* FunctionRegister::Get(const std::string& soName, const std::string& funcName) {
-    auto itr = registry.find(soName);
-    if (itr != registry.end()) {
-      return itr->second->Get(funcName);
+    void FunctionRegister::Register(const std::string& name, ::std::unique_ptr<FunctionLoader>& ptr) {
+        std::lock_guard<std::mutex> lock(mu_);
+        registry.emplace(name, std::move(ptr));
     }
-    return nullptr;
-  }
 
-  FunctionRegisterBuilder::FunctionRegisterBuilder(const std::string& name, ::std::unique_ptr<FunctionLoader>& ptr) {
-    FunctionRegister::GetInstance()->Register(name, ptr);
-  }
-  FunctionRegisterBuilder::FunctionRegisterBuilder(const std::string& soName, const std::string& funcName) {
-    FunctionRegister::GetInstance()->Register(soName, funcName);
-  }
+    void FunctionRegister::Register(const std::string& name, const std::string& funcName) {
+        auto itr = registry.find(name);
+        if (itr == registry.end()) {
+            AT_ERROR(name, " library should register first.");
+            return;
+        }
+        itr->second->Set(funcName);
+    }
+
+    void* FunctionRegister::Get(const std::string& soName, const std::string& funcName) {
+        auto itr = registry.find(soName);
+        if (itr != registry.end()) {
+            return itr->second->Get(funcName);
+        }
+        return nullptr;
+    }
+
+    FunctionRegisterBuilder::FunctionRegisterBuilder(const std::string& name, ::std::unique_ptr<FunctionLoader>& ptr) {
+        FunctionRegister::GetInstance()->Register(name, ptr);
+    }
+    FunctionRegisterBuilder::FunctionRegisterBuilder(const std::string& soName, const std::string& funcName) {
+        FunctionRegister::GetInstance()->Register(soName, funcName);
+    }
 } // namespace register_function
 
 
