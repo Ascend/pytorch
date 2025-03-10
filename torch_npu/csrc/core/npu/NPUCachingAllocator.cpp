72e58143e505288a10894c4c53d4469125481553
杜金航 <15990042527@163.com>
2025-03-10 06:35:57 +0000

    !17358 Fixed Failure to save data on oom devices except device 0 Merge pull request !17358 from 杜金航/v2.1_cleancode

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -987,7 +987,7 @@ class DeviceCachingAllocator {
 
   void attachOutOfMemoryObserver(OutOfMemoryObserver observer)
   {
-      oom_observers_.emplace_back(std::move(observer));
+      oom_observers_.emplace_back(observer);
   }
 
   bool checkUceInMemPool()
@@ -2831,7 +2831,7 @@ class NpuCachingAllocator : public NPUAllocator {
   void attachOutOfMemoryObserver(OutOfMemoryObserver observer) override
   {
       for (auto& allocator : device_allocator) {
-          allocator->attachOutOfMemoryObserver(std::move(observer));
+          allocator->attachOutOfMemoryObserver(observer);
       }
   }
 
