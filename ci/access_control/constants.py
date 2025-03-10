78dfb70cb88ebd0952577117d804dc2bb706ac95
yuhaiyan <yuhaiyan8@huawei.com>
2025-03-10 08:16:42 +0000

    !18754 Fixed a value in the slow_test_blocklist. Merge pull request !18754 from yuhaiyan/v2.4.0-dev1

--- ci/access_control/constants.py
+++ ci/access_control/constants.py
@@ -14,7 +14,7 @@ SLOW_TEST_BLOCKLIST = [
     'test_reductions',
     'test_unary_ufuncs',
     'test_ops_jit',
-    'test_jit_fuser_te.py',
+    'test_jit_fuser_te',
     'onnx/test_fx_op_consistency',
     "onnx/test_op_consistency",
     'onnx/test_pytorch_onnx_onnxruntime'
