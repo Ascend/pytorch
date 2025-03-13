51abc069df8c7137098a7b0550b3190ee38113fe
dilililiwhy <why.wuhuanyu@huawei.com>
2025-03-13 07:27:18 +0000

    !18753 [2/N] cleancode (torch_npu/csrc/aten) Merge pull request !18753 from dilililiwhy/cleancode_aten_231_part2

--- torch_npu/csrc/aten/mirror/NPUMemoryOverlap.cpp
+++ torch_npu/csrc/aten/mirror/NPUMemoryOverlap.cpp
@@ -2,13 +2,16 @@
 #include "torch_npu/csrc/core/npu/NPUException.h"
 #include <c10/core/Layout.h>
 
-namespace at_npu { namespace native {
+namespace at_npu {
+namespace native {
 
-MemOverlap has_internal_overlap(const at::Tensor& tensor) {
-  return has_internal_overlap(tensor.unsafeGetTensorImpl());
+MemOverlap has_internal_overlap(const at::Tensor& tensor)
+{
+    return has_internal_overlap(tensor.unsafeGetTensorImpl());
 }
 
-MemOverlap has_internal_overlap(at::TensorImpl* t) {
+MemOverlap has_internal_overlap(at::TensorImpl* t)
+{
     AT_ASSERT(t->layout() == at::kStrided, PTA_ERROR(ErrCode::PARAM));
 
     if (t->is_contiguous()) {
@@ -26,22 +29,26 @@ MemOverlap has_internal_overlap(at::TensorImpl* t) {
     return MemOverlap::TOO_HARD;
 }
 
-void assert_no_internal_overlap(const at::Tensor& t) {
-  assert_no_internal_overlap(t.unsafeGetTensorImpl());
+void assert_no_internal_overlap(const at::Tensor& t)
+{
+    assert_no_internal_overlap(t.unsafeGetTensorImpl());
 }
 
-void assert_no_internal_overlap(at::TensorImpl* t) {
-  TORCH_CHECK(has_internal_overlap(t) != MemOverlap::YES,
-      "unsupported operation: more than one element of the written-to tensor "
-      "refers to a single memory location. Please clone() the tensor before "
-      "performing the operation.", PTA_ERROR(ErrCode::NOT_SUPPORT));
+void assert_no_internal_overlap(at::TensorImpl* t)
+{
+    TORCH_CHECK(has_internal_overlap(t) != MemOverlap::YES,
+        "unsupported operation: more than one element of the written-to tensor "
+        "refers to a single memory location. Please clone() the tensor before "
+        "performing the operation.", PTA_ERROR(ErrCode::NOT_SUPPORT));
 }
 
-MemOverlapStatus get_overlap_status(const at::Tensor& a, const at::Tensor& b) {
-  return get_overlap_status(a.unsafeGetTensorImpl(), b.unsafeGetTensorImpl());
+MemOverlapStatus get_overlap_status(const at::Tensor& a, const at::Tensor& b)
+{
+    return get_overlap_status(a.unsafeGetTensorImpl(), b.unsafeGetTensorImpl());
 }
 
-MemOverlapStatus get_overlap_status(const at::TensorImpl* a, const at::TensorImpl* b) {
+MemOverlapStatus get_overlap_status(const at::TensorImpl* a, const at::TensorImpl* b)
+{
     if (a == b) {
         return MemOverlapStatus::FULL;
     }
@@ -66,15 +73,18 @@ MemOverlapStatus get_overlap_status(const at::TensorImpl* a, const at::TensorImp
     return MemOverlapStatus::NO;
 }
 
-void assert_no_partial_overlap(const at::Tensor& a, const at::Tensor& b) {
-  assert_no_partial_overlap(a.unsafeGetTensorImpl(), b.unsafeGetTensorImpl());
+void assert_no_partial_overlap(const at::Tensor& a, const at::Tensor& b)
+{
+    assert_no_partial_overlap(a.unsafeGetTensorImpl(), b.unsafeGetTensorImpl());
 }
 
-void assert_no_partial_overlap(at::TensorImpl* a, at::TensorImpl* b) {
-  TORCH_CHECK(get_overlap_status(a, b) != MemOverlapStatus::PARTIAL,
-      "unsupported operation: some elements of the input tensor and "
-      "the written-to tensor refer to a single memory location. "
-      "Please clone() the tensor before performing the operation.", PTA_ERROR(ErrCode::NOT_SUPPORT));
+void assert_no_partial_overlap(at::TensorImpl* a, at::TensorImpl* b)
+{
+    TORCH_CHECK(get_overlap_status(a, b) != MemOverlapStatus::PARTIAL,
+        "unsupported operation: some elements of the input tensor and "
+        "the written-to tensor refer to a single memory location. "
+        "Please clone() the tensor before performing the operation.", PTA_ERROR(ErrCode::NOT_SUPPORT));
 }
 
-}}
+}
+}
