ca2540ead6ff3d5e4ad1e57402ecc26e6e514fec
jiangpengfei <jiangpengfei24@huawei.com>
2025-03-13 02:03:28 +0000

    !18866 !18773 Fix Cpu Affinity Merge pull request !18866 from jiangpengfei/v2.3.1

--- torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
+++ torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.cpp
@@ -191,8 +191,6 @@ NpuSysCtrl::SysStatus NpuSysCtrl::Initialize(int device_id)
 
     GetAffinityInfo();
 
-    SetThreadAffinity(device_id_);
-
     init_flag_ = true;
     ASCEND_LOGD("Npu sys ctrl initialize successfully.");
 
