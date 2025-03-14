b06656aacdb6a20a3c2f5abc6da74802d574071c
wgb <wgb_strive@163.com>
2025-03-14 07:41:58 +0000

    !18899 device check support  for kwargs parameters Merge pull request !18899 from wgb/2.5_copy

--- test/custom_ops/test_npu_anti_quant.py
+++ test/custom_ops/test_npu_anti_quant.py
@@ -4,8 +4,8 @@ import torch
 
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
-from torch_npu.testing.common_utils import create_common_tensor
-DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
+from torch_npu.testing.common_utils import create_common_tensor, SupportedDevices
+from torch_npu.testing.common_distributed import skipIfUnsupportMultiNPU
 
 
 class TestAntiQuant(TestCase):
@@ -27,8 +27,7 @@ class TestAntiQuant(TestCase):
         output = torch_npu.npu_anti_quant(input_x, scale, offset=offset, dst_dtype=dst_dtype, src_dtype=src_dtype)
         return output.cpu().detach()
 
-    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
-        "OP `AscendAntiQuant` is only supported on 910B, skip this ut for this device type!")
+    @SupportedDevices(['Ascend910B'])
     def test_anti_quant(self, device="npu"):
         shape_format = [
             [[np.int8, -1, [10, 100]], [np.float32, -1, [100]], [np.float32, -1, [100]], torch.float16, None],
@@ -53,6 +52,23 @@ class TestAntiQuant(TestCase):
                 npu_output = npu_output.to(torch.float32)
                 custom_output = custom_output.to(torch.float32)
             self.assertRtolEqual(npu_output, custom_output)
+
+    @skipIfUnsupportMultiNPU(2)
+    @SupportedDevices(['Ascend910B'])
+    def test_anti_quant_device_check(self, device="npu"):
+        shape_format = [
+            [[np.int8, -1, [10, 100]], [np.float32, -1, [100]], [np.float32, -1, [100]], torch.float16, None],
+        ]
+        
+        for item in shape_format:
+            _, npu_input_x = create_common_tensor(item[0], -127, 127)
+            _, npu_scale = create_common_tensor(item[1], -100, 100)
+            _, npu_offset = (None, None) if item[2] is None else create_common_tensor(item[2], -100, 100)
+
+            msg = "Expected all tensors to be on the same device, but found at least two devices, npu:"
+            with self.assertRaisesRegex(RuntimeError, msg):
+                self.npu_op_exec(npu_input_x, npu_scale, npu_offset.to("npu:1"), *item[3:])
+
 
 if __name__ == "__main__":
     run_tests()
