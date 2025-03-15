7f5f9ac7a2fda72822aa5236eab34b6a1f1a5204
叶子凡 <vscience@qq.com>
2025-03-15 06:32:07 +0000

    !18871 CleanCode Fix Merge pull request !18871 from 叶子凡/v2.3.1_cleancode_0307

--- torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
+++ torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
@@ -472,7 +472,8 @@ bool ProcessGroupHCCL::WorkHCCL::isCompleted()
     return exception() || finishedNPUExecutionInternal();
 }
 
-bool ProcessGroupHCCL::WorkHCCL::isStarted() {
+bool ProcessGroupHCCL::WorkHCCL::isStarted()
+{
     checkAndSetException();
     return exception() || startedNPUExecutionInternal();
 }
@@ -517,7 +518,8 @@ bool ProcessGroupHCCL::WorkHCCL::finishedNPUExecution()
     return finishedNPUExecutionInternal();
 }
 
-bool ProcessGroupHCCL::WorkHCCL::startedNPUExecutionInternal() const {
+bool ProcessGroupHCCL::WorkHCCL::startedNPUExecutionInternal() const
+{
     try {
         for (const auto i : c10::irange(devices_.size())) {
             // Checking the work's corresponding ASCEND events' status
@@ -635,7 +637,8 @@ void ProcessGroupHCCL::WorkHCCL::checkDispatch()
     }
 }
 
-void ProcessGroupHCCL::WorkHCCL::synchronize() {
+void ProcessGroupHCCL::WorkHCCL::synchronize()
+{
     // Call Synchronize without a timeout. We use this method to avoid adding a
     // timeout argument to the public synchronize API.
     synchronizeInternal(kNoTimeout);
@@ -1222,17 +1225,20 @@ void ProcessGroupHCCL::workCleanupLoop()
 }
 
 std::exception_ptr ProcessGroupHCCL::WorkHCCL::checkForHCCLErrors(
-    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms) const {
+    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms) const
+{
     return checkForHCCLErrorsInternal(hcclComms);
 }
 
 std::exception_ptr ProcessGroupHCCL::checkForHCCLErrors(
-    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms) {
+    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms)
+{
     return checkForHCCLErrorsInternal(hcclComms);
 }
 
 std::exception_ptr ProcessGroupHCCL::checkForHCCLErrorsInternal(
-    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms) {
+    const std::vector<std::shared_ptr<HCCLComm>>& hcclComms)
+{
     for (const auto& hcclComm : hcclComms) {
         HcclResult hcclAsyncErr = hcclComm->checkForHcclError();
         if (hcclAsyncErr != HCCL_SUCCESS) {
