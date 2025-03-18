9459e37d4ca8fb7cefa05ea04ab38babfdf390ed
王育才 <wangyucai2@huawei.com>
2025-03-18 12:19:18 +0000

    !19043 modify copy_from_and_resize Merge pull request !19043 from 王育才/v2.6.0

--- torch_npu/csrc/aten/ops/CopyFromAndResizeKernelNpu.cpp
+++ torch_npu/csrc/aten/ops/CopyFromAndResizeKernelNpu.cpp
@@ -6,14 +6,20 @@ namespace native {
 
 at::Tensor NPUNativeFunctions::_copy_from_and_resize(const at::Tensor& self, const at::Tensor& dst)
 {
+    TORCH_CHECK(dst.defined(), "dst is undefined", OPS_ERROR(ErrCode::NOT_SUPPORT));
+    TORCH_CHECK(self.defined(), "self is undefined", OPS_ERROR(ErrCode::NOT_SUPPORT));
+    
+    if (dst.numel() == 0) {
+        dst.resize_as_(self);
+    }
     TORCH_CHECK(self.sizes() == dst.sizes(),
-        "_copy_from_and_resize now only support copy with same size!", OPS_ERROR(ErrCode::NOT_SUPPORT));
+        "_copy_from_and_resize now only support copy with same size, or dst.numel() == 0!",
+        OPS_ERROR(ErrCode::NOT_SUPPORT));
     TORCH_CHECK(self.is_cpu() && dst.device().is_privateuseone(),
         "_copy_from_and_resize now only support copy from cpu tensor to npu tensor, but got src tensor device is ",
         self.device(), " and dst device is ", dst.device(), OPS_ERROR(ErrCode::NOT_SUPPORT));
     dst.copy_(self);
     return dst;
 }
-
 }
 }
