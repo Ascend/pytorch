a1fbdaf168eb4d5ae5bad90207957efae7b8da77
yuhaiyan <yuhaiyan8@huawei.com>
2025-03-18 12:11:38 +0000

    !19008 Add APIs for CANNVersion. Merge pull request !19008 from yuhaiyan/v2.5.1-dev2

---
+++ torch_npu/csrc/npu/GetCANNInfo.h
@@ -0,0 +1,14 @@
+#ifndef THNP_GETCANNINFO_INC
+#define THNP_GETCANNINFO_INC
+#include "torch_npu/csrc/core/npu/NPUMacros.h"
+
+
+TORCH_NPU_API std::string GetCANNVersion(const std::string& module = "CANN");
+
+/*
+support version format: a.b.c, a.b.RCd, a.b.Tg, a.b.RCd.alphaf
+formula: ((a+1) * 100000000) + ((b+1) * 1000000) + ((c+1) * 10000) + ((d+1) * 100 + 5000) + ((g+1) * 100) - (100 - f)
+*/
+bool IsGteCANNVersion(const std::string version, const std::string module = "CANN");
+
+#endif
