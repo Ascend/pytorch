e17a59ab38e6b0e82277eb3db0d481e37f6860a7
yuhaiyan <yuhaiyan8@huawei.com>
2025-03-21 08:29:04 +0000

    !19253 Fixed failed testcases. Merge pull request !19253 from yuhaiyan/v2.3.1-dev1

--- test/onnx/test_combined_onnx_ops.py
+++ test/onnx/test_combined_onnx_ops.py
@@ -213,6 +213,9 @@ class TestOnnxOps(TestCase):
                 return torch_npu.npu_conv3d(input_, self.weight, self.bias,
                                             stride, paddings, dilation, groups)
 
+        torch.npu.config.allow_internal_format = True
+        torch.npu.set_compile_mode(jit_compile=True)
+
         def export_onnx(onnx_model_name):
             input_ = torch.rand([1, 128, 4, 14, 14]).npu()
             model = Model().to("npu")
