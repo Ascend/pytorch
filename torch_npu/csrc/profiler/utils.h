1a8bcee393581cee085d49c4d01f7ab857ddc99b
SCh-zx <1325467101@qq.com>
2025-03-25 06:25:28 +0000

    !19421 [cleancode]npu&profiler Merge pull request !19421 from SCh-zx/v2.4.0-7.0.0

--- torch_npu/csrc/profiler/utils.h
+++ torch_npu/csrc/profiler/utils.h
@@ -13,41 +13,42 @@
 namespace torch_npu {
 namespace profiler {
 
-std::unordered_map<std::string, c10::IValue> saveExtraArgs(const at::RecordFunction& fn);
+std::unordered_map<std::string, c10::IValue> saveExtraArgs(const at::RecordFunction &fn);
 
-uint64_t computeFlops(const std::string &op_name,
-    const std::unordered_map<std::string, c10::IValue> &extra_args);
+uint64_t computeFlops(const std::string &op_name, const std::unordered_map<std::string, c10::IValue> &extra_args);
 
 class NPURecordFunction {
 public:
-  NPURecordFunction(bool enable_ = false) : enable(enable_) {
-    if (NPURecordFunction::use_npu_simple) {
-      at::enableRecordFunction(enable);
+    NPURecordFunction(bool enable_ = false) : enable(enable_)
+    {
+        if (NPURecordFunction::use_npu_simple) {
+            at::enableRecordFunction(enable);
+        }
     }
-  }
 
-  ~NPURecordFunction() {
-    if (NPURecordFunction::use_npu_simple) {
-      at::enableRecordFunction(!enable);
+    ~NPURecordFunction()
+    {
+        if (NPURecordFunction::use_npu_simple) {
+            at::enableRecordFunction(!enable);
+        }
     }
-  }
-  bool enable = false;
-  static bool use_npu_simple;
+    bool enable = false;
+    static bool use_npu_simple;
 };
 
-inline THPCodeObjectPtr PyFrame_GetCode_NPU(PyFrameObject* frame)
+inline THPCodeObjectPtr PyFrame_GetCode_NPU(PyFrameObject *frame)
 {
     return THPCodeObjectPtr(PyFrame_GetCode(frame));
 }
 
-inline PyFrameObject* PyEval_GetFrame_NPU()
+inline PyFrameObject *PyEval_GetFrame_NPU()
 {
     auto frame = PyEval_GetFrame();
     Py_XINCREF(frame);
     return frame;
 }
 
-inline THPObjectPtr PyFrame_GetLocals_NPU(PyFrameObject* frame)
+inline THPObjectPtr PyFrame_GetLocals_NPU(PyFrameObject *frame)
 {
     return THPObjectPtr(PyFrame_GetLocals(frame));
 }
