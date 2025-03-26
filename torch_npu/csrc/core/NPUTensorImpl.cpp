acf356a3978d5181288ef1dce460d86521a28868
SCh-zx <1325467101@qq.com>
2025-03-26 01:41:32 +0000

    !19529 [cleancode]core Merge pull request !19529 from SCh-zx/v2.4.0-7.0.0

--- torch_npu/csrc/core/NPUTensorImpl.cpp
+++ torch_npu/csrc/core/NPUTensorImpl.cpp
@@ -8,56 +8,39 @@
 #include "third_party/acl/inc/acl/acl_rt.h"
 #include "torch_npu/csrc/core/NPUStorageImpl.h"
 
-namespace torch_npu
+namespace torch_npu {
+NPUTensorImpl::NPUTensorImpl(c10::Storage &&storage, const caffe2::TypeMeta &data_type)
+    : c10::TensorImpl(std::move(storage),
+    c10::DispatchKeySet{ c10::DispatchKey::PrivateUse1, c10::DispatchKey::AutogradPrivateUse1 }, data_type)
 {
-  NPUTensorImpl::NPUTensorImpl(c10::Storage &&storage, const caffe2::TypeMeta &data_type)
-      : c10::TensorImpl(std::move(storage),
-                        c10::DispatchKeySet{c10::DispatchKey::PrivateUse1,
-                                            c10::DispatchKey::AutogradPrivateUse1},
-                        data_type)
-  {
     is_non_overlapping_and_dense_ = false;
-  }
+}
 
-  void NPUTensorImpl::shallow_copy_from(const c10::intrusive_ptr<TensorImpl> &impl)
-  {
-    copy_tensor_metadata(
-        impl.get(),
-        this,
-        version_counter(),
-        allow_tensor_metadata_change());
+void NPUTensorImpl::shallow_copy_from(const c10::intrusive_ptr<TensorImpl> &impl)
+{
+    copy_tensor_metadata(impl.get(), this, version_counter(), allow_tensor_metadata_change());
     refresh_numel();
     refresh_contiguous();
-  }
+}
 
-  c10::intrusive_ptr<c10::TensorImpl> NPUTensorImpl::shallow_copy_and_detach(
-      const c10::VariableVersion &version_counter,
-      bool allow_tensor_metadata_change) const
-  {
+c10::intrusive_ptr<c10::TensorImpl> NPUTensorImpl::shallow_copy_and_detach(const c10::VariableVersion &version_counter,
+    bool allow_tensor_metadata_change) const
+{
     auto impl = c10::make_intrusive<NPUTensorImpl>(c10::Storage(this->storage()), this->data_type_);
-    copy_tensor_metadata(
-        this,
-        impl.get(),
-        version_counter,
-        allow_tensor_metadata_change);
+    copy_tensor_metadata(this, impl.get(), version_counter, allow_tensor_metadata_change);
     impl->refresh_numel();
     impl->refresh_contiguous();
     return impl;
-  }
+}
 
-  c10::intrusive_ptr<c10::TensorImpl> NPUTensorImpl::shallow_copy_and_detach(
-      c10::VariableVersion &&version_counter,
-      bool allow_tensor_metadata_change) const
-  {
+c10::intrusive_ptr<c10::TensorImpl> NPUTensorImpl::shallow_copy_and_detach(c10::VariableVersion &&version_counter,
+    bool allow_tensor_metadata_change) const
+{
     auto impl = c10::make_intrusive<NPUTensorImpl>(c10::Storage(this->storage()), this->data_type_);
-    copy_tensor_metadata(
-        this,
-        impl.get(),
-        std::move(version_counter),
-        allow_tensor_metadata_change);
+    copy_tensor_metadata(this, impl.get(), std::move(version_counter), allow_tensor_metadata_change);
     impl->refresh_numel();
     impl->refresh_contiguous();
     return impl;
-  }
-  NPUTensorImpl::~NPUTensorImpl() {}
+}
+NPUTensorImpl::~NPUTensorImpl() {}
 }
--- torch_npu/csrc/core/npu/register/OptionRegister.cpp
+++ torch_npu/csrc/core/npu/register/OptionRegister.cpp
@@ -7,12 +7,13 @@
 
 namespace c10_npu {
 namespace option {
-
-OptionInterface::OptionInterface(OptionCallBack callback) {
+OptionInterface::OptionInterface(OptionCallBack callback)
+{
     this->callback = callback;
 }
 
-void OptionInterface::Set(const std::string& in) {
+void OptionInterface::Set(const std::string &in)
+{
     this->val = in;
     if (this->callback != nullptr) {
         if (c10_npu::NpuSysCtrl::GetInstance().GetInitFlag()) {
@@ -25,24 +26,27 @@ void OptionInterface::Set(const std::string& in) {
     }
 }
 
-std::string OptionInterface::Get() {
+std::string OptionInterface::Get()
+{
     return val;
 }
 
 
 namespace register_options {
-OptionRegister* OptionRegister::GetInstance() {
+OptionRegister *OptionRegister::GetInstance()
+{
     static OptionRegister instance;
     return &instance;
 }
 
-void OptionRegister::Register(const std::string& name,
-    ::std::unique_ptr<OptionInterface>& ptr) {
+void OptionRegister::Register(const std::string &name, ::std::unique_ptr<OptionInterface> &ptr)
+{
     std::lock_guard<std::mutex> lock(mu_);
     registry.emplace(name, std::move(ptr));
 }
 
-void OptionRegister::Set(const std::string& name, const std::string& val) {
+void OptionRegister::Set(const std::string &name, const std::string &val)
+{
     auto itr = registry.find(name);
     if (itr != registry.end()) {
         itr->second->Set(val);
@@ -51,7 +55,8 @@ void OptionRegister::Set(const std::string& name, const std::string& val) {
     }
 }
 
-c10::optional<std::string> OptionRegister::Get(const std::string& name) {
+c10::optional<std::string> OptionRegister::Get(const std::string &name)
+{
     auto itr = registry.find(name);
     if (itr != registry.end()) {
         return itr->second->Get();
@@ -59,17 +64,16 @@ c10::optional<std::string> OptionRegister::Get(const std::string& name) {
     return c10::nullopt; // default value
 }
 
-OptionInterfaceBuilder::OptionInterfaceBuilder(
-    const std::string& name,
-    ::std::unique_ptr<OptionInterface>& ptr,
-    const std::string& type) {
+OptionInterfaceBuilder::OptionInterfaceBuilder(const std::string &name, ::std::unique_ptr<OptionInterface> &ptr,
+    const std::string &type)
+{
     OptionRegister::GetInstance()->Register(name, ptr);
 
     // init the value if env variable.
     if (type == "env") {
         std::string env_name = name;
         std::transform(env_name.begin(), env_name.end(), env_name.begin(), ::toupper);
-        char* env_val = std::getenv(env_name.c_str());
+        char *env_val = std::getenv(env_name.c_str());
         if (env_val != nullptr) {
             std::string val(env_val);
             OptionRegister::GetInstance()->Set(name, val);
@@ -78,19 +82,21 @@ OptionInterfaceBuilder::OptionInterfaceBuilder(
 }
 } // namespace register_options
 
-void SetOption(const std::string& key, const std::string& val) {
+void SetOption(const std::string &key, const std::string &val)
+{
     register_options::OptionRegister::GetInstance()->Set(key, val);
 }
 
-void SetOption(const std::map<std::string, std::string>& options) {
+void SetOption(const std::map<std::string, std::string> &options)
+{
     for (auto item : options) {
         SetOption(item.first, item.second);
     }
 }
 
-c10::optional<std::string> GetOption(const std::string& key) {
+c10::optional<std::string> GetOption(const std::string &key)
+{
     return register_options::OptionRegister::GetInstance()->Get(key);
 }
-
 } // namespace option
 } // namespace c10_npu
