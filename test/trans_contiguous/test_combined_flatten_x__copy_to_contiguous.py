a8ac3c4c6518bf2ae5ed8d3785aa2c3d50cde19d
yuhaiyan <yuhaiyan8@huawei.com>
2025-03-31 09:20:35 +0000

    !19756 Fixed/skip some failed testcases Merge pull request !19756 from yuhaiyan/v2.3.1-dev2

--- test/trans_contiguous/test_combined_flatten_x__copy_to_contiguous.py
+++ test/trans_contiguous/test_combined_flatten_x__copy_to_contiguous.py
@@ -28,15 +28,17 @@ class CombinedFlattenXCopyToContiguous(TestCase):
             # case 1: flatten+select
             with torch.autograd.profiler.profile(use_device='npu') as prof:
                 npu_out1 = npu_input.flatten(2).select(1, 1).contiguous()
-            self.assertEqual(check_operators_in_prof(['contiguous_h_match', 'contiguous_d_StridedSlice'], prof),
-                             True, "Error operators called!")
+            self.assertEqual(check_operators_in_prof(['contiguous_h_match', 'contiguous_d_StridedSlice'], prof)
+                             or check_operators_in_prof(['aclnnInplaceCopy'], prof),
+                             True, message="Error operators called!")
             cpu_out1 = cpu_input.flatten(2).select(1, 1).contiguous()
             self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
             # case 2: select+flatten == can be optimized as single select(contiguous_h_combined should not be called)
             with torch.autograd.profiler.profile(use_device='npu') as prof:
                 npu_out2 = npu_input.select(2, 1).flatten(1).contiguous()
-            self.assertEqual(check_operators_in_prof(['contiguous_d_StridedSlice'], prof, ['contiguous_h_combined']),
-                             True, "Error operators called!")
+            self.assertEqual(check_operators_in_prof(['contiguous_d_StridedSlice'], prof, ['contiguous_h_combined'])
+                             or check_operators_in_prof(['aclnnInplaceCopy'], prof, ['contiguous_h_combined']),
+                             True, message="Error operators called!")
             cpu_out2 = cpu_input.select(2, 1).flatten(1).contiguous()
             self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy())
 
@@ -55,16 +57,18 @@ class CombinedFlattenXCopyToContiguous(TestCase):
             # case 1: flatten+strideslice ==> can be optimized as slice(contiguous with offset) + select
             with torch.autograd.profiler.profile(use_device='npu') as prof:
                 npu_out1 = npu_input.flatten()[2:100:10].contiguous()
-            self.assertEqual(check_operators_in_prof(['contiguous_d_Reshape', 'contiguous_d_StridedSlice'], prof),
-                             True, "Error operators called!")
+            self.assertEqual(check_operators_in_prof(['contiguous_d_Reshape', 'contiguous_d_StridedSlice'], prof)
+                             or check_operators_in_prof(['aclnnInplaceCopy'], prof),
+                             True, message="Error operators called!")
             cpu_out1 = cpu_input.flatten()[2:100:10].contiguous()
             self.assertRtolEqual(npu_out1.to("cpu").numpy(), cpu_out1.numpy())
             # case 2: strideslice+flatten==> can be optimized as single strideslice
             # (contiguous_h_combined should not be called)
             with torch.autograd.profiler.profile(use_device='npu') as prof:
                 npu_out2 = npu_input[:, 2:20:3].flatten().contiguous()
-            self.assertEqual(check_operators_in_prof(['contiguous_d_StridedSlice'], prof, ['contiguous_h_combined']),
-                             True, "Error operators called!")
+            self.assertEqual(check_operators_in_prof(['contiguous_d_StridedSlice'], prof, ['contiguous_h_combined'])
+                             or check_operators_in_prof(['aclnnInplaceCopy'], prof, ['contiguous_h_combined']),
+                             True, message="Error operators called!")
             cpu_out2 = cpu_input[:, 2:20:3].flatten().contiguous()
             self.assertRtolEqual(npu_out2.to("cpu").numpy(), cpu_out2.numpy())
 
