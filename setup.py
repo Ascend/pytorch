8f99a26aa5d041edbe7c2defe5d0105d7cc63e6f
shaoyf <shaoyifan1@huawei.com>
2025-04-10 06:26:24 +0000

    !20206 update version from post10 to post11 Merge pull request !20206 from shaoyf/210_600_post

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from wheel.bdist_wheel import bdist_wheel
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 THIRD_PARTY_PATH = os.path.join(BASE_DIR, "third_party")
-VERSION = '2.1.0.post10'
+VERSION = '2.1.0.post11'
 UNKNOWN = "Unknown"
 BUILD_PERMISSION = stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
 
