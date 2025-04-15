2fb83338349a1935cc4dc932a15f804be3cba39a
闫鹏全 <yanpengquan@huawei.com>
2025-04-15 09:34:24 +0000

    !20357 Add api transfer for CudaGraph Merge pull request !20357 from 闫鹏全/master

--- torch_npu/contrib/transfer_to_npu.py
+++ torch_npu/contrib/transfer_to_npu.py
@@ -352,6 +352,9 @@ def _init():
     if hasattr(torch.distributed, 'init_device_mesh'):
         _del_nccl_device_backend_map()
         torch.distributed.init_device_mesh = _wrapper_cuda(torch.distributed.init_device_mesh)
+    
+    # CudaGraph
+    torch.cuda.CudaGraph = torch.npu.NPUGraph
 
     # torch.nn.parallel.DistributedDataParallel
     _device_wrapper(torch.nn.parallel.DistributedDataParallel, torch_distributed_fn_white_list)
