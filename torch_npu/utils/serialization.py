d10ed9c0f0e7ac728624f76aaade3dad3dda1836
dilililiwhy <why.wuhuanyu@huawei.com>
2025-04-23 10:36:47 +0000

    !20631 Reset opened_file after catching TarError Merge pull request !20631 from dilililiwhy/reset_tell_210

--- torch_npu/utils/serialization.py
+++ torch_npu/utils/serialization.py
@@ -10,7 +10,7 @@ import torch
 from torch.serialization import _check_dill_version, _open_file_like, _is_zipfile, \
     _open_zipfile_reader, _is_torchscript_zip, _weights_only_unpickler, \
     _legacy_load, _load, FILE_LIKE, MAP_LOCATION, DEFAULT_PROTOCOL, \
-    normalize_storage_type, location_tag, _open_zipfile_writer, closing, _should_read_directly
+    normalize_storage_type, location_tag, _open_zipfile_writer, _check_seekable, closing, _should_read_directly
 
 import torch_npu
 from torch_npu.utils._error_code import ErrCode, pta_error
@@ -198,6 +198,7 @@ def load(
                                    "please torch.save your checkpoint with this option in order to use mmap." +
                                    pta_error(ErrCode.PARAM))
             if weights_only:
+                _check_seekable(opened_file)
                 f_should_read_directly = _should_read_directly(opened_file)
                 if f_should_read_directly and opened_file.tell() == 0:
                     try:
@@ -209,7 +210,7 @@ def load(
                             ) from None
                     except tarfile.TarError:
                         # ignore TarError and pass opened_file to torch._legacy_load
-                        pass
+                        opened_file.seek(0)
                 try:
                     return _legacy_load(opened_file, map_location, _weights_only_unpickler, **pickle_load_args)
                 except RuntimeError as e:
