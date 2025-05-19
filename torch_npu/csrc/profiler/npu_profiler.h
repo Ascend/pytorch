97c317d27e2582cf0f17326d7bf211a24b471091
梅飞要 <1332490378@qq.com>
2025-05-19 12:30:35 +0000

    !20968 [PROF]fix mstx err Merge pull request !20968 from 梅飞要/2.4

--- torch_npu/csrc/profiler/npu_profiler.h
+++ torch_npu/csrc/profiler/npu_profiler.h
@@ -153,9 +153,11 @@ struct MstxRange {
             return;
         }
         rangeId = MstxMgr::GetInstance()->getRangeId();
-        if (at_npu::native::IsSupportMstxDomainFunc() && MstxMgr::GetInstance()->isMstxTxDomainEnable(domainName)) {
-            domainHandle = MstxMgr::GetInstance()->createProfDomain(domainName.c_str());
-            at_npu::native::MstxDomainRangeStartA(domainHandle, message.c_str(), stream, rangeId);
+        if (at_npu::native::IsSupportMstxDomainFunc()) {
+            if (MstxMgr::GetInstance()->isMstxTxDomainEnable(domainName)) {
+                domainHandle = MstxMgr::GetInstance()->createProfDomain(domainName);
+                at_npu::native::MstxDomainRangeStartA(domainHandle, message.c_str(), stream, rangeId);
+            }
         } else {
             at_npu::native::MstxRangeStartA(message.c_str(), stream, rangeId);
         }
@@ -167,7 +169,9 @@ struct MstxRange {
             return;
         }
         if (at_npu::native::IsSupportMstxDomainFunc()) {
-            at_npu::native::MstxDomainRangeEnd(domainHandle, rangeId);
+            if (domainHandle != nullptr) {
+                at_npu::native::MstxDomainRangeEnd(domainHandle, rangeId);
+            }
         } else {
             at_npu::native::MstxRangeEnd(rangeId);
         }
