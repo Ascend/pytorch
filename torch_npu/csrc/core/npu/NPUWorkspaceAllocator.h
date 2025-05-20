60833f6f35958cca2cbe6eac1822495d491f8afb
杜金航 <15990042527@163.com>
2025-05-20 02:20:09 +0000

    !21061 Add snapshots to workspace Merge pull request !21061 from 杜金航/v2.4.0

--- torch_npu/csrc/core/npu/NPUWorkspaceAllocator.h
+++ torch_npu/csrc/core/npu/NPUWorkspaceAllocator.h
@@ -2,14 +2,24 @@
 
 #include <c10/core/Allocator.h>
 #include "torch_npu/csrc/core/npu/NPUMacros.h"
+#include "torch_npu/csrc/core/npu/NPUCachingAllocator.h"
 
 namespace c10_npu {
 namespace NPUWorkspaceAllocator {
+
+using c10_npu::NPUCachingAllocator::CreateContextFn;
+using c10_npu::NPUCachingAllocator::BlockInfo;
+using c10_npu::NPUCachingAllocator::SegmentInfo;
+using c10_npu::NPUCachingAllocator::TraceEntry;
+using c10_npu::NPUCachingAllocator::SnapshotInfo;
+using c10_npu::NPUCachingAllocator::RecordContext;
 
 c10::Allocator* get();
 void init();
 c10::DataPtr malloc_with_stream(size_t size, aclrtStream stream);
 void emptyCache(int device, bool need_empty_queue, bool check_error = true);
+void recordHistory(bool enabled, CreateContextFn context_recorder, RecordContext when);
+SnapshotInfo snapshot();
 
 } // namespace NPUWorkspaceAllocator
 } // namespace c10_npu
