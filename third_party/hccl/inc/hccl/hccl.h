d6c9f377b306b580d8d95e75cd3ed85e5de1526f
shaoyf <shaoyifan1@huawei.com>
2025-06-13 09:53:06 +0000

    回退 'Pull Request !21868 : update hccl.h & hccl_type.h'

--- third_party/hccl/inc/hccl/hccl.h
+++ third_party/hccl/inc/hccl/hccl.h
@@ -206,7 +206,7 @@ inline void HcclCommConfigInit(HcclCommConfig *config)
     info->reserved = 0;
 
     config->hcclBufferSize = HCCL_COMM_DEFAULT_BUFFSIZE;
-    config->hcclDeterministic = HCCL_COMM_DETERMINISTIC_CONFIG_NOT_SET;
+    config->hcclDeterministic = HCCL_COMM_DEFAULT_DETERMINISTIC;
     config->hcclCommName[0] = '\0';
     config->hcclUdi[0] = '\0';
     config->hcclRdmaTrafficClass = HCCL_COMM_TRAFFIC_CLASS_CONFIG_NOT_SET;
