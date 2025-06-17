19e46a16b44a46287f9fa1d72b3b22e2e0469c73
闫鹏全 <yanpengquan@huawei.com>
2025-06-17 12:41:40 +0000

    !21976 support IFA update with non-out api Merge pull request !21976 from 闫鹏全/v2.1.0

--- torch_npu/npu/graphs.py
+++ torch_npu/npu/graphs.py
@@ -139,7 +139,26 @@ class _GraphDispatchMode(torch.utils._python_dispatch.TorchDispatchMode):
 
     def __torch_dispatch__(self, func, types, args=(), kwargs=None):
         if func.__name__ == "npu_fused_infer_attention_score":
-            raise RuntimeError("Only support npu_fused_infer_attention_score.out", pta_error(ErrCode.NOT_SUPPORT))
+            func_out = torch_npu.npu_fused_infer_attention_score.out
+            self.update_schema(str(func_out.__name__), str(func_out._schema))
+            stream = torch_npu.npu.current_stream()
+            event = torch.npu.ExternalEvent()
+            event.wait(stream)
+            event.reset(stream)
+            # apply tensor
+            workspace = torch_npu._npu_fused_infer_attention_score_get_max_workspace(*args, **kwargs)
+            output = torch.empty_like(args[0])
+            softmax_lse = torch.empty(1, dtype=args[0].dtype, device=args[0].device)
+            kwargs["workspace"] = workspace
+            kwargs["out"] = [output, softmax_lse]
+            # begin graph task
+            graph_task_group_begin(stream)
+            func_out(*args, **kwargs)
+            handle = graph_task_group_end(stream)
+            # save state for update
+            self.graph_dispatch_records.append(
+                self._append_dispatch_record(event, handle, args, kwargs, func_out))
+            return kwargs["out"]
         elif func.__name__ == "npu_fused_infer_attention_score.out":
             self.update_schema(str(func.__name__), str(func._schema))
             stream = torch_npu.npu.current_stream()
