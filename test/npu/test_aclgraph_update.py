e04829d36abaebf6ca3beb229d464cf4517fd55a
闫鹏全 <yanpengquan@huawei.com>
2025-06-17 12:41:41 +0000

    !21975 support IFA update with non-out api Merge pull request !21975 from 闫鹏全/v2.7.1

--- test/npu/test_aclgraph_update.py
+++ test/npu/test_aclgraph_update.py
@@ -93,6 +93,35 @@ class TestAclgraphUpdate(TestCase):
         self.assertEqual(output.cpu(), res_src[0].cpu())
         self.assertEqual(softmax_lse.cpu(), res_src[1].cpu())
 
+    @SupportedDevices(['Ascend910B'])
+    def test_ifa_update_with_non_out_and_auto_dispatch_capture(self):
+        torch.npu.set_device(0)
+        length = [29]
+        length_new = [100]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length_new)
+
+        g = torch.npu.NPUGraph()
+        output = None
+        softmax_lse = None
+
+        with torch.npu.graph(g, auto_dispatch_capture=True):
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty(1, dtype=torch.float16, device="npu")
+            output, softmax_lse = torch_npu.npu_fused_infer_attention_score(
+                query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+                next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length)
+        
+        g.update(cpu_update_input=[{"actual_seq_lengths": length_new}])
+        g.replay()
+        self.assertEqual(output.cpu(), res_src[0].cpu())
+        self.assertEqual(softmax_lse.cpu(), res_src[1].cpu())
 
 if __name__ == "__main__":
     run_tests()
