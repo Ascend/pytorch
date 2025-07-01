98ac662a99322b0e1476948b1d9679e456310f55
郭光浩 <guoguanghao@huawei.com>
2025-07-01 13:41:34 +0000

    !22466 add save_async Merge pull request !22466 from 郭光浩/v2.5.1-7.1.0

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -1,12 +1,12 @@
 __all__ = ["npu_combine_tensors", "get_part_combined_tensor", "is_combined_tensor_valid", "FlopsCounter",
-           "set_thread_affinity", "reset_thread_affinity"]
+           "set_thread_affinity", "reset_thread_affinity", "save_async"]
 
 from torch_npu import _C
 from ._module import _apply_module_patch
 from .tensor_methods import _add_tensor_methods
 from .storage import _add_storage_methods
 from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
-from .serialization import _add_serialization_methods
+from .serialization import _add_serialization_methods, save_async
 from .npu_intercept import _cann_package_check, _add_intercept_methods
 from .dtensor import _register_ops_under_dtensor_rules
 from .collect_env import _add_collect_env_methods
