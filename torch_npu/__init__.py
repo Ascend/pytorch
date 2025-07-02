2ec58dc0b5aaeeae8fe65b5e1a32a382aed0db77
王超 <wangchao430@huawei.com>
2025-07-02 10:12:15 +0000

    !22565 fix lazymodule for torch.Tensor.npu Merge pull request !22565 from 王超/v2.7.0_lazymodule

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -199,6 +199,7 @@ torch._register_device_module('npu', torch_npu.npu)
 unsupported_dtype = [torch.quint8, torch.quint4x2, torch.quint2x4, torch.qint32, torch.qint8]
 torch.utils.generate_methods_for_privateuse1_backend(for_tensor=True, for_module=True, for_storage=True,
                                                      unsupported_dtype=unsupported_dtype)
+torch.nn.parameter.UninitializedTensorMixin._allowed_methods.append(torch.Tensor.npu)
 
 # Apply monkey-patches.
 _apply_patches(all_monkey_patches)
