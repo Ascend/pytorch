066afdfc8e0532ec612c0d233df45c586dbd02a9
SCh-zx <1325467101@qq.com>
2025-07-03 09:10:08 +0000

    !22613 cleancode Merge pull request !22613 from SCh-zx/cleancode67

--- torch_npu/csrc/inductor/aoti_runner/model_container_runner_npu.h
+++ torch_npu/csrc/inductor/aoti_runner/model_container_runner_npu.h
@@ -8,32 +8,25 @@ namespace torch::inductor {
 // NOTICE: Following APIs are subject to change due to active development
 // We provide NO BC guarantee for these APIs
 class TORCH_API AOTIModelContainerRunnerNpu : public AOTIModelContainerRunner {
- public:
-  // @param device_str: cuda device string, e.g. "cuda", "cuda:0"
-  AOTIModelContainerRunnerNpu(
-      const std::string& model_so_path,
-      size_t num_models = 1,
-      const std::string& device_str = "npu",
-      const std::string& cubin_dir = "");
+public:
+    // @param device_str: cuda device string, e.g. "cuda", "cuda:0"
+    AOTIModelContainerRunnerNpu(const std::string &model_so_path, size_t num_models = 1,
+        const std::string &device_str = "npu", const std::string &cubin_dir = "");
 
-  ~AOTIModelContainerRunnerNpu();
+    ~AOTIModelContainerRunnerNpu();
 
-  std::vector<at::Tensor> run(
-    const std::vector<at::Tensor>& inputs,
-    void* stream_handle = nullptr) override;
+    std::vector<at::Tensor> run(const std::vector<at::Tensor> &inputs, void *stream_handle = nullptr) override;
 
-  std::vector<at::Tensor> run_with_npu_stream(
-      std::vector<at::Tensor>& inputs,
-      c10_npu::NPUStream npu_stream);
+    std::vector<at::Tensor> run_with_npu_stream(std::vector<at::Tensor> &inputs, c10_npu::NPUStream npu_stream);
 
-  void init_proxy_executor();
+    void init_proxy_executor();
 
-  void set_proxy_executor(AOTIProxyExecutorHandle handle);
+    void set_proxy_executor(AOTIProxyExecutorHandle handle);
 
- private:
-  std::string model_so_path_;
-  bool init_flag_;
-  std::unique_ptr<torch::aot_inductor::ProxyExecutor> proxy_executor_npu_;
+private:
+    std::string model_so_path_;
+    bool init_flag_;
+    std::unique_ptr<torch::aot_inductor::ProxyExecutor> proxy_executor_npu_;
 };
 
 } // namespace torch::inductor
