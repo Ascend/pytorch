faba85d525222eaac6bc1c08a24a6f8f9f0cd93d
yuhaiyan <yuhaiyan8@huawei.com>
2025-07-09 06:39:22 +0000

    !22797 Skip test_aclrtSetDevice on 910A because it is slow Merge pull request !22797 from yuhaiyan/v2.7.1-dev1

--- test/npu/_fault_mode_cases/error_set_device.py
+++ test/npu/_fault_mode_cases/error_set_device.py
@@ -11,7 +11,7 @@ def _worker(i: int) -> None:
 def set_device():
     torch_npu.npu.set_device(0)
     multiprocessing.set_start_method("spawn", force=True)
-    jobs = [multiprocessing.Process(target=_worker, args=(i,)) for i in range(70)]
+    jobs = [multiprocessing.Process(target=_worker, args=(i,)) for i in range(100)]
 
     for p in jobs:
         p.start()
@@ -19,5 +19,5 @@ def set_device():
     for p in jobs:
         p.join()
 
-
-set_device()
+if __name__ == "__main__":
+    set_device()
