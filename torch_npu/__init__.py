19c3cb9f9153c572bc32396fc7b0241a59c7cfde
sikingbo <929691988@qq.com>
2025-07-15 09:31:46 +0000

    !22764 _npu_format Merge pull request !22764 from sikingbo/v2.1.0

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -60,6 +60,7 @@ from torch_npu.utils import _register_ops_under_dtensor_rules
 from torch_npu.utils.exposed_api import public_npu_functions
 from torch_npu.distributed.checkpoint.checkpoint import _apply_dcp_patch
 from torch_npu.npu._stream_check import apply_sanitizer_patch
+from torch_npu.npu._format import _apply_npu_format_patch
 from torch_npu.multiprocessing.reductions import _add_reductions_methods
 from torch_npu.npu.utils import _erase_stream as erase_stream
 from torch_npu.utils._error_code import ErrCode, pta_error, _except_handler
@@ -155,6 +156,7 @@ def _apply_class_patches():
     _apply_distributed_methods_patch()
     _apply_mstx_patch()
     _add_reductions_methods()
+    _apply_npu_format_patch()
 
 
 def _apply_distributed_methods_patch():
