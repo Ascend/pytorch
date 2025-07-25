daf91d78efc20223ada3d19caf948284b8609307
闫鹏全 <yanpengquan@huawei.com>
2025-07-25 09:47:29 +0000

    !23367 support npugraph backend for torch.compile Merge pull request !23367 from 闫鹏全/v2.7.1

--- torch_npu/utils/_graph_tree.py
+++ torch_npu/utils/_graph_tree.py
@@ -1,4 +1,5 @@
 import functools
+from collections import defaultdict
 from typing import (
     Any,
     Callable,
@@ -12,6 +13,15 @@ from typing import (
 import torch
 from torch.utils._ordered_set import OrderedSet
 from torch._dynamo import utils as dynamo_utils
+from torch._dynamo.backends.common import aot_autograd
+from torch._dynamo.backends.cudagraphs import (
+    check_for_mutation_ignore_cuda_graph_managed_tensor,
+    find_input_mutations,
+    get_device_node_mapping,
+    get_stack_traces,
+)
+from torch._dynamo.backends.debugging import boxed_nop
+from torch._dynamo.backends.registry import register_backend
 from torch._inductor import config
 from torch._inductor.compile_fx import (
     get_input_idxs_to_check,
@@ -21,15 +31,25 @@ from torch._inductor.compile_fx import (
 from torch._inductor.cudagraph_utils import (
     _get_use_stack_trace,
     format_default_skip_message,
+    get_mutation_stack_trace,
+    get_placeholder_info,
+    log_cudagraph_skip_and_bump_counter,
+    BoxedDeviceIndex,
     PlaceholderInfo,
 )
 from torch._inductor.output_code import get_expanded_dims
 from torch._inductor.utils import (
     align_inputs_from_check_idxs,
     copy_misaligned_inputs,
+    count_tangents,
+    get_first_incompatible_cudagraph_node,
+    num_fw_fixed_arguments,
+    output_node,
     remove_unaligned_input_idxs,
+    BoxedBool,
     InputType,
 )
+from torch.multiprocessing.reductions import StorageWeakRef
 
 
 def npugraph_mark_step_begin():
@@ -201,7 +221,136 @@ def npugraphify_impl(
     return align_inputs_from_check_idxs(run, check_input_idxs)
 
 
+def check_for_skip(aot_model: torch.fx.GraphModule, num_fixed) -> Optional[str]:
+    if not torch._dynamo.config.cudagraph_backend_support_input_mutation:
+        mut_skip = check_for_mutation_ignore_cuda_graph_managed_tensor(
+            aot_model, num_fixed
+        )
+        if mut_skip:
+            return mut_skip
+
+    skip = check_multiple_devices_or_any_cpu_nodes(
+        get_device_node_mapping(aot_model)
+    )
+    if skip:
+        return skip
+
+    node = get_first_incompatible_cudagraph_node(aot_model)
+    if node:
+        return format_default_skip_message(f"incompatible op ({node.name})")
+
+    return None
+
+
+def get_device_index(gm) -> int:
+    device = next(iter(get_device_node_mapping(gm)))
+    if not (device.type == "npu"):
+        raise RuntimeError("check device.type == npu fail", )
+    return device.index
+
+
+def npugraphs(dynamo_model, dynamo_inputs):
+    from torch_npu.npu._graph_tree import npugraphify_impl as new_npugraphify_impl
+
+    do_npugraphs = BoxedBool(True)
+    boxed_device_index = BoxedDeviceIndex(None)
+
+    def forward_npugraphs(aot_model, aot_inputs, is_inference=False):
+        interp = boxed_nop(aot_model, aot_inputs)
+        fixed = num_fw_fixed_arguments(len(dynamo_inputs), len(aot_inputs))
+        skip_msg = check_for_skip(aot_model, fixed)
+        if skip_msg:
+            BoxedBool.disable(do_npugraphs)
+            log_cudagraph_skip_and_bump_counter(
+                f"skipping npugraphs due to {skip_msg}"
+            )
+            return interp
+
+        boxed_device_index.set(get_device_index(aot_model))
+        out = new_npugraphify_impl(
+            interp,
+            aot_inputs,
+            range(fixed),
+            device_index=boxed_device_index.value,
+            is_backward=False,
+            is_inference=False,
+            stack_traces=get_stack_traces(aot_model),
+            placeholders=get_placeholder_info(aot_model.graph),
+            mutated_input_idxs=find_input_mutations(aot_model.graph),
+        )
+        out._boxed_call = True
+        return out
+
+    def backward_npugraphs(aot_model, aot_inputs):
+        interp = boxed_nop(aot_model, aot_inputs)
+        if not do_npugraphs:
+            return aot_model
+
+        fixed = count_tangents(aot_model)
+
+        skip_msg = check_for_skip(aot_model, fixed)
+        if skip_msg:
+            log_cudagraph_skip_and_bump_counter(
+                "skipping npugraphs due to %s", skip_msg
+            )
+
+            # See [Backward Generation Handling]
+            from torch_npu.npu._graph_tree import get_manager
+            manager = get_manager(
+                boxed_device_index.value, create_if_none_exists=False
+            )
+
+            if manager is None:
+                raise RuntimeError("check manager is None fail")
+
+            def fn(inputs):
+                manager.set_to_running_backward()
+                return aot_model(inputs)
+
+            fn._boxed_call = True
+            return fn
+
+        out = new_npugraphify_impl(
+            interp,
+            aot_inputs,
+            range(fixed),
+            device_index=get_device_index(aot_model),
+            is_backward=True,
+            is_inference=False,
+            stack_traces=get_stack_traces(aot_model),
+            placeholders=get_placeholder_info(aot_model.graph),
+            mutated_input_idxs=find_input_mutations(aot_model.graph),
+        )
+        out._boxed_call = True
+        return out
+
+    aot_npugraphs = aot_autograd(
+        fw_compiler=forward_npugraphs,
+        bw_compiler=backward_npugraphs,
+        inference_compiler=functools.partial(forward_npugraphs, is_inference=True),
+        keep_inference_input_mutations=torch._dynamo.config.cudagraph_backend_keep_input_mutation,
+    )
+    return aot_npugraphs(dynamo_model, dynamo_inputs)
+
+
+class NpugraphsBackend:
+    compiler_name = "npugraphs"
+
+    @staticmethod
+    def reset():
+        from torch_npu.npu._graph_tree import reset_npugraph_trees
+
+        reset_npugraph_trees()
+
+    @staticmethod
+    def __call__(model, inputs):
+        return npugraphs(model, inputs)
+
+
 def _apply_npugraph_tree_methods():
+    # aot_npugraphs only applies graphs to the graph.  It is also helpful
+    # for debugging and can serve as a perf baseline.
+    register_backend(name="npugraphs", compiler_fn=NpugraphsBackend())
     torch._inductor.compile_fx.cudagraphify = npugraphify
     torch._inductor.cudagraph_utils.check_multiple_devices_or_any_cpu_nodes = check_multiple_devices_or_any_cpu_nodes
     torch.compiler.npugraph_mark_step_begin = npugraph_mark_step_begin
