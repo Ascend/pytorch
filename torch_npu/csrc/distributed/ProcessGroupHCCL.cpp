e81f12c2b47c00c5831ac7bc64fd93232434967b
王超 <wangchao430@huawei.com>
2025-07-26 08:12:02 +0000

    !23369 hcclreducescatterv and hcclgatherv only for 8.3.RC1 in allgather Merge pull request !23369 from 王超/v2.5.0-7.1.0_allgatherv

--- torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
+++ torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
@@ -4239,6 +4239,11 @@ c10::intrusive_ptr<c10d::Work> ProcessGroupHCCL::allgather(
     check_npu_tensors_different_devices(inputTensors);
     auto inputTensors_ = cast_to_origin_format(inputTensors);
     bool same_size = check_same_size(outputTensors.back());
+    static const bool isCannVersionGteBase = []() {
+        const std::string baseCannversion = "8.3.RC1";
+        const std::string baseCannModule = "CANN";
+        return IsGteCANNVersion(baseCannversion, baseCannModule);
+    }();
     if (same_size) {
         int outsize = static_cast<int>(outputTensors[0].size());
         uint64_t output_nums[outsize];
@@ -4311,7 +4316,7 @@ c10::intrusive_ptr<c10d::Work> ProcessGroupHCCL::allgather(
                 }
             },
             c10d::OpType::ALLGATHER);
-    } else if (hcclAllGatherVExist() && !has_empty_tensor(outputTensors.back())) {
+    } else if (isCannVersionGteBase && hcclAllGatherVExist() && !has_empty_tensor(outputTensors.back())) {
         std::vector<at::Tensor> lastOutputTensors = outputTensors.back();
         std::vector<uint64_t> outputCounts;
         std::vector<uint64_t> outputSpl;
@@ -4579,6 +4584,11 @@ c10::intrusive_ptr<c10d::Work> ProcessGroupHCCL::reduce_scatter(
     check_npu_tensors_different_devices(outputTensors);
     auto streamId = getStreamId(false, -1);
     bool same_size = check_same_size(inputTensors.back());
+    static const bool isCannVersionGteBase = []() {
+        const std::string baseCannversion = "8.3.RC1";
+        const std::string baseCannModule = "CANN";
+        return IsGteCANNVersion(baseCannversion, baseCannModule);
+    }();
     if (same_size) {
         auto inputFlattened = flatten_for_scatter_gather(inputTensors, outputTensors, size_);
         check_npu_tensors_different_devices(inputFlattened);
@@ -4639,7 +4649,7 @@ c10::intrusive_ptr<c10d::Work> ProcessGroupHCCL::reduce_scatter(
             }
         },
         c10d::OpType::REDUCE_SCATTER);
-    } else if (hcclReduceScatterVExist()) {
+    } else if (isCannVersionGteBase && hcclReduceScatterVExist()) {
         std::vector<uint64_t> inputCounts;
         std::vector<uint64_t> inputSpl;
         std::vector<at::Tensor> lastInputTensors = inputTensors.back();
