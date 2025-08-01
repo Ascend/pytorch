f2928f055a1b675a4e50449126706307a5f958ac
yuliangbin <1416490440@qq.com>
2025-08-01 01:13:00 +0000

    !23521 [feature_torch_2.7.1] profiler增加msprof权限校验 Merge pull request !23521 from yuliangbin/cann_feature_2.7.1

--- torch_npu/profiler/analysis/_profiler_config.py
+++ torch_npu/profiler/analysis/_profiler_config.py
@@ -156,7 +156,9 @@ class ProfilerConfig:
         self._rank_id = info_json.get(Constant.RANK_ID, -1)
 
     def load_timediff_info(self, profiler_path: str, info_json: dict):
-        self._localtime_diff = CANNFileParser(profiler_path).get_localtime_diff()
+        if ("ProfilerActivity.NPU" in info_json.get(Constant.CONFIG, {}).get(Constant.COMMON_CONFIG, {})
+                .get(Constant.ACTIVITIES, [])):
+            self._localtime_diff = CANNFileParser(profiler_path).get_localtime_diff()
         end_info = info_json.get(Constant.END_INFO, {})
         if not self._localtime_diff and end_info:
             self._localtime_diff = int(end_info.get(Constant.FWK_END_TIME, 0)) - int(
