8f7309944677ccf1d7e96ecf2951d94d2ddb3071
yuliangbin <1416490440@qq.com>
2025-08-07 12:55:05 +0000

    !23694 [torch_2.7.1] 修复单CPU场景PROF文件权限校验 Merge pull request !23694 from yuliangbin/bug_fix_memory_v2.7.1

--- torch_npu/profiler/analysis/_profiler_config.py
+++ torch_npu/profiler/analysis/_profiler_config.py
@@ -156,7 +156,7 @@ class ProfilerConfig:
         self._rank_id = info_json.get(Constant.RANK_ID, -1)
 
     def load_timediff_info(self, profiler_path: str, info_json: dict):
-        if ("ProfilerActivity.NPU" in info_json.get(Constant.CONFIG, {}).get(Constant.COMMON_CONFIG, {})
+        if (Constant.NPU_ACTIVITIES in info_json.get(Constant.CONFIG, {}).get(Constant.COMMON_CONFIG, {})
                 .get(Constant.ACTIVITIES, [])):
             self._localtime_diff = CANNFileParser(profiler_path).get_localtime_diff()
         end_info = info_json.get(Constant.END_INFO, {})
