8a5cc2d50c4e998bea7b42ae62492bc43fab8c76
yuhaiyan <yuhaiyan8@huawei.com>
2025-08-25 07:54:34 +0000

    !24134 Add testcases for get_cann_version and _is_gte_cann_version Merge pull request !24134 from yuhaiyan/v2.7.1-dev1

--- test/npu/test_cann_version.py
+++ test/npu/test_cann_version.py
@@ -17,12 +17,23 @@ class TestCANNversion(TestCase):
                         or re.match("([0-9]+).([0-9]+).T([0-9]+)", version)
                         or re.match("([0-9]+).([0-9]+).RC([0-9]+).alpha([0-9]+)", version))
             self.assertTrue(is_match, f"The env version is {version_env}. The format of cann version {version} is invalid.")
+        
+        version = get_cann_version(module="CAN")
+        self.assertTrue(version == "", "When module is invalid, the result of get_cann_version is not right.")
 
     def test_compare_cann_version(self):
         version_env = get_cann_version_from_env()
         if not version_env.startswith("CANN") and version_env >= "8.1.RC1":
             result = _is_gte_cann_version("8.1.RC1", module="CANN")
             self.assertTrue(result, f"The env version is {version_env}, the result from _is_gte_cann_version is False")
+
+            tags = get_cann_version(module="CANN")
+            major = int(tags[0]) + 1
+            result1 = _is_gte_cann_version(f"{major}.0.0", module="CANN")
+            result2 = _is_gte_cann_version(f"{major}.0.T10", module="CANN")
+            result3 = _is_gte_cann_version(f"{major}.0.RC1.alpha001", module="CANN")
+            self.assertTrue(not result1 and not result2 and not result3, "the result from _is_gte_cann_version is not right.")
+
         else:
             with self.assertRaisesRegex(RuntimeError,
                     "When the version 7.0.0 is less than \"8.1.RC1\", this function is not supported"):
