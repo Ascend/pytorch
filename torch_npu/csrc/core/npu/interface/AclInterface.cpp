4faba86c55f8c535884d4390abd1027ec705b2bf
SCh-zx <1325467101@qq.com>
2025-09-04 09:38:30 +0000

    !24387 [Bugfix] aclrtMemcpyAsyncWithConditon for d2h copy Merge pull request !24387 from SCh-zx/hs21

--- torch_npu/csrc/core/npu/interface/AclInterface.cpp
+++ torch_npu/csrc/core/npu/interface/AclInterface.cpp
@@ -1088,11 +1088,14 @@ aclError AclrtStreamGetId(aclrtStream stream, int32_t* stream_id)
 bool AclrtMemcpyAsyncWithConditionExist()
 {
     const static bool isAclrtMemcpyAsyncWithConditionExist = []() -> bool {
-        auto func  = GET_FUNC(aclrtMemcpyAsyncWithCondition);
+        auto func = GET_FUNC(aclrtMemcpyAsyncWithCondition)
+        bool flag = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1 ? true : false;
         if (func != nullptr) {
             ASCEND_LOGI("Successfully to find function aclrtMemcpyAsyncWithCondition");
+        } else {
+            flag = false;
         }
-        return func != nullptr;
+        return flag;
     }();
     return isAclrtMemcpyAsyncWithConditionExist;
 }
