6b28ef5267b57e4a82e67bca853d876d7c9afa41
王超 <wangchao430@huawei.com>
2025-09-13 09:00:31 +0000

    !24819 [IPC] Add the ipc_collect interface; Synchronize with unmap; LazySetDe… Merge pull request !24819 from 王超/v2.1.0_ipcfix

--- torch_npu/csrc/npu/Module.cpp
+++ torch_npu/csrc/npu/Module.cpp
@@ -44,6 +44,7 @@
 #include "torch_npu/csrc/utils/LazyInit.h"
 #include "third_party/acl/inc/acl/acl.h"
 #include "torch_npu/csrc/profiler/python/combined_traceback.h"
+#include "torch_npu/csrc/ipc/NPUIPCTypes.h"
 #include "torch_npu/csrc/core/npu/interface/OpInterface.h"
 #include "torch_npu/csrc/core/npu/GetCANNInfo.h"
 #include "torch_npu/csrc/core/npu/NPUWorkspaceAllocator.h"
@@ -988,6 +989,14 @@ PyObject* THNPModule_emptyCache(PyObject *_unused, PyObject *noargs)
 {
     HANDLE_TH_ERRORS
     c10_npu::NPUCachingAllocator::emptyCache();
+    END_HANDLE_TH_ERRORS
+    Py_RETURN_NONE;
+}
+
+PyObject* THNPModule_npu_ipc_collect(PyObject *_unused, PyObject *noargs)
+{
+    HANDLE_TH_ERRORS
+    torch_npu::ipc::NpuIPCCollect();
     END_HANDLE_TH_ERRORS
     Py_RETURN_NONE;
 }
@@ -1956,6 +1965,7 @@ static struct PyMethodDef THNPModule_methods[] = {
     {"_npu_is_jit_compile_false", (PyCFunction)THNPModule_is_jit_compile_false_wrap, METH_NOARGS, nullptr},
     {"_npu_setMemoryFraction", (PyCFunction) THNPModule_setMemoryFraction, METH_VARARGS, nullptr},
     {"_npu_emptyCache", (PyCFunction) THNPModule_emptyCache, METH_NOARGS, nullptr},
+    {"_npu_ipc_collect", (PyCFunction) THNPModule_npu_ipc_collect, METH_NOARGS, nullptr},
     {"_npu_emptyVirtAddrCache", (PyCFunction) THNPModule_emptyVirtAddrCache, METH_NOARGS, nullptr},
     {"_npu_memoryStats", (PyCFunction) THNPModule_memoryStats, METH_O, nullptr},
     {"_npu_resetAccumulatedMemoryStats", (PyCFunction) THNPModule_resetAccumulatedMemoryStats, METH_O, nullptr},
