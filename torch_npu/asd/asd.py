[SilentCheck] fix with DTensor of grad
Co-authored-by: wangchao430<wangchao430@huawei.com>



# message auto-generated for no-merge-commit merge:
merge v2.7.0_checkfix1 into v2.7.1

[SilentCheck] fix with DTensor of grad

Created-by: wangchao430
Commit-by: wangchao430
Merged-by: ascend-robot
Description: <!--  Thanks for sending a pull request! 
-->

**What type of PR is this?**
> Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
>
> /kind bug
> /kind task
> /kind feature


**What does this PR do / why do we need it**:


**Which issue(s) this PR fixes**:
<!-- 
*Automatically closes linked issue when PR is merged.
Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
-->
Fixes #

**Special notes for your reviewers**:




See merge request: Ascend/pytorch!25006

torch_npu/asd/asd.py
@@ -462,7 +462,7 @@ class _MatmulSilentCheck:
 
         if self.matmul_hook_enable >= 1:
             with torch.no_grad():
-                if isinstance(self.statistic_value, DTensor):
+                if isinstance(grad, DTensor):
                     self.statistic_value.fill_(torch.pow(torch.norm(grad, float('inf')), 2).detach().float().to_local())
                 else:
                     self.statistic_value.fill_(torch.pow(torch.norm(grad, float('inf')), 2).detach().float())