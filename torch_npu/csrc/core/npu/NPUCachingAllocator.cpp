c4fe9ee140ec9565a304f8edb89c1fe7baf9a228
wangchao430 <wangchao430@huawei.com>
2025-09-23 15:37:18 +0800

    [IPC]fix with nullptr call
    Co-authored-by: wangchao430<wangchao430@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge v2.5.0_ipcfix1 into v2.5.1
    
    [IPC]fix with nullptr call
    
    Created-by: wangchao430
    Commit-by: wangchao430
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!25142

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -3630,9 +3630,11 @@ public:
         {
             if (npu_ipc_ptr_) {
                 return npu_ipc_ptr_;
-            } else {
+            }
+            if (expandable_segment_) {
                 return expandable_segment_->ptr();
             }
+            return nullptr;
         }
         c10::DeviceIndex device_;
         ExpandableSegment* expandable_segment_{nullptr};
