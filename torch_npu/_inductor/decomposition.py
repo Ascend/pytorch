f568d107fac06edcdb732ad71ed3db5fa9a9d9fb
duchengkun <duchengkun1@huawei.com>
2025-09-28 11:39:30 +0800

    [inductor] [revert] revert decomposition of gelu due to acc fail
    Co-authored-by: duchengkun<duchengkun1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge v2.6.0-7.2.0 into v2.6.0-7.2.0
    
    [inductor] [revert] revert decomposition of gelu due to acc fail
    
    Created-by: duchengkun
    Commit-by: duchengkun
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!25286

--- torch_npu/_inductor/decomposition.py
+++ torch_npu/_inductor/decomposition.py
@@ -1,10 +1,24 @@
 import torch._ops
-from torch._inductor.decomposition import pw_cast_for_opmath, register_decomposition
+from torch._inductor.decomposition import decompositions, pw_cast_for_opmath	
+from torch._inductor.decomposition import register_decomposition
+
+from .lowering import _init_set
 
 aten = torch.ops.aten
 
+DECOMPOSITION_OVERLOAD_OP = [
+    aten.gelu
+]
+
 
 def _register_npu_inductor_decompositons():
+    overload_op_set = set()
+    _init_set(DECOMPOSITION_OVERLOAD_OP, overload_op_set)
+
+    for op in overload_op_set:
+        if (op in decompositions):
+            del decompositions[op]
+
     @register_decomposition([aten.scatter.src])
     @pw_cast_for_opmath
     def scatter_src(self, input_tensor, dim, index_tensor, source_tensor):
