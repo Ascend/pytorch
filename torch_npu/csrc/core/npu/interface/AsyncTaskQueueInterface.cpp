038cbd84d9bb3b49ec267876712d0836b4a74f2d
glf2016 <13739288457@163.com>
2025-10-15 14:11:41 +0800

    batch copy en queue
    Co-authored-by: glf2016<13739288457@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !25738 merge cherry-pick-mr-25092-1760427146935-auto into v2.9.0
    
    batch copy en queue
    
    Created-by: glf2016
    Commit-by: glf2016
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!25738

--- torch_npu/csrc/core/npu/interface/AsyncTaskQueueInterface.cpp
+++ torch_npu/csrc/core/npu/interface/AsyncTaskQueueInterface.cpp
@@ -12,6 +12,7 @@
 namespace c10_npu {
 namespace queue {
 std::atomic<uint64_t> QueueParas::g_correlation_id{ 0 };
+static std::string  batch_copy_paras = "acl_memcpy_batch_async";
 std::map<int64_t, std::string> CopyParas::COPY_PARAS_MAP{
     { ACL_MEMCPY_HOST_TO_HOST, "acl_memcpy_host_to_host" },
     { ACL_MEMCPY_HOST_TO_DEVICE, "acl_memcpy_host_to_device" },
@@ -46,6 +47,15 @@ public:
 
 private:
     CopyParas copyParam_;
+};
+
+class AsyncBatchCopyTask {
+public:
+    ~AsyncBatchCopyTask() = default;
+
+    void LaunchBatchCopyTask(void **dsts, size_t *dstLens, void **srcs, size_t *srcLens, size_t numBatches,
+                             aclrtMemcpyBatchAttr *attrs, size_t *attrsIndexes, size_t numAttrs, aclrtStream stream);
+    QueueParas LaunchBatchCopyTaskEnQueue(const string &op_name, const PROC_FUNC &acl_call);
 };
 
 class EventTask {
@@ -96,10 +106,77 @@ void AsyncCopyTask::LaunchCopyTask()
     }
 }
 
+QueueParas AsyncBatchCopyTask::LaunchBatchCopyTaskEnQueue(const string &op_name, const PROC_FUNC &acl_call)
+{
+    at_npu::native::ExecuteParasOpApiV2 execParams;
+    execParams.opName = const_cast<std::string *>(&op_name);
+    execParams.customHandler = const_cast<PROC_FUNC *>(&acl_call);
+    QueueParas params(EXECUTE_OPAPI_V2, sizeof(at_npu::native::ExecuteParasOpApiV2), &execParams);
+    c10_npu::enCurrentNPUStream(&params);
+    return params;
+}
+
+void AsyncBatchCopyTask::LaunchBatchCopyTask(void **dsts, size_t *dstLens, void **srcs, size_t *srcLens,
+                                             size_t numBatches, aclrtMemcpyBatchAttr *attrs, size_t *attrsIndexes,
+                                             size_t numAttrs, aclrtStream stream)
+{
+    RECORD_FUNCTION(batch_copy_paras, std::vector<c10::IValue>({}));
+    auto cur_stream = c10_npu::getCurrentNPUStream();
+    if (!cur_stream.isSyncLaunchStream() && c10_npu::option::OptionsManager::GetTaskQueueEnable()) {
+#ifndef BUILD_LIBTORCH
+        at_npu::native::NpuUtils::ProfReportMarkDataToNpuProfiler(0, batch_copy_paras);
+#endif
+        auto dsts_vec = std::vector<void *>(dsts, dsts + numBatches);
+        auto dstLens_vec = std::vector<size_t>(dstLens, dstLens + numBatches);
+        auto srcs_vec = std::vector<void *>(srcs, srcs + numBatches);
+        auto srcLens_vec = std::vector<size_t>(srcLens, srcLens + numBatches);
+        auto attrs_vec = std::vector<aclrtMemcpyBatchAttr>(attrs, attrs + numBatches);
+        auto attrsIndexes_vec = std::vector<size_t>(attrsIndexes, attrsIndexes + numBatches);
+        auto acl_call = [dsts_vec, dstLens_vec, srcs_vec, srcLens_vec, numBatches, attrs_vec,
+                              attrsIndexes_vec, numAttrs, stream]() -> int {
+            void *dsts[numBatches];
+            void *srcs[numBatches];
+            size_t dstLens[numBatches];
+            size_t srcLens[numBatches];
+            size_t attrsIndexes[numBatches];
+            aclrtMemcpyBatchAttr attrs[numBatches];
+            for (size_t i = 0; i < numBatches; ++i) {
+                dsts[i] = dsts_vec.at(i);
+                srcs[i] = srcs_vec.at(i);
+                dstLens[i] = dstLens_vec.at(i);
+                srcLens[i] = srcLens_vec.at(i);
+                attrsIndexes[i] = attrsIndexes_vec.at(i);
+                attrs[i] = attrs_vec.at(i);
+            }
+            size_t failIdx = SIZE_MAX;
+            return c10_npu::acl::AclrtMemcpyBatchAsync(dsts, dstLens, srcs, srcLens, numBatches, attrs, attrsIndexes,
+                                                       numAttrs, &failIdx, stream);
+        };
+        QueueParas params = LaunchBatchCopyTaskEnQueue("aclrtMemcpyBatchAsync", acl_call);
+#ifndef BUILD_LIBTORCH
+        at_npu::native::NpuUtils::ProfReportMarkDataToNpuProfiler(1, batch_copy_paras, params.correlation_id);
+#endif
+    } else {
+        size_t failIdx = SIZE_MAX;
+        NPU_CHECK_ERROR(
+            c10_npu::acl::AclrtMemcpyBatchAsync(dsts, dstLens, srcs, srcLens, numBatches, attrs, attrsIndexes,
+                                                numAttrs, &failIdx, stream));
+    }
+}
+
 aclError LaunchAsyncCopyTask(void *dst, size_t dstLen, void *src, size_t srcLen, aclrtMemcpyKind kind)
 {
     AsyncCopyTask copyTask(dst, dstLen, src, srcLen, kind);
     copyTask.LaunchCopyTask();
+    return ACL_ERROR_NONE;
+}
+
+aclError LaunchBatchAsyncCopyTask(void **dsts, size_t *dstLens, void **srcs, size_t *srcLens,
+                                  size_t numBatches, aclrtMemcpyBatchAttr *attrs, size_t *attrsIndexes, size_t numAttrs,
+                                  aclrtStream stream)
+{
+    AsyncBatchCopyTask copyTask;
+    copyTask.LaunchBatchCopyTask(dsts, dstLens, srcs, srcLens, numBatches, attrs, attrsIndexes, numAttrs, stream);
     return ACL_ERROR_NONE;
 }
 
