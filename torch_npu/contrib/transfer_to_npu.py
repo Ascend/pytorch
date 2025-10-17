69550dfc22ea982afd9f4bc9443e7cb59ce1ee89
louyujing <louyujing@huawei.com>
2025-10-17 11:13:54 +0800

    Fix the issue where torch.amp.autocast does not work
    Co-authored-by: louyujing<louyujing@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !25795 merge v2.5.1 into v2.5.1
    
    Fix the issue where torch.amp.autocast does not work
    
    Created-by: louyujing
    Commit-by: louyujing
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!25795

--- torch_npu/contrib/transfer_to_npu.py
+++ torch_npu/contrib/transfer_to_npu.py
@@ -29,7 +29,7 @@ torch_fn_white_list = ['logspace', 'randint', 'hann_window', 'rand', 'full_like'
                        'eye', '_sparse_csr_tensor_unsafe', 'empty', '_sparse_coo_tensor_unsafe', 'blackman_window',
                        'zeros_like', 'range', 'sparse_csr_tensor', 'randn_like', 'from_file',
                        '_cudnn_init_dropout_state', '_empty_affine_quantized', 'linspace', 'hamming_window',
-                       'empty_quantized', '_pin_memory', 'autocast', 'load', 'set_default_device', 'Event',
+                       'empty_quantized', '_pin_memory', 'load', 'set_default_device', 'Event',
                        'get_device_module', 'sparse_compressed_tensor']
 torch_tensor_fn_white_list = ['new_empty', 'new_empty_strided', 'new_full', 'new_ones', 'new_tensor', 'new_zeros', 'to',
                               'pin_memory']
@@ -374,6 +374,7 @@ def _init():
     _device_wrapper(torch, torch_fn_white_list)
     torch.UntypedStorage.__new__ = _wrapper_cuda(torch.UntypedStorage.__new__)
     torch.Generator = _GeneratorProxy
+    torch.amp.autocast_mode.autocast.__init__ = _wrapper_cuda(torch.amp.autocast_mode.autocast.__init__)
 
     # torch.Tensor.*
     _device_wrapper(torch.Tensor, torch_tensor_fn_white_list)
