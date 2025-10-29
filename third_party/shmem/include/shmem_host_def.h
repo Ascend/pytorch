214614e2f34c6d9289a146560180a504160f36ac
wangchao430 <wangchao430@huawei.com>
2025-10-29 21:25:14 +0800

    [SHMEM] update with shmem head file
    Co-authored-by: wangchao430<wangchao430@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26132 merge v2.8.0_shmemhead into v2.8.0
    
    [SHMEM] update with shmem head file
    
    Created-by: wangchao430
    Commit-by: wangchao430
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!26132

--- third_party/shmem/include/shmem_host_def.h
+++ third_party/shmem/include/shmem_host_def.h
@@ -10,7 +10,7 @@
 #ifndef SHMEM_HOST_DEF_H
 #define SHMEM_HOST_DEF_H
 #include <climits>
-#include "shmem_types.h"
+#include "third_party/shmem/include/shmem_types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,11 +20,10 @@ extern "C" {
  * @defgroup group_macros Macros
  * @{
 */
-
-/// \def SHMEM_XXX_VERSION
-/// \brief macros that define current version info
-#define SHMEM_MAX_IP_PORT_LEN 64
-/**@} */  // end of group_macros
+/// \def SHMEM_HOST_API
+/// \brief A macro that identifies a function on the host side.
+#define SHMEM_HOST_API   __attribute__((visibility("default")))
+/**@} */ // end of group_macros
 
 /**
  * @defgroup group_enums Enumerations
@@ -35,30 +34,23 @@ extern "C" {
  * @brief Error code for the SHMEM library.
 */
 enum shmem_error_code_t : int {
-    SHMEM_SUCCESS = 0,         ///< Task execution was successful.
-    SHMEM_INVALID_PARAM = -1,  ///< There is a problem with the parameters.
-    SHMEM_INVALID_VALUE = -2,  ///< There is a problem with the range of the value of the parameter.
-    SHMEM_SMEM_ERROR = -3,     ///< There is a problem with SMEM.
-    SHMEM_INNER_ERROR = -4,    ///< This is a problem caused by an internal error.
-    SHMEM_NOT_INITED = -5,     ///< This is a problem caused by an uninitialization.
+    SHMEM_SUCCESS = 0,          ///< Task execution was successful.
+    SHMEM_INVALID_PARAM = -1,   ///< There is a problem with the parameters.
+    SHMEM_INVALID_VALUE = -2,   ///< There is a problem with the range of the value of the parameter.
+    SHMEM_SMEM_ERROR = -3,      ///< There is a problem with SMEM.
+    SHMEM_INNER_ERROR = -4,     ///< This is a problem caused by an internal error.
+    SHMEM_NOT_INITED = -5,      ///< This is a problem caused by an uninitialization.
 };
 
 /**
  * @brief The state of the SHMEM library initialization.
 */
-enum shmem_init_status_t {
-    SHMEM_STATUS_NOT_INITIALIZED = 0,  ///< Uninitialized.
-    SHMEM_STATUS_SHM_CREATED,          ///< Shared memory heap creation is complete.
-    SHMEM_STATUS_IS_INITIALIZED,       ///< Initialization is complete.
-    SHMEM_STATUS_INVALID = INT_MAX,    ///< Invalid status code.
+enum shmem_init_status_t{
+    SHMEM_STATUS_NOT_INITIALIZED = 0,    ///< Uninitialized.
+    SHMEM_STATUS_SHM_CREATED,           ///< Shared memory heap creation is complete.
+    SHMEM_STATUS_IS_INITIALIZED,         ///< Initialization is complete.
+    SHMEM_STATUS_INVALID = INT_MAX,     ///< Invalid status code.
 };
-
-/**@} */  // end of group_enums
-
-/**
- * @defgroup group_structs Structs
- * @{
-*/
 
 constexpr uint16_t SHMEM_UNIQUE_ID_INNER_LEN = 60;
 
@@ -66,6 +58,23 @@ typedef struct {
     int32_t version;
     char internal[SHMEM_UNIQUE_ID_INNER_LEN];
 } shmem_uniqueid_t;
+
+constexpr int32_t SHMEM_UNIQUEID_VERSION = (1 << 16) + sizeof(shmem_uniqueid_t);
+
+#define SHMEM_UNIQUEID_INITIALIZER                      \
+    {                                                   \
+        SHMEM_UNIQUEID_VERSION,                         \
+        {                                               \
+            0                                           \
+        }                                               \
+    }                                                   \
+
+/**@} */ // end of group_enums
+
+/**
+ * @defgroup group_structs Structs
+ * @{
+*/
 
 /**
  * @struct shmem_init_optional_attr_t
@@ -91,18 +100,29 @@ typedef struct {
  *
  * - int my_rank: The rank of the current process.
  * - int n_ranks: The total rank number of all processes.
- * - char ip_port[SHMEM_MAX_IP_PORT_LEN]: The ip and port of the communication server. The port must not conflict
- *   with other modules and processes.
+ * - const char* ip_port: The ip and port of the communication server. The port must not conflict with other modules and processes.
  * - uint64_t local_mem_size: The size of shared memory currently occupied by current rank.
  * - shmem_init_optional_attr_t option_attr: Optional Parameters.
 */
 typedef struct {
     int my_rank;
     int n_ranks;
-    char ip_port[SHMEM_MAX_IP_PORT_LEN];
+    const char* ip_port;
     uint64_t local_mem_size;
     shmem_init_optional_attr_t option_attr;
 } shmem_init_attr_t;
+
+/**
+ * @brief Callback function of private key password decryptor, see shmem_register_decrypt_handler
+ *
+ * @param cipherText       [in] the encrypted text(private password)
+ * @param cipherTextLen    [in] the length of encrypted text
+ * @param plainText        [out] the decrypted text(private password)
+ * @param plaintextLen     [out] the length of plainText
+ */
+typedef int (*shmem_decrypt_handler)(const char *cipherText, size_t cipherTextLen, char *plainText, size_t &plainTextLen);
+
+/**@} */ // end of group_structs
 
 #ifdef __cplusplus
 }
--- third_party/shmem/include/shmem_types.h
+++ third_party/shmem/include/shmem_types.h
@@ -13,15 +13,66 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+/**
+ * @private 
+*/
+#define SHMEM_GLOBAL __global__ __aicore__
+
+/// \def SHMEM_DEVICE
+/// \brief A macro that identifies a function on the device side.
+#define SHMEM_DEVICE __attribute__((always_inline)) __aicore__ __inline__
+
+/**
+ * @addtogroup group_enums
+ * @{
+*/
+
+/**
+ * @brief Team's index.
+*/
+enum shmem_team_index_t{
+    SHMEM_TEAM_INVALID = -1,
+    SHMEM_TEAM_WORLD = 0
+};
 
 /**
  * @brief Data op engine type.
 */
 enum data_op_engine_type_t {
     SHMEM_DATA_OP_MTE = 0x01,
-    SHMEM_DATA_OP_SDMA = 0x02,
-    SHMEM_DATA_OP_ROCE = 0x04,
 };
+
+/**
+ * @brief signal ops, used by signaler in p2p synchronization
+ */
+enum {
+    SHMEM_SIGNAL_SET,
+    SHMEM_SIGNAL_ADD
+};
+
+/**
+ * @brief signal compare ops, used by signalee in p2p synchronization
+ */
+enum {
+    SHMEM_CMP_EQ,
+    SHMEM_CMP_NE,
+    SHMEM_CMP_GT,
+    SHMEM_CMP_GE,
+    SHMEM_CMP_LT,
+    SHMEM_CMP_LE
+};
+
+/**@} */ // end of group_enums
+
+/**
+ * @defgroup group_typedef Typedef
+ * @{
+
+*/
+/**
+ * @brief A typedef of int
+*/
+typedef int shmem_team_t;
 
 /**@} */ // end of group_typedef
 
@@ -29,4 +80,4 @@ enum data_op_engine_type_t {
 }
 #endif
 
-#endif /* SHMEM_TYPES_H */
+#endif /*SHMEM_TYPES_H*/
