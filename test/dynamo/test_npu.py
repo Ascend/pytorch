d94fce4403a0f35f2f5983d899447281b29522cb
xuzhenhua2025 <xuzhenhua4@huawei.com>
2025-11-10 19:43:29 +0800

    [feature] register "npugraph_ex" backend
    Co-authored-by: xuzhenhua2025<xuzhenhua4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26381 merge v2.1.0 into v2.1.0
    
    [feature] register "npugraph_ex" backend
    
    Created-by: xuzhenhua2025
    Commit-by: xuzhenhua2025
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
     /kind feature
    
    
    **What does this PR do / why do we need it**:
    Register "npugraph_ex" backend, we can use it as
    ```python
    compiled_model = torch.compile(Model().npu(), backend="npugraph_ex", fullgraph=True, dynamic=False)
    ```
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!26381

--- test/dynamo/test_npu.py
+++ test/dynamo/test_npu.py
@@ -1,10 +1,14 @@
 # Owner(s): ["module: dynamo"]
 import sys
+import unittest
 
 import torch
 from torch._dynamo.test_case import TestCase
 
 import torch_npu
+
+
+DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
 
 
 class TestNpu(TestCase):
@@ -23,7 +27,23 @@ class TestNpu(TestCase):
         x = torch.randn(10).npu()
         with self.assertRaisesRegex(RuntimeError, "Device npu not supported"):
             func(x)
- 
+
+    @unittest.skipIf(DEVICE_NAME == 'Ascend910A', "capture is not supported on 910A, skip this ut.")
+    def test_npugraph_ex_backend(self):
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x, y):
+                return x + y
+
+        compiled_model = torch.compile(Model().npu(), backend="npugraph_ex", fullgraph=True, dynamic=False)
+        x = torch.ones(1, dtype=torch.int32).npu()
+        y = torch.ones(1, dtype=torch.int32).npu()
+        z = compiled_model(x, y)
+        self.assertEqual(z.item(), 2)
+
+
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
 
