63faff5e63fc19911f488dff259abd3d406a06f0
daifu1234567 <daifu2@huawei.com>
2025-11-11 19:57:57 +0800

    [2.9.0] delete decomp for scatter
    Co-authored-by: DaiFu<daifu2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26315 merge v2.9.0 into v2.9.0
    
    [2.9.0] delete decomp for scatter
    
    Created-by: daifu1234567
    Commit-by: DaiFu
    Merged-by: ascend-robot
    Description: delete scatter decomp
    删除scatter的decomposition，因为其与aten注册不一致：
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/78ed3c50-1e33-456c-b7bb-5b2185998e51/image.png 'image.png')
    
    即使保持一致也会有功能问题：
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/a89a1e38-6e5f-426f-b7ae-e222fcc3597c/image.png 'image.png')
    
    讨论后先删除decomp的注册，结果为算子报错，继续解决算子问题即可。
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!26315

--- torch_npu/_inductor/decomposition.py
+++ torch_npu/_inductor/decomposition.py
@@ -27,17 +27,6 @@ def _register_npu_inductor_decompositons():
         if (op in decompositions):
             del decompositions[op]
 
-    @register_decomposition([aten.scatter.src])
-    @pw_cast_for_opmath
-    def scatter_src(self, input_tensor, dim, index_tensor, source_tensor):
-        (XNUMEL, YS) = input_tensor.shape
-        index_rblock = torch.arange(YS).npu().reshape((1, YS)).repeat((XNUMEL, 1))
-
-        index_tensor_brd = index_tensor.to(torch.int32).broadcast_to(XNUMEL, YS)
-        source_tensor_brd = source_tensor.broadcast_to(XNUMEL, YS).to(torch.float32)
-        scatter1 = torch.where(index_rblock == index_tensor_brd, 1.0, 0.0) * source_tensor_brd
-        return scatter1
-
     @register_decomposition([aten.expm1])
     def expm1(x):
         tensor = torch.exp(x) - torch.ones_like(x)
