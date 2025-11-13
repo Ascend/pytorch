6002c111d3b5fa47a08cb815e050a9b5579b2b50
chuboning <chuboning1@huawei.com>
2025-11-13 16:56:26 +0800

    [950] Add 950 support
    Co-authored-by: chuboning<chuboning1@huawei.com>
    Co-authored-by: lilongqianxi<lilongqianxi@h-partners.com>
    Co-authored-by: 路有兵<luyoubing@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26229 merge master into master
    
    [950] Add 950 support
    
    Created-by: chuboning
    Commit-by: chuboning;路有兵;lilongqianxi
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!26229

--- third_party/hccl/inc/hccl/hccl_types.h
+++ third_party/hccl/inc/hccl/hccl_types.h
@@ -85,7 +85,11 @@ typedef enum {
     HCCL_DATA_TYPE_UINT32 = 9,  /**< uint32 */
     HCCL_DATA_TYPE_FP64 = 10,   /**< fp64 */
     HCCL_DATA_TYPE_BFP16 = 11,  /**< bfp16 */
-    HCCL_DATA_TYPE_RESERVED     /**< reserved */
+    HCCL_DATA_TYPE_INT128 = 12,   /**< int128 */
+    HCCL_DATA_TYPE_HIF8 = 14,     /**< hif8 */
+    HCCL_DATA_TYPE_FP8E4M3 = 15,  /**< fp8e4m3 */
+    HCCL_DATA_TYPE_FP8E5M2 = 16,  /**< fp8e5m2 */
+    HCCL_DATA_TYPE_RESERVED = 255 /**< reserved */
 } HcclDataType;
 
 const uint32_t HCCL_ROOT_INFO_BYTES =  4108; // 4108: root info length
