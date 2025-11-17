ac610c0a82f9332facb86512df91a7b630584d8f
yurongkun <yurongkun@huawei.com>
2025-11-17 17:54:17 +0800

    aclgraph support ASCEND_LAUNCH_BLOCKING V2.1.0
    Co-authored-by: yurongkun<yurongkun@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26491 merge blocking-v2.1.0 into v2.1.0
    
    aclgraph support ASCEND_LAUNCH_BLOCKING V2.1.0
    
    Created-by: yurongkun
    Commit-by: yurongkun
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    aclgraph场景开放ASCEND_LAUNCH_BLOCKING宏同步调试功能，针对混合场景下单算子同步调试有效
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!26491

---
+++ test/npu/test_aclgraph_support_blocking.py
@@ -0,0 +1,125 @@
+import unittest
+from dataclasses import dataclass
+from itertools import chain
+import os
+
+import random
+import numpy as np
+import torch
+
+import torch_npu
+from torch_npu.testing.common_utils import SupportedDevices
+from torch_npu.testing.testcase import TestCase, run_tests
+os.environ["ASCEND_LAUNCH_BLOCKING"] = "1"
+
+
+class TestIFAAclgraphUpdateSupportBlocking(TestCase):
+    @SupportedDevices(['Ascend910B', 'Ascend910C'])
+    def test_ifa_update_support_blocking(self):
+        torch.npu.set_device(0)
+        length = [29]
+        length_new = [100]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length_new)
+
+        g = torch.npu.NPUGraph()
+        event = torch.npu.ExternalEvent()
+        update_stream = torch.npu.Stream()
+        handle = None
+        output = None
+        softmax_lse = None
+
+        workspace = torch_npu._npu_fused_infer_attention_score_get_max_workspace(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length)
+
+        with torch.npu.graph(g):
+            stream = torch.npu.current_stream()
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty_like(res_src[1], dtype=torch.float16, device="npu")
+            event.wait(stream)
+            event.reset(stream)
+            torch.npu.graph_task_group_begin(stream)
+            torch_npu.npu_fused_infer_attention_score.out(
+                query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535, workspace=workspace,
+                next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length, out=[output, softmax_lse])
+            handle = torch.npu.graph_task_group_end(stream)
+        
+        with torch.npu.stream(update_stream):
+            torch.npu.graph_task_update_begin(update_stream, handle)
+            torch_npu.npu_fused_infer_attention_score.out(
+                query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535, workspace=workspace,
+                next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length_new, out=[output, softmax_lse])
+            torch.npu.graph_task_update_end(update_stream)
+            event.record(update_stream)
+
+        g.replay()
+
+    @SupportedDevices(['Ascend910B', 'Ascend910C'])
+    def test_ifa_update_with_auto_dispatch_capture_support_blocking(self):
+        torch.npu.set_device(0)
+        length = [29]
+        length_new = [100]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length_new)
+
+        g = torch.npu.NPUGraph()
+        output = None
+        softmax_lse = None
+
+        workspace = torch_npu._npu_fused_infer_attention_score_get_max_workspace(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length)
+
+        with torch.npu.graph(g, auto_dispatch_capture=True):
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty_like(res_src[1], dtype=torch.float16, device="npu")
+            torch_npu.npu_fused_infer_attention_score.out(
+                query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535, workspace=workspace,
+                next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length, out=[output, softmax_lse])
+        
+        g.update(cpu_update_input=[{"actual_seq_lengths": length_new}])
+        g.replay()
+
+    @SupportedDevices(['Ascend910B', 'Ascend910C'])
+    def test_ifa_update_with_non_out_and_auto_dispatch_capture_support_blocking(self):
+        torch.npu.set_device(0)
+        length = [29]
+        length_new = [100]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score(
+            query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+            next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length_new)
+
+        g = torch.npu.NPUGraph()
+        output = None
+        softmax_lse = None
+
+        with torch.npu.graph(g, auto_dispatch_capture=True):
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty(1, dtype=torch.float16, device="npu")
+            output, softmax_lse = torch_npu.npu_fused_infer_attention_score(
+                query, key, value, num_heads=32, input_layout="BNSD", scale=scale, pre_tokens=65535,
+                next_tokens=65535, softmax_lse_flag=False, actual_seq_lengths=length)
+        
+        g.update(cpu_update_input=[{"actual_seq_lengths": length_new}])
+        g.replay()
+
+if __name__ == "__main__":
+    run_tests()
