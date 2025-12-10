93e97e3a58eb9962284128a143145cae8c25f00b
daifu1234567 <daifu2@huawei.com>
2025-12-10 16:51:41 +0800

    [master]remove matmul_backward none check
    Co-authored-by: DaiFu<daifu2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27733 merge 2512matmul_backward into master
    
    [master]remove matmul_backward none check
    
    Created-by: daifu1234567
    Commit-by: DaiFu
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    
    
    **What does this PR do / why do we need it**:
    修复matmul_backward中grad传none场景报错问题。
    有前反向绑定的api，会在前向中加入unpack（校验none）。问题场景npu跑不通，gpu跑通，是因为npu上多支持了matmul_double_backward，导致matmul_backward上多了unpack校验，把none校验住了，因此需要在生成VariableTypeEverything.cpp的地方对matmul_backward特殊处理，去掉None校验。
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    已本地验证：
    生成的VariableTypeEverything.cpp中matmul_backward已去掉unpack校验：
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/d39f6043-adc9-4d19-ae18-c6cece16c260/image.png 'image.png')
    改前：
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/6e046686-75c8-4f74-9014-47b0d61cf5b2/image.png 'image.png')
    
    构造的单api脚本本地已跑通：
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/1cddd638-281b-4691-be8a-ce64c79e01e2/image.png 'image.png')
    
    
    
    See merge request: Ascend/pytorch!27733

--- codegen/autograd/__init__.py
+++ codegen/autograd/__init__.py
@@ -5,8 +5,21 @@ import torchgen.gen
 from torchgen.code_template import CodeTemplate
 from torchgen.api.autograd import NativeFunctionWithDifferentiabilityInfo, DifferentiabilityInfo
 
-from torchgen.packaged.autograd.gen_inplace_or_view_type import gen_inplace_or_view_type_env
+from torchgen.packaged.autograd.gen_inplace_or_view_type import (
+    gen_inplace_or_view_type_env,
+    extract_bindings,
+    unpacked_name,
+    is_tensor_list_type,
+    unpack_args as ori_unpack_args
+)
 from torchgen.packaged.autograd.gen_autograd_functions import process_function
+from torchgen.context import with_native_function
+from torchgen.model import (
+    NativeFunction,
+    SelfArgument,
+    TensorOptionsArguments
+)
+from torchgen.api.types import Binding
 from codegen.gen_backend_stubs import parse_native_and_custom_yaml
 from codegen.utils import CUSTOM_YAML_NAME
 
@@ -36,12 +49,61 @@ def process_function_(info: DifferentiabilityInfo, template: CodeTemplate) -> st
         function_codegen = function_codegen.replace('_symint', '')
     return function_codegen
 
+UNPACK_TENSOR_UNABLE = CodeTemplate(
+    """\
+auto${ref} ${arg_name}_ = ${arg_name};"""
+)
+
+
+@with_native_function
+def npu_unpack_args_list(f: NativeFunction) -> tuple[list[str], list[Binding]]:
+    body: list[str] = []
+    unpacked_bindings: list[Binding] = []
+
+    for _, binding in enumerate(extract_bindings(f)):
+        if isinstance(binding.argument, SelfArgument) or isinstance(binding.argument, TensorOptionsArguments):
+            raise TypeError("VariableKernel shouldn't be SelfArgument or take TensorOptions")
+
+        is_nullable = binding.argument.type.is_nullable()
+        if not binding.argument.type.is_tensor_like() or is_nullable:
+            unpacked_bindings.append(binding)
+            continue
+
+        is_tensor_list = is_tensor_list_type(binding.argument.type)
+        ref = (not is_nullable) and not is_tensor_list
+        body.append(
+            UNPACK_TENSOR_UNABLE.substitute(
+                arg_name=binding.name,
+                ref="&" if ref else "",
+            )
+        )
+        unpacked_bindings.append(
+            Binding(
+                name=unpacked_name(binding.name),
+                nctype=binding.nctype,
+                argument=binding.argument,
+                default=binding.default,
+            )
+        )
+
+    return body, unpacked_bindings
+
+DISABLE_UNPACK_ATEN = ["matmul_backward", "matmul_backward.out"]
+
+
+@with_native_function
+def npu_unpack_args(f: NativeFunction) -> tuple[list[str], list[Binding]]:
+    if str(f.func.name) in DISABLE_UNPACK_ATEN:
+        return npu_unpack_args_list(f)
+    return ori_unpack_args(f)
+
 
 def apply_autograd_patches():
     torchgen.gen.parse_native_yaml = parse_native_and_custom_yaml_
     torchgen.packaged.autograd.gen_inplace_or_view_type.gen_inplace_or_view_type_env = \
         gen_inplace_or_view_type_env_for_npu
     torchgen.packaged.autograd.gen_autograd_functions.process_function = process_function_
+    torchgen.packaged.autograd.gen_inplace_or_view_type.unpack_args = npu_unpack_args
 
 
 apply_autograd_patches()
