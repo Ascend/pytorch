b48025ae07d3bc1991ae15e222844b11784b870b
SCh_zx <1325467101@qq.com>
2025-12-12 18:24:01 +0800

    avoid cann8.0 err
    Co-authored-by: SCh-zx<1325467101@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27770 merge host27 into v2.7.1
    
    avoid cann8.0 err
    
    Created-by: SCh_zx
    Commit-by: SCh-zx
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    原则上驱动版本判断应在cann中进行，现在pta中添加后导致cann兼容性问题
    aclrtMemcpyAsyncWitchCondition接口和IsGteDriverVersion都需要cann版本大于8.0，以此规避
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27770

--- torch_npu/csrc/core/npu/interface/AclInterface.cpp
+++ torch_npu/csrc/core/npu/interface/AclInterface.cpp
@@ -1248,6 +1248,10 @@ aclError AclrtUnSubscribeReport(uint64_t theadId, aclrtStream stream)
 bool AclrtMemcpyAsyncWithConditionExist()
 {
     const static bool isAclrtMemcpyAsyncWithConditionExist = []() -> bool {
+        std::string currentCANNVersion = GetCANNVersion("CANN");
+        if (currentCANNVersion == "") {
+            return false;
+        }
         const std::string kMinDriverVersion = "25.0.RC1";
         if (!IsGteDriverVersion(kMinDriverVersion)) {
             return false;
