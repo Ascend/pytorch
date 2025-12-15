a07dd2df9be97027e62195a7ab4d4746f49e5188
freezee <gengdongjie@huawei.com>
2025-12-15 20:01:09 +0800

    patch and support _make_nccl_premul_sum for fsdp
    Co-authored-by: freezee<gengdongjie@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27940 merge v2.6.0 into v2.6.0
    
    patch and support _make_nccl_premul_sum for fsdp
    
    Created-by: freezee
    Commit-by: freezee
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27940

--- torch_npu/distributed/fsdp/_add_fsdp_patch.py
+++ torch_npu/distributed/fsdp/_add_fsdp_patch.py
@@ -1,4 +1,6 @@
 from typing import Any, Tuple, Union, cast, List, Dict
+from dataclasses import dataclass
+from functools import wraps
 import logging
 
 import torch
@@ -14,6 +16,8 @@ from torch.distributed.fsdp._fully_shard._fsdp_param import FSDPParam, ShardedSt
 from torch.distributed.fsdp._fully_shard._fsdp_param_group import FSDPParamGroup, AllGatherState
 from torch.distributed.utils import _to_kwargs
 from torch.profiler import record_function
+from torch.distributed.distributed_c10d import ReduceOp
+
 import torch_npu
 from torch_npu.utils._error_code import ErrCode, pta_error
 
@@ -294,6 +298,32 @@ def _patched_root_pre_forward(
     return args, kwargs
 
 
+@dataclass
+class WrapReduceOPPreMul:
+    """
+    warp a ReduceOp.PREMUL for collective communication since ReduceOp.PREMUL is not supported on hccl
+    """
+    supplement: float = None
+    op: ReduceOp = ReduceOp.SUM
+
+
+def _patched_make_nccl_premul_sum(factor: float) -> WrapReduceOPPreMul:
+    return WrapReduceOPPreMul(float(factor), ReduceOp.SUM)
+
+
+def _wrap_reduce_scatter_tensor_to_support_premul(func):
+
+    @wraps(func)
+    def wrapper(output, input, op=dist.ReduceOp.SUM, group=None, async_op=False):
+        real_op = op
+        if isinstance(op, WrapReduceOPPreMul):
+            if op.supplement is not None and input is not None:
+                input.mul_(op.supplement)
+            real_op = op.op
+        return func(output, input, real_op, group, async_op)
+    return wrapper
+
+
 def _apply_fsdp_patch():
     FSDPParamGroup.finalize_backward = _patched_finalize_backward
     FSDPParamGroup.wait_for_unshard = patched_wait_for_unshard
@@ -303,3 +333,5 @@ def _apply_fsdp_patch():
     _unsafe_preserve_version_counter.__exit__ = _patched_unsafe_preserve_version_counter.__exit__
     torch.distributed.fsdp._fully_shard._fsdp_collectives._get_param_all_gather_inputs = _get_param_all_gather_inputs
     torch.distributed.fsdp._fully_shard._fsdp_state.FSDPState._root_pre_forward = _patched_root_pre_forward
+    torch.distributed._make_nccl_premul_sum = _patched_make_nccl_premul_sum
+    torch.distributed.reduce_scatter_tensor = _wrap_reduce_scatter_tensor_to_support_premul(torch.distributed.reduce_scatter_tensor)
