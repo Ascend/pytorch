488c9a0ae8fa003b88778fcfb7ba2a31d720b493
jizewei <jizewei@huawei.com>
2025-12-15 17:29:32 +0800

    [fix] register strategy for npu_fusion_attention
    Co-authored-by: jizewei<jizewei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27780 merge master_fusion_attention_strategy into master
    
    fix sharding strategy for npu_fusion_attention
    
    Created-by: jizewei
    Commit-by: jizewei
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    /kind bug
    
    **What does this PR do / why do we need it**:
    
    fix npu_fusion_attention strategy:
    1. <https://github.com/pytorch/pytorch/pull/168249> supports `register_sharding` for operation with Tensor kwargs. Apply a patch for previous pytorch version.
    2. The previous strategy followed query's placements, which may lead to inconsistent results compared single-device computation if sharing the dim of S/D. The new strategy focuses on SDPA scenario and supports DP/TP.
    3. For TP,  the head dim of qkv local tensors does not match the `head_num` arg, so `head_num` needs to be reset before local computation.
    4. For `npu_fusion_attention_backward`, redistribution handling of DTensors in kwargs is added(official pytorch ops rarely have Tensor in kwargs, so most DTensor processing only considers args and ignores kwargs).
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27780

--- torch_npu/distributed/tensor/__init__.py
+++ torch_npu/distributed/tensor/__init__.py
@@ -1,3 +1,4 @@
+import torch_npu.distributed.tensor._dtensor_patch # patch before register strategy
 import torch_npu.distributed.tensor._matrix_ops
 import torch_npu.distributed.tensor._attention
 import torch_npu.distributed.tensor._math_ops
