5c3cb783c8a508fc823919bd21d031ee961d1459
xuzhenhua2025 <xuzhenhua4@huawei.com>
2025-12-16 14:20:12 +0800

    "npugraph_ex" backend supports options
    Co-authored-by: xuzhenhua2025<xuzhenhua4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27879 merge v2.7.1 into v2.7.1
    
    "npugraph_ex" backend supports options
    
    Created-by: xuzhenhua2025
    Commit-by: xuzhenhua2025
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    /kind feature
    
    
    **What does this PR do / why do we need it**:
    1. "npugraph_ex" backend supports options:
    ```python
    model = torch.compile(model.npu(), backend="npugraph_ex", options={
    	   # options: 
           # "key1" = value1,
           # ...
    })
    ```
    2. "npugraph_ex" backend supports APIs:
    - torch.npu.npugraph_ex.inference.cache_compile
    - torch.npu.npugraph_ex.inference.readable_cache
    - torch.npu.npugraph_ex.ops.npu_print
    - torch.npu.npugraph_ex.ops.npu_create_tagged_event
    - torch.npu.npugraph_ex.ops.npu_tagged_event_record
    - torch.npu.npugraph_ex.ops.npu_tagged_event_wait
    - torch.npu.npugraph_ex.ops.npu_record_tagged_stream
    - torch.npu.npugraph_ex.scope.npu_stream_switch
    - torch.npu.npugraph_ex.scope.npu_wait_tensor
    - torch.npu.npugraph_ex.scope.limit_core_num
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27879

---
+++ torch_npu/npu/npugraph_ex/ops/__init__.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def npu_print(*args, summarize_size=3, tensor_detail=False):
+    from torch_npu.dynamo.torchair import ops
+    return ops.npu_print(*args, summarize_size=summarize_size, tensor_detail=tensor_detail)
+
+
+def npu_create_tagged_event(tag: str):
+    from torch_npu.dynamo.torchair import ops
+    return ops.npu_create_tagged_event(tag=tag)
+
+
+def npu_tagged_event_record(event):
+    from torch_npu.dynamo.torchair import ops
+    return ops.npu_tagged_event_record(event=event)
+
+
+def npu_tagged_event_wait(event):
+    from torch_npu.dynamo.torchair import ops
+    return ops.npu_tagged_event_wait(event=event)
+
+
+def npu_record_tagged_stream(input: torch.Tensor, tagged_stream: str):
+    from torch_npu.dynamo.torchair import ops
+    return ops.npu_record_tagged_stream(input=input, tagged_stream=tagged_stream)
+
+
+def wait(tensors: list):
+    from torch_npu.dynamo.torchair import ops
+    return ops.wait(tensors=tensors)
