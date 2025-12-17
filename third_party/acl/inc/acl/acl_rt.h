653991aa1e97965bea4b53fe53a67ad3991ab1f0
gleaming-spark <geyi2@huawei.com>
2025-12-17 14:23:56 +0800

    Tensor generated by malloc + hostregister memory supports asynchronous copying
    Co-authored-by: geyi<geyi2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27963 merge v2.9.0-7.3.0_pin into v2.9.0-7.3.0
    
    Tensor generated by malloc + hostregister memory supports asynchronous copying
    
    Created-by: gleaming-spark
    Commit-by: geyi
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27963

--- third_party/acl/inc/acl/acl_rt.h
+++ third_party/acl/inc/acl/acl_rt.h
@@ -216,6 +216,7 @@ typedef struct rtExceptionInfo aclrtExceptionInfo;
 typedef enum aclrtMemLocationType {
     ACL_MEM_LOCATION_TYPE_HOST = 0, /**< reserved enum, current version not support */
     ACL_MEM_LOCATION_TYPE_DEVICE,
+    ACL_MEM_LOCATION_TYPE_UNREGISTERED,
 } aclrtMemLocationType;
 
 typedef struct aclrtMemLocation {
