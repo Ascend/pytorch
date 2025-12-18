f02faf99c1d36843f47a5926ea015c6b74207d15
lihui488 <lihui488@huawei.com>
2025-12-18 11:02:25 +0800

    skip npu sync func to v2.9.0-7.3.0
    Co-authored-by: lihui<lihui488@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !27617 merge v2.9.0-7.3.0 into v2.9.0-7.3.0
    
    skip npu sync func to v2.9.0-7.3.0
    
    Created-by: lihui488
    Commit-by: lihui
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!27617

--- torch_npu/dynamo/trace_rule.py
+++ torch_npu/dynamo/trace_rule.py
@@ -1,5 +1,7 @@
 import torch
 from torch._dynamo.variables import TorchInGraphFunctionVariable
+from torch._dynamo.trace_rules import manual_torch_name_rule_map, SkipFunctionVariable
+import torch_npu
 
 __all__ = []
 
@@ -17,3 +19,4 @@ torch_c_binding_in_graph_functions_npu = dict.fromkeys(
 def _patch_npu_trace_rules():
     torch._dynamo.trace_rules.clear_lru_cache()
     torch._dynamo.trace_rules.torch_name_rule_map.append(torch_c_binding_in_graph_functions_npu)
+    manual_torch_name_rule_map["torch_npu.npu.utils.synchronize"] = SkipFunctionVariable
