24aebd518589265f25b72a20f8f997fdc37a64ae
fengye0903 <vinsmoke.feng@huawei.com>
2025-12-31 14:14:04 +0800

    support npu_fused_infer_attention_score_v2 tiling update in NPUGraph
    Co-authored-by: fengye<vinsmoke.feng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !28789 merge v2.6.0 into v2.6.0
    
    support npu_fused_infer_attention_score_v2 tiling update in NPUGraph
    
    Created-by: fengye0903
    Commit-by: fengye
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    测试用例执行结果：
    ```
    torch.npu.set_device(0)
    length = [1]
    length_new = [1]
    scale = 1 / 0.0078125
    query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
    key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
    value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
    
    res_src = torch_npu.npu_fused_infer_attention_score_v2(
                query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
                next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length_new)
    
    g = torch.npu.NPUGraph()
    output = None
    softmax_lse = None
    
    with torch.npu.graph(g, auto_dispatch_capture=True):
    output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
    softmax_lse = torch.empty(1, dtype=torch.float16, device="npu")
    output, softmax_lse = torch_npu.npu_fused_infer_attention_score_v2(
    query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
    next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length)
    
    g.update(cpu_update_input=[{"actual_seq_qlen": length_new}])
    g.replay()
    self.assertEqual(output.cpu(), res_src[0].cpu())
    
    @SupportedDevices(['Ascend910B'])
        @unittest.skip("this cann version is not supported")
    [root@devserver-hps-feb34b1c-00033 f00941739]# 
    [root@devserver-hps-feb34b1c-00033 f00941739]# 
    [root@devserver-hps-feb34b1c-00033 f00941739]# python ./pytorch/test/npu/test_aclgraph_update.py
    ssss..sssss
    ----------------------------------------------------------------------
    Ran 11 tests in 1.980s
    
    OK (skipped=9)
    ```
    整网测试结果：
    ```
    [root@devserver-hps-feb34b1c-00033 omni-npu-2]# curl http://7.150.9.163:8001/v1/completions -H "Content-Type: application/json" -d '{"prompt":"请介绍一下苏州，不少于100字，请用中文回
    复","model":"Pangu72B","temperature":0.6,"max_tokens":120}'
    {"id":"cmpl-94a127a570be5f14","object":"text_completion","created":1767012795,"model":"Pangu72B","choices":[{"index":0,"text":"。\n苏州，位于中国江苏省东南部，是一座拥有2500多年历史的
    古城，以其独特的园林艺术和丰富的文化遗产而闻名。苏州园林是中国古典园林的代表，被誉为“园林之城”，其中拙政园、留园等都是著名的景点，展现了精巧的设计和深远的文化内涵。此外，苏州还以丝绸
    制造业著称，有“丝绸之府”的美誉。苏州的评弹、昆曲等传统艺术形式也体现了这座城市深厚的文化底蕴。今天的苏州，不仅保留了丰富的历史遗产，还是一个现代化发展的城市，经济繁荣，科技和教育水平
    先进。","logprobs":null,"finish_reason":"stop","stop_reason":45892,"token_ids":null,"prompt_logprobs":null,"prompt_token_ids":null}],"service_tier":null,"system_fingerprint":null,"us
    age":{"prompt_tokens":12,"total_tokens":131,"completion_tokens":119,"prompt_tokens_details":null},"kv_transfer_params":null}[root@devserver-hps-feb34b1c-00033 omni-npu-2]# 
    [root@devserver-hps-feb34b1c-00033 omni-npu-2]# 
    ```
    
    
    
    
    
    
    See merge request: Ascend/pytorch!28789

--- test/npu/test_aclgraph_update.py
+++ test/npu/test_aclgraph_update.py
@@ -134,7 +134,69 @@ class TestIFAAclgraphUpdate(TestCase):
         g.replay()
         self.assertEqual(output.cpu(), res_src[0].cpu())
         self.assertEqual(softmax_lse.cpu(), res_src[1].cpu())
-    
+
+    @SupportedDevices(['Ascend910B'])
+    def test_ifa_v2_update_with_auto_dispatch_capture(self):
+        torch.npu.set_device(0)
+        length = [1]
+        length_new = [1]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score_v2(
+            query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
+            next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length_new)
+
+        g = torch.npu.NPUGraph()
+        output = None
+        softmax_lse = None
+
+        workspace = torch_npu._npu_fused_infer_attention_score_v2_get_max_workspace(
+            query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
+            next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length)
+
+        with torch.npu.graph(g, auto_dispatch_capture=True):
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty(1, dtype=torch.float16, device="npu")
+            torch_npu.npu_fused_infer_attention_score_v2.out(
+                query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535, workspace=workspace,
+                next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length, out=[output, softmax_lse])
+        
+        g.update(cpu_update_input=[{"actual_seq_lengths": length_new}])
+        g.replay()
+        self.assertEqual(output.cpu(), res_src[0].cpu())
+
+    @SupportedDevices(['Ascend910B'])
+    def test_ifa_v2_update_with_non_out_and_auto_dispatch_capture(self):
+        torch.npu.set_device(0)
+        length = [1]
+        length_new = [1]
+        scale = 1 / 0.0078125
+        query = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        key = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+        value = torch.randn(1, 32, 1, 128, dtype=torch.float16, device="npu")
+
+        res_src = torch_npu.npu_fused_infer_attention_score_v2(
+            query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
+            next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length_new)
+
+        g = torch.npu.NPUGraph()
+        output = None
+        softmax_lse = None
+
+        with torch.npu.graph(g, auto_dispatch_capture=True):
+            output = torch.empty(1, 32, 1, 128, dtype=torch.float16, device="npu")
+            softmax_lse = torch.empty(1, dtype=torch.float16, device="npu")
+            output, softmax_lse = torch_npu.npu_fused_infer_attention_score_v2(
+                query, key, value, num_query_heads=32, input_layout="BNSD", softmax_scale=scale, pre_tokens=65535,
+                next_tokens=65535, return_softmax_lse=False, actual_seq_qlen=length)
+        
+        g.update(cpu_update_input=[{"actual_seq_qlen": length_new}])
+        g.replay()
+        self.assertEqual(output.cpu(), res_src[0].cpu())
+
     @SupportedDevices(['Ascend910B'])
     def test_npu_fused_infer_attention_score_v2(self):
         torch.npu.set_device(0)
