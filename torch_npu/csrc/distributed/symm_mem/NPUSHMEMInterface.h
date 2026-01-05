5888baa9dc03ce6596ae225bbaa21726691a5fab
wangchao430 <wangchao430@huawei.com>
2026-01-05 20:57:54 +0800

    [shmem] fix with aclshmem
    Co-authored-by: wangchao430<wangchao430@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29019 merge v2.7.0-7.3.0_shmemacl into v2.7.1-7.3.0
    
    [shmem] fix with aclshmem
    
    Created-by: wangchao430
    Commit-by: wangchao430
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!29019

--- torch_npu/csrc/distributed/symm_mem/NPUSHMEMInterface.h
+++ torch_npu/csrc/distributed/symm_mem/NPUSHMEMInterface.h
@@ -7,26 +7,32 @@
 namespace c10d {
 namespace symmetric_memory {
 
-int32_t Shmem_set_conf_store_tls(bool enable, const char *tls_info, const uint32_t tls_info_len);
+int32_t Aclshmemx_set_conf_store_tls(bool enable, const char *tls_info, const uint32_t tls_info_len);
 
 int32_t Shmem_set_attr(int32_t my_rank, int32_t n_ranks, uint64_t local_mem_size, const char *ip_port,
                        shmem_init_attr_t **attributes);
 
-int32_t Shmem_init_attr(shmem_init_attr_t *attributes);
+int Shmemx_get_uniqueid(shmem_uniqueid_t *uid);
 
-int Shmem_get_uniqueid(shmem_uniqueid_t *uid);
+bool Aclshmemx_get_uniqueid_exist();
+
+int Aclshmemx_get_uniqueid(aclshmemx_uniqueid_t *uid);
 
 int Shmem_set_attr_uniqueid_args(int rank_id, int nranks, const shmem_uniqueid_t *uid, shmem_init_attr_t *attr);
 
-void *Shmem_malloc(size_t size);
+int Aclshmemx_set_attr_uniqueid_args(int rank_id, int nranks, int64_t local_mem_size, aclshmemx_uniqueid_t *uid, aclshmemx_init_attr_t *aclshmem_attr);
 
-void Shmem_free(void *ptr);
+int Aclshmemx_init_attr(aclshmemx_bootstrap_t bootstrap_flags, aclshmemx_init_attr_t *attributes);
 
-void *Shmem_ptr(void *ptr, int pe);
+void *Aclshmem_malloc(size_t size);
 
-bool Shmem_finalize_exist();
+void Aclshmem_free(void *ptr);
 
-int Shmem_finalize(void);
+void *Aclshmem_ptr(void *ptr, int pe);
+
+bool Aclshmem_finalize_exist();
+
+int Aclshmem_finalize(void);
 
 } // namespace symmetric_memory
 } // namespace c10d
