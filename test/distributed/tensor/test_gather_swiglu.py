89289f4b591ae07a2dbdb5c4963cfa444ec45690
jizewei <jizewei@huawei.com>
2026-01-06 10:09:09 +0800

    fix DTensor
    Co-authored-by: jizewei<jizewei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29014 merge v2.8.0_fix_dtensor_ut into v2.8.0
    
    fix DTensor
    
    Created-by: jizewei
    Commit-by: jizewei
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    /kind bug
    
    **What does this PR do / why do we need it**:
    
    1. change DTensor UT from 4 devices to 2 devices, because the CI environment only has 2 devices
    2. fix dtensor strategy
    2. adapt the newest npu_fusion_attention
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    test_attention_ops.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/08b9e9fc-25d3-4a8e-8c99-6981cd5fddf0/image.png 'image.png')
    test_gather_swiglu.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/bf896eba-0c98-4eb3-82c4-790e736bfdef/image.png 'image.png')
    test_math_ops.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/221e4023-fb34-4ff1-a1af-e60f79e35c53/image.png 'image.png')
    test_matrix_ops.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/bc710018-7b70-427c-af79-14691b274c52/image.png 'image.png')
    test_moe_ops.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/e9f1dce4-3b3c-4539-b98f-a6efae246276/image.png 'image.png')
    
    See merge request: Ascend/pytorch!29014

--- test/distributed/tensor/test_gather_swiglu.py
+++ test/distributed/tensor/test_gather_swiglu.py
@@ -1,18 +1,20 @@
 import torch
 from torch.distributed._tensor import distribute_tensor, Replicate, Shard
-from torch_npu.testing._internal.common_dtensor import NPUDTensorTestBase
 
 import torch_npu
 from torch_npu.testing.testcase import run_tests
+from torch_npu.testing._internal.common_dtensor import NPUDTensorTestBase
 from torch_npu.testing.common_distributed import with_comms, skipIfUnsupportMultiNPU
+from torch_npu.testing.common_utils import SupportedDevices
 
 
 class TestRegisterSharding(NPUDTensorTestBase):
-    @skipIfUnsupportMultiNPU(4)
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_npu_npu_swiglu(self):
         mesh = self.build_device_mesh()
-        
+
         input_tensor = torch.randn(1024, 1024, device="npu", requires_grad=True)
         grad_tensor = torch.randn(1024, 512, device="npu")
         dim = -1
@@ -26,8 +28,9 @@ class TestRegisterSharding(NPUDTensorTestBase):
         output.backward(grad_dtensor)
         self.assertEqual(output.full_tensor(), out_tensor)
         self.assertEqual(input_dtensor.grad.full_tensor(), input_tensor.grad)
-    
-    @skipIfUnsupportMultiNPU(4)
+
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_npu_npu_swiglu_shard0(self):
         mesh = self.build_device_mesh()
@@ -46,14 +49,15 @@ class TestRegisterSharding(NPUDTensorTestBase):
         self.assertEqual(output.full_tensor(), out_tensor)
         self.assertEqual(input_dtensor.grad.full_tensor(), input_tensor.grad)
 
-    @skipIfUnsupportMultiNPU(4)
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_npu_npu_swiglu_shard1(self):
         mesh = self.build_device_mesh()
 
         input_tensor = torch.randn(1024, 1024, device="npu", requires_grad=True)
         grad_tensor = torch.randn(1024, 512, device="npu")
-        dim = -1  
+        dim = -1
         out_tensor = torch_npu.npu_swiglu(input_tensor, dim)
         out_tensor.backward(grad_tensor)
 
@@ -64,8 +68,9 @@ class TestRegisterSharding(NPUDTensorTestBase):
         output.backward(grad_dtensor)
         self.assertEqual(output.full_tensor(), out_tensor)
         self.assertEqual(input_dtensor.grad.full_tensor(), input_tensor.grad)
-    
-    @skipIfUnsupportMultiNPU(4)
+
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_gather(self):
         mesh = self.build_device_mesh()
@@ -88,8 +93,9 @@ class TestRegisterSharding(NPUDTensorTestBase):
         out.backward(grad)
         self.assertEqual(out.full_tensor(), out_tensor)
         self.assertEqual(x.grad.full_tensor(), input_tensor.grad)
-    
-    @skipIfUnsupportMultiNPU(4)
+
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_gather_shard0(self):
         mesh = self.build_device_mesh()
@@ -105,15 +111,16 @@ class TestRegisterSharding(NPUDTensorTestBase):
 
         x = distribute_tensor(input_tensor, mesh, [Shard(0)])
         index = distribute_tensor(index_tensor, mesh, [Shard(0)])
-        
+
         out = torch.gather(input=x, dim=dim, index=index)
         grad = distribute_tensor(grad_tensor, mesh, out.placements)
 
         out.backward(grad)
         self.assertEqual(out.full_tensor(), out_tensor)
         self.assertEqual(x.grad.full_tensor(), input_tensor.grad)
-    
-    @skipIfUnsupportMultiNPU(4)
+
+    @SupportedDevices(['Ascend910B'])
+    @skipIfUnsupportMultiNPU(2)
     @with_comms
     def test_torch_gather_shard1(self):
         mesh = self.build_device_mesh()
@@ -129,7 +136,7 @@ class TestRegisterSharding(NPUDTensorTestBase):
 
         x = distribute_tensor(input_tensor, mesh, [Shard(1)])
         index = distribute_tensor(index_tensor, mesh, [Shard(1)])
-        
+
         out = torch.gather(input=x, dim=dim, index=index)
         grad = distribute_tensor(grad_tensor, mesh, out.placements)
 
