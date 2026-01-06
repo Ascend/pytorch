4bff295e8bb90eb1bc788d5ee7930c8c0c4281ec
lihui488 <lihui488@huawei.com>
2026-01-06 10:30:34 +0800

    fix torch_npu synchronize rule
    Co-authored-by: lihui<lihui488@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !28614 merge v2.7.1-7.3.0 into v2.7.1-7.3.0
    
    fix torch_npu synchronize rule
    
    Created-by: lihui488
    Commit-by: lihui
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!28614

--- torch_npu/dynamo/trace_rule.py
+++ torch_npu/dynamo/trace_rule.py
@@ -1,6 +1,5 @@
 import torch
 from torch._dynamo.variables import TorchInGraphFunctionVariable
-from torch._dynamo.trace_rules import manual_torch_name_rule_map, SkipFunctionVariable
 import torch_npu
 
 __all__ = []
@@ -11,6 +10,7 @@ torch_c_binding_in_graph_functions_npu = dict.fromkeys(
         "torch.npu.default_stream",
         "torch.npu.stream",
         "torch.npu.set_stream",
+        "torch_npu.npu.utils.synchronize",
     ],
     TorchInGraphFunctionVariable,
 )
@@ -19,4 +19,3 @@ torch_c_binding_in_graph_functions_npu = dict.fromkeys(
 def _patch_npu_trace_rules():
     torch._dynamo.trace_rules.clear_lru_cache()
     torch._dynamo.trace_rules.torch_name_rule_map.append(torch_c_binding_in_graph_functions_npu)
-    manual_torch_name_rule_map["torch_npu.npu.utils.synchronize"] = SkipFunctionVariable
