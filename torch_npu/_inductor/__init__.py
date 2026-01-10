d5ec4538e8e67abe300e2ffc21b7a276876008a8
huawuyi <liuxuehui4@huawei.com>
2026-01-10 10:52:50 +0800

    cpu_compile
    Co-authored-by: huawuyi<liuxuehui4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29369 merge v2.7.1-7.3.0 into v2.7.1-7.3.0
    
    cpu_compile
    
    Created-by: huawuyi
    Commit-by: huawuyi
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    
    > /kind feature
    
    **What does this PR do / why do we need it**:
    Add cpu_compile
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!29369

--- torch_npu/_inductor/__init__.py
+++ torch_npu/_inductor/__init__.py
@@ -119,11 +119,36 @@ else:
     InductorChoices.should_use_persistent_reduction = should_use_persistent_reduction
     autotune_cache._load_cached_autotuning = _load_cached_autotuning
 
+
+    def patch_device_override_func():
+        def get_device_op_overrides_patch(device_name: str):
+            def register_cpu_backend():
+                from torch._inductor.codegen import cpu_device_op_overrides
+
+                return
+
+            def register_mps_backend():
+                from torch._inductor.codegen import mps_device_op_overrides
+
+                return
+
+            backend_factory = {"cpu": register_cpu_backend, "mps": register_mps_backend}
+
+            if device_name not in torch._inductor.codegen.common.device_op_overrides_dict:
+                if device_name not in backend_factory:
+                    raise ValueError("backend not found: ", device_name)
+                backend_factory[device_name]()
+
+            return torch._inductor.codegen.common.device_op_overrides_dict[device_name]
+
+        torch._inductor.graph.get_device_op_overrides = get_device_op_overrides_patch
+
     register_fa_pass()
     patch_cache_base_get_system()
     patch_is_gpu()
     patch_has_triton()
     disable_foreach()
+    patch_device_override_func()
 
 
 
