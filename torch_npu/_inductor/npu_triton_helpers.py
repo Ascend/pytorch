26a3fdcc66b7c3796136245060a022581fe901dc
lihui488 <lihui488@huawei.com>
2026-01-21 15:02:05 +0800

    change libdevice path
    Co-authored-by: lihui<lihui488@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29683 merge v2.7.1 into v2.7.1
    
    change libdevice path
    
    Created-by: lihui488
    Commit-by: lihui
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!29683

--- torch_npu/_inductor/npu_triton_helpers.py
+++ torch_npu/_inductor/npu_triton_helpers.py
@@ -1,10 +1,16 @@
 import triton
 import triton.language as tl
 
+try:
+    import triton.language.extra.cann.libdevice as libdevice
+except ImportError:
+    import triton.language.extra.ascend.libdevice as libdevice
 from torch._inductor.runtime import triton_helpers
 
-libdevice = tl.extra.cann.libdevice
-extension = tl.extra.cann.extension
+try:
+    libdevice = tl.extra.cann.libdevice
+except AttributeError:
+    libdevice = tl.extra.ascend.libdevice
 math = tl.math
 
 
