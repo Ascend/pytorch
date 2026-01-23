7056cbaf2ebe79bba99d6af2b6ad974d17d5e0ed
xuzhenhua2025 <xuzhenhua4@huawei.com>
2026-01-23 15:05:50 +0800

    Decouple npugraph_ex and torchair-ge
    Co-authored-by: xuzhenhua2025<xuzhenhua4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !29910 merge master into master
    
    Decouple npugraph_ex and torchair-ge
    
    Created-by: xuzhenhua2025
    Commit-by: xuzhenhua2025
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    /kind feature
    
    
    **What does this PR do / why do we need it**:
    The cache compile function supports decoupling of npugraph_ex and torchair-ge.
    Use an independent configuration parameter to enable the cache compile function of npugraph_ex.
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!29910

--- test/dynamo/test_npu_backend.py
+++ test/dynamo/test_npu_backend.py
@@ -1,8 +1,11 @@
 # Owner(s): ["module: dynamo"]
 import unittest
+import dataclasses
+from typing import List
 
 import torch
 from torch._dynamo.test_case import TestCase
+from torch._dynamo.testing import same
 
 import torch_npu
 
@@ -34,6 +37,47 @@ class TestNpuBackend(TestCase):
         y = torch.ones(1, dtype=torch.int32).npu()
         z = compiled_model(x, y)
         self.assertEqual(z.item(), 2)
+
+    @unittest.skipIf(DEVICE_NAME == 'Ascend910A', "capture is not supported on 910A, skip this ut.")
+    def test_npugraph_ex_cache_compile(self):
+        @dataclasses.dataclass
+        class InputMeta:
+            data: torch.Tensor
+            is_prompt: bool
+
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.linear1 = torch.nn.Linear(2, 1)
+                self.linear2 = torch.nn.Linear(2, 1)
+                for param in self.parameters():
+                    torch.nn.init.ones_(param)
+                self.cached_prompt = torch.npu.npugraph_ex.inference.cache_compile(self.prompt)
+                self.cached_decode = torch.npu.npugraph_ex.inference.cache_compile(self.decode)
+
+            def forward(self, x: InputMeta, kv: List[torch.Tensor]):
+                if x.is_prompt:
+                    return self.cached_prompt(x, kv)
+                return self.cached_decode(x, kv)
+
+            def _forward(self, x, kv):
+                return self.linear2(x.data) + self.linear2(kv[0])
+
+            def prompt(self, x, y):
+                return self._forward(x, y)
+
+            def decode(self, x, y):
+                return self._forward(x, y)
+
+        x = InputMeta(data=torch.ones(2, 2).npu(), is_prompt=True)
+        kv = [torch.ones(2, 2).npu()]
+        model = Model().npu()
+        res_prompt = model(x, kv)
+        x.is_prompt = False
+        res_decode = model(x, kv)
+        res = torch.empty(2, 1).npu().fill_(6.0)
+        self.assertTrue(same(res, res_prompt))
+        self.assertTrue(same(res, res_decode))
 
 
 if __name__ == "__main__":
