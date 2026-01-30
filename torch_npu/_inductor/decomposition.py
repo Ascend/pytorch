665c0117cb48722de2cdbce008eb945bd6206236
daifu1234567 <daifu2@huawei.com>
2026-01-30 17:46:16 +0800

    [2.9.0]fix dropout
    Co-authored-by: DaiFu<daifu2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30016 merge v2.9.0 into v2.9.0
    
    [2.9.0]fix dropout
    
    Created-by: daifu1234567
    Commit-by: DaiFu
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!30016

--- torch_npu/_inductor/decomposition.py
+++ torch_npu/_inductor/decomposition.py
@@ -1,6 +1,7 @@
 import torch._ops
 from torch._inductor.decomposition import decompositions, pw_cast_for_opmath
 from torch._inductor.decomposition import register_decomposition
+from torch._prims_common.wrappers import out_wrapper
 
 from .lowering import _init_set
 
@@ -15,7 +16,9 @@ DECOMPOSITION_OVERLOAD_OP = [
     aten._log_softmax_backward_data,
     aten.embedding_dense_backward,
     aten.addmm,
-    aten.gelu
+    aten.gelu,
+    aten.native_dropout,
+    aten.native_dropout_backward
 ]
 
 
@@ -36,3 +39,25 @@ def _register_npu_inductor_decompositons():
     def erfc(x):
         tensor = torch.ones_like(x) - torch.exp(x)
         return tensor
+
+    @register_decomposition(aten.native_dropout)
+    @out_wrapper("out0", "out1")
+    def native_dropout(tensor_input, p, train):
+        if torch._inductor.config.fallback_random:
+            if train and p != 0:
+                return torch.ops.npu._npu_dropout(tensor_input, p)
+            return (tensor_input, torch.ones_like(tensor_input, dtype=torch.bool))
+        else:
+            from torch._decomp.decompositions import native_dropout
+            return native_dropout(tensor_input, p, train)
+
+    @register_decomposition(aten.native_dropout_backward)
+    @out_wrapper()
+    def native_dropout_backward(grad_output, mask, scale):
+        if torch._inductor.config.fallback_random:
+            p = 1 if scale == 0 else (1 - 1 / scale)
+            r = torch.ops.npu.npu_dropout_backward(grad_output, mask, p)
+            return r
+        else:
+            from torch._decomp.decompositions import native_dropout_backward
+            return native_dropout_backward(grad_output, mask, scale)
