5f4cfd53e685a7cbbdd85a17d2fad54a0f428bdb
SCh_zx <1325467101@qq.com>
2026-01-30 18:06:24 +0800

    support compatible impl
    Co-authored-by: SCh-zx<1325467101@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30255 merge v2.7.1-7.3.0 into v2.7.1-7.3.0
    
    support compatible impl
    
    Created-by: SCh_zx
    Commit-by: SCh-zx
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!30255

---
+++ test/npu/test_compatible_impl.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+import torch
+import torch_npu
+
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SkipIfNotGteCANNVersion
+
+
+@unittest.skip("This test is not supported yet")
+class TestCompatibleImpl(TestCase):
+    def test_are_compatible_impl_enable(self):
+        self.assertEqual(torch_npu.npu.are_compatible_impl_enable(), True)
+
+    @SkipIfNotGteCANNVersion("9.0.0")
+    def test_npu_matmul(self):
+        torch.set_default_device('npu')
+
+        B = 2048
+        D = 4096
+        R = 1000
+        a = torch.linspace(-R, R, B * D).reshape(B, D)
+        b = torch.linspace(-R, R, D * D).reshape(D, D)
+        out1 = torch.mm(a[:1], b)
+        out2 = torch.mm(a, b)[:1]
+        self.assertEqual(np.array_equal(out1.cpu().numpy().view(np.uint32), out2.cpu().numpy().view(np.uint32)), True)
+
+
+if __name__ == "__main__":
+    torch_npu.npu.use_compatible_impl(True)
+    run_tests()
