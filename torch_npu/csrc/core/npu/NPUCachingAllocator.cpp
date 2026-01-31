08fe98dde9b4920679902ada93d4ad88a7c7a487
glf2016 <13739288457@163.com>
2026-01-31 09:36:17 +0800

    del 32 allocator
    Co-authored-by: guanlongfeng<guanlongfeng1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30259 merge cherry-pick-mr-30020-1769753576556-auto into v2.6.0
    
    del 32 allocator
    
    Created-by: glf2016
    Commit-by: guanlongfeng
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!30259

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -185,6 +185,19 @@ bool IsMallocPage1GMem(bool is_small_pool)
     return !is_small_pool && is_support_page_size_1g;
 }
 
+size_t AddPadSize()
+{
+    static size_t add_size = -1;
+    if (add_size == -1) {
+        if (GetSocVersion() >= SocVersion::Ascend910_95) {
+            add_size = 0;
+        } else {
+            add_size = 32;
+        }
+    }
+    return add_size;
+}
+
 struct Block;
 struct PrivatePool;
 using Comparison = bool (*)(const Block *, const Block *);
@@ -2391,9 +2404,7 @@ public:
 
     static size_t round_size(size_t size)
     {
-        constexpr size_t kPadSize = 32;
-        size += kPadSize;
-
+        size += AddPadSize();
         if (size < kMinBlockSize) {
             return kMinBlockSize;
         } else {
@@ -3821,7 +3832,7 @@ public:
         if (size != 0) {
             if (c10_npu::option::OptionsManager::CheckForceUncached()) {
                 deleteFunc = &uncached_delete;
-                size_t alloc_size = size + 32;
+                size_t alloc_size = size + AddPadSize();
                 NPU_CHECK_ERROR(c10_npu::acl::AclrtMallocAlign32(&devPtr, alloc_size,
                     aclrtMemMallocPolicy::ACL_MEM_MALLOC_HUGE_FIRST));
                 ASCEND_LOGD("Without NPUCachingAllocator, malloc by "
@@ -3851,7 +3862,7 @@ public:
         if (size != 0) {
             if (c10_npu::option::OptionsManager::CheckForceUncached()) {
                 deleteFunc = &uncached_delete;
-                size_t alloc_size = size + 32 + aligned;
+                size_t alloc_size = size + AddPadSize() + aligned;
                 NPU_CHECK_ERROR(c10_npu::acl::AclrtMallocAlign32(&realPtr, alloc_size,
                                                                  aclrtMemMallocPolicy::ACL_MEM_MALLOC_HUGE_FIRST));
                 ASCEND_LOGD("Without NPUCachingAllocator, malloc by "
