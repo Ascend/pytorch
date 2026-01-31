3fe1ae21e9b740ec95a4d1781c79611b1fab5015
glf2016 <13739288457@163.com>
2026-01-31 09:36:20 +0800

    del 32 allocator
    Co-authored-by: guanlongfeng<guanlongfeng1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30020 merge del_32_allocator into v2.7.1
    
    del 32 allocator
    
    Created-by: glf2016
    Commit-by: guanlongfeng
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!30020

--- test/npu/test_allocator_envs.py
+++ test/npu/test_allocator_envs.py
@@ -1,29 +1,47 @@
 import os
+import math
 import shutil
 import unittest
 import torch
 import torch_npu
+
+import torch_npu.npu.utils as utils
 
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.common_utils import SupportedDevices
 
 
 class TestAllocator(TestCase):
-    @SupportedDevices(['Ascend910B'])
     def test_huge_memory_alloc_20M(self):
         prev = torch_npu.npu.memory_allocated()
         a = torch.rand(1024 * 1024 * 40, dtype=torch.float32).npu()
-        torch.npu.synchronize()
         # 实际申请1G内存
-        self.assertEqual(torch_npu.npu.memory_allocated(), prev + ((4 * 40 * 1024 * 1024 + 32) // 512 + 1) * 512)
+        if (utils.get_soc_version() >= 260):
+            self.assertEqual(torch_npu.npu.memory_allocated(), prev + math.ceil((4 * 40 * 1024 * 1024) / 512) * 512)
+        else:
+            self.assertEqual(torch_npu.npu.memory_allocated(),
+                             prev + math.ceil((4 * 40 * 1024 * 1024 + 32) / 512) * 512)
 
-    @SupportedDevices(['Ascend910B'])
     def test_huge_memory_alloc_512B(self):
+        os.environ["PYTORCH_NPU_ALLOC_CONF"] = "expandable_segments:False"
         prev = torch_npu.npu.memory_allocated()
-        a = torch.rand(8 * 8 * 16, dtype=torch.float32).npu() # 512B
-        torch.npu.synchronize()
+        a = torch.rand(8 * 8 * 16, dtype=torch.float32).npu()  # 512B
         # 实际申请1M内存
-        self.assertEqual(torch_npu.npu.memory_allocated(), prev + ((8 * 8 * 16 * 4 + 32) // 512 + 1) * 512)
+        if (utils.get_soc_version() >= 260):
+            self.assertEqual(torch_npu.npu.memory_allocated(), prev + math.ceil((8 * 8 * 16 * 4) / 512) * 512)
+        else:
+            self.assertEqual(torch_npu.npu.memory_allocated(), prev + math.ceil((8 * 8 * 16 * 4 + 32) / 512) * 512)
+
+    def test_huge_memory_alloc_512B_by_vm(self):
+        os.environ["PYTORCH_NPU_ALLOC_CONF"] = "expandable_segments:True"
+        prev = torch_npu.npu.memory_allocated()
+        a = torch.rand(8 * 8 * 16, dtype=torch.float32).npu()  # 512B
+        # 实际申请1M内存
+        if (utils.get_soc_version() >= 260):
+            self.assertEqual(torch_npu.npu.memory_allocated(), prev + math.ceil((8 * 8 * 16 * 4) / 512) * 512)
+        else:
+            self.assertEqual(torch_npu.npu.memory_allocated(), prev + math.ceil((8 * 8 * 16 * 4 + 32) / 512) * 512)
+        del os.environ["PYTORCH_NPU_ALLOC_CONF"]
 
 if __name__ == '__main__':
     run_tests()
