c1971e49b0eb28f61966d9bc0a5d5922cf21b6fc
SoraAzzz <694099604@qq.com>
2026-02-03 16:48:56 +0800

    [v2.6.0][Profiler]rename block dim to block num
    Co-authored-by: xieanran<694099604@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30417 merge v2.6.0 into v2.6.0
    
    [v2.6.0][Profiler]rename block dim to block num
    
    Created-by: SoraAzzz
    Commit-by: xieanran
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    
    /kind feature
    
    **What does this PR do / why do we need it**:
    
    rename blockdim to blocknum to adapte msprof revise
    **Which issue(s) this PR fixes**:
    <!-- 
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/pytorch!30417

--- torch_npu/profiler/analysis/prof_common_func/_csv_headers.py
+++ torch_npu/profiler/analysis/prof_common_func/_csv_headers.py
@@ -2,6 +2,6 @@ class CsvHeaders(object):
     # op_summary
     TASK_START_TIME = "Task Start Time(us)"
     OP_SUMMARY_SHOW_HEADERS = ["Device_id", "Op Name", "OP Type", "Task Type", TASK_START_TIME, "Task Duration(us)",
-                               "Task Wait Time(us)", "Block Dim"]
+                               "Task Wait Time(us)", "Block Num"]
     OP_SUMMARY_KERNEL_BASE_HEADERS = ["Device_id", "Name", "Type", "Accelerator Core", "Start Time(us)", "Duration(us)",
-                                      "Wait Time(us)", "Block Dim"]
+                                      "Wait Time(us)", "Block Num"]
