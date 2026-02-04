d1b6bfcf5340c01c21e34003f2c093c3a4094920
xuzhenhua2025 <xuzhenhua4@huawei.com>
2026-02-04 14:37:28 +0800

    Provides npugraph_ex APIs for user-defined backends and passes
    Co-authored-by: xuzhenhua2025<xuzhenhua4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30370 merge v2.6.0 into v2.6.0
    
    fix: npugraph_ex补充用户自定义backend和自定义pass缺失的接口
    
    Created-by: xuzhenhua2025
    Commit-by: xuzhenhua2025
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > (如有)请关联需求文档/issue链接
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    1. npugraph_ex补充用户自定义backends接口
    2. npugraph_ex补充用户自定义passes相关接口
    	2.1 补充注册replacement接口
        2.2 补充自定义pass流record接口
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    已在新版本资料中补充接口说明，资料配套发布。
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    1. 补充user-defined backends接口
    ```python
    def my_backend(gm: torch.fx.GraphModule, example_inputs):
        compiler = torch.npu.npugraph_ex.compile_fx()
        return aot_module_simplified(gm, example_inputs, fw_compiler=compiler)
     ```
    2. 补充user-defined passes接口
    ```python
    torch.npu.npugraph_ex.register_replacement(
        search_fn=search_fn,
        replace_fn=replace_fn,
        example_inputs=(input_tensor(), input_tensor(), kwargs_tensor()),
        extra_check=extra_check
    )
    ```
    ```python
    # record and wait should be used in pairs
    with torch.npu.npugraph_ex.scope.npu_stream_switch('1'):
        # ...
        record = torch.npu.npugraph_ex.ops.record()
        # ...
    torch.npu.npugraph_ex.ops.wait([record])
    ```
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    新增测试用例看护：
    1. compile_fx接口用例：
    /test/dynamo/test_npugraph_ex.TestNpuGraphEx.test_compile_fx
    2. 自定义pass接口用例：
    /test/dynamo/test_npugraph_ex.TestNpuGraphEx.test_register_replacement
    /test/dynamo/test_npugraph_ex.TestNpuGraphEx.test_record_and_wait
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/d383d445-2324-4b7f-9b4b-1c1935a64f81/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    See merge request: Ascend/pytorch!30370

--- torch_npu/npu/npugraph_ex/__init__.py
+++ torch_npu/npu/npugraph_ex/__init__.py
@@ -1,4 +1,35 @@
+__all__ = ["compile_fx", "register_replacement"]
+
+from typing import Match
+
+try:
+    from torch._inductor.pattern_matcher import fwd_only
+except ImportError:
+    from torch._inductor.pattern_matcher import inference_graph as fwd_only
+
 from . import experimental
 from . import inference
 from . import ops
 from . import scope
+
+
+def compile_fx(options: dict = None):
+    import torchair
+    from torchair.configs import npugraphex_config
+
+    compiler_config = torchair.CompilerConfig()
+    compiler_config.mode = "npugraph_ex"
+    npugraphex_config._process_kwargs_options(compiler_config, {options: options})
+    return torchair.get_compiler(compiler_config)
+
+
+def _return_true(match: Match):
+    return True
+
+
+def register_replacement(search_fn, replace_fn, example_inputs, trace_fn=fwd_only, extra_check=_return_true,
+                         search_fn_pattern=None):
+    import torchair
+    return torchair.patterns.pattern_pass_manager.register_replacement(search_fn, replace_fn, example_inputs,
+                                                                       trace_fn=trace_fn, extra_check=extra_check,
+                                                                       search_fn_pattern=search_fn_pattern)
