216e6aecf36bb104a6457cb75747d2eca66b2942
cuiduo <cuiduo1@huawei.com>
2026-02-04 09:44:06 +0800

    meta_check
    Co-authored-by: cuiduo<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30142 merge v2.9.0 into v2.9.0
    
    meta_check
    
    Created-by: cuiduo
    Commit-by: cuiduo
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    
    > /kind task
    
    **Which issue(s) this PR fixes**:Fix the "unknown format type" error for resize_as_ and squeeze_ operators in Triton mode, ensuring their proper integration into Inductor.
    
    
    
    
    
    See merge request: Ascend/pytorch!30142

---
+++ test/_inductor/test_resize_as.py
@@ -0,0 +1,31 @@
+import torch
+from torch.testing._internal.common_utils import (
+    run_tests,
+    parametrize,
+    instantiate_parametrized_tests,
+)
+from testutils import TestUtils
+import torch_npu
+
+
+class TestResizeAs(TestUtils):
+    def op_calc(self, x, target, adder):
+        return torch.ops.aten.resize_as_.default(x, target) + adder
+
+    def test_default(self):
+        shape = (2, 8)
+        target_shape = (8, 2)
+        x = torch.randn(shape, dtype=torch.float16, device="npu")
+        target = torch.randn(target_shape, dtype=torch.float16, device="npu")
+        adder = torch.randn(target_shape, dtype=torch.float16, device="npu")
+        std_out = self.op_calc(x, target, adder)
+        compile_out = torch.compile(self.op_calc, backend='inductor')(x, target, adder)
+        self.assertEqual(std_out, compile_out, atol=1e-5, rtol=1e-5)
+
+
+instantiate_parametrized_tests(TestResizeAs)
+
+
+if __name__ == "__main__":
+    run_tests()
+s
