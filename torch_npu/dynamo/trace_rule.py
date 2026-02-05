6e2913c674c4c58b3b3912929127fee7bb5cda4c
cuiduo <cuiduo1@huawei.com>
2026-02-05 10:27:51 +0800

    API_trace_relu changes
    Co-authored-by: cuiduo<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30393 merge v2.7.1 into v2.7.1
    
    API_trace_relu changes
    
    Created-by: cuiduo
    Commit-by: cuiduo
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > (如有)请关联需求文档/issue链接
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 修改trace_rule.py文件，确保NPU部分API在图模式下，入图行为与标杆一致。修改set-rng_state函数处理多余算子以及torch.npu.current_device白名单处理以确保算子生成fx图与标杆一致。
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > 新增测试文件test/_inductor/test_npu_current_device.py，参与ut看护
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!30393

--- torch_npu/dynamo/trace_rule.py
+++ torch_npu/dynamo/trace_rule.py
@@ -1,5 +1,8 @@
 import torch
 from torch._dynamo.variables import TorchInGraphFunctionVariable
+from torch._dynamo.trace_rules import manual_torch_name_rule_map, SkipFunctionVariable
+import torch._dynamo.variables.torch as torch_module
+from torch._dynamo.utils import common_constant_types
 import torch_npu
 
 __all__ = []
@@ -11,12 +14,43 @@ torch_c_binding_in_graph_functions_npu = dict.fromkeys(
         "torch.npu.stream",
         "torch.npu.set_stream",
         "torch_npu.npu.utils.synchronize",
+        "torch.npu.current_device",
+        "torch.npu.get_device_capability",
+        "torch.npu.get_device_properties",
+        "torch.npu.graphs.graph_pool_handle",
+        "torch.npu.ipc_collect",
+        "torch.npu.is_available",
+        "torch.npu.memory._dump_snapshot",
+        "torch.npu.memory._free_mutex",
+        "torch.npu.memory._record_memory_history_impl",
+        "torch.npu.memory._set_allocator_settings",
+        "torch.npu.memory.empty_cache",
+        "torch.npu.mem_get_info",
     ],
     TorchInGraphFunctionVariable,
+)
+
+skip_functions_npu = dict.fromkeys(
+    [
+        "torch.npu.set_device",
+        "torch.npu._get_generator",
+        "torch.npu._memory_viz._frames_fmt",
+        "torch.npu._memory_viz._frame_fmt",
+        "torch.npu.amp.autocast_mode.custom_bwd",
+        "torch.npu.amp.autocast_mode.custom_fwd",
+        "torch.npu.is_initialized"
+        
+    ],
+    SkipFunctionVariable
 )
 
 
 def _patch_npu_trace_rules():
     torch._dynamo.trace_rules.clear_lru_cache()
     torch._dynamo.trace_rules.torch_name_rule_map.append(torch_c_binding_in_graph_functions_npu)
+    torch._dynamo.trace_rules.torch_name_rule_map.append(skip_functions_npu)
+    torch_module.constant_fold_functions[torch.npu.current_device] = True
+    torch_module.constant_fold_functions[torch.npu.get_device_properties] = True
+    torch_module.constant_fold_functions[torch.npu.is_available] = True
+    common_constant_types.add(torch_npu._C._NPUDeviceProperties)
 
