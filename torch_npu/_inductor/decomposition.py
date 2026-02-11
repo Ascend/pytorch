359c5803e1b24a982d6bb75154271bb327c78876
weizhan4 <weizhan4@huawei.com>
2026-02-11 22:38:39 +0800

    [Inductor] A5 Fallback max_pool2d
    Co-authored-by: weizhan4<weizhan4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30808 merge w00609825_fallbackmaxpool into v2.7.1
    
    [Inductor] A5 Fallback max_pool2d
    
    Created-by: weizhan4
    Commit-by: weizhan4
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > (如有)请关联需求文档/issue链接
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    Inductor
    A5代际fallback max_pool2d
    Triton max_pool2d性能暂未达标，暂时fallback
    
    # 【资料变更】
    “不涉及”
    
    # 【接口变更】
    “不涉及”
    
    # 【功能验证】
    运行resnet50模型，功能正常，性能达标
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!30808

--- torch_npu/_inductor/decomposition.py
+++ torch_npu/_inductor/decomposition.py
@@ -2,7 +2,7 @@ import torch._ops
 from torch._inductor.decomposition import decompositions, pw_cast_for_opmath
 from torch._inductor.decomposition import register_decomposition
 from torch._prims_common.wrappers import out_wrapper
-
+from .config import get_soc_version, Ascend910_9391
 from .lowering import _init_set
 
 aten = torch.ops.aten
@@ -20,6 +20,9 @@ DECOMPOSITION_OVERLOAD_OP = [
     aten.native_dropout,
     aten.native_dropout_backward
 ]
+
+if get_soc_version() >= Ascend910_9391:
+    DECOMPOSITION_OVERLOAD_OP.append(aten.max_pool2d_with_indices)
 
 
 def _register_npu_inductor_decompositons():
