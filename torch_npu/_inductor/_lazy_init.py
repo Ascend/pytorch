beac5137b8d3edc5962cec924ebc4db9466fd679
zichun_ye <zichun.ye@huawei.com>
2026-02-12 09:05:58 +0800

    fix bug in importing mlir module
    Co-authored-by: Zichun Ye<zichun.ye@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30817 merge mlir_fix_v27_730 into v2.7.1-7.3.0
    
    fix bug in importing mlir module
    
    Created-by: zichun_ye
    Commit-by: Zichun Ye
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > (如有)请关联需求文档/issue链接
    
    - [ ] 需求
    - [X ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!30817

--- torch_npu/_inductor/_lazy_init.py
+++ torch_npu/_inductor/_lazy_init.py
@@ -3,16 +3,10 @@ try:
     from torch_mlir import ir
 except Exception as error:
     raise ImportError("torch_mlir is not installed, install it first.") from error
-from .ascend_npu_ir.build_ext import build_ascend_npu_ir_ext, set_torch_npu_library_path
-_has_inited = False
-if not _has_inited:
-    _has_inited = True
-    build_ascend_npu_ir_ext()
-    
+
 
 def import_npu_inductor_plugin():
     from .ascend_npu_ir.ascend_npu_ir.npu import npu_inductor_plugin
 
 
-set_torch_npu_library_path()
 import_npu_inductor_plugin()
