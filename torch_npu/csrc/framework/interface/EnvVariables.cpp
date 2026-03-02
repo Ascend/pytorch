ce62be732426c6a93dd1faf0d26895e832d2a5ee
daifu1234567 <daifu2@huawei.com>
2026-03-02 09:48:25 +0800

    [2.9.0]support aclnn extension
    Co-authored-by: DaiFu<daifu2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31149 merge v2.9.0 into v2.9.0
    
    [2.9.0]support aclnn extension
    
    Created-by: daifu1234567
    Commit-by: DaiFu
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    1、get_npu_data_unsafe_flag等函数加TORCH_NPU_API对外暴露，extension中可以从libtorch_npu.so中获取对应函数
    2、修改CustomRegisterSchema.cpp生成逻辑，extension场景用TORCH_LIBRARY_FRAGMENT代替TORCH_LIBRARY
    3、修复bug，extension场景下无需获取VERSION_PART
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    1、extension可以正常获取所需函数
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/37de6f85-f1c3-4de4-bb75-fe9715df96bc/image.png 'image.png')
    
    2、extension中生成的CustomRegisterSchema.cpp中使用TORCH_LIBRARY_FRAGMENT
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/73f39b9e-e9d2-47b7-8d5f-e53ccac92f70/image.png 'image.png')
    
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!31149

--- torch_npu/csrc/framework/interface/EnvVariables.cpp
+++ torch_npu/csrc/framework/interface/EnvVariables.cpp
@@ -10,6 +10,7 @@
 #include "torch_npu/csrc/core/npu/NpuVariables.h"
 #include "torch_npu/csrc/core/npu/register/OptionRegister.h"
 #include "torch_npu/csrc/framework/LazyInitAclops.h"
+#include "torch_npu/csrc/framework/interface/EnvVariables.h"
 namespace at_npu {
 namespace native {
 namespace env {
