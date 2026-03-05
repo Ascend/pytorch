045fbbd4ad42cc0d8a6899769b6bf5f35195bb30
xuzhenhua2025 <xuzhenhua4@huawei.com>
2026-03-05 14:52:27 +0800

    feat: Support independent packaging and deployment of npugraph_ex
    Co-authored-by: xuzhenhua2025<xuzhenhua4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31410 merge v2.7.1 into v2.7.1
    
    feat: Support independent packaging and deployment of npugraph_ex
    
    Created-by: xuzhenhua2025
    Commit-by: xuzhenhua2025
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    torch_npu/dynamo 新增npugraph_ex独立包目录，实现npugraph_ex backend功能，不依赖torchair包
    npugraph_ex后端注册方法使用`import npugraph_ex`, 并支持lazy import
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    无新增内容，无需新增用例/UT测试用例已适配变更内容看护.
    
    # 【CheckList】
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!31410

--- torch_npu/npu/npugraph_ex/experimental/inference/__init__.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-def use_internal_format_weight(model: torch.nn.Module):
-    from torch_npu.dynamo.torchair.experimental import inference
-    return inference.use_internal_format_weight(model)
