ae64b9a850260fa220cf8b2a699f3517d38c0b50
donglinzhuo <donglinzhuo@huawei.com>
2026-03-05 10:32:47 +0800

    [feat]Torch_npu aclgraph basic interface modification alignment pytorch
    Co-authored-by: 董林卓<donglinzhuo@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31304 merge v2.10.0 into v2.10.0
    
    [feat]Torch_npu aclgraph basic interface modification alignment pytorch
    
    Created-by: donglinzhuo
    Commit-by: 董林卓
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    1、随机数生成器构造函数处增加非捕获状态的校验。
    2、对齐pytorch原生代码，set_philox_offset_per_thread与philox_offset_per_thread在捕获状态下设置值或者取值为图内偏移量
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > “不涉及”
    
    # 【接口变更】
    >“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    在图捕获期间，创建随机数生成器查看是否能触发校验。符合预期
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/8fdfaf0a-67dd-4b95-88e5-0b9f937122f7/image.png 'image.png')
    两种场景下set值后去读philox_offset_per_thread，查看其取到的是全局偏移还是图内偏移，是否符合预期。
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/87042d29-a34c-4bca-a94b-5a98c5baa10e/image.png 'image.png')
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    See merge request: Ascend/pytorch!31304

--- torch_npu/csrc/npu/Graph.cpp
+++ torch_npu/csrc/npu/Graph.cpp
@@ -303,7 +303,7 @@ void TORCH_NPU_API THNPGraph_init(PyObject* module) {
         .def(
             "debug_dump",
             torch::wrap_pybind_function_no_gil(&c10_npu::NPUGraph::debug_dump),
-             py::arg("debug_path"))
+            py::arg("debug_path"))
         .def(
             "enable_debug_mode",
             torch::wrap_pybind_function_no_gil(&c10_npu::NPUGraph::enable_debug_mode));
