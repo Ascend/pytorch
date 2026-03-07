2c5a3fdb03e5f25154295879b8a9122c1502f0aa
culechan <chenshuai81@huawei.com>
2026-03-07 17:56:05 +0800

    [feat] add nestedtensor backend
    Co-authored-by: culechan<chenshuai81@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !30508 merge 2.7_nested into v2.7.1
    
    [feat] add nestedtensor backend
    
    Created-by: culechan
    Commit-by: culechan
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > (如有)请关联需求文档/issue链接
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 创建Nestedtensor相关的后端，参考quantized后端的实现，因为二者较为类似，因此在文件代码自动生成逻辑部分做了合并重构。
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增已适配UT测试用例看护，并补充测试自验证截图
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/2a752de0-200f-41f4-b2f7-40bb54b8b7e6/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!30508

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -23,7 +23,7 @@ class TestNestedTensor(TestCase):
                 length = np.random.randint(1, max_seq_len)
             row = list(np.random.randint(low=0, high=vocab_size, size=(length,)))
             data.append(row)
-            nested_tensor_ref_list.append(torch.tensor(row))
+            nested_tensor_ref_list.append(torch.tensor(row).npu())
         nested_tensor = torch.nested.nested_tensor(data)
         nested_tensor_list = nested_tensor.unbind()
         for i in range(batch_size):
@@ -43,7 +43,7 @@ class TestNestedTensor(TestCase):
             row = list(np.random.randint(low=0, high=vocab_size, size=(length,)))
             row = [list(item * np.arange(max_seq_len)) for item in row]
             data.append(row)
-            nested_tensor_ref_list.append(torch.tensor(row))
+            nested_tensor_ref_list.append(torch.tensor(row).npu())
         nested_tensor = torch.nested.nested_tensor(data)
         nested_tensor_list = nested_tensor.unbind()
         for i in range(batch_size):
@@ -63,45 +63,57 @@ class TestNestedTensor(TestCase):
             row = list(np.random.randint(low=0, high=vocab_size, size=(length,)))
             row = [list(item * np.arange(max_seq_len)) for item in row]
             data.append(row)
-            nested_tensor_ref_list.append(torch.tensor(row))
+            nested_tensor_ref_list.append(torch.tensor(row).npu())
         nested_tensor = torch.nested.nested_tensor(data)
         nested_tensor_list = nested_tensor.unbind()
         for i in range(batch_size):
             self.assertEqual(nested_tensor_list[i], nested_tensor_ref_list[i].type(torch.float32))
 
     def _test_unbind_case(self, a, b):
-        nt = torch.nested.nested_tensor([a, b], dtype=a.dtype)
+        nt = torch.nested.nested_tensor([a.npu(), b.npu()], dtype=a.dtype)
         nt_list = nt.unbind()
         self.assertEqual(len(nt_list), 2)
         self.assertEqual(nt_list[0], a)
         self.assertEqual(nt_list[1], b)
+
+    def _test_asnested_case(self, a, b):
+        nt = torch.nested.nested_tensor([a.npu(), b.npu()], dtype=a.dtype)
+        nt_as = torch.nested.as_nested_tensor([a.npu(), b.npu()], dtype=a.dtype)
+        nt_list = nt.unbind()
+        nt_as_list = nt_as.unbind()
+        self.assertEqual(len(nt_as_list), len(nt_list))
+        self.assertEqual(nt_as_list[0], nt_list[0])
+        self.assertEqual(nt_as_list[1], nt_list[1])
     
-    def test_unbind_case1(self):
+    def test_unbind_and_asnested_int64(self):
         a = torch.tensor([[1, 2, 3], [4, 5, 6]])
         b = torch.tensor([[7, 8], [10, 11]])
         self._test_unbind_case(a, b)
+        self._test_asnested_case(a, b)
         
-    def test_unbind_case2(self):
+    def test_unbind_and_asnested_float32(self):
         a = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32)
         b = torch.tensor([[7, 8], [10, 11]], dtype=torch.float32)
         self._test_unbind_case(a, b)
+        self._test_asnested_case(a, b)
 
-    def test_unbind_case3(self):
+    def test_unbind_and_asnested_empty(self):
         a = torch.tensor([[], []])
         b = torch.tensor([[], [], []])
         self._test_unbind_case(a, b)
+        self._test_asnested_case(a, b) 
 
     def test_default_options_nested_tensor(self):
-        default_nested_tensor = torch.nested.nested_tensor([])
-        default_tensor = torch.tensor([])
+        default_nested_tensor = torch.nested.nested_tensor([], device="npu:0")
+        default_tensor = torch.tensor([]).npu()
         self.assertEqual(default_nested_tensor.dtype, default_tensor.dtype)
         self.assertEqual(default_nested_tensor.device, default_tensor.device)
         self.assertEqual(default_nested_tensor.layout, default_tensor.layout)
         self.assertEqual(default_nested_tensor.dim(), default_tensor.dim())
         self.assertEqual(default_nested_tensor.requires_grad, default_tensor.requires_grad)
     
-    def test_nested_tensor_size(self):
-        nt = torch.nested.nested_tensor([torch.tensor([[1, 2, 3], [4, 5, 6]]), torch.tensor([[7, 8], [10, 11], [12, 13]])])
+    def test_nested_tensor_errsize(self):
+        nt = torch.nested.nested_tensor([torch.tensor([[1, 2, 3], [4, 5, 6]]).npu(), torch.tensor([[7, 8], [10, 11], [12, 13]]).npu()])
         self.assertEqual(nt.size(0), 2)
         self.assertRaisesRegex(RuntimeError,
         "Given dimension 1 is irregular and does not have a size", 
