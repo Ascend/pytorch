2454d10eefc3e98e6f21751757cf8da2d6705d32
LucciC <cuiliangcheng2@hisilicon.com>
2026-03-12 09:16:43 +0800

    add npu_meta_registration.py
    Co-authored-by: LucciC<cuiliangcheng2@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31111 merge v2.7.1 into v2.7.1
    
    add npu_meta_registration.py
    
    Created-by: LucciC
    Commit-by: LucciC
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!31111

---
+++ torch_npu/utils/_npu_meta_registration.py
@@ -0,0 +1,108 @@
+import os
+import sys
+import operator
+from functools import wraps, reduce, lru_cache
+from typing import Callable
+import torch
+from torch import Tensor
+from torch._ops import OpOverload, OpOverloadPacket
+from torch._subclasses import fake_tensor as _subclasses_fake_tensor
+from torch._C import DispatchKey
+from torch._refs import div as refs_div, _broadcast_shapes
+from torch._prims_common import corresponding_real_dtype, corresponding_complex_dtype
+from torch._prims_common.wrappers import out_wrapper
+from torch._decomp import decomposition_table, decompositions_for_rng, get_decompositions
+from torch._dynamo.symbolic_convert import break_graph_if_unsupported, InstructionTranslatorBase, stack_op
+from torch._dynamo.exc import Unsupported
+from torch._dynamo.variables.lists import TupleVariable
+from torch._dynamo.variables.nn_module import NNModuleVariable
+import torch_npu
+
+aten = torch.ops.aten
+npu = torch.ops.npu
+
+
+def run_once(f):
+    """Runs a function (successfully) only once.
+    The running can be reset by setting the `has_run` attribute to False
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            result = f(*args, **kwargs)
+            wrapper.has_run = True
+            return result
+        return None
+    wrapper.has_run = False
+    return wrapper
+
+
+npu_meta_table = {}
+break_fn_table = {}
+avoid_make_fallback_table = []
+
+
+def _add_op_to_meta_table(op, fn, avoid_fallback_flag=False):
+    overloads = []
+    if isinstance(op, OpOverload):
+        overloads.append(op)
+    else:
+        if not isinstance(op, OpOverloadPacket):
+            raise AssertionError("op must be instance of OpOverloadPacket.")
+        for ol in op.overloads():
+            overloads.append(getattr(op, ol))
+
+    for op_overload in overloads:
+        if op_overload in npu_meta_table:
+            raise RuntimeError(f"duplicate registrations for npu_meta_table {op_overload}")
+        npu_meta_table[op_overload] = fn
+        if avoid_fallback_flag:
+            avoid_make_fallback_table.append(op_overload)
+
+
+def patch_torch_decomp_decompositions():
+    '''
+    Because source torch_decomp_decompositions only enable the decompositions in
+    torch/_decomp/decompositions.py. Patch it to make decompositions in this file work.
+    '''
+    src_func = _subclasses_fake_tensor.torch_decomp_decompositions
+
+    @lru_cache(None)
+    def torch_decomp_decompositions_new(func):
+        if func in npu_meta_table.keys():
+            return True
+        return src_func(func)
+    _subclasses_fake_tensor.torch_decomp_decompositions = torch_decomp_decompositions_new
+
+
+def register_meta_npu(op, avoid_fallback_flag=False):
+    def meta_decorator(fn: Callable):
+        _add_op_to_meta_table(op, fn, avoid_fallback_flag)
+        return fn
+
+    return meta_decorator
+
+
+@run_once
+def npu_patch_meta():
+    '''
+    Torch official register decompostions and meta func for some aten ops,
+    which will raise conflict when npu outputs' dtype and shape are different
+    from native impl. Delete decompositions and meta func of these ops and add
+    npu decompositions and meta func.
+    '''
+    for op_overload, fn in npu_meta_table.items():
+        if not isinstance(op_overload, OpOverload):
+            raise AssertionError("op_overload must be instance of OpOverload.")
+        if op_overload not in avoid_make_fallback_table:
+            decomposition_table[op_overload] = fn
+        op_overload.py_kernels.pop(DispatchKey.Meta, None)
+        op_overload.py_impl(DispatchKey.Meta)(fn)
+
+    patch_torch_decomp_decompositions()
+
+
+@register_meta_npu(aten.index_put.default)
+def meta_index_put_patch(self, indices, values, accumulate=False):
+    return self.new_empty(self.shape)
+
