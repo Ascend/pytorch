00510dc3a820fead0f030b4c4f53df43bab8ff9d
sunhaolun <sunhaolun3@huawei.com>
2026-03-19 18:56:42 +0800

    add_guard
    Co-authored-by: sunhaolun<sunhaolun3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31895 merge v2.6.0 into v2.6.0
    
    add_guard
    
    Created-by: sunhaolun
    Commit-by: sunhaolun
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    https://codehub-y.huawei.com/c00502729/PTA_56_testing/issues/105
    将torch.npu.current_device添加进guard列表中，触发重编译。
    https://codehub-y.huawei.com/c00502729/PTA_56_testing/issues/82
    添加trace_rule避免断图
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    社区用例验证
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!31895

--- torch_npu/dynamo/trace_rule.py
+++ torch_npu/dynamo/trace_rule.py
@@ -74,7 +74,7 @@ skip_functions_npu = dict.fromkeys(
         "torch.npu.amp.autocast_mode.custom_bwd",
         "torch.npu.amp.autocast_mode.custom_fwd",
         "torch_npu._C._npu_resetAccumulatedMemoryStats",
-        
+        "torch_npu._C._npu_setStream",
     ],
     SkipFunctionVariable
 )
@@ -87,6 +87,7 @@ def _patch_npu_trace_rules():
     torch._dynamo.trace_rules.torch_name_rule_map.append(skip_functions_npu)
     torch_module.constant_fold_functions[torch.npu.current_device] = True
     torch_module.constant_fold_functions[torch.npu.get_device_properties] = True
+    torch_module.constant_fold_functions_need_guards[torch.npu.current_device] = True
     torch_module.constant_fold_functions[torch.npu.is_available] = True
     common_constant_types.add(torch_npu._C._NPUDeviceProperties)
 
