ad99d6c22fe33f0143cce471b7b0be9e16749b23
kuhn7 <chenkun82@huawei.com>
2026-03-24 19:35:21 +0800

    Release 2.11.0rc1
    Co-authored-by: chenkun<chenkun82@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !32267 merge v2.11.0rc1 into v2.11.0
    
    Release 2.11.0rc1
    
    Created-by: kuhn7
    Commit-by: chenkun
    Merged-by: ascend-robot
    Description: Release 2.11.0rc1
    
    See merge request: Ascend/pytorch!32267

--- setup.py
+++ setup.py
@@ -703,8 +703,10 @@ classifiers = [
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
 ]
 
+requirements = ['torch==2.11.0+cpu' if platform.machine() == 'x86_64' else 'torch==2.11.0']
 
 setup(
     name=os.environ.get('TORCH_NPU_PACKAGE_NAME', 'torch_npu'),
@@ -729,6 +731,7 @@ setup(
                 define_macros=[('_GLIBCXX_USE_CXX11_ABI', '1' if USE_CXX11_ABI else '0'), ('GLIBCXX_USE_CXX11_ABI', '1' if USE_CXX11_ABI else '0')]
             ),
     ],
+    install_requires=requirements,
     extras_require={
     },
     package_data={
