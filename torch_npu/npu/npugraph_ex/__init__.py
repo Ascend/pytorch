e59b4c35595e887ff7533e14e89b136d321e0d1d
mihudan <wangkai579@huawei.com>
2026-03-24 20:21:47 +0800

    修改npugraph_ex.compile_fx接口签名
    Co-authored-by: wangkai<wangkai579@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !32080 merge v2.8.0 into v2.8.0
    
    修改npugraph_ex.compile_fx接口签名
    
    Created-by: mihudan
    Commit-by: wangkai
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [x] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    修改npugraph_ex.compile_fx接口 compile_fx(gm, example_inputs=None, options=None)
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    资料已对应修改：https://gitcode.com/Ascend/torchair/pull/2744
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    资料已对应修改：https://gitcode.com/Ascend/torchair/pull/2744
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    对应用例也已同步修改
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/15fe052e-28f3-435a-81eb-d330f9f6c461/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x]  代码注释完备，正确记录错误日志
    - [x]  代码实现进行了返回值、空指针等校验
    - [x]  PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x]  PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!32080

--- torch_npu/npu/npugraph_ex/__init__.py
+++ torch_npu/npu/npugraph_ex/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["compile_fx", "register_replacement"]
 
 from collections.abc import Collection, Generator, Iterable, Mapping, Sequence
-from typing import Any, Callable, NoReturn, Optional, Protocol, TypeVar, Union, Match
+from typing import Any, Callable, NoReturn, Optional, Protocol, TypeVar, Union, Match, List
 
 try:
     from torch._inductor.pattern_matcher import fwd_only, SearchFn, ReplaceFn, TraceFn, PatternExpr
@@ -12,14 +12,15 @@ from . import inference
 from . import scope
 
 
-def compile_fx(options: dict = None):
+def compile_fx(gm, example_inputs=None, options=None):
     import npugraph_ex
     from npugraph_ex.configs import npugraphex_config
 
     compiler_config = npugraph_ex.CompilerConfig()
     compiler_config.mode = "npugraph_ex"
     npugraphex_config._process_kwargs_options(compiler_config, {"options": {} if options is None else options})
-    return npugraph_ex.get_compiler(compiler_config)
+    compiler = npugraph_ex.get_compiler(compiler_config)
+    return compiler(gm, example_inputs)
 
 
 def _return_true(match: Match):
