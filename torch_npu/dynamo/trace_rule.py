00e474c7f2ed7bc371a83ed9c0216087688f2d31
cuiduo <cuiduo1@huawei.com>
2026-03-25 09:33:58 +0800

    [fix]pickle_local_object_error
    Co-authored-by: cuiduo<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !31830 merge v2.9.0 into v2.9.0
    
    [fix]pickle_local_object_error
    
    Created-by: cuiduo
    Commit-by: cuiduo
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 修复闭包问题，pickle无法追踪闭包函数，迁移为公共函数
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > 已增加UT，torchair测试通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!31830

--- torch_npu/dynamo/trace_rule.py
+++ torch_npu/dynamo/trace_rule.py
@@ -46,6 +46,7 @@ torch_non_c_binding_in_graph_functions_npu = dict.fromkeys(
         "torch.npu.amp.autocast_mode.custom_fwd",
         "torch.npu._get_current_allocator",
         "torch.npu.is_bf16_supported",
+        "torch.npu.memory._get_current_allocator",
     ],
     TorchInGraphFunctionVariable,
 )
