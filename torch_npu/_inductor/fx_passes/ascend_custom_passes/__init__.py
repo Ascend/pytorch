828e670fc4b96ff064241c3f9db74eb2eeb885d2
cuiduo <cuiduo1@huawei.com>
2026-03-30 09:44:01 +0800

    [fix]tiling_key_error
    Co-authored-by: cuiduo<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !32356 merge v2.8.0 into v2.8.0
    
    [fix]tiling_key_error
    
    Created-by: cuiduo
    Commit-by: cuiduo
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    >1. 在codegen的tiling阶段，会调用get_axis_dtype来获取node的type，当axis为sympy.Expr类时会报错
    >```
    >File "/home/cuiduo/miniconda3/envs/py311/lib/python3.11/site-packages/torch_npu/_inductor/codegen/triton.py", line 790, in get_axis_dtype
    >dim = self.range_tree_nodes_removed[key]
    >torch ._ dynamo. exc. BackendCompilerFailed: backend='inductor' raised:
    >KeyError: x2 + 512*y1 + 2048*z0
    >```
    >因此需要增加校验，提取sympy.Expr中的Symbol以进行dtype的判断
    2.在fold类fx_pass的入口处添加stable_topological_sort(gm)进行排序，避免乱序图带来的影响
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    >不涉及
    
    # 【功能验证】
    > 自验完成
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!32356

--- torch_npu/_inductor/fx_passes/ascend_custom_passes/__init__.py
+++ torch_npu/_inductor/fx_passes/ascend_custom_passes/__init__.py
@@ -4,6 +4,7 @@ from ..utils.check_mode import is_inference_check
 from .register_custom_pass import ASCEND_CUSTOME_PASS_REGISTER
 from ..utils.fx_pass_level import FxPassLevel, PassType
 from ...config import log
+from torch._inductor.pattern_matcher import stable_topological_sort
 
 
 for _, module_name, _ in pkgutil.iter_modules(__path__):
@@ -28,6 +29,7 @@ def run_register_pre_custom_passes(gm):
 def run_register_post_custom_passes(gm):
     log.debug(f"before post_grad graph optimizer pass, graph is: {gm}")
     if is_inference_check():
+        stable_topological_sort(gm)
         for level in sorted(FxPassLevel):
             for fn in ASCEND_CUSTOME_PASS_REGISTER[PassType.POST][level]:
                 fn(gm)
