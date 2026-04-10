0e8a507fc1819474215883d38084192890664f4a
glf2016 <13739288457@163.com>
2026-04-10 17:03:18 +0800

    npugraphex config update
    Co-authored-by: guanlongfeng<guanlongfeng1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !33230 merge npugraphex_config into v2.7.1
    
    npugraphex config update
    
    Created-by: glf2016
    Commit-by: guanlongfeng
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!33230

--- torch_npu/npu/npugraph_ex/inference/__init__.py
+++ torch_npu/npu/npugraph_ex/inference/__init__.py
@@ -3,11 +3,11 @@ __all__ = ["cache_compile", "readable_cache"]
 
 def cache_compile(func, *, dynamic: bool = True, cache_dir=None, global_rank=None, tp_rank=None, pp_rank=None, **kwargs):
     import npugraph_ex
-    from npugraph_ex.configs import npugraphex_config
+    from npugraph_ex.configs import compiler_config
 
     compile_config = npugraph_ex.CompilerConfig()
     compile_config.mode = "npugraph_ex"
-    npugraphex_config._process_kwargs_options(compile_config, kwargs)
+    compiler_config._process_kwargs_options(compile_config, kwargs)
     return npugraph_ex.inference.cache_compile(func, config=compile_config, dynamic=dynamic, cache_dir=cache_dir,
                                             global_rank=global_rank, tp_rank=tp_rank, pp_rank=pp_rank, **kwargs)
 
