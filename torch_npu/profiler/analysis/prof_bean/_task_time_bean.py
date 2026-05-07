ea0e6edad49216a08c8bc569eed7a3b791030000
ascend-robot <zhongyuanke@huawei.com>
2026-05-07 11:40:35 +0800

    [feat]profiler move task_time soc_pmu
    Co-authored-by: hehongzhe<935062458@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !34919 merge move10 into v2.10.0
    
    [feat]profiler move task_time soc_pmu
    
    Created-by: hhz0
    Commit-by: hehongzhe
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [x] issue/工单
    https://gitcode.com/Ascend/pytorch/issues/1814
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    1、新增移动task_time_*.csv，文件名称为task_time.csv
    2、新增移动soc_pmu_*.csv，文件名称为soc_pmu.csv
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    打开l2_cahce=True进行数据采集，完成后查看ASCEND_PROFILER_OUTPUT
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/b637381c-7d94-43f1-9884-01dfd8e45f8a/image.png 'image.png')
    预期文件正常生成
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!34919

---
+++ torch_npu/profiler/analysis/prof_bean/_task_time_bean.py
@@ -0,0 +1,16 @@
+__all__ = []
+
+from ._common_bean import CommonBean
+
+
+class TaskTimeBean(CommonBean):
+    def __init__(self, data: dict):
+        super().__init__(data)
+
+    @property
+    def row(self) -> list:
+        return list(self._data.values())
+
+    @property
+    def headers(self) -> list:
+        return list(self._data.keys())
