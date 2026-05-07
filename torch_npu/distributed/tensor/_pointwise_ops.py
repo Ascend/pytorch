94d8514caf2284c1285fa32ee1fe13fba5ccbcd6
kisnwang <wangkaisheng2@huawei.com>
2026-05-07 11:05:56 +0800

    refactor pointwise rule for torch version 2.12
    Co-authored-by: kisnwang<wangkaisheng2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !34809 merge master into master
    
    refactor pointwise rule for torch version 2.12
    
    Created-by: kisnwang
    Commit-by: kisnwang
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [x] 重构优化  [#1743](https://gitcode.com/Ascend/pytorch/issues/1743) 
    - [ ] 资料更新
    
    # 【修改方案】
    最新torch版本已经移除pointwise_strategy方法，改用_register_single_dim_pointwise方法，自定义算子需要对齐社区
    
    # 【资料变更】
    “不涉及”
    
    # 【接口变更】
    “不涉及”
    
    # 【功能验证】
    使用门禁已有UT验证
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!34809

--- torch_npu/distributed/tensor/_pointwise_ops.py
+++ torch_npu/distributed/tensor/_pointwise_ops.py
@@ -1,30 +1,49 @@
-
 import torch
 from torch.distributed.tensor._op_schema import OpSchema, RuntimeSchemaInfo
 from torch_npu._compat.distributed import register_op_strategy
-from torch.distributed.tensor._ops._pointwise_ops import pointwise_strategy
+
+
+try:
+    # remove pointwise_strategy from version 2.12
+    from torch.distributed.tensor._ops._pointwise_ops import pointwise_strategy
+except ImportError:
+    pointwise_strategy = None
+    from torch.distributed.tensor._ops._pointwise_ops import (
+        _ALL_PARTIAL_BINARY_PRESERVING_RULES,
+        _register_single_dim_pointwise,
+    )
+
 
 aten = torch.ops.aten
 npu = torch.ops.npu
 
 
-custom_linear_pointwise_ops = {
-    npu.npu_dtype_cast.default: 0,
-    npu._npu_dtype_cast.default: 0,
-    npu.npu_dtype_cast_backward.default: 0,
-    npu._npu_dtype_cast_backward.default: 0,
-}
+if pointwise_strategy is not None:
+    custom_linear_pointwise_ops = {
+        npu.npu_dtype_cast.default: 0,
+        npu._npu_dtype_cast.default: 0,
+        npu.npu_dtype_cast_backward.default: 0,
+        npu._npu_dtype_cast_backward.default: 0,
+    }
 
+    def custom_linear_pointwise_strategy(op_schema: OpSchema):
+        op_type = custom_linear_pointwise_ops.get(op_schema.op, -1)
+        return pointwise_strategy(op_schema, linearity=op_type)
 
-def custom_linear_pointwise_strategy(op_schema: OpSchema):
-    op_type = custom_linear_pointwise_ops.get(op_schema.op, -1)
-    return pointwise_strategy(op_schema, linearity=op_type)
+    for op in custom_linear_pointwise_ops:
+        register_op_strategy(
+            op, schema_info=RuntimeSchemaInfo(static_kwargkey=["out"])
+        )(custom_linear_pointwise_strategy)
+else:
+    custom_linear_pointwise_ops = [
+        npu.npu_dtype_cast.default,
+        npu._npu_dtype_cast.default,
+        npu.npu_dtype_cast_backward.default,
+        npu._npu_dtype_cast_backward.default,
+    ]
 
-
-for op in custom_linear_pointwise_ops:
-    register_op_strategy(
-        op, schema_info=RuntimeSchemaInfo(static_kwargkey=["out"])
-    )(custom_linear_pointwise_strategy)
+    for op in custom_linear_pointwise_ops:
+        _register_single_dim_pointwise(op, _ALL_PARTIAL_BINARY_PRESERVING_RULES)
 
 
 custom_pointwise_ops = [
@@ -38,11 +57,14 @@ custom_pointwise_ops = [
     npu.npu_layer_norm_eval.default,
     # backward point-wise ops
     # please keep the entries below alphabetically sorted
-    npu.npu_fast_gelu_backward.default
+    npu.npu_fast_gelu_backward.default,
 ]
 
-
-for op in custom_pointwise_ops:
-    register_op_strategy(
-        op, schema_info=RuntimeSchemaInfo(static_kwargkey=["out"])
-    )(pointwise_strategy)
+if pointwise_strategy is not None:
+    for op in custom_pointwise_ops:
+        register_op_strategy(
+            op, schema_info=RuntimeSchemaInfo(static_kwargkey=["out"])
+        )(pointwise_strategy)
+else:
+    for op in custom_pointwise_ops:
+        _register_single_dim_pointwise(op)
