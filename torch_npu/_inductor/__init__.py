f5ea1e486358c121c195db796e307b38b7216e46
sunhaolun <sunhaolun3@huawei.com>
2026-05-07 19:36:10 +0800

    remove_patch
    Co-authored-by: sunhaolun<sunhaolun3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !34954 merge master into master
    
    remove_patch
    
    Created-by: sunhaolun
    Commit-by: sunhaolun
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    将config.py中的patch转移到utils/_dynamo中，compile模式为default时触发。并对config.py，_dynamo.py以及_inductor/init.py进行代码重构
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    通过UT验证。
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!34954

--- torch_npu/_inductor/__init__.py
+++ torch_npu/_inductor/__init__.py
@@ -1,52 +1,63 @@
 import os
 
 from .codegen.common import register_device_op_overrides_npu
+
+
 register_device_op_overrides_npu()
 
-if os.getenv('TORCHINDUCTOR_NPU_BACKEND', 'default') == 'mlir':
+if os.getenv("TORCHINDUCTOR_NPU_BACKEND", "default") == "mlir":
     try:
         import torch_mlir
         from torch_mlir import ir
-    except:
-        raise ImportError("torch_mlir is not installed, install it first.")
+    except ImportError as err:
+        raise ImportError("torch_mlir is not installed, install it first.") from err
     from .ascend_npu_ir.ascend_npu_ir.npu import npu_inductor_plugin
-    from .utils import patch_is_gpu,patch_has_triton
+    from .utils import patch_has_triton, patch_is_gpu
+
     patch_is_gpu()
     patch_has_triton()
 else:
     import torch
-    from torch._dynamo.device_interface import register_interface_for_device, get_interface_for_device
+    from torch._dynamo.device_interface import (
+        get_interface_for_device,
+        register_interface_for_device,
+    )
     from torch._inductor import lowering as inductor_lowering
     from torch._inductor.choices import InductorChoices
-    from torch._inductor.codegen.common import register_backend_for_device, register_device_op_overrides
+    from torch._inductor.codegen.common import (
+        register_backend_for_device,
+        register_device_op_overrides,
+    )
     from torch._inductor.runtime import autotune_cache
     from torch_npu.npu import device_count
-    from torch_npu.utils._dynamo_device import NpuInterface, current_device, set_device
+    from torch_npu.utils._dynamo_device import current_device, NpuInterface, set_device
     from torch_npu.utils._inductor import NPUDeviceOpOverrides
 
-    from . import config as npu_config
-    from . import codegen
-    from .npu_fusion_attention_graph import register_fa_pass
-    from .config import aggresive_autotune, num_vector_core, set_compile_threads, disable_comprehensive_padding
-    from .config import log as npulog
+    from . import codegen, config as npu_config
+    from .codecache import patch_aot_code_compiler_compile, patch_cache_base_get_system
+    from .config import aggresive_autotune, log as npulog, num_vector_core
     from .decomposition import _register_npu_inductor_decompositons
     from .lowering import make_reduction, npu_make_fallback
     from .npu_choices import should_use_persistent_reduction
     from .npu_device import NewNPUDeviceOpOverrides
+    from .npu_fusion_attention_graph import register_fa_pass
     from .runtime import _load_cached_autotuning
-    from .utils import get_current_raw_stream, patch_is_gpu, patch_has_triton, disable_foreach, patch_fx_node_is_input_dependent_cudagraph_unsafe
-    from .codecache import patch_aot_code_compiler_compile, patch_cache_base_get_system
-
-    set_compile_threads()
-    disable_comprehensive_padding()
-
+    from .utils import (
+        disable_foreach,
+        get_current_raw_stream,
+        patch_fx_node_is_input_dependent_cudagraph_unsafe,
+        patch_has_triton,
+        patch_is_gpu,
+    )
 
     def _inductor_register_backend_for_device():
+        from .codegen.cpp_wrapper import CppWrapperNpu
         from .codegen.scheduling import NPUTritonScheduling
         from .codegen.wrapper import NPUWrapperCodeGen
-        from .codegen.cpp_wrapper import CppWrapperNpu
-        register_backend_for_device('npu', NPUTritonScheduling, NPUWrapperCodeGen, CppWrapperNpu)
 
+        register_backend_for_device(
+            "npu", NPUTritonScheduling, NPUWrapperCodeGen, CppWrapperNpu
+        )
 
     _inductor_register_backend_for_device()
 
@@ -55,14 +66,13 @@ else:
     inductor_lowering.make_reduction = make_reduction
     inductor_lowering.make_fallback = npu_make_fallback
 
-
     def patch_torch_for_aoti():
-        from .graph import patch_codegen_with_cpp_wrapper
-        from .cpp_builder import patch_get_cpp_torch_device_options
         from .codegen.cpp_utils import patch_device_to_aten
-        from .utils import patch_is_same_tensor
+        from .cpp_builder import patch_get_cpp_torch_device_options
         from .fx_passes.joint_graph import patch_constant_fold_uniform_value
+        from .graph import patch_codegen_with_cpp_wrapper
         from .ir import patch_fallback_kernel_codegen
+        from .utils import patch_is_same_tensor
 
         patch_codegen_with_cpp_wrapper()
         patch_get_cpp_torch_device_options()
@@ -72,25 +82,27 @@ else:
         patch_fallback_kernel_codegen()
 
         from .ir import patch_extern_kernel_codegen_size_asserts
+
         patch_extern_kernel_codegen_size_asserts()
 
         patch_aot_code_compiler_compile()
 
-
     if os.environ.get("DISABLE_AOTI_PATCH", "0") != "1":
         patch_torch_for_aoti()
-
 
     if npu_config.dump_fx_graph:
         from .codegen.ir_fx import _patch_npu_inductor_ir
 
         _patch_npu_inductor_ir()
 
-    from .lowering import _register_npu_inductor_fallbacks, _enable_full_lowering_fallback
+    from .lowering import (
+        _enable_full_lowering_fallback,
+        _register_npu_inductor_fallbacks,
+    )
 
     _register_npu_inductor_decompositons()
 
-    if npu_config.enable_full_lowering_fallback.strip()=='allfallback':
+    if npu_config.enable_full_lowering_fallback.strip() == "allfallback":
         _enable_full_lowering_fallback()
     else:
         _register_npu_inductor_fallbacks()
@@ -98,12 +110,13 @@ else:
     # register fx_pass should be put behind of _register_npu_inductor_decompositons
     def _replace_benchmark_all_configs():
         from torch_npu._compat.inductor import get_CachingAutotuner
+
         CachingAutotuner = get_CachingAutotuner()
         from .npu_triton_heuristics import benchmark_all_configs
+
         CachingAutotuner.benchmark_all_configs = benchmark_all_configs
 
-
-    if (aggresive_autotune):
+    if aggresive_autotune:
         _replace_benchmark_all_configs()
         import os
 
