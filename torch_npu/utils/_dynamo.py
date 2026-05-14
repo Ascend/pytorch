db77de13afd7281862867861bf74d1761d0797ec
cuiduo <cuiduo1@huawei.com>
2026-05-14 14:01:04 +0800

    [fix]str | None = None_not_suit
    Co-authored-by: cuiduo<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !35600 merge v2.9.0 into v2.9.0
    
    [fix]str | None = None_not_suit
    
    Created-by: cuiduo
    Commit-by: cuiduo
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 修复_loaded_backend: str | None = None写法不兼容错误
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > UT已过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!35600

--- torch_npu/utils/_dynamo.py
+++ torch_npu/utils/_dynamo.py
@@ -113,8 +113,7 @@ def TensorVariable_call_method(self, tx, name, args, kwargs):
 
 class _InductorNpuRegistry:
     _disabled_register = False
-    _loaded_backend: str | None = None
-
+    _loaded_backend = None
     @classmethod
     def register_inductor_npu(cls):
         if cls._disabled_register:
