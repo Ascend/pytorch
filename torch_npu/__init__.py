8e0b6884a28ffa0ac7df25b458f093abbf04a0d4
bellatan <tanmei2@huawei.com>
2026-05-14 20:23:45 +0800

    fix for inductor patches
    Co-authored-by: bellatan<tanmei2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !35627 merge v2.10.0_torch_npu_init_refactor into v2.10.0
    
    fix for inductor patches
    
    Created-by: bellatan
    Commit-by: bellatan
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    1. rpc注册校验删除，由于op和torch_npu的CI 编译机制，该校验需要删除，同时不在初始化阶段完成rpc的注册和初始化
    
    2. 接口重名，`_initialize` 接口中 `_apply_patches` 和 torch_npu/contrib/transfer_to_npu.py的 `_patch_cuda`、`_patch_profiler` 的内部接口 `_apply_patches` 重名:
    torch_npu/_init/patches/patch_manager.py的 `_apply_patches`  -> `_apply_all_patches`，并同步调用位置处修改
    torch_npu/contrib/transfer_to_npu.py的  `_patch_cuda`、`_patch_profiler` 的内部改成使用内部接口 `from torch_npu._init.patches.monkey_patches import _apply_patches`
    
    3. torch_npu/_init/patches/patch_manager.py 部分代码使用 py39不支持的语法
    `_custom_full_patch_order: list[str] | None = None`  ->  `_custom_full_patch_order: Optional[List[str]] = None`
    
    4. 调整 torch_npu/__init__.py 的 _initialize 内部顺序
    完成 `_initialize_runtime_lifecycle`后 再调用 `_enable_optional_features`，内部 TORCH_TRANSFER_TO_NPU 相关特性依赖初始化完整流程
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    ci中涉及的相关用例/操作，本地验证结果：
    python -c "import torch_npu"
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/29d09b17-6c5f-4019-bb74-97e77830dfec/image.png 'image.png')
    
    pytest -sv _inductor/test_triton.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/a4b63f92-9118-40e1-89d3-2aae4b1f4fa4/image.png 'image.png')
    
    
    pytest -sv contrib/test_transfer_to_npu_env.py 
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/9f7db8b8-f1f1-46c1-ad7c-386fd82d26c7/image.png 'image.png')
    
    pytest -sv contrib/test_transfer_to_npu.py 
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/f315380c-5fe5-4ed4-842b-240c698d1eb1/image.png 'image.png')
    
    
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!35627

--- torch_npu/__init__.py
+++ torch_npu/__init__.py
@@ -16,7 +16,7 @@ import torch_npu.utils.patch_getenv
 from torch_npu._init.core.module_loader import _load_core_modules
 from torch_npu._init.core.optional_features import _enable_optional_features
 from torch_npu._init.core.runtime_lifecycle import _initialize_runtime_lifecycle
-from torch_npu._init.patches.patch_manager import _apply_patches
+from torch_npu._init.patches.patch_manager import _apply_all_patches
 from torch_npu._init.registry.registry_manager import _register_components
 from torch_npu.version import __version__ as __version__
 
@@ -53,13 +53,13 @@ def _initialize():
     _register_components()
 
     # 4. apply patches
-    _apply_patches()
+    _apply_all_patches()
 
-    # 5. optional runtime features
-    _enable_optional_features()
-
-    # 6. final extension barrier and shutdown hook
+    # 5. final extension barrier and shutdown hook
     _initialize_runtime_lifecycle()
+
+    # 6. optional runtime features 
+    _enable_optional_features()
 
 
 _initialize()
