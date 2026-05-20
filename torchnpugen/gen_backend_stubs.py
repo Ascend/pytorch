b98263bcadee73d7ce0674557db8cdc2d4e37af1
jiangna1111 <jiangna12@huawei.com>
2026-05-20 18:29:16 +0800

    Fix v2.7.1 index_copy overlap checks
    (cherry picked from commit 6ccc63b598a92f0251280f141f3ddd1a95079c0b)

--- torchnpugen/gen_backend_stubs.py
+++ torchnpugen/gen_backend_stubs.py
@@ -599,6 +599,7 @@ def gen_dispatcher_registrations(
 
     native_func_header += """#include "torch_npu/csrc/framework/utils/ForceAclnnList.h"
 #include "torch_npu/csrc/framework/OpHook.h"
+#include "torch_npu/csrc/aten/mirror/NPUMemoryOverlap.h"
 #include "op_plugin/OpInterface.h"
 """
     static_template = CodeTemplate(
