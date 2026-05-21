e8138309703017cd1d3656dacd01cd2ad3ad2f7c
hbhu_bin <hubin79@huawei.com>
2026-05-21 11:16:35 +0800

    add record_stream sanitizer
    Co-authored-by: bellatan<tanmei2@huawei.com>
    Co-authored-by: hb_hubin<hubin79@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !34841 merge record_stream_sanitizer into master
    
    add record_stream sanitizer
    
    Created-by: bellatan
    Commit-by: bellatan;hb_hubin
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    本地验证
    1. test/npu/test_sanitizer.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/db51597e-f6bf-4b34-8ba1-170ba8bb5bde/image.png 'image.png')
    
    2. test/npu/test_sanitizer_record_stream.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/630b87e8-dc9c-4c9a-a2d4-932b9ad18dca/image.png 'image.png')
    
    3. test/test_npu_sanitizer.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/ecb4d1f1-aef9-4b9b-a082-75ff16991064/image.png 'image.png')
    
    4. test/test_sanitizer_pluggable_allocator.py
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/ffa87f39-c755-41fe-8176-9cb38cd73a09/image.png 'image.png')
    
    5. ci结果
    ·UT_ARM_A2_Part_01：test/test_npu_sanitizer.py，pass
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/d5fb5242-cb11-435d-ae2b-36960a9522f8/image.png 'image.png')
    ·UT_ARM_A2_Part_02：test/npu/test_sanitizer.py，pass；test/test_sanitizer_pluggable_allocator.py	pass
    ![1778915605465.png](https://raw.gitcode.com/user-images/assets/7404318/243673b4-b5b0-4c5d-8c21-3d22f84cce82/1778915605465.png '1778915605465.png')
    ·UT_ARM_A2_Part_03：test/npu/test_sanitizer_record_stream.py，pass
    ![1778915660126.png](https://raw.gitcode.com/user-images/assets/7404318/b1e2f979-f4f7-4f24-b4b9-b1987de33677/1778915660126.png '1778915660126.png')
    
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!34841

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -3409,6 +3409,14 @@ public:
         // block must not be null reaching here
         TORCH_INTERNAL_ASSERT(block != nullptr, "No allocated block can be found", PTA_ERROR(ErrCode::NOT_FOUND));
         device_allocator[block->device]->recordStream(block, stream);
+#ifndef BUILD_LIBTORCH
+        const c10_npu::impl::PyCallbackTrigger *trigger = c10_npu::impl::NPUTrace::getTrace();
+        if (C10_UNLIKELY(trigger)) {
+            trigger->traceNpuRecordStream(
+                reinterpret_cast<uintptr_t>(ptr.get()),
+                reinterpret_cast<uintptr_t>(stream.stream(false)));
+        }
+#endif
     }
 
     void eraseStream(const c10::DataPtr &ptr, c10_npu::NPUStream stream)
@@ -3441,6 +3449,14 @@ public:
         }
 
         device_allocator[block->device]->eraseStream(block, stream);
+#ifndef BUILD_LIBTORCH
+        const c10_npu::impl::PyCallbackTrigger* trigger = c10_npu::impl::NPUTrace::getTrace();
+        if (C10_UNLIKELY(trigger)) {
+            trigger->traceNpuEraseStream(
+                reinterpret_cast<uintptr_t>(ptr.get()),
+                reinterpret_cast<uintptr_t>(stream.stream(false)));
+        }
+#endif
     }
 
     void eraseStreamWithBlockPtr(void* block_ptr, c10_npu::NPUStream stream, void* work_ptr) override
@@ -3463,6 +3479,14 @@ public:
         }
 
         device_allocator[block->device]->eraseStream(block, stream);
+#ifndef BUILD_LIBTORCH
+        const c10_npu::impl::PyCallbackTrigger* trigger = c10_npu::impl::NPUTrace::getTrace();
+        if (C10_UNLIKELY(trigger)) {
+            trigger->traceNpuEraseStream(
+                reinterpret_cast<uintptr_t>(block->ptr),
+                reinterpret_cast<uintptr_t>(stream.stream(false)));
+        }
+#endif
     }
 
     void* getBlockPtr(const c10::DataPtr& ptr) override
