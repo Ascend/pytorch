24ac91514c8a44224985a0e143a6994645519417
yvjc <yujincheng7@huawei.com>
2026-05-26 11:39:42 +0800

    feat: add the expm1 decomposition to mlir.
    Co-authored-by: yvjc<yujincheng7@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !36484 merge decomp11 into v2.11.0
    
    feat: add the expm1 decomposition to mlir.
    
    Created-by: yvjc
    Commit-by: yvjc
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    https://gitcode.com/Ascend/pytorch/issues/1962
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    inductor添加mlir后端expm1的decomposition实现,与其他版本对齐.
    
    原本存在的test_inductor_fallback_list.py这个ut存在将triton和mlir后端在同一进程中运行的问题，在inductor初始化的时候首先初始化了triton后端的相关配置，然后在mlir测试例子时又初始化mlir后端的相关配置，导致在triton后端中已经对expm1进行注册，在mlir初始化时会注册失败。该问题属于ut问题，因此将test_inductor_fallback_list.py拆为test_inductor_fallback_list_triton.py和test_inductor_fallback_list_mlir.py两个ut。
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    不涉及
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    不涉及
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    运行torchbench网络tts_angular, aot_eager, mlir, dvm能够成功运行, 不报错
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!36484

---
+++ test/_inductor/test_inductor_fallback_list_mlir.py
@@ -0,0 +1,31 @@
+import os
+
+import torch
+from torch.testing._internal.common_utils import run_tests
+from torch._inductor.utils import run_and_get_code
+
+from testutils import TestUtils
+
+
+os.environ["NPU_INDUCTOR_FALLBACK_LIST"] = "aten.div,aten.add.Tensor"
+
+
+class TestFallback(TestUtils):
+
+    def add_op(self, x, y):
+        return x / y
+    
+    def test_add_fallback_detection_mlir(self):
+
+        compiled_add = torch.compile(self.add_op, backend="inductor", options={"npu_backend": "mlir"})
+
+        x = torch.randn(4, 4, dtype=torch.float32).to("npu")
+        y = torch.randn(4, 4, dtype=torch.float32).to("npu")
+
+        _ , codes = run_and_get_code(compiled_add, x, y)
+
+        self.assertTrue('mlir_fused_div' not in codes[0])
+
+
+if __name__ == "__main__":
+    run_tests()
