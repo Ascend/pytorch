e51764e80cf01244c8b47d1fa7b061e7e111c607
maoyuanpeng1 <maoyuanpeng@huawei.com>
2026-05-28 11:32:12 +0800

    support strutured meta
    Co-authored-by: maoyuanpeng<maoyuanpeng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !36641 merge br_support_structure_ops_v2.12.0 into v2.12.0
    
    support strutured meta
    
    Created-by: maoyuanpeng1
    Commit-by: maoyuanpeng
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    https://gitcode.com/Ascend/pytorch/issues/2110
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!36641

---
+++ torchnpugen/templates/NPUStructuredNativeFunctions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// ${generated_comment}
+
+#include <ATen/Tensor.h>
+#include <ATen/TensorIterator.h>
+
+${meta_includes}
+
+namespace at_npu {
+namespace native {
+
+${structured_declarations}
+
+} // namespace native
+} // namespace at_npu
