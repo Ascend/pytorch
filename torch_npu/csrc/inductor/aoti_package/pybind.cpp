8fd9b9c650d3cb536bae57432e50b33b863a04a1
qiaoyaodan888 <qiaoyaodan@huawei.com>
2026-06-02 11:25:23 +0800

    cherry-pick aoti_load_package 2.7.1->2.9.0
    Co-authored-by: qiaoyaodan<qiaoyaodan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !37195 merge aoti_2.9.0 into v2.9.0
    
    cherry-pick aoti_load_package 2.7.1->2.9.0
    
    Created-by: qiaoyaodan888
    Commit-by: qiaoyaodan
    Merged-by: ascend-robot
    Description: 
    <!--
    PR描述模板更新日期：20260203
    -->
    
    原pr ：https://gitcode.com/Ascend/pytorch/pull/35687
    
    # 【合入来源】
    https://gitcode.com/Ascend/pytorch/issues/2215
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!37195

---
+++ torch_npu/csrc/inductor/aoti_package/pybind.cpp
@@ -0,0 +1,70 @@
+#ifndef BUILD_LIBTORCH
+#ifdef USE_NPU
+#include <torch_npu/csrc/inductor/aoti_runner/model_container_runner_npu.h>
+#endif
+
+#include <torch/csrc/autograd/python_variable.h>
+
+#include <torch_npu/csrc/inductor/aoti_package/model_package_loader.h>
+#include <torch_npu/csrc/inductor/aoti_runner/model_container_runner.h>
+#include <torch_npu/csrc/inductor/aoti_runner/pybind.h>
+
+namespace torch::inductor {
+
+class AOTIModelPackageLoaderPybind : public AOTIModelPackageLoader {
+ public:
+  AOTIModelPackageLoaderPybind(
+      const std::string& model_package_path,
+      const std::string& model_name,
+      const bool run_single_threaded)
+      : AOTIModelPackageLoader(
+            model_package_path,
+            model_name,
+            run_single_threaded) {}
+
+  py::list boxed_run(py::list& inputs, void* stream_handle = nullptr) {
+    std::vector<at::Tensor> input_tensors;
+    input_tensors.reserve(inputs.size());
+    for (auto& item : inputs) {
+      input_tensors.emplace_back(py::cast<at::Tensor>(item));
+    }
+    // Explicitly clear the passed-in Python list
+    inputs.attr("clear")();
+
+    std::vector<at::Tensor> result_tensors = AOTIModelPackageLoader::boxed_run(
+        std::move(input_tensors), stream_handle);
+
+    py::list outputs;
+    for (const auto& tensor : result_tensors) {
+      outputs.append(
+          py::reinterpret_steal<py::object>(THPVariable_Wrap(tensor)));
+    }
+    return outputs;
+  }
+};
+
+void initAOTIPackageBindings(PyObject* module) {
+  auto rootModule = py::handle(module).cast<py::module>();
+  auto m = rootModule.def_submodule("_aoti");
+
+  py::class_<AOTIModelPackageLoaderPybind>(m, "AOTIModelPackageLoader")
+      .def(py::init<const std::string&, const std::string&, const bool>())
+      .def("get_metadata", &AOTIModelPackageLoaderPybind::get_metadata)
+      .def(
+          "run",
+          &AOTIModelPackageLoaderPybind::run,
+          py::arg("inputs"),
+          py::arg("stream_handle") = nullptr)
+      .def(
+          "boxed_run",
+          &AOTIModelPackageLoaderPybind::boxed_run,
+          py::arg("inputs"),
+          py::arg("stream_handle") = nullptr)
+      .def("get_call_spec", &AOTIModelPackageLoaderPybind::get_call_spec)
+      .def("load_constants", &AOTIModelPackageLoaderPybind::load_constants)
+      .def(
+          "get_constant_fqns",
+          &AOTIModelPackageLoaderPybind::get_constant_fqns);
+}
+} // namespace torch::inductor
+#endif
