bdf9607c4d0a27e50adfa49026e382c98a617772
p00669756 <pangjiayi@huawei.com>
2026-06-09 15:18:04 +0800

    modify split axis selection
    (cherry picked from commit d4f0f2a543dde083808e1a0ebc2a542964dd48fc)

--- torch_npu/_inductor/codegen/split_tiling.py
+++ torch_npu/_inductor/codegen/split_tiling.py
@@ -88,7 +88,7 @@ class SplitTiling:
                 val = sv.size_hint(current_numels)
             except TypeError:
                 return len(self.kernel.split_axis) >= 3
-            if val >= num_vector_core:
+            if val >= num_vector_core and  val // ( ( val + num_vector_core) // num_vector_core ) > 40:
                 return True
             if len(self.kernel.split_axis) == 3:
                 return True
