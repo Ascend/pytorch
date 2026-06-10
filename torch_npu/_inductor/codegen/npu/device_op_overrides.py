9fb40e96b4f6a26db24b6528d77720c0354b59ad
stonexxx <sheny1xuan@163.com>
2026-06-10 15:51:02 +0800

    inductor: use simt_template as kernel default compile_mode
    Co-authored-by: Stonexx<sheny1xuan@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !37939 merge zj0607-yx into v2.7.1
    
    inductor: use simt_template as kernel default compile_mode
    
    Created-by: stonexxx
    Commit-by: Stonexx
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!37939

--- torch_npu/_inductor/codegen/npu/device_op_overrides.py
+++ torch_npu/_inductor/codegen/npu/device_op_overrides.py
@@ -1,8 +1,13 @@
 from torch._inductor.codegen.common import DeviceOpOverrides, register_device_op_overrides
+import torch_npu
+from torch_npu._inductor.codegen.catlass.catlass_utils import try_import_catlass
 
 
 class NewNPUDeviceOpOverrides(DeviceOpOverrides):
     def import_get_raw_stream_as(self, name):
+        enabled_catlass = try_import_catlass()
+        if not enabled_catlass and hasattr(torch_npu._C, "_npu_getCurrentRawStreamNoWait"):
+            return f"from torch_npu._C import _npu_getCurrentRawStreamNoWait as {name}"
         return f"from torch_npu._C import _npu_getCurrentRawStream as {name}"
 
     def set_device(self, device_idx):
