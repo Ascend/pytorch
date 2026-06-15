e78f417f98a67a78993f10f81f40898af2eeed95
wanlinan <wanlinan@huawei.com>
2026-06-15 20:09:50 +0800

    [fix] add set_device in startcoalescing
    Co-authored-by: wanlinan<wanlinan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !38250 merge v2.8.0-26.0.0 into v2.8.0-26.0.0
    
    [fix] add set_device in startcoalescing
    
    Created-by: wanlinan
    Commit-by: wanlinan
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    [[Bug]: batch_send_recv接口报错HcclGroupEnd: not in a group call.](https://gitcode.com/Ascend/pytorch/issues/2355)
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    hccl变更导致的下游问题，hccl把一个计数变量的作用范围改成了线程内，暴露了线程混乱问题。
    分析根因是调RunOpApiV3前没有做set_device，修改方案是在调用前加set_device。
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    社区用例测试通过
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/b0f2b429-2558-4df4-8bef-7805a4326652/image.png 'image.png')
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!38250

--- torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
+++ torch_npu/csrc/distributed/ProcessGroupHCCL.cpp
@@ -2841,6 +2841,8 @@ std::vector<std::shared_ptr<HCCLComm>>& ProcessGroupHCCL::createHCCLComm(
     hcclComms.resize(devices.size());
     std::vector<c10_npu::NPUStream> streamVal;
     streamVal.reserve(devices.size());
+    c10_npu::OptionalNPUGuard npuGuard;
+    npuGuard.set_index(getDeviceForRank(getRank()).index());
 
     TORCH_NPU_HCCL_LOGI("Create HCCL comm, devicesKey %s, commType %d, p2pRank %d.", devicesKey.c_str(), commType, p2pRank);
 
@@ -2855,6 +2857,7 @@ std::vector<std::shared_ptr<HCCLComm>>& ProcessGroupHCCL::createHCCLComm(
     if (!createHCCLCommEx(devicesKey, devices, commType, commConfig, hcclComms, streamVal, p2pRank)) {
         createHCCLCommOrigin(devicesKey, devices, commType, commConfig, hcclComms, streamVal, p2pRank);
     }
+    npuGuard.set_index(getDeviceForRank(getRank()).index());
     // restart the HcclGroupStart
     for (const auto i : c10::irange(hcclActiveGroupCounter_)) {
         (void)i;
@@ -6042,6 +6045,8 @@ void ProcessGroupHCCL::startCoalescing()
     coalescedTensors_.clear();
     coalescedP2PFormatCasts_.clear();
     coalescing_state_ |= CoalActive;
+    c10_npu::OptionalNPUGuard npuGuard;
+    npuGuard.set_index(getDeviceForRank(getRank()).index());
     groupStart();
 }
 
