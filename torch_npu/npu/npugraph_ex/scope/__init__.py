9bd9f2718a245c78c04eea1027e9d6f80e0b111d
mihudan <wangkai579@huawei.com>
2026-06-24 16:41:24 +0800

    【bugfix】npugraph_ex.scope.limit_core_num增加默认参数stream
    Co-authored-by: wangkai<wangkai579@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !39061 merge v2.9.0-26.1.0 into v2.9.0-26.1.0
    
    【bugfix】npugraph_ex.scope.limit_core_num增加默认参数stream
    
    Created-by: mihudan
    Commit-by: wangkai
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    https://gitcode.com/Ascend/pytorch/issues/2445
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [x] 重构优化
    - [ ] 资料更新
    torchair仓合入PR:【refeat】limit_core_num控核支持单算子和可选参数streamhttps://gitcode.com/Ascend/torchair/pull/3181 
    资料修改： 【doc】limit_core_num支持可选参数streamhttps://gitcode.com/Ascend/torchair/pull/3227
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    ex资料修改已合入
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    npugraph_ex接口增加默认参数，ex资料修改已合入，组内已检视
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    已增加传入stream参数场景
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/8650deb0-1659-41ee-8f95-cf989da25d80/image.png 'image.png')
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!39061

--- torch_npu/npu/npugraph_ex/scope/__init__.py
+++ torch_npu/npu/npugraph_ex/scope/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["limit_core_num"]
 
 
-def limit_core_num(op_aicore_num: int, op_vectorcore_num: int):
+def limit_core_num(op_aicore_num: int, op_vectorcore_num: int, stream=None):
     from torch_npu.dynamo.npugraph_ex import scope
-    return scope.limit_core_num(op_aicore_num, op_vectorcore_num)
+    return scope.limit_core_num(op_aicore_num, op_vectorcore_num, stream=stream)
