7ff7ccfd350e5a0bada4e0f921215474251ec631
cuiduo <cuiduo1@huawei.com>
2026-06-24 09:50:17 +0800

    add_triton_warning
    Co-authored-by: 1479518308<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !39037 merge v2.12.0 into v2.12.0
    
    add_triton_warning
    
    Created-by: cuiduo
    Commit-by: 1479518308
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 社区用例https://codehub-y.huawei.com/c00502729/PTA_56_testing/issues/656解决，删除相关patch
    >torch_npu_inductor处新增警告信息，triton缺失
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > 本地验证通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!39037

--- torch_npu/_inductor/__init__.py
+++ torch_npu/_inductor/__init__.py
@@ -37,6 +37,8 @@ def _load_triton_backend():
     import torch
     has_triton = torch.utils._triton.has_triton()
     if not has_triton:
+        import warnings
+        warnings.warn("triton-ascend is not installed, install it first.")
         return
     from torch._dynamo.device_interface import (
         get_interface_for_device,
