f0a90dc27e72d760fa8ec09fb05b254b9b63fd81
cuiduo <cuiduo1@huawei.com>
2026-06-27 12:00:01 +0800

    run_once_fix
    Co-authored-by: 1479518308<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !39289 merge v2.9.0-26.1.0 into v2.9.0-26.1.0
    
    run_once_fix
    
    Created-by: cuiduo
    Commit-by: 1479518308
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 修复run_once在triton路径下导入MLIR校验问题
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及”
    
    # 【功能验证】
    > 本地验证通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!39289

--- torch_npu/_inductor/decomposition.py
+++ torch_npu/_inductor/decomposition.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Optional, Tuple
 import torch
 import torch._ops
@@ -8,12 +9,25 @@ from torch._C import DispatchKey
 from torch._decomp import remove_decompositions
 from torch._prims_common.wrappers import out_wrapper
 import torch.nn.functional as F
-from .ascend_npu_ir.ascend_npu_ir.npu.utils import run_once
 from .ascend_npu_ir.ascend_npu_ir import config as anir_config
 
 
 aten = torch.ops.aten
 npu = torch.ops.npu
+
+def run_once(f):
+    """Runs a function (successfully) only once.
+    The running can be reset by setting the `has_run` attribute to False
+    """
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            result = f(*args, **kwargs)
+            wrapper.has_run = True
+            return result
+        return None
+    wrapper.has_run = False
+    return wrapper
 
 
 @run_once
