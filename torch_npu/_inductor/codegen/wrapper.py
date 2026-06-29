c03422cf6d79f85d2ad95e81020d8718510c8777
ascend-robot <zhongyuanke@huawei.com>
2026-06-29 11:55:48 +0800

    resolve the unexecutable problem when only aten ops in a single graph
    Co-authored-by: ascend-robot<zhongyuanke@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !39432 merge v2.9.0 into v2.9.0
    
    resolve the unexecutable problem when only aten ops in a single graph
    
    Created-by: lvjiangdong
    Commit-by: ascend-robot
    Merged-by: ascend-robot
    Description: [#2523](https://gitcode.com/Ascend/pytorch/issues/2523) 
    <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!39432

--- torch_npu/_inductor/codegen/wrapper.py
+++ torch_npu/_inductor/codegen/wrapper.py
@@ -131,6 +131,12 @@ class NPUPythonWrapperCodeGen(_NPUKernelCodegenMixin, PythonWrapperCodegen):
             return NPUSubgraphPythonWrapperCodegen(subgraph_name, parent_wrapper, partition_signatures)
         return NPUPythonWrapperCodeGen()
 
+    def write_header(self) -> None:
+        super().write_header()
+        self.imports.splice(
+            V.graph.device_ops.import_get_raw_stream_as("get_raw_stream")
+        )
+
     @cache_on_self
     def write_triton_header_once(self) -> None:
         super().write_triton_header_once()
