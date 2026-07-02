5d69d0bda8adc7f4aa59591d1dd126df0dec76c5
mihudan <wangkai579@huawei.com>
2026-07-02 20:09:27 +0800

    【fix】patch_record_stream参数不匹配报错修复
    Co-authored-by: wangkai<wangkai579@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !39839 merge v2.11.0_bug into v2.11.0
    
    【fix】patch_record_stream参数不匹配报错修复
    
    Created-by: mihudan
    Commit-by: wangkai
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    https://gitcode.com/Ascend/pytorch/issues/2580
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    ```
    def method_record_stream(self, tx, s):
        return torch._dynamo.variables.TorchInGraphFunctionVariable(
            torch.npu.fake_record_stream
        ).call_function(tx, [self, s], {})
    ```
    修改签名增加tx参数
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    不涉及
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    不涉及
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/5c348b50-d656-4a92-b1f2-c6acb9a58fb6/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!39839

--- test/dynamo/test_npugraph_ex.py
+++ test/dynamo/test_npugraph_ex.py
@@ -208,6 +208,35 @@ class TestNpuGraphEx(TestCase):
         in4 = torch.randn(1000, 1000, dtype=torch.float16, device="npu")
         res = compiled_model(in1, in2, in3, in4)
 
+    def test_dynamo_trace_stream_event(self):
+
+        def my_backend(gm, example_inputs):
+            print(gm.graph)
+            node_names = (node.name for node in gm.graph.nodes)
+            self.assertIn("current_stream", node_names)
+            self.assertIn("set_stream", node_names)
+            self.assertIn("fake_record_stream", node_names)
+            return gm
+
+        @torch.compile(backend=my_backend)
+        def test_stream_in_graph(a):
+            s = torch.npu.Stream()
+            event = torch.npu.Event()
+            r = torch.add(a, 2)
+            event.record()
+            with torch.npu.stream(s):
+                event.wait()
+                r = torch.add(r, 1)
+                r.record_stream(s)
+            r = torch.add(r, 1)
+            return r
+
+        i = torch.randn([3, 3], device="npu:0")
+        with self.assertRaises(RuntimeError) as context:
+            r = test_stream_in_graph(i)
+            return r
+        self.assertIn("tensor.record_stream is not supported on torch.compile", str(context.exception))
+
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
 
