e5eb8662a74dca532cb816947d7309f6fb52f47f
cuiduo <cuiduo1@huawei.com>
2026-07-31 10:43:23 +0800

    move_has_triton_to_dynamo
    Co-authored-by: 1479518308<cuiduo1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !43318 merge v2.12.0 into v2.12.0
    
    move_has_triton_to_dynamo
    
    Created-by: cuiduo
    Commit-by: 1479518308
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > has_triton在utils阶段的patch挪到init阶段
    
    # 【资料变更】
    > 不涉及
    
    # 【接口变更】
    > 不涉及
    
    # 【功能验证】
    > 本地验证通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    Related: https://gitcode.com/Ascend/pytorch/issues/1978
    
    See merge request: Ascend/pytorch!43318

--- torch_npu/_inductor/utils.py
+++ torch_npu/_inductor/utils.py
@@ -49,43 +49,8 @@ def resolve_npu_device_index(device_idx=None) -> int:
 
 
 def patch_has_triton():
-    from torch.utils._triton import has_triton_package
+    from torch_npu.utils._dynamo import has_triton
 
-    @functools.lru_cache(None)
-    def has_triton() -> bool:
-        if not has_triton_package():
-            return False
-
-        from torch._dynamo.device_interface import get_interface_for_device
-
-        def cuda_extra_check(device_interface):
-            return True
-
-        def cpu_extra_check(device_interface):
-            import triton.backends
-
-            return "cpu" in triton.backends.backends
-
-        def _return_true(device_interface):
-            return True
-
-        triton_supported_devices = {
-            "cuda": cuda_extra_check,
-            "xpu": _return_true,
-            "cpu": cpu_extra_check,
-            "npu": _return_true,
-        }
-
-        def is_device_compatible_with_triton():
-            for device, extra_check in triton_supported_devices.items():
-                device_interface = get_interface_for_device(device)
-                if device_interface.is_available() and extra_check(device_interface):
-                    return True
-            return False
-
-        return is_device_compatible_with_triton()
-
-    torch.utils._triton.has_triton = has_triton
     torch._inductor.scheduler.has_triton = has_triton
     torch._inductor.compile_fx.has_triton = has_triton
 
