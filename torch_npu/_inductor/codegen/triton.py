7e637ba4f0fc144384f517f6edb49aa07b56b6a9
luqichao <luqichao1@huawei.com>
2026-08-14 15:55:06 +0800

    fix(inductor): fix grouped dynamic runtime handling
    Co-authored-by: luqichao<luqichao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !44543 merge bugfix/group_runtime_args into v2.10.0
    
    fix(inductor): fix grouped dynamic runtime handling
    
    Created-by: luqichao
    Commit-by: luqichao
    Merged-by: ascend-robot
    Description: 1. _grouped_runtime_args_snapshot 使用之后未释放 存在内存泄露
    2. 修复mask问题，应该只按动态轴来生成mask，不能因为有动态轴导致所有轴都生成mask
    
    
    See merge request: Ascend/pytorch!44543

--- torch_npu/_inductor/codegen/triton.py
+++ torch_npu/_inductor/codegen/triton.py
@@ -448,6 +448,11 @@ def get_allow_dynamic():
     return False
 
 
+def is_dynamic_axis(axis):
+    length = V.graph.sizevars.simplify(axis.length)
+    return not isinstance(length, (int, sympy.Integer))
+
+
 @staticmethod
 def select_index_dtype(node_schedule, numel, reduction_numel):
     return "tl.int32"
@@ -496,9 +501,8 @@ class IterationRangesEntryNPUIndex(IterationRangesEntry):
 
     # axis mask
     def _codegen_mask(self):
-        allow_dynamic = get_allow_dynamic()
         codegen_mask = self.is_tiling_axis and (
-            not self.is_no_loop_axis or allow_dynamic
+            not self.is_no_loop_axis or is_dynamic_axis(self)
         )
         if V.kernel.is_unified_simt_kernel():
             codegen_mask = self.is_tiling_axis
@@ -4251,7 +4255,6 @@ class NPUIndexTritonKernel(TritonKernel):
             )
 
         for node in self.sorted_axis:
-            allow_dynamic = get_allow_dynamic()
             is_persistent_reduction_axis = (
                 self.persistent_reduction and node.is_reduction
             )
@@ -4265,7 +4268,7 @@ class NPUIndexTritonKernel(TritonKernel):
                 if (
                     not node.is_tiling_axis
                     or is_persistent_reduction_axis
-                    or (node.is_no_loop_axis and not allow_dynamic)
+                    or (node.is_no_loop_axis and not is_dynamic_axis(node))
                 ):
                     continue
 
