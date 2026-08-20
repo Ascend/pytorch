7c5c892518884b726320682a713c6b725a2892bd
liuyutong_bury <liuyutong25@huawei.com>
2026-08-20 17:21:02 +0800

    feat: add TORCH_WARM_POOL env to control AsyncCompile.warm_pool()
    Co-authored-by: liuyutong_bury<liuyutong25@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !44944 merge sync-warm-pool-to-v2.10.0 into v2.10.0
    
    feat: add TORCH_WARM_POOL env to control AsyncCompile.warm_pool()
    
    Created-by: liuyutong_bury
    Commit-by: liuyutong_bury
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    https://gitcode.com/Ascend/pytorch/issues/4216
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [x] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    遵守Pytorch的TORCH_WARM_POOL = 0 子进程约定，只在顶层进程warm pool
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    See merge request: Ascend/pytorch!44944

--- torch_npu/_inductor/__init__.py
+++ torch_npu/_inductor/__init__.py
@@ -5,7 +5,8 @@ os.environ["TORCH_DEVICE_BACKEND_AUTOLOAD"] = "0"
 from torch._inductor.async_compile import AsyncCompile
 
 
-AsyncCompile.warm_pool()
+if os.environ.get("TORCH_WARM_POOL", "1") == "1":
+    AsyncCompile.warm_pool()
 os.environ["TORCH_DEVICE_BACKEND_AUTOLOAD"] = ORG_AUTOLOAD
 
 import os
