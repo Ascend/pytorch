3c67b1042552e6b94f05ca10d9b80b4a4bf597f2
SoraAzzz <694099604@qq.com>
2026-08-20 21:10:12 +0800

    [feat][v2.9.0]torch_npu profiler analysis ability of display prof level0 && kernel shapes
    Co-authored-by: xieanran<694099604@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !44927 merge l0shape2.9.0 into v2.9.0
    
    [feat][v2.9.0]torch_npu profiler analysis ability of display prof level0 && kernel shapes
    
    Created-by: SoraAzzz
    Commit-by: xieanran
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [x] issue/工单https://gitcode.com/Ascend/pytorch/issues/4212
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    新增形状表头常量 OP_SUMMARY_SHAPE_HEADERS: 在 _csv_headers.py 中新增形状相关列定义（Input Shapes、Input Data Types、Input Formats、Output Shapes、Output Data Types、Output Formats），为 kernel details输出 shape 信息提供表头
    新增 _get_kernel_headers 过滤逻辑: 在 _kernel_view_parser.py 中新增类方法 _get_kernel_headers，当 is_all_kernel_headers 为真时返回全部表头；否则仅返回 OP_SUMMARY_SHOW_HEADERS 拼接源数据中存在的表头
    generate_view 接入表头过滤: 将 OpSummaryBean.headers 的赋值改为通过 _get_kernel_headers(all_data[0].all_headers, ProfilerConfig().is_all_kernel_headers()) 计算，使 level0 模式下输出 CSV 时过滤掉 Model ID 等非展示字段，同时保留base 表头以及shape表头及数据
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    > kernel_details.csv在l0下存在新增表头
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    > 不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    ![image.png](https://raw.gitcode.com/user-images/assets/7404318/34f28414-8550-4d52-9ff8-d03f7b30878e/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!44927

--- torch_npu/profiler/analysis/prof_common_func/_csv_headers.py
+++ torch_npu/profiler/analysis/prof_common_func/_csv_headers.py
@@ -3,5 +3,7 @@ class CsvHeaders(object):
     TASK_START_TIME = "Task Start Time(us)"
     OP_SUMMARY_SHOW_HEADERS = ["Device_id", "Op Name", "OP Type", "Task Type", TASK_START_TIME, "Task Duration(us)",
                                "Task Wait Time(us)", "Block Num"]
+    OP_SUMMARY_SHAPE_HEADERS = ["Input Shapes", "Input Data Types", "Input Formats", "Output Shapes",
+                                "Output Data Types", "Output Formats"]
     OP_SUMMARY_KERNEL_BASE_HEADERS = ["Device_id", "Name", "Type", "Accelerator Core", "Start Time(us)", "Duration(us)",
                                       "Wait Time(us)", "Block Num"]
