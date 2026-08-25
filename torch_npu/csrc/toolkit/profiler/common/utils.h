604e20098db3a3bdc6d1600fe7bb8abb621c9a23
mei-feiyao <meifeiyao@h-partners.com>
2026-08-25 16:51:45 +0800

    [feat]msprof support torch_npu.npu.mstx
    Co-authored-by: mei-feiyao<meifeiyao@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !45088 merge 10 into v2.10.0
    
    [feat]msprof support torch_npu.npu.mstx
    
    Created-by: mei-feiyao
    Commit-by: mei-feiyao
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\https://gitcode.com/Ascend/pytorch/issues/4086
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    1.msprof命令行方式下，会先设置环境变量PROFILER_SAMPLECONFIG，再拉起pytorch程序；
    2.pytorch程序执行到mstx打点接口时，mstx模块读取环境变量，并确认开启mstx开关被开启后，支持调用mstx库接口实现打点；否则直接返回不打点；
    3.通过环境变量里的domain_include和domain_exclude配置此次采集要使能的domain范围；
    4.保留torch_npu.profiler逻辑，在每次start的时候配置domain_include和domain_exclude
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    不涉及
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    pytorch模型调用torch_npu.npu.mstx接口，使用msprof命令行方式采集性能数据，并打开msproftx开关，比如msprof --msproftx=on --application="python3 my.py"
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!45088

--- torch_npu/csrc/toolkit/profiler/common/utils.h
+++ torch_npu/csrc/toolkit/profiler/common/utils.h
@@ -12,6 +12,7 @@
 #include <stdint.h>
 
 #include <string>
+#include <vector>
 #include "torch_npu/csrc/framework/interface/LibAscendHal.h"
 
 namespace torch_npu {
@@ -187,6 +188,8 @@ public:
     static uint64_t GetHostUid();
 
     static int safe_strcpy_s(char* dest, const char* src, size_t destSize);
+
+    static std::vector<std::string> SplitString(const std::string& str, const std::string& sep);
 };
 } // profiler
 } // toolkit
