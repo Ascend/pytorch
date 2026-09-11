fix(test): correct expected buffer size calculation in test_hccl_shared_buffer
Co-authored-by: limuan<liyijie16@huawei.com>



# message auto-generated for no-merge-commit merge:
!45920 merge master into master

fix(test): correct expected buffer size calculation in test_hccl_shared_buffer

Created-by: Tamaru_DFM
Commit-by: limuan
Merged-by: ascend-robot
Description: <!--
PR描述模板更新日期：20260203
-->

# 【合入来源】
> <font color="red">**如有社区issue，请关联issue链接**</font>\
> <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>

- [ ] 需求
- [x] 问题单
- [ ] issue/工单
- [ ] 重构优化
- [ ] 资料更新

# 【修改方案】
用例中,原来内存占用的预期值为:groupnum*buffer_size + tol (tol为100,在通信域较多,可能无法coverbuffersize外的通信域内存开销),现在改为
每个通信域多预留100MB，100MB远远小于用例本身buffersize的400MB,可以看护共享内存功能

# 【资料变更】
不涉及

# 【接口变更】
不涉及

# 【功能验证】
![屏幕截图 2026-09-10 085459.png](https://raw.gitcode.com/user-images/assets/7404318/553013b5-ab3b-44a9-af4d-a98d4ab90e16/屏幕截图_2026-09-10_085459.png '屏幕截图 2026-09-10 085459.png')

# 【CheckList】
> PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]

- [ ] 代码注释完备，正确记录错误日志
- [ ] 代码实现进行了返回值、空指针等校验
- [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
- [ ] PR持续集成流水线（CI）执行通过，代码检查无异常


See merge request: Ascend/pytorch!45920

test/distributed/test_hccl_shared_buffer.py
@@ -1,6 +1,4 @@
-import unittest
 import os
-from functools import wraps
 
 import numpy as np
 import torch
@@ -33,7 +31,7 @@ class HcclSharedBufferTest(TestCase):
     @classmethod
     # pylint:disable=huawei-too-many-arguments
     def _test_reduce(cls, rank, input1, world_size, init_pg, c2p, p2c, seq):
-        #使用共享buffer进行通信，检查最终精度。
+        # 使用共享buffer进行通信，检查最终精度。
         pg = init_pg(rank, world_size, seq)
         input1 = input1.npu()
         pg.all_reduce(input1, op=dist.ReduceOp.SUM, async_op=True)
@@ -69,8 +67,8 @@ class HcclSharedBufferTest(TestCase):
         for _ in range(world_size):
             rank, output, mem_diff = c2p.get()
             self.assertEqual(output, expected,
-                                "rank {} world_size {} dtype {} shape {} Expect receive tensor {} but got {}.".format(
-                                    rank, world_size, expected.dtype, expected.shape, expected, output))
+                             "rank {} world_size {} dtype {} shape {} Expect receive tensor {} but got {}.".format(
+                                 rank, world_size, expected.dtype, expected.shape, expected, output))
 
             # For case where we want to examine the memory usage of ccl buffer
             if mem_diff:
@@ -91,7 +89,7 @@ class HcclSharedBufferTest(TestCase):
         for _ in range(world_size):
             op1_expected += inputs
 
-        #dist.ReduceOp.AVG
+        # dist.ReduceOp.AVG
         expected = 0
         for _ in range(world_size):
             expected += op1_expected
@@ -131,12 +129,12 @@ class HcclSharedBufferTest(TestCase):
         input1 = input1.npu()
         options = torch_npu._C._distributed_c10d.ProcessGroupHCCL.Options()
 
-        hccl_config1 = {"hccl_buffer_name": "shared0"} # 创建一个共享buffer
+        hccl_config1 = {"hccl_buffer_name": "shared0"}   # 创建一个共享buffer
         options.hccl_config = hccl_config1
         dist.init_process_group(backend='hccl', world_size=world_size, rank=rank, pg_options=options)
         dist.all_reduce(input1, async_op=True)
 
-        hccl_config2 = {"hccl_buffer_name": "shared2"} # 注释1：只在创建pg的时候指定参数。后续修改不影响创建的名字,当前pg保持创建时的名字。
+        hccl_config2 = {"hccl_buffer_name": "shared2"}   # 注释1：只在创建pg的时候指定参数。后续修改不影响创建的名字,当前pg保持创建时的名字。
         options.hccl_config = hccl_config2
         dist.all_reduce(input1, async_op=True)
 
@@ -147,7 +145,7 @@ class HcclSharedBufferTest(TestCase):
         pg = dist.new_group(backend='hccl', ranks=ranks, pg_options=options)
         dist.all_reduce(input1, group=pg, async_op=True)
 
-        hccl_config4 = {"hccl_buffer_name": "shared4"} # 同注释1。
+        hccl_config4 = {"hccl_buffer_name": "shared4"}   # 同注释1。
         options.hccl_config = hccl_config4
         dist.all_reduce(input1, group=pg, async_op=True)
 
@@ -156,7 +154,7 @@ class HcclSharedBufferTest(TestCase):
         pg2 = dist.new_group(backend='hccl', ranks=ranks, pg_options=options)
         dist.all_reduce(input1, group=pg2, async_op=True)
 
-        hccl_config6 = {"hccl_buffer_name": "shared6"} # 同注释1。
+        hccl_config6 = {"hccl_buffer_name": "shared6"}   # 同注释1。
         options.hccl_config = hccl_config6
         dist.all_reduce(input1, group=pg2, async_op=True)
 
@@ -176,8 +174,8 @@ class HcclSharedBufferTest(TestCase):
         dtype_list = [np.uint8]
         format_list = [2]
         shape_format = [
-                           [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
-                       ] + [[i, j, [1]] for i in dtype_list for j in format_list]
+            [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
+        ] + [[i, j, [1]] for i in dtype_list for j in format_list]
         seq = 0
         for world_size in ranks:
             for shape in shape_format:
@@ -200,7 +198,7 @@ class HcclSharedBufferTest(TestCase):
         torch.npu.set_device(rank)
         input1 = input1.npu()
         options = torch_npu._C._distributed_c10d.ProcessGroupHCCL.Options()
-        hccl_config = {"hccl_buffer_name": "sharedBuffer"} # 创建一个共享buffer
+        hccl_config = {"hccl_buffer_name": "sharedBuffer"}   # 创建一个共享buffer
         options.hccl_config = hccl_config
         dist.init_process_group(backend='hccl', world_size=world_size, rank=rank, pg_options=options)
 
@@ -222,7 +220,7 @@ class HcclSharedBufferTest(TestCase):
         del pg1
         del pg2
 
-        hccl_config3 = {"hccl_buffer_name": "subSharedBuffer"} #
+        hccl_config3 = {"hccl_buffer_name": "subSharedBuffer"}  #
         options.hccl_config = hccl_config3
         pg3 = dist.new_group(backend='hccl', ranks=ranks, pg_options=options)
         dist.all_reduce(input1, group=pg3, async_op=True)
@@ -239,8 +237,8 @@ class HcclSharedBufferTest(TestCase):
         dtype_list = [np.uint8]
         format_list = [2]
         shape_format = [
-                           [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
-                       ] + [[i, j, [1]] for i in dtype_list for j in format_list]
+            [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
+        ] + [[i, j, [1]] for i in dtype_list for j in format_list]
         seq = 0
         for world_size in ranks:
             for shape in shape_format:
@@ -313,7 +311,7 @@ class HcclSharedBufferTest(TestCase):
         _, _, mem_after = cls.get_memory_info()
         diff = (memory_info_before[0] - mem_after[0]) / 1024 / 1024
         num_different_buffer_group += 1
-        expected = buffer_size * num_different_buffer_group + tol
+        expected = (buffer_size + tol) * num_different_buffer_group
         mem_diff["pg1"] = [diff, expected]
 
         # create pg2 with the same buffer
@@ -325,7 +323,7 @@ class HcclSharedBufferTest(TestCase):
         # get pg2 mem info
         _, _, mem_after = cls.get_memory_info()
         diff = (memory_info_before[0] - mem_after[0]) / 1024 / 1024
-        expected = buffer_size * num_different_buffer_group + tol
+        expected = (buffer_size + tol) * num_different_buffer_group
         mem_diff["pg2"] = [diff, expected]
 
         # create pg3 with a different buffer
@@ -338,7 +336,7 @@ class HcclSharedBufferTest(TestCase):
         _, _, mem_after = cls.get_memory_info()
         diff = (memory_info_before[0] - mem_after[0]) / 1024 / 1024
         num_different_buffer_group += 1
-        expected = buffer_size * num_different_buffer_group + tol
+        expected = (buffer_size + tol) * num_different_buffer_group
         mem_diff["pg3"] = [diff, expected]
         torch_npu.npu.synchronize()
 
@@ -439,8 +437,8 @@ class HcclSharedBufferTest(TestCase):
         dtype_list = [np.uint8]
         format_list = [2]
         shape_format = [
-                        [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
-                    ] + [[i, j, [1]] for i in dtype_list for j in format_list]
+            [i, j, [12, 56, 256]] for i in dtype_list for j in format_list
+        ] + [[i, j, [1]] for i in dtype_list for j in format_list]
         seq = 0
         for world_size in ranks:
             for shape in shape_format: