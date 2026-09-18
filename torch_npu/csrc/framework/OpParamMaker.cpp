c517c02fbd4cdd141c7c7d99d1d6ace7ff157316
culechan <chenshuai81@huawei.com>
2026-09-18 15:42:19 +0800

    [fix] fix cache
    Co-authored-by: chenshuai<chenshuai81@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !46537 merge cherry-pick-mr-46536-1789702739195-auto into v2.11.0
    
    [fix] fix cache
    
    Created-by: culechan
    Commit-by: chenshuai
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    增加一个cache，使deterministic_level一直为0时，就跳过读deterministic_level，提升性能
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    修改前：
    ![img_v3_0215l_59a770da-d8a7-4d93-81ca-4c52638b8f5g.jpg](https://raw.gitcode.com/user-images/assets/7404318/d544fd74-9c4d-4fc0-89e9-981b5cd6ee22/img_v3_0215l_59a770da-d8a7-4d93-81ca-4c52638b8f5g.jpg 'img_v3_0215l_59a770da-d8a7-4d93-81ca-4c52638b8f5g.jpg')
    修改后：
    ![img_v3_0215l_872b6c42-2a45-4584-ab5b-fd1a6265c24g.jpg](https://raw.gitcode.com/user-images/assets/7404318/fa2e69b7-70df-4898-bb25-32a19bb3af3e/img_v3_0215l_872b6c42-2a45-4584-ab5b-fd1a6265c24g.jpg 'img_v3_0215l_872b6c42-2a45-4584-ab5b-fd1a6265c24g.jpg')
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/pytorch!46537

--- torch_npu/csrc/framework/OpParamMaker.cpp
+++ torch_npu/csrc/framework/OpParamMaker.cpp
@@ -28,6 +28,13 @@ namespace native {
 
 std::recursive_mutex deterministic_launch_mutex;
 
+struct CachedDeterministicRuntimeState {
+    bool valid = false;
+    uint32_t effective_level = 0;
+};
+
+static std::unordered_map<aclrtContext, CachedDeterministicRuntimeState> deterministic_runtime_cache;
+
 void OpAttrMaker::Set(aclopAttr *attr, const string &name, bool value)
 {
     aclopSetAttrBool(attr, name.c_str(), value);
@@ -105,6 +112,16 @@ void OpCommandImpl::SetEnginePriority()
     }
 }
 
+aclrtContext GetCurrentContextForDeterministicCache()
+{
+    aclrtContext context = nullptr;
+    aclError ret = aclrtGetCurrentContext(&context);
+    if (ret != ACL_ERROR_NONE) {
+        return nullptr;
+    }
+    return context;
+}
+
 bool IsAclStrongConsistencyExist()
 {
     static const bool isAclStrongConsistencyExist = []() {
@@ -152,6 +169,14 @@ void ApplyDeterministicSnapshotLocked(
     const c10_npu::DeterministicSnapshot& snapshot,
     bool isOpapi)
 {
+    auto context = GetCurrentContextForDeterministicCache();
+    auto& cached_state = deterministic_runtime_cache[context];
+    if (cached_state.valid && cached_state.effective_level == snapshot.effective_level &&
+        cached_state.effective_level == 0) {
+        return;
+    }
+    cached_state.effective_level = snapshot.effective_level;
+    cached_state.valid = true;
     int64_t cur_level = 0;
     NPU_CHECK_ERROR(AclrtGetSysParamOpt(
             aclSysParamOpt::ACL_OPT_DETERMINISTIC, &cur_level));
